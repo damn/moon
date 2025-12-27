06dae0c8ef764c00cba22f695e16aa1b200bcd5f
Michael Sappler <michaelsappler@gmail.com>
2025-12-27 22:39:48 +0530

    Enable font markup ui & dispose skin

--- src/moon/application/create/skin.clj
+++ src/moon/application/create/skin.clj
@@ -1,14 +1,15 @@
 (ns moon.application.create.skin
   (:require [gdl.files :as files]
+            [gdl.graphics.bitmap-font :as bitmap-font]
+            [gdl.graphics.bitmap-font.data :as bitmap-font-data]
             [gdl.ui.skin :as skin]
             [moon.ui]))
 
 (defn step [{:keys [ctx/files] :as ctx}]
-
-  ; (-> (vis-ui/skin) (skin/font "default-font") bitmap-font/data (bmfont-data/set-enable-markup! true)
-
-  ; TODO DISPOSE
-
   (let [skin (skin/create (files/internal files "uiskin.json"))]
+    (-> skin
+        (skin/font "default-font")
+        bitmap-font/data
+        (bitmap-font-data/set-enable-markup! true))
     (.bindRoot #'moon.ui/skin skin)
     (assoc ctx :ctx/skin skin)))
--- src/moon/application/dispose.clj
+++ src/moon/application/dispose.clj
@@ -1,5 +1,6 @@
 (ns moon.application.dispose
-  (:require [moon.audio :as audio]
+  (:require [gdl.utils.disposable :as disposable]
+            [moon.audio :as audio]
             [moon.graphics :as graphics]
             [moon.ui :as ui]
             [moon.world :as world]))
@@ -7,11 +8,13 @@
 (defn do!
   [{:keys [ctx/audio
            ctx/graphics
+           ctx/skin
            ctx/stage
            ctx/world]
     :as ctx}]
   (audio/dispose! audio)
   (graphics/dispose! graphics)
   (ui/dispose! stage)
+  (disposable/dispose! skin)
   (world/dispose! world)
   ctx)
