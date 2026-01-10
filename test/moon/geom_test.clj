6bc9921f26b78c0524d26e17dc3ffbb33c365a88
Michael Sappler <michaelsappler@gmail.com>
2026-01-10 20:41:54 +0530

    Remove docs/

---
+++ test/moon/geom_test.clj
@@ -0,0 +1,21 @@
+(ns moon.geom-test)
+;(ns moon.application.render.draw-on-world-viewport.geom-test)
+;
+;(comment
+; (require '[moon.world.grid :as grid])
+; (require '[moon.math.geom :as geom])
+;
+; (defn geom-test
+;   [{:keys [ctx/graphics
+;            ctx/world]}]
+;   (let [position (:graphics/world-mouse-position graphics)
+;         radius 0.8
+;         circle {:position position
+;                 :radius radius}]
+;     (conj (cons [:draw/circle position radius [1 0 0 0.5]]
+;                 (for [[x y] (map #(:position @%) (grid/circle->cells (:world/grid world) circle))]
+;                   [:draw/rectangle x y 1 1 [1 0 0 0.5]]))
+;           (let [{:keys [x y width height]} (geom/circle->outer-rectangle circle)]
+;             [:draw/rectangle x y width height [0 0 1 1]]))))
+;
+; )
