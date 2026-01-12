b2ddb40265e77db7ba631d61129750451d771a46
Michael Sappler <michaelsappler@gmail.com>
2026-01-13 00:09:54 +0530

    remove unused code visui tooltip

--- src/moon/ui/tooltip.clj
+++ src/moon/ui/tooltip.clj
@@ -1,25 +1,13 @@
 (ns moon.ui.tooltip
   (:import (com.badlogic.gdx.scenes.scene2d.ui Skin
-                                               TextTooltip)
-           (moon Stage)))
+                                               TextTooltip)))
 
 (defn add! [actor tooltip-text ^Skin skin]
   (.addListener actor (TextTooltip. (str tooltip-text) skin))
-  #_(tooltip/create {:update-fn (fn [tooltip]
-                                (when-not (string? tooltip-text)
-                                  (let [actor (tooltip/target tooltip)
-                                        ctx (when-let [stage (.getStage actor)]
-                                              (.ctx ^Stage stage))]
-                                    (when ctx
-                                      (tooltip/set-text! tooltip (tooltip-text ctx))))))
-                   :target actor
-                   :content (doto (label/create (if (string? tooltip-text) tooltip-text "")
-                                                skin)
-                              (label/set-alignment! align/center))})
   actor)
 
 (defn remove! [actor]
-  ; TODO ?
-  #_(tooltip/remove! actor)
-
+  ; TODO
+  ;(.removeListener actor (.getListeners actor))
+  ; ... first find the listener
   )
