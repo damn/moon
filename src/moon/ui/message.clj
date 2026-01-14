77305736d58d451fd74188bfbcef8b0c633c3097
Michael Sappler <michaelsappler@gmail.com>
2026-01-14 22:12:58 +0530

    PlayerMessage protocol

--- src/moon/ui/message.clj
+++ src/moon/ui/message.clj
@@ -1,35 +1,4 @@
-(ns moon.ui.message
-  (:require [moon.graphics :as graphics])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor)
-           (com.badlogic.gdx.utils.viewport Viewport)))
+(ns moon.ui.message)
 
-(defn- draw-message [state vp-width vp-height]
-  (when-let [text (:text @state)]
-    [:draw/text {:x (/ vp-width 2)
-                 :y (+ (/ vp-height 2) 200)
-                 :text text
-                 :scale 2.5
-                 :up? true}]))
-
-(defn create [duration-seconds]
-  (doto (proxy [Actor] []
-          (draw [_batch _parent-alpha]
-            (when-let [stage (.getStage this)]
-              (graphics/draw! (:ctx/graphics (.ctx stage))
-                              [(draw-message (Actor/.getUserObject this)
-                                             (Viewport/.getWorldWidth  (.getViewport stage))
-                                             (Viewport/.getWorldHeight (.getViewport stage)))])))
-          (act [delta]
-            (let [state (Actor/.getUserObject this)]
-              (when (:text @state)
-                (swap! state update :counter + delta)
-                (when (>= (:counter @state) duration-seconds)
-                  (reset! state nil))))
-            (let [^Actor this this]
-              (proxy-super act delta))))
-    (.setName "player-message")
-    (.setUserObject (atom nil))))
-
-(defn show! [this text]
-  (.setUserObject this (atom {:text text
-                              :counter 0})))
+(defprotocol PlayerMessage
+  (show! [_ text]))
--- src/moon/ui_actors/player_message.clj
+++ src/moon/ui_actors/player_message.clj
@@ -1,7 +1,40 @@
 (ns moon.ui-actors.player-message
-  (:require [moon.ui.message :as message]))
+  (:require [moon.graphics :as graphics]
+            [moon.ui.message :as message])
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)
+           (com.badlogic.gdx.utils.viewport Viewport)))
+
+(defn- draw-message [state vp-width vp-height]
+  (when-let [text (:text @state)]
+    [:draw/text {:x (/ vp-width 2)
+                 :y (+ (/ vp-height 2) 200)
+                 :text text
+                 :scale 2.5
+                 :up? true}]))
 
 (def message-duration-seconds 0.5)
 
 (defn create [_ctx]
-  (message/create message-duration-seconds))
+  (doto (proxy [Actor] []
+          (draw [_batch _parent-alpha]
+            (when-let [stage (.getStage this)]
+              (graphics/draw! (:ctx/graphics (.ctx stage))
+                              [(draw-message (Actor/.getUserObject this)
+                                             (Viewport/.getWorldWidth  (.getViewport stage))
+                                             (Viewport/.getWorldHeight (.getViewport stage)))])))
+          (act [delta]
+            (let [state (Actor/.getUserObject this)]
+              (when (:text @state)
+                (swap! state update :counter + delta)
+                (when (>= (:counter @state) message-duration-seconds)
+                  (reset! state nil))))
+            (let [^Actor this this]
+              (proxy-super act delta))))
+    (.setName "player-message")
+    (.setUserObject (atom nil))))
+
+(extend-type Actor
+  message/PlayerMessage
+  (show! [this text]
+    (.setUserObject this (atom {:text text
+                                :counter 0}))))
