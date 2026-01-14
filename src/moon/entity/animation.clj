d36990fca2507e69608582f87adbb389c2abd6e0
Michael Sappler <michaelsappler@gmail.com>
2026-01-14 22:08:12 +0530

    integrate moon.animation in moon.entity.animation

--- src/moon/entity/animation.clj
+++ src/moon/entity/animation.clj
@@ -1,21 +1,52 @@
 (ns moon.entity.animation
-  (:require [moon.animation :as animation]
-            [moon.entity :as entity]
+  (:require [moon.entity :as entity]
             [moon.entity.image :as image]))
 
+(defprotocol Animation
+  (tick [_ delta])
+  (stopped? [_])
+  (current-frame [_]))
+
+(defrecord RAnimation
+  [frames frame-duration looping? cnt maxcnt delete-after-stopped?]
+  Animation
+  (tick [this delta]
+    (let [maxcnt (float maxcnt)
+          newcnt (+ (float cnt) (float delta))]
+      (assoc this :cnt (cond (< newcnt maxcnt) newcnt
+                             looping? (min maxcnt (- newcnt maxcnt))
+                             :else maxcnt))))
+
+  (stopped? [_]
+    (and (not looping?) (>= cnt maxcnt)))
+
+  (current-frame [this]
+    (frames (min (int (/ (float cnt) (float frame-duration)))
+                 (dec (count frames))))))
+
 (defmethod entity/create :entity/animation
-  [[_ v] _world]
-  (animation/create v nil))
+  [[_k {:keys [animation/frames
+               animation/frame-duration
+               animation/looping?
+               delete-after-stopped?]}] _world]
+  (assert (not (and looping? delete-after-stopped?)))
+  (map->RAnimation
+   {:frames (vec frames)
+    :frame-duration frame-duration
+    :looping? looping?
+    :cnt 0
+    :maxcnt (* (count frames) (float frame-duration))
+    :delete-after-stopped? delete-after-stopped?}))
 
 (defmethod entity/tick :entity/animation
   [[_k animation] eid {:keys [world/delta-time]}]
-  [[:tx/assoc eid :entity/animation (animation/tick animation delta-time)]
+  [[:tx/assoc eid :entity/animation (tick animation delta-time)]
    (when (and (:delete-after-stopped? animation)
-              (animation/stopped? animation))
+              (stopped? animation))
      [:tx/mark-destroyed eid])])
 
 (defmethod entity/render :entity/animation
   [[_k animation] entity ctx]
-  (image/draw-image (animation/current-frame animation)
+  (image/draw-image (current-frame animation)
                     entity
                     ctx))
