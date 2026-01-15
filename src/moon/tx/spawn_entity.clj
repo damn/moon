3dbbdffe2ae7accf6258d7949274240186a8d7f1
Michael Sappler <michaelsappler@gmail.com>
2026-01-16 03:51:24 +0530

    upfactor spawn-entity!

--- src/moon/tx/spawn_entity.clj
+++ src/moon/tx/spawn_entity.clj
@@ -34,32 +34,28 @@
 
 (q/defrecord Entity [entity/body])
 
-(defn- world-spawn-entity
- [{:keys [world/content-grid
-          world/entity-ids
-          world/grid
-          world/id-counter]
-   :as world}
-  entity]
- (mu/validate-humanize components-schema entity)
- (let [entity (reduce (fn [m [k v]]
-                        (assoc m k (entity/create [k v] world)))
-                      {}
-                      entity)
-       _ (assert (and (not (contains? entity :entity/id))))
-       entity (assoc entity :entity/id (swap! id-counter inc))
-       entity (merge (map->Entity {}) entity)
-       eid (atom entity)]
-   (let [id (:entity/id @eid)]
-     (assert (number? id))
-     (swap! entity-ids assoc id eid))
-   (content-grid/add-entity! content-grid eid)
-   ; https://github.com/damn/core/issues/58
-   ;(assert (valid-position? grid @eid))
-   (grid/set-touched-cells! grid eid)
-   (when (:body/collides? (:entity/body @eid))
-     (grid/set-occupied-cells! grid eid))
-   (mapcat #(entity/after-create % eid world) @eid)))
-
 (defn do! [{:keys [ctx/world]} entity]
-  (world-spawn-entity world entity))
+  (mu/validate-humanize components-schema entity)
+  (let [{:keys [world/content-grid
+                world/entity-ids
+                world/grid
+                world/id-counter]
+         :as world} world
+        entity (reduce (fn [m [k v]]
+                         (assoc m k (entity/create [k v] world)))
+                       {}
+                       entity)
+        _ (assert (and (not (contains? entity :entity/id))))
+        entity (assoc entity :entity/id (swap! id-counter inc))
+        entity (merge (map->Entity {}) entity)
+        eid (atom entity)]
+    (let [id (:entity/id @eid)]
+      (assert (number? id))
+      (swap! entity-ids assoc id eid))
+    (content-grid/add-entity! content-grid eid)
+    ; https://github.com/damn/core/issues/58
+    ;(assert (valid-position? grid @eid))
+    (grid/set-touched-cells! grid eid)
+    (when (:body/collides? (:entity/body @eid))
+      (grid/set-occupied-cells! grid eid))
+    (mapcat #(entity/after-create % eid world) @eid)))
