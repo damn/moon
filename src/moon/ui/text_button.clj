d8a1e1067787aeb22d000aa5a7f69e286136966d
Michael Sappler <michaelsappler@gmail.com>
2026-01-16 03:50:36 +0530

    Remove comments

--- src/moon/ui/text_button.clj
+++ src/moon/ui/text_button.clj
@@ -5,16 +5,7 @@
            (com.badlogic.gdx.scenes.scene2d.utils ChangeListener)
            (moon Stage)))
 
-; Assumption! We are using 'moon.Stage'
-; actually the on-changed can just return [event actor]
-; and everytime (.ctx??)?
-
 (defn create
-  "Creates a `com.badlogic.gdx.scenes.scene2d.ui.TextButton` with the following options:
-
-  * `text`: `String`.
-  * `skin`: `com.badlogic.gdx.scenes.scene2d.ui.Skin`
-  * `on-clicked`: `(fn [actor ctx])` for side-effects."
   [{:keys [text
            on-clicked
            ^Skin skin]}]
@@ -22,6 +13,4 @@
     (.addListener
      (proxy [ChangeListener] []
        (changed [^Event event actor]
-         ; TODO does too much - just pass event,actor ... /// ... ///
-         ;; then can just be a 'clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-button' namespace?
          (on-clicked actor (.ctx ^Stage (.getStage event))))))))
