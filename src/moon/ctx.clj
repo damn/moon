5bdc967d7aea15371b6aa97a92888ba6372e1ca2
Michael Sappler <michaelsappler@gmail.com>
2026-01-16 22:20:32 +0530

    Remove moon.txs-impl

--- src/moon/ctx.clj
+++ src/moon/ctx.clj
@@ -1,40 +1,7 @@
-(ns moon.ctx
-  (:require [moon.tx-handler :as tx-handler]))
+(ns moon.ctx)
 
-(declare reaction-txs-fn-map
-         txs-fn-map)
-
-(defn- reduce-actions!
-  [txs-fn-map ctx txs]
-  (loop [ctx ctx
-         txs txs]
-    (if (empty? txs)
-      ctx
-      (let [[k & params :as tx] (first txs)]
-        (if tx
-          (let [_ (assert (vector? tx))
-                f (get txs-fn-map k)
-                new-ctx (try
-                         (if (nil? f)
-                           ctx
-                           (apply f ctx params))
-                         (catch Throwable t
-                           (throw (ex-info "Error handling tx"
-                                           {:tx tx}
-                                           t))))]
-            (recur new-ctx
-                   (rest txs)))
-          (recur ctx
-                 (rest txs)))))))
-
-(defn handle! [ctx txs]
-  (let [handled-txs (try (tx-handler/actions! txs-fn-map ctx txs)
-                         (catch Throwable t
-                           (throw (ex-info "Error handling txs"
-                                           {:txs txs} t))))]
-    (reduce-actions! reaction-txs-fn-map
-                     ctx
-                     handled-txs)))
+(defprotocol TransactionHandler
+  (handle! [_ txs]))
 
 (defprotocol Graphics
   (draw! [_ draws]))
