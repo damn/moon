1a3e820638aa4b99f488073e10aa59b473fbbc14
Michael Sappler <michaelsappler@gmail.com>
2026-01-16 20:00:06 +0530

    add comments

--- project.clj
+++ project.clj
@@ -1,27 +1,46 @@
 (defproject moon "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
+                 ;; libgdx stuff
                  [com.badlogicgames.gdx/gdx                   "1.14.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3    "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.14.0" :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform          "1.14.0" :classifier "natives-desktop"]
                  [space.earlygrey/shapedrawer "2.6.0"]
-                 [com.github.cdorrat/reduce-fsm "fe1c914d68"]
+                 ;;
+
+
+                 ;; my libs
                  [com.github.damn/clojure.grid2d "538fc4c44b"]
                  [com.github.damn/clojure.math.raycaster "0956fc0e9b"]
                  [com.github.damn/clojure.math.vector2 "9b3fd73f9b"]
                  [com.github.damn/clojure.rand "6a273c942b"]
                  [com.github.damn/malli.utils "5da493efcb"]
+                 ;;
+
+
+                 ; game logic
+                 [com.github.cdorrat/reduce-fsm "fe1c914d68"]
+
+                 ; performance
                  [fr.reuz/qrecord "0.1.0"]
+
+                 ; cuteness
                  [org.clj-commons/pretty "3.2.0"]
+
+                 ; clojure
                  [org.clojure/clojure "1.12.0"]
 
-                 ; dev-only
+                 ;; dev-only:
+
                  [nrepl "0.9.0"]
                  [org.clojure/tools.namespace "1.3.0"]
                  [lein-hiera "2.0.0"]
+
+                 ; my lib
                  [com.github.damn/clojure.dev-loop "ef54a03"]
+                 ;
                  ]
   :source-paths ["src"]
   :java-source-paths ["java-src"]
