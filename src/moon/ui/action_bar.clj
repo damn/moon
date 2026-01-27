99e8ff47029d054917e34628e2ac953f287715b1
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 06:50:16 +0530

    Remove Action-Bar protocol

--- src/moon/ui/action_bar.clj
+++ src/moon/ui/action_bar.clj
@@ -1,6 +1,60 @@
-(ns moon.ui.action-bar)
+(ns moon.ui.action-bar
+  (:require [moon.ui :as ui]
+            [moon.ui.actor :as actor]
+            [moon.ui.group :as group])
+  (:import (com.badlogic.gdx.scenes.scene2d Actor
+                                            Group)
+           (com.badlogic.gdx.scenes.scene2d.ui Button
+                                               ButtonGroup
+                                               HorizontalGroup
+                                               Table)))
 
-(defprotocol ActionBar
-  (selected-skill [_])
-  (add-skill! [_ {:keys [skill-id texture-region tooltip-text]} skin])
-  (remove-skill! [_ skill-id]))
+(defn create []
+  (ui/actor
+   {:type :ui/table
+    :rows [[{:actor (doto (doto (HorizontalGroup.)
+                            (.space 2)
+                            (.pad 2))
+                      (.setName "moon.ui.action-bar.horizontal-group")
+                      (.setUserObject (doto (ButtonGroup.)
+                                        (.setMaxCheckCount 1)
+                                        (.setMinCheckCount 0))))
+             :expand? true
+             :bottom? true}]]
+    :actor/name "moon.ui.action-bar"
+    :cell-defaults {:pad 2}
+    :fill-parent? true}))
+
+(defn- get-data [action-bar]
+  (let [group (group/find-actor action-bar "moon.ui.action-bar.horizontal-group")]
+    {:horizontal-group group
+     :button-group (Actor/.getUserObject group)}))
+
+(defn selected-skill [action-bar]
+  (when-let [skill-button (ButtonGroup/.getChecked (:button-group (get-data action-bar)))]
+    (.getUserObject skill-button)))
+
+(defn add-skill!
+  [action-bar
+   {:keys [skill-id
+           texture-region
+           tooltip-text]}
+   skin]
+  (let [{:keys [horizontal-group button-group]} (get-data action-bar)
+        button (ui/actor
+                {:type :ui/image-button
+                 :actor/user-object skill-id
+                 :drawable/texture-region texture-region
+                 :drawable/scale 2
+                 :tooltip tooltip-text
+                 :skin skin})]
+    (Group/.addActor horizontal-group button)
+    (ButtonGroup/.add button-group ^Button button)
+    nil))
+
+(defn remove-skill! [action-bar skill-id]
+  (let [{:keys [horizontal-group button-group]} (get-data action-bar)
+        button (get horizontal-group skill-id)]
+    (actor/remove! button)
+    (ButtonGroup/.remove button-group ^Button button)
+    nil))
