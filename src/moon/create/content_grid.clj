02316b26e64270a98655a739045d0a9eac33c96e
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 06:30:32 +0530

    Remove content-grid protocol

--- src/moon/create/content_grid.clj
+++ src/moon/create/content_grid.clj
@@ -1,54 +1,11 @@
 (ns moon.create.content-grid
-  (:require [clojure.grid2d :as g2d]
-            [moon.content-grid :as content-grid])
+  (:require [moon.content-grid :as content-grid])
   (:import (com.badlogic.gdx.maps.tiled TiledMap)))
 
-(defn- update-entity! [{:keys [grid cell-w cell-h]} eid]
-  (let [{:keys [moon.content-grid/content-cell
-                entity/body]} @eid
-        [x y] (:body/position body)
-        new-cell (get grid [(int (/ x cell-w))
-                            (int (/ y cell-h))])]
-    (when-not (= content-cell new-cell)
-      (swap! new-cell update :entities conj eid)
-      (swap! eid assoc :moon.content-grid/content-cell new-cell)
-      (when content-cell
-        (swap! content-cell update :entities disj eid)))))
-
-(defrecord ContentGrid []
-  content-grid/ContentGrid
-  (add-entity! [this eid]
-    (update-entity! this eid))
-
-  (remove-entity! [_ eid]
-    (-> @eid
-        :moon.content-grid/content-cell
-        (swap! update :entities disj eid)))
-
-  (position-changed! [this eid]
-    (update-entity! this eid))
-
-  (active-entities [{:keys [grid]} center-entity]
-    (->> (let [idx (-> center-entity
-                       :moon.content-grid/content-cell
-                       deref
-                       :idx)]
-           (cons idx (g2d/get-8-neighbour-positions idx)))
-         (keep grid)
-         (mapcat (comp :entities deref)))))
-
-(defn- create-content-grid [width height cell-size]
-  (map->ContentGrid
-   {:grid (g2d/create-grid
-           (inc (int (/ width  cell-size)))
-           (inc (int (/ height cell-size)))
-           (fn [idx]
-             (atom {:idx idx,
-                    :entities #{}})))
-    :cell-w cell-size
-    :cell-h cell-size}))
-
-(defn step [{:keys [^TiledMap ctx/tiled-map] :as ctx} cell-size]
-  (assoc ctx :ctx/content-grid (create-content-grid (.get (.getProperties tiled-map) "width")
+(defn step
+  [{:keys [^TiledMap ctx/tiled-map]
+    :as ctx}
+   cell-size]
+  (assoc ctx :ctx/content-grid (content-grid/create (.get (.getProperties tiled-map) "width")
                                                     (.get (.getProperties tiled-map) "height")
                                                     cell-size)))
