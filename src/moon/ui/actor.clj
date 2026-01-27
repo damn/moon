ca7b7d5e5fa0fe3603db5c5b9631fd09db5c170c
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 10:25:21 +0530

    Remove comments

--- src/moon/ui/actor.clj
+++ src/moon/ui/actor.clj
@@ -1,23 +1,18 @@
-; finally the whole thing is 'clj.api.*'?
 (ns moon.ui.actor
   (:import (com.badlogic.gdx.scenes.scene2d Actor)))
 
 (defn set-position! [^Actor actor [x y]]
   (.setPosition actor x y))
 
-; setPosition has alignment param -> maybe can use?
-; but anyway prefer to write clojure instead of java
 (defn set-center! [^Actor actor [x y]]
   (.setPosition actor
                 (- x (/ (.getWidth  actor) 2))
                 (- y (/ (.getHeight actor) 2))))
 
-; these two are simple 'clj.api.com.badlogic.gdx.scenes.scene2d.actor' fns ?!
-
-(defn toggle-visible! [^Actor actor] ; maybe make PR @ libgdx?
+(defn toggle-visible! [^Actor actor]
   (.setVisible actor (not (.isVisible actor))))
 
-(defn find-ancestor ; maybe function exists already? firstAscendant ?
+(defn find-ancestor
   [^Actor actor clazz]
   (if-let [parent (.getParent actor)]
     (if (instance? clazz parent)
