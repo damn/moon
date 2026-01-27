b62838a5af0fd152688874899fbd43ea77c69bf0
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 06:35:02 +0530

    Remove DB protocol

--- src/moon/db_impl.clj
+++ src/moon/db_impl.clj
@@ -1,7 +1,6 @@
 (ns moon.db-impl
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.pprint :as pprint]
             [malli.core :as m]
             [malli.utils :as mu]
             [moon.db :as db]
@@ -9,21 +8,6 @@
             [moon.schema :as schema]
             [moon.schemas :as schemas]
             [moon.utils :as utils]))
-
-(defn- save!
-  [{:keys [db/data db/file]}]
-  (let [data (->> (vals data)
-                  (sort-by property/type)
-                  (map utils/recur-sort-map)
-                  doall)]
-    (.start
-     (Thread.
-      (fn []
-        (binding [*print-level* nil]
-          (->> data
-               pprint/pprint
-               with-out-str
-               (spit file))))))))
 
 (defrecord Schemas []
   schemas/Schemas
@@ -50,50 +34,6 @@
     (mu/create-map-schema ks (fn [k]
                                (schema/malli-form (get schemas k) schemas)))))
 
-(defrecord RDB []
-  db/DB
-  (property-types [{:keys [db/schemas]}]
-    (filter #(= "properties" (namespace %)) (keys schemas)))
-
-  (update! [{:keys [db/data db/schemas]
-             :as this}
-            {:keys [property/id] :as property}]
-    (assert (contains? property :property/id))
-    (assert (contains? data id))
-    (schemas/validate schemas (property/type property) property)
-    (let [new-db (update this :db/data assoc id property)]
-      (save! new-db)
-      new-db))
-
-  (delete! [{:keys [db/data] :as this} property-id]
-    (assert (contains? data property-id))
-    (let [new-db (update this :db/data dissoc property-id)]
-      (save! new-db)
-      new-db))
-
-  (get-raw [{:keys [db/data]} property-id]
-    {:pre [(contains? data property-id)]}
-    (get data property-id))
-
-  (all-raw [{:keys [db/data]} property-type]
-    (->> (vals data)
-         (filter #(= property-type (property/type %)))))
-
-  (build
-    [{:keys [db/schemas]
-      :as this}
-     property-id]
-    (schemas/build-values schemas
-                          (db/get-raw this property-id)
-                          this))
-
-  (build-all
-    [{:keys [db/schemas]
-      :as this}
-     property-type]
-    (map #(schemas/build-values schemas % this)
-         (db/all-raw this property-type))))
-
 (defn create []
   (let [schemas (-> "schema.edn" io/resource slurp edn/read-string)
         schemas (map->Schemas schemas)
@@ -103,7 +43,6 @@
                 (apply distinct? (map :property/id properties))))
     (doseq [property properties]
       (schemas/validate schemas (property/type property) property))
-    (merge (map->RDB {})
-           {:db/data (zipmap (map :property/id properties) properties)
-            :db/file properties-file
-            :db/schemas schemas})))
+    {:db/data (zipmap (map :property/id properties) properties)
+     :db/file properties-file
+     :db/schemas schemas}))
