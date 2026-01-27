8d61f812d717f65a50cdeec58c47dbc5454e8600
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 09:55:23 +0530

    split up uf-caves

---
+++ src/moon/uf_caves/fix_nads.clj
@@ -0,0 +1,11 @@
+(ns moon.uf-caves.fix-nads
+  (:require [clojure.grid2d :as g2d]
+            [moon.nads :as nads]))
+
+(defn step
+  [{:keys [level/grid]
+    :as level}]
+  (assert (= #{:wall :ground} (set (g2d/cells grid))))
+  (let [grid (nads/fix-nads grid)]
+    (assert (= #{:wall :ground} (set (g2d/cells grid))))
+    (assoc level :level/grid grid)))
---
+++ src/moon/uf_caves/initial_grid.clj
@@ -0,0 +1,14 @@
+(ns moon.uf-caves.initial-grid
+  (:require [clojure.grid2d :as g2d]
+            [moon.caves :as caves]))
+
+(defn step
+  [{:keys [size
+           cave-style
+           random]
+    :as level}]
+  (let [{:keys [start grid]} (caves/create random size size cave-style)]
+    (assert (= #{:wall :ground} (set (g2d/cells grid))))
+    (assoc level
+           :level/start start
+           :level/grid grid)))
