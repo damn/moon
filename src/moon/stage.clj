2433b911bdcca2c83c7fbed5ac59b3bc1b79cb8d
Michael Sappler <michaelsappler@gmail.com>
2026-01-27 06:46:50 +0530

    Remove Stage protocol

--- src/moon/stage.clj
+++ src/moon/stage.clj
@@ -1,8 +1,19 @@
-(ns moon.stage)
+(ns moon.stage
+  (:require [clj.api.com.badlogic.gdx.utils.viewport :as viewport])
+  (:import (moon Stage)))
 
-(defprotocol Stage
-  (ctx [_])
-  (add-actor! [_ actor])
-  (root [_])
-  (mouseover-actor [_ [x y]])
-  (viewport [_]))
+(defn ctx [^Stage stage]
+  (.ctx stage))
+
+(defn add-actor! [^Stage stage actor]
+  (.addActor stage actor))
+
+(defn root [^Stage stage]
+  (.getRoot stage))
+
+(defn viewport [^Stage stage]
+  (.getViewport stage))
+
+(defn mouseover-actor [^Stage stage position]
+  (let [[x y] (viewport/unproject (viewport stage) position)]
+    (.hit stage x y true)))
