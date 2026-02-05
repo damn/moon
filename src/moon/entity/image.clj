8b789166f6e65086de91ac8fc1c294782a1d89c7
Michael Sappler <michaelsappler@gmail.com>
2026-02-05 05:50:37 +0530

    Remove unnecessary moon.entity.image/draw-image fn

--- src/moon/entity/image.clj
+++ src/moon/entity/image.clj
@@ -1,17 +1,11 @@
 (ns moon.entity.image
   (:require [moon.textures :as textures]))
 
-(defn draw-image
-  [image
-   {:keys [entity/body]}
-   {:keys [ctx/textures]}]
+(defn render
+  [[_k image] {:keys [entity/body]} {:keys [ctx/textures]}]
   [[:draw/texture-region
     (textures/texture-region textures image)
     (:body/position body)
     {:center? true
      :rotation (or (:body/rotation-angle body)
                    0)}]])
-
-(defn render
-  [[_k image] entity ctx]
-  (draw-image image entity ctx))
