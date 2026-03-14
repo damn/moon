ac0f6958889cf8aad169bb0da1097aecb473b80b
Michael Sappler <michaelsappler@gmail.com>
2026-03-14 10:31:15 +0530

    clj.api...cell -> into moon.table

--- src/moon/table.clj
+++ src/moon/table.clj
@@ -1,14 +1,33 @@
 (ns moon.table
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.cell :as cell])
   (:import (com.badlogic.gdx.scenes.scene2d Actor)
-           (com.badlogic.gdx.scenes.scene2d.ui Table)))
+           (com.badlogic.gdx.scenes.scene2d.ui Cell
+                                               Table)))
+
+(defn- set-cell-opts! [^Cell cell opts]
+  (doseq [[option arg] opts]
+    (case option
+      :fill-x?    (.fillX     cell)
+      :fill-y?    (.fillY     cell)
+      :expand?    (.expand    cell)
+      :expand-x?  (.expandX   cell)
+      :expand-y?  (.expandY   cell)
+      :bottom?    (.bottom    cell)
+      :colspan    (.colspan   cell (int   arg))
+      :pad        (.pad       cell (float arg))
+      :pad-top    (.padTop    cell (float arg))
+      :pad-bottom (.padBottom cell (float arg))
+      :width      (.width     cell (float arg))
+      :height     (.height    cell (float arg))
+      :center?    (.center    cell)
+      :right?     (.right     cell)
+      :left?      (.left      cell))))
 
 (defn add-rows! [^Table table rows]
   (doseq [row rows]
     (doseq [props-or-actor row]
       (cond
        (map? props-or-actor) (-> (.add table ^Actor (:actor props-or-actor))
-                                 (cell/set-opts! (dissoc props-or-actor :actor)))
+                                 (set-cell-opts! (dissoc props-or-actor :actor)))
        ; TODO Remove else case
        :else (.add table ^Actor props-or-actor)))
     (.row table))
@@ -16,7 +35,7 @@
 
 ; TODO use doto.
 (defn set-opts! [^Table table {:keys [rows cell-defaults] :as opts}]
-  (cell/set-opts! (.defaults table) cell-defaults)
+  (set-cell-opts! (.defaults table) cell-defaults)
   (add-rows! table rows)
   table)
 
