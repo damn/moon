dc8c01f0aac00973ccca0510feb156e1af9932fe
Michael Sappler <michaelsappler@gmail.com>
2026-03-24 11:00:10 +0530

    db protocol

--- src/moon/create/db.clj
+++ src/moon/create/db.clj
@@ -1,7 +1,10 @@
 (ns moon.create.db
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [clojure.pprint :as pprint]
+            [moon.db :as db]
             [moon.property :as property]
+            [moon.map :as map]
             [moon.schemas :as schemas]))
 
 (defn step [ctx {:keys [schemas properties]}]
@@ -18,3 +21,54 @@
             :db/schemas schemas})))
 
 ; TODO schema separate ctx key ?
+
+(defn- save!
+  [{:keys [db/data db/file]}]
+  (let [data (->> (vals data)
+                  (sort-by property/type)
+                  (map map/recur-sort)
+                  doall)]
+    (.start
+     (Thread.
+      (fn []
+        (binding [*print-level* nil]
+          (->> data
+               pprint/pprint
+               with-out-str
+               (spit file))))))))
+
+(extend-type clojure.lang.PersistentArrayMap
+  db/DB
+  (property-types [{:keys [db/schemas]}]
+    (filter #(= "properties" (namespace %)) (keys schemas)))
+
+  (update! [{:keys [db/data db/schemas] :as this} {:keys [property/id] :as property}]
+    (assert (contains? property :property/id))
+    (assert (contains? data id))
+    (schemas/validate schemas (property/type property) property)
+    (let [new-db (update this :db/data assoc id property)]
+      (save! new-db)
+      new-db))
+
+  (delete! [{:keys [db/data] :as this} property-id]
+    (assert (contains? data property-id))
+    (let [new-db (update this :db/data dissoc property-id)]
+      (save! new-db)
+      new-db))
+
+  (get-raw [{:keys [db/data]} property-id]
+    (assert (contains? data property-id))
+    (get data property-id))
+
+  (all-raw [{:keys [db/data]} property-type]
+    (->> (vals data)
+         (filter #(= property-type (property/type %)))))
+
+  (build [{:keys [db/schemas] :as this} property-id]
+    (schemas/build-values schemas
+                          (db/get-raw this property-id)
+                          this))
+
+  (build-all [{:keys [db/schemas] :as this} property-type]
+    (map #(schemas/build-values schemas % this)
+         (db/all-raw this property-type))))
--- src/moon/db.clj
+++ src/moon/db.clj
@@ -1,62 +1,10 @@
-(ns moon.db
-  (:require [clojure.pprint :as pprint]
-            [moon.map :as map]
-            [moon.property :as property]
-            [moon.schemas :as schemas]))
+(ns moon.db)
 
-(defn- save!
-  [{:keys [db/data db/file]}]
-  (let [data (->> (vals data)
-                  (sort-by property/type)
-                  (map map/recur-sort)
-                  doall)]
-    (.start
-     (Thread.
-      (fn []
-        (binding [*print-level* nil]
-          (->> data
-               pprint/pprint
-               with-out-str
-               (spit file))))))))
-
-(defn property-types [{:keys [db/schemas]}]
-  (filter #(= "properties" (namespace %)) (keys schemas)))
-
-(defn update! [{:keys [db/data db/schemas]
-                :as this}
-               {:keys [property/id] :as property}]
-  (assert (contains? property :property/id))
-  (assert (contains? data id))
-  (schemas/validate schemas (property/type property) property)
-  (let [new-db (update this :db/data assoc id property)]
-    (save! new-db)
-    new-db))
-
-(defn delete! [{:keys [db/data] :as this} property-id]
-  (assert (contains? data property-id))
-  (let [new-db (update this :db/data dissoc property-id)]
-    (save! new-db)
-    new-db))
-
-(defn get-raw [{:keys [db/data]} property-id]
-  {:pre [(contains? data property-id)]}
-  (get data property-id))
-
-(defn all-raw [{:keys [db/data]} property-type]
-  (->> (vals data)
-       (filter #(= property-type (property/type %)))))
-
-(defn build
-  [{:keys [db/schemas]
-    :as this}
-   property-id]
-  (schemas/build-values schemas
-                        (get-raw this property-id)
-                        this))
-
-(defn build-all
-  [{:keys [db/schemas]
-    :as this}
-   property-type]
-  (map #(schemas/build-values schemas % this)
-       (all-raw this property-type)))
+(defprotocol DB
+  (property-types [_])
+  (update! [_ property])
+  (delete! [_ property-id])
+  (get-raw [_ property-id])
+  (all-raw [_ property-type])
+  (build [_ property-id])
+  (build-all [_ property-type]))
