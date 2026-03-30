a3ee29db14b1875c01f8a1e522ae7df77152faae
Michael Sappler <michaelsappler@gmail.com>
2026-03-30 11:32:54 +0530

    actor clj api

---
+++ src/clj/api/com/badlogic/gdx/scenes/scene2d/actor.clj
@@ -0,0 +1,23 @@
+(ns clj.api.com.badlogic.gdx.scenes.scene2d.actor
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)))
+
+(defn add-listener! [^Actor actor listener]
+  (.addListener actor listener))
+
+(defn user-object [^Actor actor]
+  (.getUserObject actor))
+
+(defn stage [^Actor actor]
+  (.getStage actor))
+
+(defn set-position! [^Actor actor [x y]]
+  (.setPosition actor x y))
+
+(defn set-visible! [^Actor actor visible?]
+  (.setVisible actor visible?))
+
+(defn visible? [^Actor actor]
+  (.isVisible actor))
+
+(defn parent [^Actor actor]
+  (.getParent actor))
