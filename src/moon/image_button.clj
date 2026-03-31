60e9d959aacdc09f5b17e215add3d97505c4c8fd
Michael Sappler <michaelsappler@gmail.com>
2026-03-31 10:17:03 +0530

    clj.api image-button

--- src/moon/image_button.clj
+++ src/moon/image_button.clj
@@ -1,30 +1,30 @@
 (ns moon.image-button
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-tooltip :as text-tooltip])
-  (:import (com.badlogic.gdx.graphics.g2d TextureRegion)
-           (com.badlogic.gdx.scenes.scene2d Event)
-           (com.badlogic.gdx.scenes.scene2d.ui ImageButton)
-           (com.badlogic.gdx.scenes.scene2d.utils ChangeListener
-                                                  Drawable
-                                                  TextureRegionDrawable)
-           (moon Stage)))
+  (:require [clj.api.com.badlogic.gdx.graphics.g2d.texture-region :as texture-region]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.event :as event]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.image-button :as image-button]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-tooltip :as text-tooltip]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.utils.change-listener :as change-listener]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.utils.drawable :as drawable]
+            [clj.api.com.badlogic.gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
+            [moon.actor :as actor]
+            [moon.stage :as stage]))
 
 (defn create
-  ^ImageButton
-  [{:keys [^TextureRegion drawable/texture-region
+  [{:keys [drawable/texture-region
            on-clicked
            drawable/scale
            skin]
     :as opts}]
   (let [scale (or scale 1)
-        [w h] [(.getRegionWidth  texture-region)
-               (.getRegionHeight texture-region)]
-        drawable (doto (TextureRegionDrawable. texture-region)
-                   (.setMinSize (* scale w) (* scale h)))
-        image-button (ImageButton. ^Drawable drawable)]
+        [w h] [(texture-region/width  texture-region)
+               (texture-region/height texture-region)]
+        drawable (doto (texture-region-drawable/create texture-region)
+                   (drawable/set-min-size! (* scale w) (* scale h)))
+        image-button (image-button/create drawable)]
     (when on-clicked
-      (.addListener image-button (proxy [ChangeListener] []
-                                   (changed [event actor]
-                                     (on-clicked actor (.ctx ^Stage (Event/.getStage event)))))))
+      (actor/add-listener! image-button (change-listener/create
+                                          (fn [event actor]
+                                            (on-clicked actor (stage/ctx (event/stage event)))))))
     (when-let [tooltip (:tooltip opts)]
-      (.addListener image-button (text-tooltip/create (str tooltip) skin)))
+      (actor/add-listener! image-button (text-tooltip/create (str tooltip) skin)))
     image-button))
