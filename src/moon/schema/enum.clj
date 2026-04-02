43bf2b2f296af5d6cc1f1e3f74ee5ad4d936ff4a
Michael Sappler <michaelsappler@gmail.com>
2026-04-02 15:35:20 +0530

    clj.api.com.badlogic.gdx.scenes.scene2d.ui.select-box

--- src/moon/schema/enum.clj
+++ src/moon/schema/enum.clj
@@ -1,16 +1,15 @@
 (ns moon.schema.enum
-  (:require [clojure.edn :as edn]
-            [moon.edn])
-  (:import (com.badlogic.gdx.scenes.scene2d.ui SelectBox
-                                               Skin)))
+  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.select-box :as select-box]
+            [clojure.edn :as edn]
+            [moon.edn]))
 
 (defn malli-form [[_ & params] _schemas]
   (apply vector :enum params))
 
 (defn create [schema v {:keys [ctx/skin]}]
-  (doto (SelectBox. ^Skin skin)
-    (.setItems ^"[Lcom.badlogic.gdx.scenes.scene2d.Actor;" (into-array (map moon.edn/->str (rest schema))))
-    (.setSelected (moon.edn/->str v))))
+  (doto (select-box/create skin)
+    (select-box/set-items! (map moon.edn/->str (rest schema)))
+    (select-box/set-selected! (moon.edn/->str v))))
 
 (defn value [_  widget _schemas]
-  (edn/read-string (SelectBox/.getSelected widget)))
+  (edn/read-string (select-box/selected widget)))
