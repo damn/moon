b466c41e78c9644b8f66916b357d52e5207a0b2c
Michael Sappler <michaelsappler@gmail.com>
2026-04-03 16:15:03 +0530

    rectangle java code

--- src/clj/api/com/badlogic/gdx/math/rectangle.clj
+++ src/clj/api/com/badlogic/gdx/math/rectangle.clj
@@ -6,7 +6,9 @@
   (Rectangle. x y width height))
 
 (defn overlaps? [^Rectangle rectangle ^Rectangle other-rectangle]
+  ; return x < r.x + r.width && x + width > r.x && y < r.y + r.height && y + height > r.y;
   (.overlaps rectangle other-rectangle))
 
 (defn contains? [^Rectangle rectangle [x y]]
+  ; return this.x <= x && this.x + this.width >= x && this.y <= y && this.y + this.height >= y;
   (.contains rectangle x y))
