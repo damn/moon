d57f8d258aa73148d37ad08f81afed5e9e9a7658
Michael Sappler <michaelsappler@gmail.com>
2026-04-03 19:12:31 +0530

    audio one step fn

--- src/moon/create/audio.clj
+++ src/moon/create/audio.clj
@@ -2,21 +2,14 @@
   (:require [clj.api.com.badlogic.gdx.audio :as audio]
             [clj.api.com.badlogic.gdx.files :as files]))
 
-(defn- load-sounds*
-  [audio files {:keys [sound-names path-format]}]
-  (let [sound-name->file-handle (into {}
-                                      (for [sound-name sound-names
-                                            :let [path (format path-format sound-name)]]
-                                        [sound-name
-                                         (files/internal files path)]))]
-    (into {}
-          (for [[sound-name file-handle] sound-name->file-handle]
-            [sound-name
-             (audio/new-sound audio file-handle)]))))
-
 (defn do!
-  [{:keys [ctx/audio
-           ctx/files]
-    :as ctx}
-   config]
-  (assoc ctx :ctx/audio (load-sounds* audio files config)))
+  [ctx {:keys [sound-names path-format]}]
+  (assoc ctx :ctx/audio (let [sound-name->file-handle (into {}
+                                                            (for [sound-name sound-names
+                                                                  :let [path (format path-format sound-name)]]
+                                                              [sound-name
+                                                               (files/internal (:ctx/files ctx) path)]))]
+                          (into {}
+                                (for [[sound-name file-handle] sound-name->file-handle]
+                                  [sound-name
+                                   (audio/new-sound (:ctx/audio ctx) file-handle)])))))
