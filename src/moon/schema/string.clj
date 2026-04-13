178d896182684cdb2f88128efd26248dc1c16bfc
Michael Sappler <michaelsappler@gmail.com>
2026-04-14 03:51:39 +0530

    val-max and string no clj.api

--- src/moon/schema/string.clj
+++ src/moon/schema/string.clj
@@ -1,14 +1,16 @@
 (ns moon.schema.string
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-field :as text-field]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-tooltip :as text-tooltip]
-            [moon.actor :as actor]))
+  (:require [gdl.scene2d.ui.text-field :as text-field]
+            [moon.ui :as ui]))
 
 (defn malli-form [_ _schemas]
   :string)
 
 (defn create [schema v {:keys [ctx/skin]}]
-  (doto (text-field/create (str v) skin)
-    (actor/add-listener! (text-tooltip/create (str schema) skin))))
+  (ui/create
+   {:type :ui/text-field
+    :text (str v)
+    :skin skin
+    :actor/listeners {:listener/text-tooltip [(str schema) skin]}}))
 
 (defn value [_ widget _schemas]
   (text-field/text widget))
--- src/moon/schema/val_max.clj
+++ src/moon/schema/val_max.clj
@@ -1,9 +1,8 @@
 (ns moon.schema.val-max
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-field :as text-field]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-tooltip :as text-tooltip]
+  (:require [gdl.scene2d.ui.text-field :as text-field]
             [clojure.edn :as edn]
-            [moon.actor :as actor]
             [moon.edn]
+            [moon.ui :as ui]
             [moon.val-max :as val-max]))
 
 (defn malli-form [_ _schemas]
@@ -11,8 +10,11 @@
 
 (defn create
   [schema v {:keys [ctx/skin]}]
-  (doto (text-field/create (moon.edn/->str v) skin)
-    (actor/add-listener! (text-tooltip/create (str schema) skin))))
+  (ui/create
+   {:type :ui/text-field
+    :text (moon.edn/->str v)
+    :skin skin
+    :actor/listeners {:listener/text-tooltip [(str schema) skin]}}))
 
 (defn value
   [_  widget _schemas]
