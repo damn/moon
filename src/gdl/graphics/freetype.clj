ffe029674a69f1b023dbb714dd0092b563823b04
Michael Sappler <michaelsappler@gmail.com>
2026-04-13 13:33:45 +0530

    freetype genrate font

--- src/gdl/graphics/freetype.clj
+++ src/gdl/graphics/freetype.clj
@@ -1,9 +1,11 @@
 (ns gdl.graphics.freetype
-  (:require [clj.api.com.badlogic.gdx.graphics.g2d.freetype.font-generator :as font-generator]
+  (:require [clj.api.com.badlogic.gdx.graphics.texture.filter :as texture.filter]
+            [clj.api.com.badlogic.gdx.graphics.g2d.freetype.font-generator :as font-generator]
             [clj.api.com.badlogic.gdx.graphics.g2d.freetype.font-generator.parameter :as parameter]
-            [clj.api.com.badlogic.gdx.utils.disposable :as disposable]))
+            [clj.api.com.badlogic.gdx.utils.disposable :as disposable]
+            [gdl.graphics.bitmap-font :as font]))
 
-(defn generate-font
+(defn generate-font*
   [file-handle
    {:keys [size min-filter mag-filter]}]
   (let [generator (font-generator/create file-handle)
@@ -14,3 +16,16 @@
                                              (parameter/set-mag-filter! mag-filter)))]
     (disposable/dispose! generator)
     font))
+
+(defn generate-font
+  [file-handle {:keys [size
+                       quality-scaling
+                       enable-markup?
+                       use-integer-positions?]}]
+  (doto (generate-font* file-handle
+                        {:size (* size quality-scaling)
+                         :min-filter texture.filter/linear
+                         :mag-filter texture.filter/linear})
+    (font/set-scale! (/ quality-scaling))
+    (font/enable-markup! enable-markup?)
+    (font/use-integer-positions! use-integer-positions?)))
--- src/moon/create/default_font.clj
+++ src/moon/create/default_font.clj
@@ -1,25 +1,9 @@
 (ns moon.create.default-font
-  (:require [clj.api.com.badlogic.gdx.graphics.texture.filter :as texture.filter]
-            [gdl.files :as files]
-            [gdl.graphics.bitmap-font :as font]
+  (:require [gdl.files :as files]
             [gdl.graphics.freetype :as freetype]))
-
-(defn- generate-font
-  [file-handle {:keys [size
-                       quality-scaling
-                       enable-markup?
-                       use-integer-positions?]}]
-  (doto (freetype/generate-font file-handle
-                                {:size (* size quality-scaling)
-                                 :min-filter texture.filter/linear
-                                 :mag-filter texture.filter/linear})
-    (font/set-scale! (/ quality-scaling))
-    (font/enable-markup! enable-markup?)
-    (font/use-integer-positions! use-integer-positions?)))
 
 (defn do!
   [{:keys [ctx/files]
     :as ctx}
    {:keys [path params]}]
-  (assoc ctx :ctx/default-font (generate-font (files/internal files path)
-                                              params)))
+  (assoc ctx :ctx/default-font (freetype/generate-font (files/internal files path) params)))
