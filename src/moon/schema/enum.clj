78455160b8fb5d051333334733c30d520d0d63e9
Michael Sappler <michaelsappler@gmail.com>
2026-04-13 22:18:48 +0530

    :ui/select-box

--- src/moon/schema/enum.clj
+++ src/moon/schema/enum.clj
@@ -1,15 +1,18 @@
 (ns moon.schema.enum
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.select-box :as select-box]
-            [clojure.edn :as edn]
-            [moon.edn]))
+  (:require [clojure.edn :as edn]
+            [gdl.scene2d.ui.select-box :as select-box]
+            [moon.edn]
+            [moon.ui :as ui]))
 
 (defn malli-form [[_ & params] _schemas]
   (apply vector :enum params))
 
 (defn create [schema v {:keys [ctx/skin]}]
-  (doto (select-box/create skin)
-    (select-box/set-items! (map moon.edn/->str (rest schema)))
-    (select-box/set-selected! (moon.edn/->str v))))
+  (ui/create
+   {:type :ui/select-box
+    :skin skin
+    :items (map moon.edn/->str (rest schema))
+    :selected (moon.edn/->str v)}))
 
 (defn value [_  widget _schemas]
   (edn/read-string (select-box/selected widget)))
---
+++ src/moon/ui/select_box.clj
@@ -0,0 +1,7 @@
+(ns moon.ui.select-box
+  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.select-box :as select-box]))
+
+(defn create [{:keys [items selected skin]}]
+  (doto (select-box/create skin)
+    (select-box/set-items! items)
+    (select-box/set-selected! selected)))
