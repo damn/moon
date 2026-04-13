b8f77ea1c922d7624a6b994393c3f247cd7c11c5
Michael Sappler <michaelsappler@gmail.com>
2026-04-13 17:41:04 +0530

    :ui/group

--- src/moon/ui/group.clj
+++ src/moon/ui/group.clj
@@ -1,8 +1,13 @@
 (ns moon.ui.group
-  (:require [moon.ui.actor :as actor]
+  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.group :as gdx-group]
+            [moon.ui.actor :as actor]
             [moon.group :as group]))
 
 (defn set-opts! [group opts]
   (when-let [actors (:group/actors opts)]
     (group/add-actors! group actors))
   (actor/set-opts! group opts))
+
+(defn create [opts]
+  (doto (gdx-group/create)
+    (set-opts! opts)))
--- src/moon/ui_actors/windows.clj
+++ src/moon/ui_actors/windows.clj
@@ -1,10 +1,9 @@
 (ns moon.ui-actors.windows
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.group :as gdx-group]
-            [moon.actor :as actor]
-            [moon.group :as group]))
+  (:require [moon.ui :as ui]))
 
 (defn create
   [ctx actors]
-  (doto (gdx-group/create)
-    (group/add-actors! (map (fn [[f & params]] (apply f ctx params)) actors))
-    (actor/set-name! "moon.ui.windows")))
+  (ui/create
+   {:type :ui/group
+    :group/actors (map (fn [[f & params]] (apply f ctx params)) actors)
+    :actor/name "moon.ui.windows"}))
