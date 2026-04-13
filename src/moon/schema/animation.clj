c93e21f4d7967b021207e00b62e5ac53ffa2a0a6
Michael Sappler <michaelsappler@gmail.com>
2026-04-13 22:09:21 +0530

    moon.schema.animation no clj.api

--- src/moon/schema/animation.clj
+++ src/moon/schema/animation.clj
@@ -1,12 +1,7 @@
 (ns moon.schema.animation
-  (:require [gdl.texture-region :as texture-region]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.image-button :as image-button]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.ui.table :as gdx-table]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.utils.drawable :as drawable]
-            [clj.api.com.badlogic.gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
-            [moon.schemas :as schemas]
-            [moon.table :as table]
-            [moon.textures :as textures]))
+  (:require [moon.schemas :as schemas]
+            [moon.textures :as textures]
+            [moon.ui :as ui]))
 
 (defn malli-form [_ schemas]
   (schemas/create-map-schema schemas
@@ -17,10 +12,11 @@
 (defn create
   [_ animation {:keys [ctx/skin
                        ctx/textures]}]
-  (doto (gdx-table/create)
-    (table/set-cell-defaults! {:pad 1})
-    (table/add-rows! [(for [image (:animation/frames animation)]
-                        {:actor (let [texture-region (textures/texture-region textures image)]
-                                  (image-button/create (doto (texture-region-drawable/create texture-region)
-                                                         (drawable/set-min-size! (* 2 (texture-region/width texture-region))
-                                                                                 (* 2 (texture-region/height texture-region))))))})])))
+  (ui/create
+   {:type :ui/table
+    :table/cell-defaults {:pad 1}
+    :table/rows [(for [image (:animation/frames animation)]
+                   {:actor (ui/create
+                            {:type :ui/image-button
+                             :drawable {:drawable/texture-region (textures/texture-region textures image)
+                                        :drawable/scale 2}})})]}))
