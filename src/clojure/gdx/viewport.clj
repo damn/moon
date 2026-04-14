60366af5d0f39377407704cbddd70e80b0bf9b2c
Michael Sappler <michaelsappler@gmail.com>
2026-04-14 20:25:21 +0530

    clojure.gdx.viewport

---
+++ src/clojure/gdx/viewport.clj
@@ -0,0 +1,20 @@
+(ns clojure.gdx.viewport
+  (:require [clj.api.com.badlogic.gdx.math.vector2 :as vector2]
+            [clj.api.com.badlogic.gdx.utils.viewport :as viewport]))
+
+(defn camera [viewport]
+  (viewport/camera viewport))
+
+(defn world-width [viewport]
+  (viewport/world-width viewport))
+
+(defn world-height [viewport]
+  (viewport/world-height viewport))
+
+(defn update! [viewport screen-width screen-height center-camera?]
+  (viewport/update! viewport screen-width screen-height center-camera?))
+
+(defn unproject [viewport position]
+  (-> viewport
+      (viewport/unproject (vector2/->java position))
+      vector2/->clj))
--- src/gdl/viewport.clj
+++ src/gdl/viewport.clj
@@ -1,20 +1,8 @@
-(ns gdl.viewport
-  (:require [clj.api.com.badlogic.gdx.math.vector2 :as vector2]
-            [clj.api.com.badlogic.gdx.utils.viewport :as viewport]))
+(ns gdl.viewport)
 
-(defn camera [viewport]
-  (viewport/camera viewport))
-
-(defn world-width [viewport]
-  (viewport/world-width viewport))
-
-(defn world-height [viewport]
-  (viewport/world-height viewport))
-
-(defn update! [viewport screen-width screen-height center-camera?]
-  (viewport/update! viewport screen-width screen-height center-camera?))
-
-(defn unproject [viewport position]
-  (-> viewport
-      (viewport/unproject (vector2/->java position))
-      vector2/->clj))
+(defprotocol Viewport
+  (camera [_])
+  (world-width [_])
+  (world-height [_])
+  (update! [_ screen-width screen-height center-camera?])
+  (unproject [_ position]))
