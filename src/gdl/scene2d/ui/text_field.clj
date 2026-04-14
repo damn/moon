4856d5fccd245b7396209a1149ebb8f684ea7a0f
Michael Sappler <michaelsappler@gmail.com>
2026-04-14 19:20:44 +0530

    text-field protocol

--- src/gdl/scene2d/ui/text_field.clj
+++ src/gdl/scene2d/ui/text_field.clj
@@ -1,4 +1,4 @@
-(ns gdl.scene2d.ui.text-field
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.text-field :as text-field]))
+(ns gdl.scene2d.ui.text-field)
 
-(def text text-field/text)
+(defprotocol TextField
+  (text [_]))
