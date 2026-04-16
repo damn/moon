3f1e1ad0bb84d76dc8609107c7be7d56b8510683
Michael Sappler <michaelsappler@gmail.com>
2026-04-16 22:57:11 +0530

    Use clojure.gdx API clojure.gdx.tiled-map.tmx

--- src/moon/modules/load_schema_tiled_map.clj
+++ src/moon/modules/load_schema_tiled_map.clj
@@ -1,5 +1,5 @@
 (ns moon.modules.load-schema-tiled-map
-  (:require [clj.api.com.badlogic.gdx.maps.tiled.tmx-map-loader :as tmx-map-loader]))
+  (:require [clojure.gdx.tiled-map.tmx :as tmx-map-loader]))
 
 (defn step [w]
   (assoc w :schema-tiled-map (tmx-map-loader/load! "maps/modules.tmx")))
