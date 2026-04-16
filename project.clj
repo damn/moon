7a79a3d18d3a000ded22aa1e779ab73039f214a2
Michael Sappler <michaelsappler@gmail.com>
2026-04-16 10:47:56 +0530

    remove comments

--- project.clj
+++ project.clj
@@ -6,17 +6,12 @@
 (defproject moon "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
-                 ; gdl/moon/cdq ?
-                 ; all deps == 'moon engine' without ctx/?
-                 ; (ctx is wiring)
-                 ; where does the 'engine' or 'libraries' end and the 'game' start?
                  [com.badlogicgames.gdx/gdx                   "1.14.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3    "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.14.0" :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform          "1.14.0" :classifier "natives-desktop"]
                  [space.earlygrey/shapedrawer "2.6.0"]
-                 ;
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
                  [metosin/malli "0.13.0"]
                  [fr.reuz/qrecord "0.1.0"]
