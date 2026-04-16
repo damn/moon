5da5221f9adcf28ef69b452c8511441e75680464
Michael Sappler <michaelsappler@gmail.com>
2026-04-16 12:31:09 +0530

    pass listener/config create

---
+++ src/clojure/gdx/application_listener.clj
@@ -0,0 +1,22 @@
+(ns clojure.gdx.application-listener
+  (:require [clj.api.com.badlogic.gdx.application.listener :as listener]))
+
+(defn create [{:keys [state
+                      create!
+                      dispose!
+                      render!
+                      resize!]}]
+  (listener/create
+   (let [state @state]
+     {:create! (fn []
+                 (reset! state (let [[create-fn create-params] create!]
+                                 (create-fn create-params))))
+      :dispose! (fn []
+                  (dispose! @state))
+      :render! (fn []
+                 (let [[render-fn render-params] render!]
+                   (swap! state render-fn render-params)))
+      :resize! (fn [width height]
+                 (resize! @state width height))
+      :pause! (fn [])
+      :resume! (fn [])})))
--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,31 +1,10 @@
 (ns clojure.gdx.backends.lwjgl
-  (:require [clj.api.com.badlogic.gdx.application.listener :as listener]
-            [clj.api.com.badlogic.gdx.backends.lwjgl3.application :as application]
-            [clojure.gdx.backends.lwjgl.config :as config]))
+  (:require [clj.api.com.badlogic.gdx.backends.lwjgl3.application :as application]))
 
 (defn application!
-  [
-   {:keys [
-           config
-           state
-           create!
-           dispose!
-           render!
-           resize!
-           ]}
-   ]
-  (application/create (listener/create
-                       (let [state @state
-                             [create-fn create-params] create!
-                             [render-fn render-params] render!]
-                         {:create! (fn []
-                                     (reset! state (create-fn create-params)))
-                          :dispose! (fn []
-                                      (dispose! @state))
-                          :render! (fn []
-                                     (swap! state render-fn render-params))
-                          :resize! (fn [width height]
-                                     (resize! @state width height))
-                          :pause! (fn [])
-                          :resume! (fn [])}))
-                      (config/create config)))
+  [{:keys [application-listener
+           config]}]
+  (application/create (let [[f params] application-listener]
+                        (f params))
+                      (let [[f params] config]
+                        (f params))))
