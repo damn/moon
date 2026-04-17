9091704c57ebaee089b4c67ec557a6111d768da3
Michael Sappler <michaelsappler@gmail.com>
2026-04-17 10:18:32 +0530

    remove clj api application listener

--- src/clojure/gdx/application_listener.clj
+++ src/clojure/gdx/application_listener.clj
@@ -1,23 +1,28 @@
 (ns clojure.gdx.application-listener
-  (:require [clj.api.com.badlogic.gdx.application.listener :as listener]))
+  (:import (com.badlogic.gdx ApplicationListener)))
 
-(defn create [{:keys [state
-                      create!
-                      dispose!
-                      render!
-                      resize!]}]
-  (listener/create
-   (let [state @state]
-     {:create! (fn []
-                 ; just pass Gdx/app ??
-                 (reset! state (let [[create-fn create-params] create!]
-                                 (create-fn create-params))))
-      :dispose! (fn []
-                  (dispose! @state))
-      :render! (fn []
-                 (let [[render-fn render-params] render!]
-                   (swap! state render-fn render-params)))
-      :resize! (fn [width height]
-                 (resize! @state width height))
-      :pause! (fn [])
-      :resume! (fn [])})))
+(defn create
+  [{:keys [state
+           create!
+           dispose!
+           render!
+           resize!]}]
+  (let [state @state]
+    (reify ApplicationListener
+      (create [_]
+        (reset! state (let [[create-fn create-params] create!]
+                        (create-fn create-params))))
+
+      (dispose [_]
+        (dispose! @state))
+
+      (render [_]
+        (let [[render-fn render-params] render!]
+          (swap! state render-fn render-params)))
+
+      (resize [_ width height]
+        (resize! @state width height))
+
+      (pause [_])
+
+      (resume [_]))))
