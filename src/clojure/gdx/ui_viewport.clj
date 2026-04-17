be4f8673932e8b1386f0b49cfffe7b57209fe3a5
Michael Sappler <michaelsappler@gmail.com>
2026-04-17 15:18:35 +0530

    Integrate world/ui-viewport

--- src/clojure/gdx/ui_viewport.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure.gdx.ui-viewport
-  (:require [clj.api.com.badlogic.gdx.graphics.orthographic-camera :as orthographic-camera]
-            [clj.api.com.badlogic.gdx.utils.viewport.fit-viewport :as fit-viewport]))
-
-(defn create [width height]
-  (fit-viewport/create width height (orthographic-camera/create)))
--- src/moon/create/ui_viewport.clj
+++ src/moon/create/ui_viewport.clj
@@ -1,5 +1,6 @@
 (ns moon.create.ui-viewport
-  (:require [clojure.gdx.ui-viewport :as ui-viewport]))
+  (:require [clj.api.com.badlogic.gdx.graphics.orthographic-camera :as orthographic-camera]
+            [clj.api.com.badlogic.gdx.utils.viewport.fit-viewport :as fit-viewport]))
 
 (defn step [ctx {:keys [width height]}]
-  (assoc ctx :ctx/ui-viewport (ui-viewport/create width height)))
+  (assoc ctx :ctx/ui-viewport (fit-viewport/create width height (orthographic-camera/create))))
