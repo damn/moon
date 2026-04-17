7165590033e179de695fddbec7fad67341c9f25a
Michael Sappler <michaelsappler@gmail.com>
2026-04-17 18:17:29 +0530

    Integrate ApplicationListener

--- src/moon/application.clj
+++ src/moon/application.clj
@@ -1,13 +1,34 @@
 (ns moon.application
-  (:require [clojure.gdx.application-listener :as application-listener]
-            [clojure.gdx.backends.lwjgl :as lwjgl]
+  (:require [clojure.gdx.backends.lwjgl :as lwjgl]
             [clojure.gdx.backends.lwjgl.config :as config]
-            [clojure.gdx.colors :as colors]))
+            [clojure.gdx.colors :as colors])
+  (:import (com.badlogic.gdx ApplicationListener)))
 
 (def state (atom nil))
 
 (defn start! [{:keys [colors listener config]}]
   (colors/put! colors)
   (config/use-glfw-async!)
-  (lwjgl/application! (application-listener/create listener)
+  (lwjgl/application! (let [{:keys [create!
+                                    dispose!
+                                    render!
+                                    resize!]} listener]
+                        (reify ApplicationListener
+                          (create [_]
+                            (reset! state (let [[create-fn create-params] create!]
+                                            (create-fn create-params))))
+
+                          (dispose [_]
+                            (dispose! @state))
+
+                          (render [_]
+                            (let [[render-fn render-params] render!]
+                              (swap! state render-fn render-params)))
+
+                          (resize [_ width height]
+                            (resize! @state width height))
+
+                          (pause [_])
+
+                          (resume [_])))
                       (config/create config)))
