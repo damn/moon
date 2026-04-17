ebbbc8249575d39de08313a358c85994d77a4129
Michael Sappler <michaelsappler@gmail.com>
2026-04-17 15:58:25 +0530

    Move clj.api to clojure.gdx

--- src/clojure/gdx/scene2d/event.clj
+++ src/clojure/gdx/scene2d/event.clj
@@ -1,4 +1,4 @@
-(ns clj.api.com.badlogic.gdx.scenes.scene2d.event
+(ns clojure.gdx.scene2d.event
   (:import (com.badlogic.gdx.scenes.scene2d Event)))
 
 (defn stage [^Event event]
--- src/clojure/gdx/scene2d/ui/button_group.clj
+++ src/clojure/gdx/scene2d/ui/button_group.clj
@@ -1,7 +1,17 @@
 (ns clojure.gdx.scene2d.ui.button-group
-  (:require [clj.api.com.badlogic.gdx.scenes.scene2d.ui.button-group :as button-group]))
+  (:import (com.badlogic.gdx.scenes.scene2d.ui Button
+                                               ButtonGroup)))
 
-(def create button-group/create)
-(def add! button-group/add!)
-(def remove! button-group/remove!)
-(def checked button-group/checked)
+(defn create [{:keys [max-check-count min-check-count]}]
+  (doto (ButtonGroup.)
+    (.setMaxCheckCount max-check-count)
+    (.setMinCheckCount min-check-count)))
+
+(defn add! [^ButtonGroup button-group ^Button button]
+  (.add button-group button))
+
+(defn remove! [^ButtonGroup button-group ^Button button]
+  (.remove button-group button))
+
+(defn checked [^ButtonGroup button-group]
+  (.getChecked button-group))
