cd672ad2b0e5d922e646bd23f208766bce455aba
Michael Sappler <michaelsappler@gmail.com>
2026-04-17 22:55:21 +0530

    db/schemas no protocols

--- src/moon/db.clj
+++ src/moon/db.clj
@@ -1,10 +1,68 @@
-(ns moon.db)
+(ns moon.db
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pprint]
+            [moon.property :as property]
+            [moon.map :as map]
+            [moon.schemas :as schemas]))
 
-(defprotocol DB
-  (property-types [_])
-  (update! [_ property])
-  (delete! [_ property-id])
-  (get-raw [_ property-id])
-  (all-raw [_ property-type])
-  (build [_ property-id])
-  (build-all [_ property-type]))
+(defn create [{:keys [schemas properties]}]
+  (let [schemas (-> schemas io/resource slurp edn/read-string)
+        properties-file (io/resource properties)
+        properties (-> properties-file slurp edn/read-string)]
+    (assert (or (empty? properties)
+                (apply distinct? (map :property/id properties))))
+    (doseq [property properties]
+      (schemas/validate schemas (property/type property) property))
+    {:db/data (zipmap (map :property/id properties) properties)
+     :db/file properties-file
+     :db/schemas schemas}))
+
+(defn- save!
+  [{:keys [db/data db/file]}]
+  (let [data (->> (vals data)
+                  (sort-by property/type)
+                  (map map/recur-sort)
+                  doall)]
+    (.start
+     (Thread.
+      (fn []
+        (binding [*print-level* nil]
+          (->> data
+               pprint/pprint
+               with-out-str
+               (spit file))))))))
+
+(defn property-types [{:keys [db/schemas]}]
+  (filter #(= "properties" (namespace %)) (keys schemas)))
+
+(defn update! [{:keys [db/data db/schemas] :as this} {:keys [property/id] :as property}]
+  (assert (contains? property :property/id))
+  (assert (contains? data id))
+  (schemas/validate schemas (property/type property) property)
+  (let [new-db (update this :db/data assoc id property)]
+    (save! new-db)
+    new-db))
+
+(defn delete! [{:keys [db/data] :as this} property-id]
+  (assert (contains? data property-id))
+  (let [new-db (update this :db/data dissoc property-id)]
+    (save! new-db)
+    new-db))
+
+(defn get-raw [{:keys [db/data]} property-id]
+  (assert (contains? data property-id))
+  (get data property-id))
+
+(defn all-raw [{:keys [db/data]} property-type]
+  (->> (vals data)
+       (filter #(= property-type (property/type %)))))
+
+(defn build [{:keys [db/schemas] :as this} property-id]
+  (schemas/build-values schemas
+                        (get-raw this property-id)
+                        this))
+
+(defn build-all [{:keys [db/schemas] :as this} property-type]
+  (map #(schemas/build-values schemas % this)
+       (all-raw this property-type)))
--- src/moon/schemas.clj
+++ src/moon/schemas.clj
@@ -1,7 +1,29 @@
-(ns moon.schemas)
+(ns moon.schemas
+  (:require [moon.schema :as schema]
+            [moon.malli :as m]))
 
-(defprotocol Schemas
-  (build-values [schemas property db])
-  (default-value [schemas k])
-  (validate [schemas k value])
-  (create-map-schema [schemas ks]))
+(defn build-values [schemas property db]
+  (reduce (fn [m k]
+            (assoc m k
+                   (try (schema/create-value (get schemas k) (k m) db)
+                        (catch Throwable t
+                          (throw (ex-info " " {:k k
+                                               :v (k m)} t))))))
+          property
+          (keys property)))
+
+(defn default-value [schemas k]
+  (let [schema (get schemas k)]
+    (cond
+     (#{:s/map} (schema 0)) {}
+     :else nil)))
+
+(defn validate [schemas k value]
+  (-> (get schemas k)
+      (schema/malli-form schemas)
+      m/schema
+      (m/validate-humanize value)))
+
+(defn create-map-schema [schemas ks]
+  (m/create-map-schema ks (fn [k]
+                            (schema/malli-form (get schemas k) schemas))))
