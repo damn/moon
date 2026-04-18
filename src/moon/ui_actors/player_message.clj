f45a40d102bb42dc856dfca2d0fab2be565ceaec
Michael Sappler <michaelsappler@gmail.com>
2026-04-19 02:16:19 +0530

    player-message actor no private/def

--- src/moon/ui_actors/player_message.clj
+++ src/moon/ui_actors/player_message.clj
@@ -1,33 +1,30 @@
 (ns moon.ui-actors.player-message
   (:require [clojure.graphics.viewport :as viewport]
             [clojure.scene2d.actor :as actor]
-            [moon.draws :as draws]
-            [clojure.scene2d.stage :as stage]))
-
-(defn- draw-message [state vp-width vp-height]
-  (when-let [text (:text @state)]
-    [:draw/text {:x (/ vp-width 2)
-                 :y (+ (/ vp-height 2) 200)
-                 :text text
-                 :scale 2.5
-                 :up? true}]))
-
-(def message-duration-seconds 0.5)
+            [clojure.scene2d.stage :as stage]
+            [moon.draws :as draws]))
 
 (defn create [_ctx]
-  (actor/create
-   {:type :ui/actor
-    :actor/name "player-message"
-    :actor/user-object (atom nil)
-    :draw! (fn [this _batch _parent-alpha]
-             (when-let [stage (actor/stage this)]
-               (draws/handle (stage/ctx stage)
-                              [(draw-message (actor/user-object this)
-                                             (viewport/world-width  (stage/viewport stage))
-                                             (viewport/world-height (stage/viewport stage)))])))
-    :act! (fn [this delta]
-            (let [state (actor/user-object this)]
-              (when (:text @state)
-                (swap! state update :counter + delta)
-                (when (>= (:counter @state) message-duration-seconds)
-                  (reset! state nil)))))}))
+  (let [message-duration-seconds 0.5]
+    (actor/create
+     {:type :ui/actor
+      :actor/name "player-message"
+      :actor/user-object (atom nil)
+      :draw! (fn [this _batch _parent-alpha]
+               (when-let [stage (actor/stage this)]
+                 (draws/handle (stage/ctx stage)
+                               [(let [state (actor/user-object this)
+                                      vp-width (viewport/world-width (stage/viewport stage))
+                                      vp-height (viewport/world-height (stage/viewport stage))]
+                                  (when-let [text (:text @state)]
+                                    [:draw/text {:x (/ vp-width 2)
+                                                 :y (+ (/ vp-height 2) 200)
+                                                 :text text
+                                                 :scale 2.5
+                                                 :up? true}]))])))
+      :act! (fn [this delta]
+              (let [state (actor/user-object this)]
+                (when (:text @state)
+                  (swap! state update :counter + delta)
+                  (when (>= (:counter @state) message-duration-seconds)
+                    (reset! state nil)))))})))
