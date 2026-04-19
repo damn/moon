4cf9a6ec1b4794256da7870aad23033cfe14fb92
Michael Sappler <michaelsappler@gmail.com>
2026-04-19 11:03:53 +0530

    window not depend on text-button

--- src/clojure/gdx/scene2d/ui/window.clj
+++ src/clojure/gdx/scene2d/ui/window.clj
@@ -1,24 +1,22 @@
 (ns clojure.gdx.scene2d.ui.window
-  (:require [clojure.gdx.scene2d.ui.text-button :as text-button]
-            [clojure.scene2d.actor :as actor]
+  (:require [clojure.scene2d.actor :as actor]
             [clojure.scene2d.ui.table :as table])
   (:import (com.badlogic.gdx.scenes.scene2d.ui Skin
                                                Window)))
 
-(defn- add-close-button! [window skin]
-  (table/add!
-   (Window/.getTitleTable window)
-   {:actor (doto (text-button/create {:text "X"
-                                      :skin skin})
-             (actor/add-listener!
-              [:listener/change (fn [_event _actor]
-                                  (actor/remove! window))]))}))
-
 (defn set-opts! [window opts]
   (when (:window/modal? opts)
     (Window/.setModal window true))
+
   (when-let [skin (:window/close-button? opts)]
-    (add-close-button! window skin))
+    (table/add! (Window/.getTitleTable window)
+                {:actor (actor/create
+                         {:type :ui/text-button
+                          :text "X"
+                          :skin skin
+                          :actor/listeners {:listener/change (fn [_event _actor]
+                                                               (actor/remove! window))}})}))
+
   (table/set-opts! window opts))
 
 (defn create
