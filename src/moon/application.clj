f3d95042ea43372869d98eb564342d55c63ec3bc
Michael Sappler <michaelsappler@gmail.com>
2026-04-19 10:02:40 +0530

    Move out set colors

--- src/moon/application.clj
+++ src/moon/application.clj
@@ -1,14 +1,12 @@
 (ns moon.application
   (:require [clojure.gdx.backends.lwjgl :as lwjgl]
             [clojure.gdx.backends.lwjgl.config :as config]
-            [clojure.gdx.colors :as colors]
             [clojure.gdx.scene2d.ui.tooltip-manager :as tooltip-manager])
   (:import (com.badlogic.gdx ApplicationListener)))
 
 (def state (atom nil))
 
-(defn start! [{:keys [colors listener config]}]
-  (colors/put! colors)
+(defn start! [{:keys [listener config]}]
   (config/use-glfw-async!)
   (lwjgl/application! (let [{:keys [create!
                                     create-params
