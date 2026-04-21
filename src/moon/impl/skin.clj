fea441144393e2261af7e97b6b491825f2b8ff58
Michael Sappler <michaelsappler@gmail.com>
2026-04-21 10:53:59 +0530

    Use path params at impl skin

--- src/moon/impl/skin.clj
+++ src/moon/impl/skin.clj
@@ -4,6 +4,6 @@
             [clojure.graphics.bitmap-font :as bitmap-font]))
 
 (defn create [{:keys [ctx/files]} path]
-  (let [skin (skin/create (files/internal files "uiskin.json"))]
+  (let [skin (skin/create (files/internal files path))]
     (bitmap-font/enable-markup! (skin/font skin "default-font") true)
     skin))
