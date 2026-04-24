558937f5f7d1e0b16b6af27519bb3426cde0901f
Michael Sappler <michaelsappler@gmail.com>
2026-04-24 22:34:48 +0530

    schema default as defmethods

--- src/moon/schema.clj
+++ src/moon/schema.clj
@@ -1,7 +1,21 @@
-(ns moon.schema)
+(ns moon.schema
+  (:require [clojure.scene2d.actor :as actor]
+            [moon.edn :as edn]
+            [moon.string :as string]))
 
 (defmulti create (fn [[schema-k :as _schema] v ctx]
                    schema-k))
 
 (defmulti value (fn [[schema-k :as _schema] widget schemas]
                   schema-k))
+
+(defmethod create :default
+  [_ v {:keys [ctx/skin]}]
+  (actor/create
+   {:type :ui/label
+    :text (string/truncate (edn/->str v) 60)
+    :skin skin}))
+
+(defmethod value :default
+  [_  widget _schemas]
+  ((actor/user-object widget) 1))
