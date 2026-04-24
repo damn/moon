ff54a39d3110c8e4bf55ff2dcbedb44fed07e9bc
Michael Sappler <michaelsappler@gmail.com>
2026-04-24 15:51:56 +0530

    SpriteBatch extends Batch

--- src/clojure/gdx/graphics/g2d/sprite_batch.clj
+++ src/clojure/gdx/graphics/g2d/sprite_batch.clj
@@ -1,5 +1,42 @@
 (ns clojure.gdx.graphics.g2d.sprite-batch
-  (:import (com.badlogic.gdx.graphics.g2d SpriteBatch)))
+  (:require clojure.graphics.batch)
+  (:import (com.badlogic.gdx.graphics.g2d SpriteBatch
+                                          TextureRegion)))
 
 (defn create []
   (SpriteBatch.))
+
+(extend-type SpriteBatch
+  clojure.graphics.batch/Batch
+  (draw!
+    ([batch texture-region x y origin-x origin-y width height scale-x scale-y rotation]
+     (.draw batch
+            texture-region
+            x
+            y
+            origin-x
+            origin-y
+            width
+            height
+            scale-x
+            scale-y
+            rotation))
+    ([batch ^TextureRegion texture-region x y w h]
+     (.draw batch
+            texture-region
+            (float x)
+            (float y)
+            (float w)
+            (float h))))
+
+  (set-color! [batch r g b a]
+    (.setColor batch r g b a))
+
+  (set-projection-matrix! [batch matrix]
+    (.setProjectionMatrix batch matrix))
+
+  (begin! [batch]
+    (.begin batch))
+
+  (end! [batch]
+    (.end batch)))
