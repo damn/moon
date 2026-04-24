cd10a3ef92b01977a40e586d6e09fb3c64959474
Michael Sappler <michaelsappler@gmail.com>
2026-04-24 14:43:14 +0530

    malli form and create value to moon.schemas

--- src/moon/schema.clj
+++ src/moon/schema.clj
@@ -5,12 +5,3 @@
 
 (defmulti value (fn [[schema-k :as _schema] widget schemas]
                   schema-k))
-
-(defmulti malli-form (fn [[k] _schemas]
-                       k))
-
-(defmulti create-value (fn [[k] _v _db]
-                         k))
-
-(defmethod create-value :default [_ v _db]
-  v)
--- src/moon/schemas.clj
+++ src/moon/schemas.clj
@@ -1,11 +1,16 @@
 (ns moon.schemas
-  (:require [moon.schema :as schema]
-            [moon.malli :as m]))
+  (:require [moon.malli :as m]))
+
+(defmulti create-value (fn [[k] _v _db]
+                         k))
+
+(defmethod create-value :default [_ v _db]
+  v)
 
 (defn build-values [schemas property db]
   (reduce (fn [m k]
             (assoc m k
-                   (try (schema/create-value (get schemas k) (k m) db)
+                   (try (create-value (get schemas k) (k m) db)
                         (catch Throwable t
                           (throw (ex-info " " {:k k
                                                :v (k m)} t))))))
@@ -18,21 +23,24 @@
      (#{:s/map} (schema 0)) {}
      :else nil)))
 
+(defmulti malli-form (fn [[k] _schemas]
+                       k))
+
 (defn validate [schemas k value]
   (-> (get schemas k)
-      (schema/malli-form schemas)
+      (malli-form schemas)
       m/schema
       (m/validate-humanize value)))
 
 (defn create-map-schema [schemas ks]
   (m/create-map-schema ks (fn [k]
-                            (schema/malli-form (get schemas k) schemas))))
+                            (malli-form (get schemas k) schemas))))
 
 (defn map-keys [schemas schema]
-  (m/map-keys (schema/malli-form schema schemas)))
+  (m/map-keys (malli-form schema schemas)))
 
 (defn optional? [schemas schema k]
-  (m/optional? k (schema/malli-form schema schemas)))
+  (m/optional? k (malli-form schema schemas)))
 
 (defn optional-keyset [schemas schema]
-  (m/optional-keyset (schema/malli-form schema schemas)))
+  (m/optional-keyset (malli-form schema schemas)))
