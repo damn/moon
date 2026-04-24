eecc46fb3daa3e242d48f5f471fd9e9d3312c3bc
Michael Sappler <michaelsappler@gmail.com>
2026-04-24 22:59:34 +0530

    Integrate ApplicationListener

--- src/moon/application.clj
+++ src/moon/application.clj
@@ -1,6 +1,7 @@
 (ns moon.application
   (:require [clojure.gdx.colors :as colors])
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+  (:import (com.badlogic.gdx ApplicationListener)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
                                              Lwjgl3ApplicationConfiguration)))
 
 (def state (atom nil))
@@ -11,12 +12,31 @@
            title
            window
            fps
-           listener
+           create!
+           create-params
+           dispose!
+           render!
+           render-params
+           resize!
            ]}]
   (colors/put! colors)
   (Lwjgl3ApplicationConfiguration/useGlfwAsync)
-  (Lwjgl3Application. (let [[f params] listener]
-                        (f params))
+  (Lwjgl3Application. (reify ApplicationListener
+                        (create [_]
+                          (reset! state (create! create-params)))
+
+                        (dispose [_]
+                          (dispose! @state))
+
+                        (render [_]
+                          (swap! state render! render-params))
+
+                        (resize [_ width height]
+                          (resize! @state width height))
+
+                        (pause [_])
+
+                        (resume [_]))
                       (doto (Lwjgl3ApplicationConfiguration.)
                         (.setTitle title)
                         (.setWindowedMode (:width window) (:height window))
