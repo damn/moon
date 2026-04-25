caf24ddc2805870309c14c665e8373339603f83b
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 10:44:47 +0530

    moon.game/draw-tiled-map!

--- src/moon/game.clj
+++ src/moon/game.clj
@@ -9,6 +9,7 @@
             [clojure.gdx.scene2d.ui.skin :as skin]
             [clojure.gdx.scene2d.ui.tooltip-manager :as tooltip-manager]
             [clojure.gdx.shape-drawer]
+            [clojure.gdx.maps.tiled.renderer :as tiled-map-renderer]
             [clojure.gdx.orthographic-camera :as orthographic-camera]
             [clojure.gdx.viewport]
             [clojure.graphics :as graphics]
@@ -1085,3 +1086,77 @@
                         :on-click (fn [])})))
 
  )
+
+
+(defn- tile-color-setter*
+  [{:keys [ray-blocked?
+           explored-tile-corners
+           light-position
+           see-all-tiles?
+           explored-tile-color
+           visible-tile-color
+           invisible-tile-color]}]
+  #_(reset! do-once false)
+  (let [light-cache (atom {})]
+    (fn tile-color-setter [_color x y]
+      (let [position [(int x) (int y)]
+            explored? (get @explored-tile-corners position) ; TODO needs int call ?
+            base-color (if explored?
+                         explored-tile-color
+                         invisible-tile-color)
+            cache-entry (get @light-cache position :not-found)
+            blocked? (if (= cache-entry :not-found)
+                       (let [blocked? (ray-blocked? light-position position)]
+                         (swap! light-cache assoc position blocked?)
+                         blocked?)
+                       cache-entry)]
+        #_(when @do-once
+            (swap! ray-positions conj position))
+        (if blocked?
+          (if see-all-tiles?
+            visible-tile-color
+            base-color)
+          (do (when-not explored?
+                (swap! explored-tile-corners assoc (mapv int position) true))
+              visible-tile-color))))))
+
+(comment
+ (def ^:private count-rays? false)
+
+ (def ray-positions (atom []))
+ (def do-once (atom true))
+
+ (count @ray-positions)
+ 2256
+ (count (distinct @ray-positions))
+ 608
+ (* 608 4)
+ 2432
+ )
+
+(defn- tile-color-setter
+  [{:keys [ctx/colors
+           ctx/explored-tile-corners
+           ctx/raycaster
+           ctx/world-viewport]}]
+  (tile-color-setter*
+   {:ray-blocked? (partial raycaster/blocked? raycaster)
+    :explored-tile-corners explored-tile-corners
+    :light-position (camera/position (viewport/camera world-viewport))
+    :see-all-tiles? false
+    :explored-tile-color  (:colors/explored-tile colors)
+    :visible-tile-color   (:colors/visible-tile colors)
+    :invisible-tile-color (:colors/invisible-tile colors)}))
+
+(defn draw-tiled-map!
+  [{:keys [ctx/batch
+           ctx/tiled-map
+           ctx/world-unit-scale
+           ctx/world-viewport]
+    :as ctx}]
+  (tiled-map-renderer/draw! batch
+                            world-unit-scale
+                            (viewport/camera world-viewport)
+                            tiled-map
+                            (tile-color-setter ctx))
+  ctx)
