fc588098053e9fdc45ccd700e2124ac1c06a2bd9
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 23:29:30 +0530

    Add moon.backends.lwjgl-test

--- project.clj
+++ project.clj
@@ -84,6 +84,7 @@
   :aliases {"dev"      ["run" "-m" "moon.dev-loop" "((requiring-resolve 'moon.start/-main))"]
             "levelgen" ["run" "-m" "moon.dev-loop" "((requiring-resolve 'moon.levelgen/-main))"]
             "nsgraph"  ["run" "-m" "moon.dev-loop" "((requiring-resolve 'ns-graph.core/-main))"]
+            "app-test" ["run" "-m" "moon.dev-loop" "((requiring-resolve 'moon.backends.lwjgl-test/-main))"]
             "ns"       ["hiera" ":layout" ":horizontal"]}
   :plugins [[lein-hiera "2.0.0"]
             [lein-codox "0.10.8"]]
---
+++ test/moon/backends/lwjgl_test.clj
@@ -0,0 +1,16 @@
+(ns moon.backends.lwjgl-test
+  (:require [moon.backends.lwjgl :as lwjgl])
+  (:import (com.badlogic.gdx ApplicationListener)))
+
+(defn -main []
+  (lwjgl/application! (reify ApplicationListener
+                        (create [_])
+                        (dispose [_])
+                        (render [_])
+                        (resize [_ width height])
+                        (pause [_])
+                        (resume [_]))
+                      {:title "foobar"
+                       :width 800
+                       :height 600
+                       :fps 60}))
