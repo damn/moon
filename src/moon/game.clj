09dbf8e268dea45639b95456e2eaafc0e884964f
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 09:58:48 +0530

    Update mouseover eid

--- src/moon/game.clj
+++ src/moon/game.clj
@@ -40,6 +40,7 @@
             [moon.inventory :as inventory]
             [moon.inventory-window :as inventory-window]
             [moon.malli :as m]
+            [moon.raycaster :as raycaster]
             [moon.start :refer [edn-resource]]
             [moon.state :as state]
             [moon.stats :as stats]
@@ -962,6 +963,34 @@
                  (-> ctx
                      (assoc :ctx/world-mouse-position (viewport/unproject world-viewport mp))
                      (assoc :ctx/ui-mouse-position (viewport/unproject ui-viewport mp)))))]
+
+            [(fn
+               [{:keys [ctx/input
+                        ctx/mouseover-eid
+                        ctx/stage
+                        ctx/player-eid
+                        ctx/grid
+                        ctx/raycaster
+                        ctx/render-z-order
+                        ctx/world-mouse-position]
+                 :as ctx}]
+               (let [mouseover-actor (stage/mouseover-actor stage (input/mouse-position input))
+                     position world-mouse-position
+                     new-eid (if mouseover-actor
+                               nil
+                               (let [player @player-eid
+                                     hits (remove #(= (:body/z-order (:entity/body @%)) :z-order/effect)
+                                                  (grid/point->entities grid position))]
+                                 (->> render-z-order
+                                      (order/sort-by-order hits #(:body/z-order (:entity/body @%)))
+                                      reverse
+                                      (filter #(raycaster/line-of-sight? raycaster player @%))
+                                      first)))]
+                 (when mouseover-eid
+                   (swap! mouseover-eid dissoc :entity/mouseover?))
+                 (when new-eid
+                   (swap! new-eid assoc :entity/mouseover? true))
+                 (assoc ctx :ctx/mouseover-eid new-eid)))]
 
             ]
            render-fns)))
