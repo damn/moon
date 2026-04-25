a2d42ef0a3abf49ff7a197e31b167d08a96840a7
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 09:56:29 +0530

    Update mouse

--- src/moon/game.clj
+++ src/moon/game.clj
@@ -952,6 +952,17 @@
             [(fn [ctx]
                (m/validate-humanize schema ctx)
                ctx)]
+
+            [(fn
+               [{:keys [ctx/input
+                        ctx/ui-viewport
+                        ctx/world-viewport]
+                 :as ctx}]
+               (let [mp (input/mouse-position input)]
+                 (-> ctx
+                     (assoc :ctx/world-mouse-position (viewport/unproject world-viewport mp))
+                     (assoc :ctx/ui-mouse-position (viewport/unproject ui-viewport mp)))))]
+
             ]
            render-fns)))
 
