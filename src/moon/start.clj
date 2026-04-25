f4c7b1aa061feef2238bea5e88359bfba11fa9bf
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 22:56:19 +0530

    application into start...

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,12 +1,33 @@
 (ns moon.start
-  (:require [moon.application :as application])
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+  (:require [moon.application.create :as create]
+            [moon.application.dispose :as dispose]
+            [moon.application.render :as render]
+            [moon.application.resize :as resize])
+  (:import (com.badlogic.gdx ApplicationListener)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
                                              Lwjgl3ApplicationConfiguration))
   (:gen-class))
 
+(def state (atom nil))
+
 (defn -main []
   (Lwjgl3ApplicationConfiguration/useGlfwAsync)
-  (Lwjgl3Application. application/listener
+  (Lwjgl3Application. (reify ApplicationListener
+                        (create [_]
+                          (reset! state (create/do!)))
+
+                        (dispose [_]
+                          (dispose/do! @state))
+
+                        (render [_]
+                          (swap! state render/do!))
+
+                        (resize [_ width height]
+                          (resize/do! @state width height))
+
+                        (pause [_])
+
+                        (resume [_]))
                       (doto (Lwjgl3ApplicationConfiguration.)
                         (.setTitle "Moon")
                         (.setWindowedMode 1440 900)
