5796e1aae30978634b2e3b9ec4aee8d87d102293
Michael Sappler <michaelsappler@gmail.com>
2026-04-25 23:32:53 +0530

    Implement useGlfwAsync

--- src/moon/backends/lwjgl.clj
+++ src/moon/backends/lwjgl.clj
@@ -1,9 +1,13 @@
 (ns moon.backends.lwjgl
   (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
-                                             Lwjgl3ApplicationConfiguration)))
+                                             Lwjgl3ApplicationConfiguration)
+           (com.badlogic.gdx.utils SharedLibraryLoader
+                                   Os)
+           (org.lwjgl.system Configuration)))
 
 (defn application! [application-listener {:keys [title width height fps]}]
-  (Lwjgl3ApplicationConfiguration/useGlfwAsync)
+  (when (= SharedLibraryLoader/os Os/MacOsX)
+    (.set Configuration/GLFW_LIBRARY_NAME "glfw_async"))
   (Lwjgl3Application. application-listener
                       (doto (Lwjgl3ApplicationConfiguration.)
                         (.setTitle title)
