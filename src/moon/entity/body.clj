dcbee76c2860fc692cfc011d5b73c952e6b1e091
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 17:52:27 +0530

    Remove body protocol

--- src/moon/entity/body.clj
+++ src/moon/entity/body.clj
@@ -1,48 +1,12 @@
 (ns moon.entity.body
-  (:require [clojure.gdx.math.rectangle :as gdx-rectangle]
-            [moon.body :as body]
-            [moon.rectangle :as rectangle]
-            [clojure.math.vector2 :as v]
-            [qrecord.core :as q]))
+  (:require [qrecord.core :as q]))
 
-; TODO is a body really necessary? just entity/foo ?
-; pull stuff out which is not used together w. body functions ?
 (q/defrecord Body [body/position
                    body/width
                    body/height
                    body/collides?
                    body/z-order
-                   body/rotation-angle]
-  body/Body
-  (rectangle
-    [{:keys [body/position
-             body/width
-             body/height]}]
-    (let [[x y] [(- (position 0) (/ width  2))
-                 (- (position 1) (/ height 2))]]
-      (gdx-rectangle/create x y width height)))
-
-  (touched-tiles
-    [{:keys [body/position
-             body/width
-             body/height]}]
-    (rectangle/touched-tiles
-     {:x (- (position 0) (/ width  2))
-      :y (- (position 1) (/ height 2))
-      :width  width
-      :height height}))
-
-  (overlaps? [body other-body]
-    (gdx-rectangle/overlaps? (body/rectangle body)
-                             (body/rectangle other-body)))
-
-  (distance [body other-body]
-    (v/distance (:body/position body)
-                (:body/position other-body)))
-
-  (direction [body other-body]
-    (v/direction (:body/position body)
-                 (:body/position other-body))))
+                   body/rotation-angle])
 
 (defn create
   [[_k
