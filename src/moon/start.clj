02e467b536f419fd7c05d896f66cc9fc3e2b31bf
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 18:44:43 +0530

    Integrate player-idle

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -250,6 +250,120 @@
                (world-item? mouseover-actor))
       [[:tx/event eid :drop-item]])))
 
+(defn- interaction-state->txs [[k params] stage player-eid]
+  (case k
+    :interaction-state/mouseover-actor nil
+
+    :interaction-state/clickable-mouseover-eid
+    (let [{:keys [clicked-eid
+                  in-click-range?]} params]
+      (if in-click-range?
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/player
+          [[:tx/toggle-inventory-visible]]
+
+          :clickable/item
+          (let [item (:entity/item @clicked-eid)]
+            (cond
+             (-> stage
+                 (stage/find-actor "moon.ui.windows.inventory")
+                 actor/visible?)
+             [[:tx/sound "bfxr_takeit"]
+              [:tx/mark-destroyed clicked-eid]
+              [:tx/event player-eid :pickup-item item]]
+
+             (inventory/can-pickup-item? (:entity/inventory @player-eid) item)
+             [[:tx/sound "bfxr_pickup"]
+              [:tx/mark-destroyed clicked-eid]
+              [:tx/pickup-item player-eid item]]
+
+             :else
+             [[:tx/sound "bfxr_denied"]
+              [:tx/show-message "Your Inventory is full"]])))
+        [[:tx/sound "bfxr_denied"]
+         [:tx/show-message "Too far away"]]))
+
+    :interaction-state.skill/usable
+    (let [[skill effect-ctx] params]
+      [[:tx/event player-eid :start-action [skill effect-ctx]]])
+
+    :interaction-state.skill/not-usable
+    (let [state params]
+      [[:tx/sound "bfxr_denied"]
+       [:tx/show-message (case state
+                           :cooldown "Skill is still on cooldown"
+                           :not-enough-mana "Not enough mana"
+                           :invalid-params "Cannot use this here")]])
+
+    :interaction-state/no-skill-selected
+    [[:tx/sound "bfxr_denied"]
+     [:tx/show-message "No selected skill"]]))
+
+(defmethod state/cursor :player-idle
+  [_ eid {:keys [ctx/interaction-state]}]
+  (let [[k params] interaction-state]
+    (case k
+      :interaction-state/mouseover-actor
+      (let [[actor-type params] params
+            inventory-cell-with-item? (and (= actor-type :mouseover-actor/inventory-cell)
+                                           (let [inventory-slot params]
+                                             (get-in (:entity/inventory @eid) inventory-slot)))]
+        (cond
+         inventory-cell-with-item?
+         :cursors/hand-before-grab
+
+         (= actor-type :mouseover-actor/window-title-bar)
+         :cursors/move-window
+
+         (= actor-type :mouseover-actor/button)
+         :cursors/over-button
+
+         (= actor-type :mouseover-actor/unspecified)
+         :cursors/default
+
+         :else
+         :cursors/default))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/item (if in-click-range?
+                            :cursors/hand-before-grab
+                            :cursors/hand-before-grab-gray)
+          :clickable/player :cursors/bag))
+
+      :interaction-state.skill/usable
+      :cursors/use-skill
+
+      :interaction-state.skill/not-usable
+      :cursors/skill-not-usable
+
+      :interaction-state/no-skill-selected
+      :cursors/no-skill-selected)))
+
+(defmethod state/pause-game? :player-idle
+  [_]
+  true)
+
+(defmethod state/clicked-inventory-cell :player-idle
+  [_ eid cell]
+  (when-let [item (get-in (:entity/inventory @eid) cell)]
+    [[:tx/sound "bfxr_takeit"]
+     [:tx/event eid :pickup-item item]
+     [:tx/remove-item eid cell]]))
+
+(defmethod state/handle-input :player-idle
+  [_ player-eid {:keys [ctx/input
+                        ctx/interaction-state
+                        ctx/stage] :as ctx}]
+  (if-let [movement-vector (moon.input/player-movement-vector input)]
+    [[:tx/event player-eid :movement-input movement-vector]]
+    (when (input/button-just-pressed? input :input.buttons/left)
+      (interaction-state->txs interaction-state
+                              stage
+                              player-eid))))
+
 (q/defrecord Entity [entity/body])
 
 (defn- send-event! [ctx eid event params]
