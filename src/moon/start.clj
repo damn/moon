d8a62c5c4d282245f41d6db76bbe126a451b1121
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 22:38:17 +0530

    Integrate entity/image

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -241,6 +241,15 @@
                                      :state initial-state)]
    [:tx/assoc eid initial-state (state/create [initial-state nil] eid ctx)]])
 
+(defmethod entity/render :entity/image
+  [[_k image] {:keys [entity/body]} {:keys [ctx/textures]}]
+  [[:draw/texture-region
+    (textures/texture-region textures image)
+    (:body/position body)
+    {:center? true
+     :rotation (or (:body/rotation-angle body)
+                   0)}]])
+
 (defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
   (/ action-time
      (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
