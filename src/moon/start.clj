c6729f4191fb3ef3f48440299a708748b2ec26df
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 18:53:20 +0530

    Integrate active-skill

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -16,6 +16,7 @@
             [clojure.graphics.viewport :as viewport]
             [clojure.input :as input]
             [clojure.java.io :as io]
+            [clojure.math :as math]
             [clojure.math.vector2 :as v]
             [clojure.multifn :as multifn]
             [clojure.gdx.scene2d.actor :as actor]
@@ -68,6 +69,89 @@
            (com.badlogic.gdx.graphics.g2d SpriteBatch)
            (com.badlogic.gdx.utils Disposable))
   (:gen-class))
+
+(defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
+  (/ action-time
+     (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
+         1)))
+
+(defmethod state/create :active-skill
+  [[_k [skill effect-ctx]] eid {:keys [ctx/elapsed-time]}]
+  {:skill skill
+   :effect-ctx effect-ctx
+   :counter (->> skill
+                 :skill/action-time
+                 (apply-action-speed-modifier @eid skill)
+                 (timer/create elapsed-time))})
+
+(defn- update-effect-ctx
+  [raycaster {:keys [effect/source effect/target] :as effect-ctx}]
+  (if (and target
+           (not (:entity/destroyed? @target))
+           (raycaster/line-of-sight? raycaster @source @target))
+    effect-ctx
+    (dissoc effect-ctx :effect/target)))
+
+(defmethod entity/tick :active-skill
+  [[_k {:keys [skill effect-ctx counter]}]
+   eid
+   {:keys [ctx/elapsed-time
+           ctx/raycaster]}]
+  (let [effect-ctx (update-effect-ctx raycaster effect-ctx)]
+    (cond
+     (not (seq (filter #(effect/applicable? % effect-ctx)
+                       (:skill/effects skill))))
+     [[:tx/event eid :action-done]]
+
+     (timer/stopped? elapsed-time counter)
+     [[:tx/effect effect-ctx (:skill/effects skill)]
+      [:tx/event eid :action-done]])))
+
+(def ^:private skill-image-radius-world-units
+  (let [tile-size 48
+        image-width 32]
+    (/ (/ image-width tile-size) 2)))
+
+(defmethod entity/render :active-skill
+  [[_k {:keys [skill effect-ctx counter]}]
+   entity
+   {:keys [ctx/colors
+           ctx/elapsed-time
+           ctx/textures]
+    :as ctx}]
+  (let [{:keys [entity/image skill/effects]} skill]
+    (concat (let [action-counter-ratio (timer/ratio elapsed-time counter)
+                  texture-region (textures/texture-region textures image)
+                  radius skill-image-radius-world-units
+                  [x y] (:body/position (:entity/body entity))
+                  y (+ (float y)
+                       (float (/ (:body/height (:entity/body entity)) 2))
+                       (float 0.15))
+                  center [x (+ y radius)]]
+              [[:draw/filled-circle center radius (:colors/active-skill-circle colors)]
+               [:draw/sector
+                center
+                radius
+                (math/to-radians 90) ; start-angle
+                (math/to-radians (* (float action-counter-ratio) 360)) ; degree
+                (:colors/active-skill-sector colors)]
+               [:draw/texture-region texture-region [(- (float x) radius) y]]])
+            (mapcat #(effect/render % effect-ctx ctx)  ; update-effect-ctx here too ?
+                    effects))))
+
+(defmethod state/enter :active-skill
+  [[_k {:keys [skill]}] eid]
+  [[:tx/sound (:skill/start-action-sound skill)]
+   [:tx/set-cooldown eid skill]
+   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
+
+(defmethod state/cursor :active-skill
+  [_ _eid _ctx]
+  :cursors/sandclock)
+
+(defmethod state/pause-game? :active-skill
+  [_]
+  false)
 
 (defn- creature-speed [{:keys [entity/stats]}]
   (or (stats/get-stat-value stats :stats/movement-speed)
