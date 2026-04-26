fe0d96264bf37bc09444eb21ca6988d62dc156c6
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 22:46:40 +0530

    :entity/projectile-collision

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -340,6 +340,39 @@
            [:tx/assoc-in eid [:entity/body :body/rotation-angle] (v/angle-from-vector direction)])
          [:tx/move-entity eid]]))))
 
+(defmethod entity/create :entity/projectile-collision
+  [[_ v] _ctx]
+  (assoc v :already-hit-bodies #{}))
+
+(defmethod entity/tick :entity/projectile-collision
+  [[_k {:keys [entity-effects already-hit-bodies piercing?]}]
+   eid
+   {:keys [ctx/grid]}]
+  (let [entity @eid
+        cells* (map deref (g2d/get-cells grid (body/touched-tiles (:entity/body entity))))
+        hit-entity (first (filter #(and (not (contains? already-hit-bodies %))
+                                        (not= (:entity/faction entity)
+                                              (:entity/faction @%))
+                                        (:body/collides? (:entity/body @%))
+                                        (body/overlaps? (:entity/body entity)
+                                                        (:entity/body @%)))
+                                  (grid/cells->entities cells*)))
+        destroy? (or (and hit-entity (not piercing?))
+                     (some #(cell/blocked? % (:body/z-order (:entity/body entity))) cells*))]
+    [(when destroy?
+       [:tx/mark-destroyed eid])
+     (when hit-entity
+       [:tx/assoc-in
+        eid
+        [:entity/projectile-collision
+         :already-hit-bodies]
+        (conj already-hit-bodies hit-entity)])
+     (when hit-entity
+       [:tx/effect
+        {:effect/source eid
+         :effect/target hit-entity}
+        entity-effects])]))
+
 (defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
   (/ action-time
      (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
