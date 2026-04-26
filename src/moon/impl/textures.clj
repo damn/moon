6f2d1e1b8ebe04efd2f579d6acf8ad37308576c4
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 10:35:03 +0530

    Remove clojure.files.file-handle

--- src/moon/impl/textures.clj
+++ src/moon/impl/textures.clj
@@ -1,11 +1,27 @@
 (ns moon.impl.textures
   (:require [clojure.files :as files]
-            [clojure.files.file-handle :as file-handle]
             [clojure.gdx.graphics.texture :as texture]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import (com.badlogic.gdx.files FileHandle)))
+
+(defn- recursively-search
+  [^FileHandle folder extensions]
+  (loop [[^FileHandle file & remaining] (.list folder)
+         result []]
+    (cond (nil? file)
+          result
+
+          (.isDirectory file)
+          (recur (concat remaining (.list file)) result)
+
+          (extensions (.extension file))
+          (recur remaining (conj result (.path file)))
+
+          :else
+          (recur remaining result))))
 
 (defn create [{:keys [ctx/files]} {:keys [folder extensions]}]
   (into {} (for [path (map (fn [path]
                              (str/replace-first path folder ""))
-                           (file-handle/recursively-search (files/internal files folder) extensions))]
+                           (recursively-search (files/internal files folder) extensions))]
              [path (texture/create path)])))
