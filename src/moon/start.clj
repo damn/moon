f17d03fee3412eb5ffc159c8ecb17332cdeee693
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 22:25:40 +0530

    Integrate fsm

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -71,6 +71,63 @@
            (com.badlogic.gdx.utils Disposable))
   (:gen-class))
 
+(comment
+
+ ; 1. quote the data structur ebecause of arrows
+ ; 2.
+ (eval `(fsm/fsm-inc ~data))
+ )
+
+(def ^:private npc-fsm
+  (fsm/fsm-inc
+   [[:npc-sleeping
+     :kill -> :npc-dead
+     :stun -> :stunned
+     :alert -> :npc-idle]
+    [:npc-idle
+     :kill -> :npc-dead
+     :stun -> :stunned
+     :start-action -> :active-skill
+     :movement-direction -> :npc-moving]
+    [:npc-moving
+     :kill -> :npc-dead
+     :stun -> :stunned
+     :timer-finished -> :npc-idle]
+    [:active-skill
+     :kill -> :npc-dead
+     :stun -> :stunned
+     :action-done -> :npc-idle]
+    [:stunned
+     :kill -> :npc-dead
+     :effect-wears-off -> :npc-idle]
+    [:npc-dead]]))
+
+(def ^:private player-fsm
+  (fsm/fsm-inc
+   [[:player-idle
+     :kill -> :player-dead
+     :stun -> :stunned
+     :start-action -> :active-skill
+     :pickup-item -> :player-item-on-cursor
+     :movement-input -> :player-moving]
+    [:player-moving
+     :kill -> :player-dead
+     :stun -> :stunned
+     :no-movement-input -> :player-idle]
+    [:active-skill
+     :kill -> :player-dead
+     :stun -> :stunned
+     :action-done -> :player-idle]
+    [:stunned
+     :kill -> :player-dead
+     :effect-wears-off -> :player-idle]
+    [:player-item-on-cursor
+     :kill -> :player-dead
+     :stun -> :stunned
+     :drop-item -> :player-idle
+     :dropped-item -> :player-idle]
+    [:player-dead]]))
+
 (defmethod entity/create :entity/animation
   [[_k {:keys [animation/frames
                animation/frame-duration
@@ -173,6 +230,16 @@
   [[:tx/audiovisual
     (:body/position (:entity/body @eid))
     audiovisuals-id]])
+
+(defmethod entity/after-create :entity/fsm ; TODO do @ creature?
+  [[_k {:keys [fsm initial-state]}] eid ctx]
+  ; fsm throws when initial-state is not part of states, so no need to assert initial-state
+  ; initial state is nil, so associng it. make bug report at reduce-fsm?
+  [[:tx/assoc eid :entity/fsm (assoc ((case fsm
+                                        :fsms/player player-fsm
+                                        :fsms/npc npc-fsm) initial-state nil)
+                                     :state initial-state)]
+   [:tx/assoc eid initial-state (state/create [initial-state nil] eid ctx)]])
 
 (defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
   (/ action-time
