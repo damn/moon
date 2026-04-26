3af02562141cb0d6363f42b39d82fb037488af8b
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 19:03:04 +0530

    Integrate alert friendlies

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -70,6 +70,19 @@
            (com.badlogic.gdx.utils Disposable))
   (:gen-class))
 
+(defmethod entity/tick :entity/alert-friendlies-after-duration
+  [[_k {:keys [counter faction]}]
+   eid
+   {:keys [ctx/elapsed-time
+           ctx/grid]}]
+  (when (timer/stopped? elapsed-time counter)
+    (cons [:tx/mark-destroyed eid]
+          (for [friendly-eid (->> {:position (:body/position (:entity/body @eid))
+                                   :radius 4}
+                                  (grid/circle->entities grid)
+                                  (filter #(= (:entity/faction @%) faction)))]
+            [:tx/event friendly-eid :alert]))))
+
 (defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
   (/ action-time
      (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
