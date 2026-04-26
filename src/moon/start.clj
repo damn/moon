01afe4bb4e4624830bff0b479dab529af6916177
Michael Sappler <michaelsappler@gmail.com>
2026-04-26 19:07:09 +0530

    Integrate entity body

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -70,6 +70,41 @@
            (com.badlogic.gdx.utils Disposable))
   (:gen-class))
 
+(q/defrecord Body [body/position
+                   body/width
+                   body/height
+                   body/collides?
+                   body/z-order
+                   body/rotation-angle])
+
+(defmethod entity/create :entity/body
+  [[_k
+    {[x y] :position
+     :keys [position
+            width
+            height
+            collides?
+            z-order
+            rotation-angle]}]
+   {:keys [ctx/minimum-size
+           ctx/z-orders]}]
+  (assert position)
+  (assert width)
+  (assert height)
+  (assert (>= width  (if collides? minimum-size 0)))
+  (assert (>= height (if collides? minimum-size 0)))
+  (assert (or (boolean? collides?) (nil? collides?)))
+  (assert ((set z-orders) z-order))
+  (assert (or (nil? rotation-angle)
+              (<= 0 rotation-angle 360)))
+  (map->Body
+   {:position (mapv float position)
+    :width  (float width)
+    :height (float height)
+    :collides? collides?
+    :z-order z-order
+    :rotation-angle (or rotation-angle 0)}))
+
 (defmethod entity/tick :entity/alert-friendlies-after-duration
   [[_k {:keys [counter faction]}]
    eid
