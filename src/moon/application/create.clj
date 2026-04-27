2a7ab0ef2b30820b3a8d49ede6194abecc4812ad
Michael Sappler <michaelsappler@gmail.com>
2026-04-27 12:31:09 +0530

    create step

---
+++ src/moon/application/create.clj
@@ -0,0 +1,91 @@
+(ns moon.application.create
+  (:require moon.create.audio
+            moon.create.spawn-enemies
+            moon.create.raycaster
+            moon.create.spawn-player
+            moon.create.grid
+            moon.create.content-grid
+            moon.create.explored-tile-corners
+            moon.create.add-stage-actors
+            moon.create.tiled-map
+            moon.create.into-record
+            moon.create.render-z-order
+            moon.create.max-speed
+            moon.create.db
+            moon.create.ctx-colors
+            moon.create.controls
+            moon.create.batch
+            moon.create.colors
+            moon.create.shape-drawer
+            moon.create.shape-drawer-texture
+            moon.create.default-font
+            moon.create.ui-viewport
+            moon.create.stage
+            moon.create.tooltip-config
+            moon.create.set-input-processor
+            moon.create.skin
+            moon.create.cursors
+            moon.create.textures
+            moon.create.world-unit-scale
+            moon.create.world-viewport)
+  (:import (com.badlogic.gdx Gdx)))
+
+(defn do!
+  []
+  (reduce (fn [ctx [f & params]]
+            (apply f ctx params))
+          {
+           :ctx/audio     Gdx/audio
+           :ctx/files     Gdx/files
+           :ctx/graphics  Gdx/graphics
+           :ctx/input     Gdx/input
+           :ctx/active-entities nil
+           :ctx/delta-time nil
+           :ctx/mouseover-eid nil
+           :ctx/ui-mouse-position nil
+           :ctx/world-mouse-position nil
+           :ctx/elapsed-time 0
+           :ctx/paused? false
+           :ctx/unit-scale (atom 1)
+           :ctx/factions-iterations {:good 15 :evil 5}
+           :ctx/max-delta 0.04
+           :ctx/minimum-size 0.39
+           :ctx/z-orders [:z-order/on-ground
+                          :z-order/ground
+                          :z-order/flying
+                          :z-order/effect]
+           :ctx/potential-field-cache (atom nil)
+           :ctx/id-counter (atom 0)
+           :ctx/entity-ids (atom {})
+           }
+          [
+           [moon.create.tooltip-config/step]
+           [moon.create.colors/step]
+           [moon.create.audio/step]
+           [moon.create.batch/step]
+           [moon.create.shape-drawer-texture/step]
+           [moon.create.shape-drawer/step]
+           [moon.create.ui-viewport/step]
+           [moon.create.stage/step]
+           [moon.create.set-input-processor/step]
+           [moon.create.skin/step]
+           [moon.create.cursors/step]
+           [moon.create.textures/step]
+           [moon.create.world-unit-scale/step]
+           [moon.create.world-viewport/step]
+           [moon.create.default-font/step]
+           [moon.create.controls/step]
+           [moon.create.ctx-colors/step]
+           [moon.create.render-z-order/step]
+           [moon.create.max-speed/step]
+           [moon.create.db/step]
+           [moon.create.into-record/step]
+           [moon.create.add-stage-actors/step]
+           [moon.create.tiled-map/step]
+           [moon.create.grid/step]
+           [moon.create.content-grid/step]
+           [moon.create.explored-tile-corners/step]
+           [moon.create.raycaster/step]
+           [moon.create.spawn-player/step]
+           [moon.create.spawn-enemies/step]
+           ]))
--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,35 +1,7 @@
 (ns moon.start
-  (:require [moon.application.dispose :as dispose]
+  (:require [moon.application.create :as create]
+            [moon.application.dispose :as dispose]
             [moon.application.resize :as resize]
-            moon.create.audio
-            moon.create.spawn-enemies
-            moon.create.raycaster
-            moon.create.spawn-player
-            moon.create.grid
-            moon.create.content-grid
-            moon.create.explored-tile-corners
-            moon.create.add-stage-actors
-            moon.create.tiled-map
-            moon.create.into-record
-            moon.create.render-z-order
-            moon.create.max-speed
-            moon.create.db
-            moon.create.ctx-colors
-            moon.create.controls
-            moon.create.batch
-            moon.create.colors
-            moon.create.shape-drawer
-            moon.create.shape-drawer-texture
-            moon.create.default-font
-            moon.create.ui-viewport
-            moon.create.stage
-            moon.create.tooltip-config
-            moon.create.set-input-processor
-            moon.create.skin
-            moon.create.cursors
-            moon.create.textures
-            moon.create.world-unit-scale
-            moon.create.world-viewport
             moon.render.draw-tiled-map
             [clojure.animation :as animation]
             [clojure.gdx.backends.lwjgl :as lwjgl]
@@ -82,8 +54,7 @@
             [moon.val-max :as val-max]
             [qrecord.core :as q]
             [reduce-fsm :as fsm])
-  (:import (com.badlogic.gdx ApplicationListener
-                             Gdx)
+  (:import (com.badlogic.gdx ApplicationListener)
            (com.badlogic.gdx.graphics.g2d SpriteBatch))
   (:gen-class))
 
@@ -999,67 +970,6 @@
 
  )
 
-(defn- create!
-  []
-  (reduce (fn [ctx [f & params]]
-            (apply f ctx params))
-          {
-           :ctx/audio     Gdx/audio
-           :ctx/files     Gdx/files
-           :ctx/graphics  Gdx/graphics
-           :ctx/input     Gdx/input
-           :ctx/active-entities nil
-           :ctx/delta-time nil
-           :ctx/mouseover-eid nil
-           :ctx/ui-mouse-position nil
-           :ctx/world-mouse-position nil
-           :ctx/elapsed-time 0
-           :ctx/paused? false
-           :ctx/unit-scale (atom 1)
-           :ctx/factions-iterations {:good 15 :evil 5}
-           :ctx/max-delta 0.04
-           :ctx/minimum-size 0.39
-           :ctx/z-orders [:z-order/on-ground
-                          :z-order/ground
-                          :z-order/flying
-                          :z-order/effect]
-           :ctx/potential-field-cache (atom nil)
-           :ctx/id-counter (atom 0)
-           :ctx/entity-ids (atom {})
-           }
-          [
-           [moon.create.tooltip-config/step]
-           [moon.create.colors/step]
-           [moon.create.audio/step]
-           [moon.create.batch/step]
-           [moon.create.shape-drawer-texture/step]
-           [moon.create.shape-drawer/step]
-           [moon.create.ui-viewport/step]
-           [moon.create.stage/step]
-           [moon.create.set-input-processor/step]
-           [moon.create.skin/step]
-           [moon.create.cursors/step]
-           [moon.create.textures/step]
-           [moon.create.world-unit-scale/step]
-           [moon.create.world-viewport/step]
-           [moon.create.default-font/step]
-           [moon.create.controls/step]
-           [moon.create.ctx-colors/step]
-           [moon.create.render-z-order/step]
-           [moon.create.max-speed/step]
-           [moon.create.db/step]
-           [moon.create.into-record/step]
-           [moon.create.add-stage-actors/step]
-           [moon.create.tiled-map/step]
-           [moon.create.grid/step]
-           [moon.create.content-grid/step]
-           [moon.create.explored-tile-corners/step]
-           [moon.create.raycaster/step]
-           [moon.create.spawn-player/step]
-           [moon.create.spawn-enemies/step]
-           ]))
-
-
 (defn draw-on-world-viewport!
   [{:keys [^SpriteBatch ctx/batch
            ctx/shape-drawer
@@ -1553,7 +1463,7 @@
   (lwjgl/use-glfw-async!)
   (lwjgl/application! (reify ApplicationListener
                         (create [_]
-                          (reset! state (create!)))
+                          (reset! state (create/do!)))
 
                         (dispose [_]
                           (dispose/do! @state))
