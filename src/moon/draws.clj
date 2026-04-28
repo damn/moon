39d6a7f735d76c2648879d06475cf0dd2690efcc
Michael Sappler <michaelsappler@gmail.com>
2026-04-28 23:50:26 +0530

    Remove private draw-text!

--- src/moon/draws.clj
+++ src/moon/draws.clj
@@ -7,19 +7,6 @@
 
 (declare handle)
 
-(defn- draw-text! [font batch {:keys [scale text x y up? h-align target-width wrap?]}]
-  (let [old-scale (bitmap-font/scale-x font)]
-    (bitmap-font/set-scale! font (* old-scale scale))
-    (bitmap-font/draw! font
-                       batch
-                       text
-                       x
-                       (+ y (if up? (bitmap-font/text-height font text) 0))
-                       target-width
-                       (or h-align :align/center)
-                       wrap?)
-    (bitmap-font/set-scale! font old-scale)))
-
 (def draw-fns
   {
    :draw/circle           (fn
@@ -69,17 +56,22 @@
                                      ctx/unit-scale
                                      ctx/default-font]}
                              {:keys [font scale x y text h-align up?]}]
-                            (draw-text! (or font default-font)
-                                        batch
-                                        {:scale (* (float @unit-scale)
-                                                   (float (or scale 1)))
-                                         :text text
-                                         :x x
-                                         :y y
-                                         :up? up?
-                                         :h-align h-align
-                                         :target-width 0
-                                         :wrap? false}))
+                            (let [font (or font default-font)
+                                  old-scale (bitmap-font/scale-x font)
+                                  target-width 0
+                                  wrap? false
+                                  scale (* (float @unit-scale)
+                                           (float (or scale 1)))]
+                              (bitmap-font/set-scale! font (* old-scale scale))
+                              (bitmap-font/draw! font
+                                                 batch
+                                                 text
+                                                 x
+                                                 (+ y (if up? (bitmap-font/text-height font text) 0))
+                                                 target-width
+                                                 (or h-align :align/center)
+                                                 wrap?)
+                              (bitmap-font/set-scale! font old-scale)))
    :draw/texture-region   (fn
                             [{:keys [^Batch ctx/batch
                                      ctx/unit-scale
