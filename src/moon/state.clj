1af6cd3abe8de0637a60764b1d07aa93d84e6c91
Michael Sappler <michaelsappler@gmail.com>
2026-04-29 11:56:34 +0530

    fix state/create

--- src/moon/state.clj
+++ src/moon/state.clj
@@ -15,7 +15,7 @@
      (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
          1)))
 
-(defmethod state/create :active-skill
+(defmethod create :active-skill
   [[_k [skill effect-ctx]] eid {:keys [ctx/elapsed-time]}]
   {:skill skill
    :effect-ctx effect-ctx
@@ -24,21 +24,21 @@
                  (apply-action-speed-modifier @eid skill)
                  (timer/create elapsed-time))})
 
-(defmethod state/create :stunned
+(defmethod create :stunned
   [[_k duration] _eid {:keys [ctx/elapsed-time]}]
   {:counter (timer/create elapsed-time duration)})
 
-(defmethod state/create :player-moving
+(defmethod create :player-moving
   [[_k movement-vector] eid _ctx]
   {:movement-vector movement-vector})
 
-(defmethod state/create :player-item-on-cursor
+(defmethod create :player-item-on-cursor
   [[_k item] _eid _ctx]
   {:item item})
 
 (def reaction-time-multiplier 0.016)
 
-(defmethod state/create :npc-moving
+(defmethod create :npc-moving
   [[_k movement-vector] eid {:keys [ctx/elapsed-time]}]
   {:movement-vector movement-vector
    :timer (timer/create elapsed-time
