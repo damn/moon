6ec5a7a8c6f1da2da172485adc3be2709daad77f
Michael Sappler <michaelsappler@gmail.com>
2026-04-30 10:36:09 +0530

    Remove comments

--- src/moon/application/create/unorganised.clj
+++ src/moon/application/create/unorganised.clj
@@ -13,30 +13,21 @@
 
 (defn step [ctx]
   (assoc ctx
-          ; frame
           :ctx/active-entities nil
           :ctx/delta-time nil
           :ctx/ui-mouse-position nil
           :ctx/world-mouse-position nil
-          ;
           :ctx/mouseover-eid nil
-          ; graphics
           :ctx/unit-scale (atom 1)
-          ;
-          ;
-          ; world
           :ctx/elapsed-time 0
           :ctx/paused? false
           :ctx/factions-iterations {:good 15 :evil 5}
-          ; TODO if it doens't change put in defs?
           :ctx/max-delta 0.04
           :ctx/minimum-size 0.39
           :ctx/z-orders [:z-order/on-ground
                          :z-order/ground
                          :z-order/flying
                          :z-order/effect]
-          ;
           :ctx/potential-field-cache (atom nil)
           :ctx/id-counter (atom 0)
-          :ctx/entity-ids (atom {})
-         ))
+          :ctx/entity-ids (atom {})))
