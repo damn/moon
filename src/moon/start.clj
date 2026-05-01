e5551ac36ef5d8cd6dcdb10d81256d6c1d38f16c
Michael Sappler <michaelsappler@gmail.com>
2026-05-01 18:35:39 +0530

    Bring application together

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,12 +1,34 @@
 (ns moon.start
-  (:require [moon.application :as application])
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+  (:require [moon.application.create :as create]
+            [moon.application.dispose :as dispose]
+            [moon.application.render :as render]
+            [moon.application.resize :as resize])
+  (:import (com.badlogic.gdx ApplicationListener
+                             Gdx)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
                                              Lwjgl3ApplicationConfiguration))
   (:gen-class))
 
+(def state (atom nil))
+
 (defn -main []
   (Lwjgl3ApplicationConfiguration/useGlfwAsync)
-  (Lwjgl3Application. application/listener
+  (Lwjgl3Application. (reify ApplicationListener
+                        (create [_]
+                          (reset! state (create/do! Gdx/app)))
+
+                        (dispose [_]
+                          (dispose/do! @state))
+
+                        (render [_]
+                          (swap! state render/do!))
+
+                        (resize [_ width height]
+                          (resize/do! @state width height))
+
+                        (pause [_])
+
+                        (resume [_]))
                       (doto (Lwjgl3ApplicationConfiguration.)
                         (.setTitle "Cyber Dungeon Quest")
                         (.setWindowedMode 1440 900)
