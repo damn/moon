e5aa2486aca857b5a048a9e6836c110da35e658e
Michael Sappler <michaelsappler@gmail.com>
2026-05-01 22:58:36 +0530

    Fix ui impls

--- src/moon/application/create/ui_impls.clj
+++ src/moon/application/create/ui_impls.clj
@@ -1,7 +1,8 @@
 (ns moon.application.create.ui-impls
   ; make one folder 'impl' with defmethods
   ; and one folder with protocols
-  (:require clojure.gdx.scene2d.ui.image-button
+  (:require clojure.gdx.scene2d.ui.horizontal-group
+            clojure.gdx.scene2d.ui.image-button
             clojure.gdx.scene2d.ui.scroll-pane
             clojure.gdx.scene2d.ui.stack
             clojure.gdx.scene2d.ui.text-button
