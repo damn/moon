63a12c1f50f7d810068463509a31a7d5bb04f28c
Michael Sappler <michaelsappler@gmail.com>
2026-05-04 11:05:30 +0530

    dispose/resize split

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -2,10 +2,9 @@
   (:require [com.badlogic.gdx.application-listener :as application-listener]
             [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl-app]
             [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application-configuration :as config]
-            [com.badlogic.gdx.utils.disposable :as disposable]
 
-            [clojure.gdx.scene2d.stage :as stage]
-            [clojure.gdx.utils.viewport :as viewport]
+            moon.application.dispose
+            moon.application.resize
 
             [clojure.config :refer [edn-resource]]
             )
@@ -26,22 +25,7 @@
 
                       :dispose!
                       (fn []
-                        (let [{:keys [ctx/audio
-                                      ctx/batch
-                                      ctx/cursors
-                                      ctx/default-font
-                                      ctx/shape-drawer-texture
-                                      ctx/skin
-                                      ctx/textures
-                                      ctx/tiled-map]} @state]
-                          (run! disposable/dispose! (vals audio))
-                          (disposable/dispose! batch)
-                          (run! disposable/dispose! (vals cursors))
-                          (disposable/dispose! default-font)
-                          (disposable/dispose! shape-drawer-texture)
-                          (disposable/dispose! skin)
-                          (run! disposable/dispose! (vals textures))
-                          (disposable/dispose! tiled-map)))
+                        (moon.application.dispose/do! @state))
 
                       :render!
                       (fn []
@@ -54,13 +38,13 @@
 
                       :resize!
                       (fn [width height]
-                        (let [{:keys [ctx/stage
-                                      ctx/world-viewport]} @state]
-                          (viewport/update! (stage/viewport stage) width height true)
-                          (viewport/update! world-viewport width height false)))
+                        (moon.application.resize/do! @state width height))
 
-                      :pause!  (fn [])
-                      :resume! (fn [])
+                      :pause!
+                      (fn [])
+
+                      :resume!
+                      (fn [])
                       })
                     (config/create
                      {:title "Moon"
