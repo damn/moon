d24483fab94235646ff758aa3b1e91b00c6ec42d
Michael Sappler <michaelsappler@gmail.com>
2026-05-06 16:39:44 +0530

    Change start

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,6 +1,6 @@
 (ns moon.start
-  (:require [com.badlogic.gdx.application-listener :as application-listener]
-            [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl-app]
+  (:require [com.badlogic.gdx.application-listener :as listener]
+            [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as application]
             [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application-configuration :as config]
             [clojure.config :refer [edn-resource]])
   (:gen-class))
@@ -8,42 +8,42 @@
 (def state (atom nil))
 
 (defn -main []
-  (config/use-glfw-async!)
   (let [{:keys [create-pipeline
                 dispose!
                 render-pipeline
                 resize!
                 config]} (edn-resource "game.edn")]
-    (lwjgl-app/create (application-listener/create
-                       {:create!
-                        (fn []
-                          (reset! state
-                                  (reduce (fn [ctx [f & params]]
-                                            (apply f ctx params))
-                                          {}
-                                          create-pipeline)))
+    (config/use-glfw-async!)
+    (application/create (listener/create
+                         {:create!
+                          (fn []
+                            (reset! state
+                                    (reduce (fn [ctx [f & params]]
+                                              (apply f ctx params))
+                                            {}
+                                            create-pipeline)))
 
-                        :dispose!
-                        (fn []
-                          (dispose! @state))
+                          :dispose!
+                          (fn []
+                            (dispose! @state))
 
-                        :render!
-                        (fn []
-                          (swap! state
-                                 (fn [ctx]
-                                   (reduce (fn [ctx [f & params]]
-                                             (apply f ctx params))
-                                           ctx
-                                           render-pipeline))))
+                          :render!
+                          (fn []
+                            (swap! state
+                                   (fn [ctx]
+                                     (reduce (fn [ctx [f & params]]
+                                               (apply f ctx params))
+                                             ctx
+                                             render-pipeline))))
 
-                        :resize!
-                        (fn [width height]
-                          (resize! @state width height))
+                          :resize!
+                          (fn [width height]
+                            (resize! @state width height))
 
-                        :pause!
-                        (fn [])
+                          :pause!
+                          (fn [])
 
-                        :resume!
-                        (fn [])
-                        })
-                      (config/create config))))
+                          :resume!
+                          (fn [])
+                          })
+                        (config/create config))))
