f488c38c6f02904774e9fcd015bd9f0cb5c4dcf3
Michael Sappler <michaelsappler@gmail.com>
2026-05-07 22:33:14 +0530

    Remove moon.graphics

--- src/moon/draws.clj
+++ src/moon/draws.clj
@@ -1,5 +1,10 @@
 (ns moon.draws
-  (:require [moon.graphics :as graphics]
+  (:require [com.badlogic.gdx.graphics.g2d.batch :as batch]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font :as font]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.data :as font.data]
+            [com.badlogic.gdx.graphics.g2d.texture-region :as texture-region]
+            [com.badlogic.gdx.utils.align :as align]
+            [clojure.string :as str]
             [space.earlygrey.shapedrawer.shape-drawer :as shape-drawer]))
 
 (declare handle)
@@ -48,8 +53,58 @@
                             [{:keys [ctx/shape-drawer]} [center-x center-y] radius start-radians radians color-float-bits]
                             (shape-drawer/set-color! shape-drawer color-float-bits)
                             (shape-drawer/sector! shape-drawer center-x center-y radius start-radians radians))
-   :draw/text             graphics/draw-text!
-   :draw/texture-region   graphics/draw-texture-region!
+   :draw/text             (fn
+                            [{:keys [ctx/batch
+                                     ctx/unit-scale
+                                     ctx/default-font]}
+                             {:keys [font scale x y text h-align up?]}]
+                            (let [font (or font default-font)
+                                  old-scale (font.data/scale-x (font/data font))
+                                  target-width 0
+                                  wrap? false
+                                  scale (* (float @unit-scale)
+                                           (float (or scale 1)))]
+                              (font.data/set-scale! (font/data font) (* old-scale scale))
+                              (font/draw! font
+                                          batch
+                                          text
+                                          x
+                                          (+ y (if up?
+                                                 (-> text
+                                                     (str/split #"\n")
+                                                     count
+                                                     (* (font/line-height font)))
+                                                 0))
+                                          target-width
+                                          (align/k->value :align/center)
+                                          wrap?)
+                              (font.data/set-scale! (font/data font) old-scale)))
+   :draw/texture-region   (fn
+                            [{:keys [ctx/batch
+                                     ctx/unit-scale
+                                     ctx/world-unit-scale]}
+                             texture-region
+                             [x y]
+                             & {:keys [center? rotation]}]
+                            (let [[w h] (let [dimensions [(texture-region/width  texture-region)
+                                                          (texture-region/height texture-region)]]
+                                          (if (= @unit-scale 1)
+                                            dimensions
+                                            (mapv (comp float (partial * world-unit-scale))
+                                                  dimensions)))]
+                              (if center?
+                                (batch/draw! batch
+                                             texture-region
+                                             (- (float x) (/ (float w) 2))
+                                             (- (float y) (/ (float h) 2))
+                                             (/ (float w) 2)
+                                             (/ (float h) 2)
+                                             w
+                                             h
+                                             1
+                                             1
+                                             (or rotation 0))
+                                (batch/draw! batch texture-region x y w h))))
    :draw/with-line-width  (fn
                             [{:keys [ctx/shape-drawer]
                               :as ctx}
