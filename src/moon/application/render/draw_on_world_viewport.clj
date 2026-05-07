1a2c380a5bf4ba44ca03361a32bcc496dfafd5c8
Michael Sappler <michaelsappler@gmail.com>
2026-05-07 22:31:14 +0530

    Integrate graphics/draw-on-world-viewport to render

--- src/moon/application/render/draw_on_world_viewport.clj
+++ src/moon/application/render/draw_on_world_viewport.clj
@@ -1,11 +1,31 @@
 (ns moon.application.render.draw-on-world-viewport
-  (:require [moon.draws :as draws]
-            [moon.graphics :as graphics]))
+  (:require [com.badlogic.gdx.graphics.g2d.batch :as batch]
+            [clojure.gdx.utils.viewport :as viewport]
+            [clojure.graphics.orthographic-camera :as camera]
+            [moon.draws :as draws]
+            [space.earlygrey.shapedrawer.shape-drawer :as shape-drawer]))
 
 (defn step
-  [ctx draw-fns]
-  (graphics/draw-on-world-viewport! ctx
-                                    (fn []
-                                      (doseq [f draw-fns]
-                                        (draws/handle ctx (f ctx)))))
+  [{:keys [ctx/batch
+           ctx/shape-drawer
+           ctx/unit-scale
+           ctx/world-unit-scale
+           ctx/world-viewport]
+    :as ctx}
+   draw-fns]
+  ; fix scene2d.ui.tooltip flickering
+  ; _everything_ flickers with TextToolTip!
+  ; it changes batch color somehow and does not change it back ! FIXME
+  (batch/set-color! batch 1 1 1 1)
+  ;
+  (batch/set-projection-matrix! batch (camera/combined (viewport/camera world-viewport)))
+  (batch/begin! batch)
+  (let [old-line-width (shape-drawer/default-line-width shape-drawer)]
+    (shape-drawer/set-default-line-width! shape-drawer (* world-unit-scale old-line-width))
+    (reset! unit-scale world-unit-scale)
+    (doseq [f draw-fns]
+      (draws/handle ctx (f ctx)))
+    (reset! unit-scale 1)
+    (shape-drawer/set-default-line-width! shape-drawer old-line-width))
+  (batch/end! batch)
   ctx)
--- src/moon/graphics.clj
+++ src/moon/graphics.clj
@@ -4,10 +4,7 @@
             [com.badlogic.gdx.graphics.g2d.bitmap-font.data :as font.data]
             [com.badlogic.gdx.graphics.g2d.texture-region :as texture-region]
             [com.badlogic.gdx.utils.align :as align]
-            [clojure.gdx.utils.viewport :as viewport]
-            [clojure.graphics.orthographic-camera :as camera]
-            [clojure.string :as str]
-            [space.earlygrey.shapedrawer.shape-drawer :as shape-drawer]))
+            [clojure.string :as str]))
 
 (defn draw-text!
   [{:keys [ctx/batch
@@ -62,25 +59,3 @@
                    1
                    (or rotation 0))
       (batch/draw! batch texture-region x y w h))))
-
-(defn draw-on-world-viewport!
-  [{:keys [ctx/batch
-           ctx/shape-drawer
-           ctx/unit-scale
-           ctx/world-unit-scale
-           ctx/world-viewport]}
-   f]
-  ; fix scene2d.ui.tooltip flickering
-  ; _everything_ flickers with TextToolTip!
-  ; it changes batch color somehow and does not change it back ! FIXME
-  (batch/set-color! batch 1 1 1 1)
-  ;
-  (batch/set-projection-matrix! batch (camera/combined (viewport/camera world-viewport)))
-  (batch/begin! batch)
-  (let [old-line-width (shape-drawer/default-line-width shape-drawer)]
-    (shape-drawer/set-default-line-width! shape-drawer (* world-unit-scale old-line-width))
-    (reset! unit-scale world-unit-scale)
-    (f)
-    (reset! unit-scale 1)
-    (shape-drawer/set-default-line-width! shape-drawer old-line-width))
-  (batch/end! batch))
