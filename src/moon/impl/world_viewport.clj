38f6a9e648ab8b1095df3ea4263f6be5cde8737e
Michael Sappler <michaelsappler@gmail.com>
2026-05-11 20:30:44 +0530

    fix world impl

--- src/moon/impl/world_viewport.clj
+++ src/moon/impl/world_viewport.clj
@@ -6,7 +6,7 @@
   [{:keys [ctx/world-unit-scale]}]
   (let [world-width  (* 1440 world-unit-scale)
         world-height (* 900  world-unit-scale)]
-    (fit-viewport/create world-width
-                         world-height
-                         (doto (orthographic-camera/create)
-                           (orthographic-camera/set-to-ortho! false world-width world-height)))))
+    (viewport/create world-width
+                     world-height
+                     (doto (orthographic-camera/create)
+                       (orthographic-camera/set-to-ortho! false world-width world-height)))))
