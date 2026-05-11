c571c6dc851e17700b0bf5e03a5e9c9da2775846
Michael Sappler <michaelsappler@gmail.com>
2026-05-11 20:35:54 +0530

    Remove table interop ns

--- src/clojure/gdx/scene2d/ui/table.clj
+++ src/clojure/gdx/scene2d/ui/table.clj
@@ -1,45 +1,39 @@
 (ns clojure.gdx.scene2d.ui.table
   (:require [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell]
-            [com.badlogic.gdx.scenes.scene2d.ui.table :as table]
             [moon.ui.actor :as actor]
-            moon.ui.table
-            [moon.ui.widget-group :as widget-group]))
+            [moon.ui.table :as table]
+            [moon.ui.widget-group :as widget-group])
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)
+           (com.badlogic.gdx.scenes.scene2d.ui Table)))
 
-(extend com.badlogic.gdx.scenes.scene2d.ui.Table
-  moon.ui.table/Table
-  {
-   :add!
-   (fn [table cell-declaration]
-     (-> (table/add! table (:actor cell-declaration))
-         (cell/set-opts! (dissoc cell-declaration :actor))))
+(extend-type Table
+  table/Table
+  (add! [table cell-declaration]
+    (-> (.add table ^Actor (:actor cell-declaration))
+        (cell/set-opts! (dissoc cell-declaration :actor))))
 
-   :add-rows!
-   (fn [table rows]
-     (doseq [row rows]
-       (doseq [props-or-actor row]
+  (add-rows! [table rows]
+    (doseq [row rows]
+      (doseq [props-or-actor row]
 
-         (cond
-          (map? props-or-actor)
-          (moon.ui.table/add! table props-or-actor)
+        (cond
+         (map? props-or-actor)
+         (table/add! table props-or-actor)
 
-          ; TODO Remove else case
-          :else (table/add! table props-or-actor)
+         ; TODO Remove else case
+         :else (.add table ^Actor props-or-actor)
+         ))
+      (.row table))
+    table)
 
-          ))
-       (table/row! table))
-     table)
-
-   :set-opts!
-   (fn [table opts]
+   (set-opts! [table opts]
      (when-let [rows (:table/rows opts)]
-       (moon.ui.table/add-rows! table rows)
+       (table/add-rows! table rows)
        (widget-group/pack! table))
      (when-let [defaults (:table/cell-defaults opts)]
-       (cell/set-opts! (table/defaults table) defaults))
-     (widget-group/set-opts! table opts))
-   }
-  )
+       (cell/set-opts! (.defaults table) defaults))
+     (widget-group/set-opts! table opts)))
 
 (defmethod actor/create :ui/table [opts]
-  (doto (table/create)
-    (moon.ui.table/set-opts! opts)))
+  (doto (Table.)
+    (table/set-opts! opts)))
