3757d84608791bb23c84e7e782af53f8103946d7
Michael Sappler <michaelsappler@gmail.com>
2026-05-11 20:38:16 +0530

    REmove com.badlogic.gdx.scenes.scene2d.group

--- src/clojure/gdx/scene2d/group.clj
+++ src/clojure/gdx/scene2d/group.clj
@@ -1,19 +1,27 @@
 (ns clojure.gdx.scene2d.group
-  (:require [com.badlogic.gdx.scenes.scene2d.group :as group]
-            [moon.ui.actor :as actor]
-            moon.ui.group))
+  (:require [moon.ui.actor :as actor]
+            [moon.ui.group :as group])
+  (:import (com.badlogic.gdx.scenes.scene2d Group)))
 
-(extend com.badlogic.gdx.scenes.scene2d.Group
-  moon.ui.group/Group
-  {:add-actor! group/add-actor!
-   :children group/children
-   :find-actor group/find-actor
-   :clear-children! group/clear-children!
-   :set-opts! (fn [group opts]
-                (when-let [actors (:group/actors opts)]
-                  (run! #(moon.ui.group/add-actor! group %) actors))
-                (actor/set-opts! group opts))})
+(extend-type Goup
+  group/Group
+  (add-actor! [group actor]
+    (.addActor group actor))
+
+  (children [group]
+    (.getChildren group))
+
+  (find-actor [group name]
+    (.findActor group name))
+
+  (clear-children! [group]
+    (.clearChildren group))
+
+  (set-opts! [group opts]
+    (when-let [actors (:group/actors opts)]
+      (run! #(group/add-actor! group %) actors))
+    (actor/set-opts! group opts)))
 
 (defmethod actor/create :ui/group [opts]
-  (doto (group/create)
-    (moon.ui.group/set-opts! opts)))
+  (doto (Group.)
+    (group/set-opts! opts)))
