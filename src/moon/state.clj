40ae91a7979c626cc131803c5d3eae6423ed5118
Michael Sappler <michaelsappler@gmail.com>
2026-05-11 11:04:08 +0530

    moon.state-impl

--- src/moon/state.clj
+++ src/moon/state.clj
@@ -1,6 +1,4 @@
-(ns moon.state
-  (:require [moon.stats :as stats]
-            [moon.timer :as timer]))
+(ns moon.state)
 
 (defmulti create
   (fn [[k _v] _eid _ctx]
@@ -9,41 +7,6 @@
 (defmethod create :default
   [[_k v] _eid _ctx]
   v)
-
-(defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
-  (/ action-time
-     (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
-         1)))
-
-(defmethod create :active-skill
-  [[_k [skill effect-ctx]] eid {:keys [ctx/elapsed-time]}]
-  {:skill skill
-   :effect-ctx effect-ctx
-   :counter (->> skill
-                 :skill/action-time
-                 (apply-action-speed-modifier @eid skill)
-                 (timer/create elapsed-time))})
-
-(defmethod create :stunned
-  [[_k duration] _eid {:keys [ctx/elapsed-time]}]
-  {:counter (timer/create elapsed-time duration)})
-
-(defmethod create :player-moving
-  [[_k movement-vector] eid _ctx]
-  {:movement-vector movement-vector})
-
-(defmethod create :player-item-on-cursor
-  [[_k item] _eid _ctx]
-  {:item item})
-
-(def reaction-time-multiplier 0.016)
-
-(defmethod create :npc-moving
-  [[_k movement-vector] eid {:keys [ctx/elapsed-time]}]
-  {:movement-vector movement-vector
-   :timer (timer/create elapsed-time
-                        (* (stats/get-stat-value (:entity/stats @eid) :stats/reaction-time)
-                           reaction-time-multiplier))})
 
 (defmulti enter
   (fn [[k _v] _eid]
