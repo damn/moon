22c7a80f117b7baeb7c13b2a89184afc552a2a26
Michael Sappler <michaelsappler@gmail.com>
2026-05-11 11:15:01 +0530

    Remove com.badlogic.gdx.graphics.colors

--- src/moon/application/create/gdx_colors.clj
+++ src/moon/application/create/gdx_colors.clj
@@ -1,8 +1,8 @@
 (ns moon.application.create.gdx-colors
-  (:require [com.badlogic.gdx.graphics.color :as color]
-            [com.badlogic.gdx.graphics.colors :as colors]))
+  (:require [com.badlogic.gdx.graphics.color :as color])
+  (:import (com.badlogic.gdx.graphics Colors)))
 
 (defn step [ctx]
   (doseq [[name rgba] {"PRETTY_NAME" [0.84 0.8 0.52 1]}]
-    (colors/put! name (color/create rgba)))
+    (Colors/put name (color/create rgba)))
   ctx)
