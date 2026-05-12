b22f2fbfc3904fe75a5e424ef789d3b61eb84247
Michael Sappler <michaelsappler@gmail.com>
2026-05-13 03:15:26 +0530

    Viewport protocol

--- src/clojure/gdx/utils/viewport.clj
+++ src/clojure/gdx/utils/viewport.clj
@@ -1,23 +1,8 @@
-(ns clojure.gdx.utils.viewport
-  (:require [com.badlogic.gdx.math.vector2 :as vector2])
-  (:import (com.badlogic.gdx.utils.viewport FitViewport)))
+(ns clojure.gdx.utils.viewport)
 
-(defn create [width height camera]
-  (FitViewport. width height camera))
-
-(defn camera [^FitViewport viewport]
-  (.getCamera viewport))
-
-(defn world-width [^FitViewport viewport]
-  (.getWorldWidth viewport))
-
-(defn world-height [^FitViewport viewport]
-  (.getWorldHeight viewport))
-
-(defn update! [^FitViewport viewport screen-width screen-height center-camera?]
-  (.update viewport screen-width screen-height center-camera?))
-
-(defn unproject [^FitViewport viewport position]
-  (-> viewport
-      (.unproject (vector2/->java position))
-      vector2/->clj))
+(defprotocol Viewport
+  (camera [_])
+  (world-width [_])
+  (world-height [_])
+  (update! [_ screen-width screen-height center-camera?])
+  (unproject [_ position]))
--- src/moon/impl/world_viewport.clj
+++ src/moon/impl/world_viewport.clj
@@ -1,12 +1,33 @@
 (ns moon.impl.world-viewport
-  (:require [com.badlogic.gdx.graphics.orthographic-camera :as orthographic-camera]
-            [clojure.gdx.utils.viewport :as viewport]))
+  (:require [clojure.gdx.utils.viewport :as viewport]
+            [com.badlogic.gdx.graphics.orthographic-camera :as orthographic-camera]
+            [com.badlogic.gdx.math.vector2 :as vector2])
+  (:import (com.badlogic.gdx.utils.viewport FitViewport)))
 
 (defn create
   [{:keys [ctx/world-unit-scale]}]
   (let [world-width  (* 1440 world-unit-scale)
         world-height (* 900  world-unit-scale)]
-    (viewport/create world-width
-                     world-height
-                     (doto (orthographic-camera/create)
-                       (orthographic-camera/set-to-ortho! false world-width world-height)))))
+    (FitViewport. world-width
+                  world-height
+                  (doto (orthographic-camera/create)
+                    (orthographic-camera/set-to-ortho! false world-width world-height)))))
+
+(extend-type FitViewport
+  viewport/Viewport
+  (camera [viewport]
+    (.getCamera viewport))
+
+  (world-width [viewport]
+    (.getWorldWidth viewport))
+
+  (world-height [viewport]
+    (.getWorldHeight viewport))
+
+  (update! [viewport screen-width screen-height center-camera?]
+    (.update viewport screen-width screen-height center-camera?))
+
+  (unproject [viewport position]
+    (-> viewport
+        (.unproject (vector2/->java position))
+        vector2/->clj)))
