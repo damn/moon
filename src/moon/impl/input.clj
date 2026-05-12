e380b225a20ee08be0f0a136f853731da34f5c9b
Michael Sappler <michaelsappler@gmail.com>
2026-05-12 21:11:15 +0530

    moon.input protocol

--- src/moon/impl/input.clj
+++ src/moon/impl/input.clj
@@ -1,5 +1,37 @@
 (ns moon.impl.input
-  (:import (com.badlogic.gdx Gdx)))
+  (:require [com.badlogic.gdx.input.keys :as input.keys]
+            [clojure.math.vector2 :as v]
+            [moon.input :as input])
+  (:import (com.badlogic.gdx Gdx
+                             Input)))
 
 (defn create [_ctx]
   Gdx/input)
+
+(extend-type Input
+  moon.input/Input
+  (set-processor! [this input-processor]
+    (.setInputProcessor this input-processor))
+
+  (key-pressed? [this key]
+    (.isKeyPressed this key))
+
+  (key-just-pressed? [this key]
+    (.isKeyJustPressed this key))
+
+  (button-just-pressed? [this button]
+    (.isButtonJustPressed this button))
+
+  (mouse-position [this]
+    [(.getX this)
+     (.getY this)])
+
+  (player-movement-vector [this]
+    (let [r (when (input/key-pressed? this input.keys/d) [1  0])
+          l (when (input/key-pressed? this input.keys/a) [-1 0])
+          u (when (input/key-pressed? this input.keys/w) [0  1])
+          d (when (input/key-pressed? this input.keys/s) [0 -1])]
+      (when (or r l u d)
+        (let [v (v/add-vs (remove nil? [r l u d]))]
+          (when (pos? (v/length v))
+            v))))))
--- src/moon/input.clj
+++ src/moon/input.clj
@@ -1,29 +1,9 @@
-(ns moon.input
-  (:require [com.badlogic.gdx.input.keys :as input.keys]
-            [clojure.math.vector2 :as v])
-  (:import (com.badlogic.gdx Input)))
+(ns moon.input)
 
-(def set-processor! Input/.setInputProcessor)
-
-(defn key-pressed? [^Input this key]
-  (.isKeyPressed this key))
-
-(defn key-just-pressed? [^Input this key]
-  (.isKeyJustPressed this key))
-
-(defn button-just-pressed? [^Input this button]
-  (.isButtonJustPressed this button))
-
-(defn mouse-position [^Input this]
-  [(.getX this)
-   (.getY this)])
-
-(defn player-movement-vector [input]
-  (let [r (when (key-pressed? input input.keys/d) [1  0])
-        l (when (key-pressed? input input.keys/a) [-1 0])
-        u (when (key-pressed? input input.keys/w) [0  1])
-        d (when (key-pressed? input input.keys/s) [0 -1])]
-    (when (or r l u d)
-      (let [v (v/add-vs (remove nil? [r l u d]))]
-        (when (pos? (v/length v))
-          v)))))
+(defprotocol Input
+  (set-processor! [_ input-processor])
+  (key-pressed? [_ key])
+  (key-just-pressed? [_ key])
+  (button-just-pressed? [_ button])
+  (mouse-position [_])
+  (player-movement-vector [_]))
