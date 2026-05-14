a8133371638e3c12ab027dc8dfbd580050310309
Michael Sappler <michaelsappler@gmail.com>
2026-05-15 00:26:07 +0530

    clojure.core-ext/extend-types

--- src/com/badlogic/gdx/application.clj
+++ src/com/badlogic/gdx/application.clj
@@ -1,42 +1,39 @@
 (ns com.badlogic.gdx.application
-  (:require [moon.app :as app])
   (:import (com.badlogic.gdx Application)
            (com.badlogic.gdx.graphics GL20)))
 
-(extend-type Application
-  app/App
-  (new-sound [app path]
-    (.newSound (.getAudio app)
-               (.internal (.getFiles app) path)))
+(defn new-sound [^Application app path]
+  (.newSound (.getAudio app)
+             (.internal (.getFiles app) path)))
 
-  (frames-per-second [app]
-    (.getFramesPerSecond (.getGraphics app)))
+(defn frames-per-second [^Application app]
+  (.getFramesPerSecond (.getGraphics app)))
 
-  (delta-time [app]
-    (.getDeltaTime (.getGraphics app)))
+(defn delta-time [^Application app]
+  (.getDeltaTime (.getGraphics app)))
 
-  (new-cursor [app pixmap hotspot-x hotspot-y]
-    (.newCursor (.getGraphics app) pixmap hotspot-x hotspot-y))
+(defn new-cursor [^Application app pixmap hotspot-x hotspot-y]
+  (.newCursor (.getGraphics app) pixmap hotspot-x hotspot-y))
 
-  (set-cursor! [app cursor]
-    (.setCursor (.getGraphics app) cursor))
+(defn set-cursor! [^Application app cursor]
+  (.setCursor (.getGraphics app) cursor))
 
-  (clear! [app r g b a]
-    (.glClearColor (.getGL20 (.getGraphics app)) r g b a)
-    (.glClear      (.getGL20 (.getGraphics app)) GL20/GL_COLOR_BUFFER_BIT))
+(defn clear! [^Application app r g b a]
+  (.glClearColor (.getGL20 (.getGraphics app)) r g b a)
+  (.glClear      (.getGL20 (.getGraphics app)) GL20/GL_COLOR_BUFFER_BIT))
 
-  (set-input-processor! [this input-processor]
-    (.setInputProcessor (.getInput this) input-processor))
+(defn set-input-processor! [^Application this input-processor]
+  (.setInputProcessor (.getInput this) input-processor))
 
-  (key-pressed? [this key]
-    (.isKeyPressed (.getInput this) key))
+(defn key-pressed? [^Application this key]
+  (.isKeyPressed (.getInput this) key))
 
-  (key-just-pressed? [this key]
-    (.isKeyJustPressed (.getInput this) key))
+(defn key-just-pressed? [^Application this key]
+  (.isKeyJustPressed (.getInput this) key))
 
-  (button-just-pressed? [this button]
-    (.isButtonJustPressed (.getInput this) button))
+(defn button-just-pressed? [^Application this button]
+  (.isButtonJustPressed (.getInput this) button))
 
-  (mouse-position [this]
-    [(.getX (.getInput this))
-     (.getY (.getInput this))]))
+(defn mouse-position [^Application this]
+  [(.getX (.getInput this))
+   (.getY (.getInput this))])
