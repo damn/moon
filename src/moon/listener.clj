679642e20d1ea6ebb8805f20711f3d35a351f980
Michael Sappler <michaelsappler@gmail.com>
2026-05-14 23:20:26 +0530

    moon.listener

---
+++ src/moon/listener.clj
@@ -0,0 +1,34 @@
+(ns moon.listener
+  (:import (com.badlogic.gdx ApplicationListener)))
+
+(defn create
+  [{:keys [state-var
+           create-pipeline
+           dispose!
+           render-pipeline
+           resize!]}]
+  (let [state @state-var]
+    (reify ApplicationListener
+      (create [_]
+        (reset! state
+                (reduce (fn [ctx [f & params]]
+                          (apply f ctx params))
+                        {}
+                        create-pipeline)))
+
+      (dispose [_]
+        (dispose! @state))
+
+      (render [_]
+        (swap! state
+               (fn [ctx]
+                 (reduce (fn [ctx [f & params]]
+                           (apply f ctx params))
+                         ctx
+                         render-pipeline))))
+
+      (resize [_ width height]
+        (resize! @state width height))
+
+      (pause [_])
+      (resume [_]))))
--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,14 +1,13 @@
 (ns moon.start
   (:require [clojure.config :refer [edn-resource]]
-            [com.badlogic.gdx.application-listener :as listener]
             [com.badlogic.gdx.backends.lwjgl3.application :as application]
             [com.badlogic.gdx.backends.lwjgl3.config :as config]
-            moon.application-listener)
+            [moon.listener :as listener])
   (:gen-class))
 
 (defn -main []
   (let [{:keys [listener
                 config]} (edn-resource "game.edn")]
     (config/use-glfw-async!)
-    (application/create (listener/create (moon.application-listener/create listener))
+    (application/create (listener/create listener)
                         (config/create config))))
