2ddb62f5b1c79da2a905a0bd9676d076bdeb87dc
Michael Sappler <michaelsappler@gmail.com>
2026-05-14 23:28:08 +0530

    simplest moon.start

--- src/moon/start.clj
+++ src/moon/start.clj
@@ -1,13 +1,7 @@
 (ns moon.start
-  (:require [clojure.config :refer [edn-resource]]
-            [com.badlogic.gdx.backends.lwjgl3.application :as application]
-            [com.badlogic.gdx.backends.lwjgl3.config :as config]
-            [moon.listener :as listener])
+  (:require [clojure.config :refer [edn-resource]])
   (:gen-class))
 
 (defn -main []
-  (let [{:keys [listener
-                config]} (edn-resource "start.edn")]
-    (config/use-glfw-async!)
-    (application/create (listener/create listener)
-                        (config/create config))))
+  (doseq [[f & params] (edn-resource "start.edn")]
+    (apply f params)))
