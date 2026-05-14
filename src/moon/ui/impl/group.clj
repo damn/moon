2de5daa861d80b8105d5d217f5073c90c3e2bd08
Michael Sappler <michaelsappler@gmail.com>
2026-05-15 01:53:27 +0530

    actor to clojure.gdx

--- src/moon/ui/impl/group.clj
+++ src/moon/ui/impl/group.clj
@@ -1,6 +1,6 @@
 (ns moon.ui.impl.group
   (:require [moon.ui.actor]
-            [moon.ui.impl.actor])
+            [com.badlogic.gdx.scenes.scene2d.actor :as actor])
   (:import (com.badlogic.gdx.scenes.scene2d Group)))
 
 (defn add-actor! [^Group group actor]
@@ -18,7 +18,7 @@
 (defn set-opts! [group opts]
   (when-let [actors (:group/actors opts)]
     (run! #(add-actor! group (moon.ui.actor/create %)) actors))
-  (moon.ui.impl.actor/set-opts! group opts))
+  (actor/set-opts! group opts))
 
 (defn create [opts]
   (doto (Group.)
