a7312a8c2bb1e9f58b44cc2ee9e0d6f0da5fa230
Michael Sappler <michaelsappler@gmail.com>
2026-05-15 00:45:31 +0530

    extend-type

--- src/moon/camera.clj
+++ src/moon/camera.clj
@@ -8,32 +8,12 @@
   (viewport-height [_])
   (zoom [_])
   (set-position! [_ [x y]])
-  (set-zoom! [_ amount]))
+  (set-zoom! [_ amount])
+  (inc-zoom! [cam by])
+  (calculate-zoom [_ & {:keys [left top right bottom]}])
+  (visible-tiles [_]))
 
-(defn inc-zoom! [cam by]
-  (set-zoom! cam (max 0.1 (+ (zoom cam) by))))
 
-(defn visible-tiles [camera]
-  (let [[left-x right-x bottom-y top-y] (frustum camera)]
-    (for [x (range (int left-x)   (int right-x))
-          y (range (int bottom-y) (+ 2 (int top-y)))]
-      [x y])))
 
-(defn calculate-zoom
-  "calculates the zoom value for camera to see all the 4 points."
-  [camera & {:keys [left top right bottom]}]
-  (let [viewport-width  (viewport-width  camera)
-        viewport-height (viewport-height camera)
-        [px py] (position camera)
-        px (float px)
-        py (float py)
-        leftx (float (left 0))
-        rightx (float (right 0))
-        x-diff (max (- px leftx) (- rightx px))
-        topy (float (top 1))
-        bottomy (float (bottom 1))
-        y-diff (max (- topy py) (- py bottomy))
-        vp-ratio-w (/ (* x-diff 2) viewport-width)
-        vp-ratio-h (/ (* y-diff 2) viewport-height)
-        new-zoom (max vp-ratio-w vp-ratio-h)]
-    new-zoom))
+
+
--- src/moon/gdx/orthographic_camera.clj
+++ src/moon/gdx/orthographic_camera.clj
@@ -1,43 +1,68 @@
 (ns moon.gdx.orthographic-camera
-  (:require [com.badlogic.gdx.math.vector3 :as vector3]
-            [moon.camera :as camera])
+  (:require [com.badlogic.gdx.math.vector3 :as vector3])
   (:import (com.badlogic.gdx.graphics OrthographicCamera)))
 
 (defn create [{:keys [y-down? world-width world-height]}]
   (doto (OrthographicCamera.)
     (.setToOrtho y-down? world-width world-height)))
 
-(extend-type OrthographicCamera
-  camera/OrthographicCamera
-  (combined [camera]
-    (.combined camera))
+(defn combined [^OrthographicCamera camera]
+  (.combined camera))
 
-  (frustum [camera]
-    (let [plane-points (mapv vector3/->clj (.planePoints (.frustum camera)))
-          frustum-points (take 4 plane-points)
-          left-x   (apply min (map first  frustum-points))
-          right-x  (apply max (map first  frustum-points))
-          bottom-y (apply min (map second frustum-points))
-          top-y    (apply max (map second frustum-points))]
-      [left-x right-x bottom-y top-y]))
+(defn frustum [^OrthographicCamera camera]
+  (let [plane-points (mapv vector3/->clj (.planePoints (.frustum camera)))
+        frustum-points (take 4 plane-points)
+        left-x   (apply min (map first  frustum-points))
+        right-x  (apply max (map first  frustum-points))
+        bottom-y (apply min (map second frustum-points))
+        top-y    (apply max (map second frustum-points))]
+    [left-x right-x bottom-y top-y]))
 
-  (position [camera]
-    (vector3/->clj (.position camera)))
+(defn position [^OrthographicCamera camera]
+  (vector3/->clj (.position camera)))
 
-  (viewport-width [camera]
-    (.viewportWidth camera))
+(defn viewport-width [^OrthographicCamera camera]
+  (.viewportWidth camera))
 
-  (viewport-height [camera]
-    (.viewportHeight camera))
+(defn viewport-height [^OrthographicCamera camera]
+  (.viewportHeight camera))
 
-  (zoom [camera]
-    (.zoom camera))
+(defn zoom [^OrthographicCamera camera]
+  (.zoom camera))
 
-  (set-position! [camera [x y]]
-    (set! (.x (.position camera)) x)
-    (set! (.y (.position camera)) y)
-    (.update camera))
+(defn set-position! [^OrthographicCamera camera [x y]]
+  (set! (.x (.position camera)) x)
+  (set! (.y (.position camera)) y)
+  (.update camera))
 
-  (set-zoom! [camera amount]
-    (set! (.zoom camera) amount)
-    (.update camera)))
+(defn set-zoom! [^OrthographicCamera camera amount]
+  (set! (.zoom camera) amount)
+  (.update camera))
+
+(defn inc-zoom! [cam by]
+  (set-zoom! cam (max 0.1 (+ (zoom cam) by))))
+
+(defn visible-tiles [camera]
+  (let [[left-x right-x bottom-y top-y] (frustum camera)]
+    (for [x (range (int left-x)   (int right-x))
+          y (range (int bottom-y) (+ 2 (int top-y)))]
+      [x y])))
+
+(defn calculate-zoom
+  "calculates the zoom value for camera to see all the 4 points."
+  [camera & {:keys [left top right bottom]}]
+  (let [viewport-width  (viewport-width  camera)
+        viewport-height (viewport-height camera)
+        [px py] (position camera)
+        px (float px)
+        py (float py)
+        leftx (float (left 0))
+        rightx (float (right 0))
+        x-diff (max (- px leftx) (- rightx px))
+        topy (float (top 1))
+        bottomy (float (bottom 1))
+        y-diff (max (- topy py) (- py bottomy))
+        vp-ratio-w (/ (* x-diff 2) viewport-width)
+        vp-ratio-h (/ (* y-diff 2) viewport-height)
+        new-zoom (max vp-ratio-w vp-ratio-h)]
+    new-zoom))
