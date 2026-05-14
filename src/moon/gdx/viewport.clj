ec5bf4a5e66465f15f64ac446d8d7366555b3ea0
Michael Sappler <michaelsappler@gmail.com>
2026-05-14 13:11:28 +0530

    Remove comments

--- src/moon/gdx/viewport.clj
+++ src/moon/gdx/viewport.clj
@@ -5,7 +5,6 @@
            (com.badlogic.gdx.utils.viewport FitViewport)))
 
 (defn create [width height camera]
-  ; ScalingViewport Scaling.fit
   (proxy [FitViewport ILookup] [width height camera]
     (valAt [k]
       (case k
@@ -21,31 +20,9 @@
     (.getWorldHeight viewport))
 
   (update! [viewport screen-width screen-height center-camera?]
-
-    ; from ScalingViewport
-
-    ;	public void update (int screenWidth, int screenHeight, boolean centerCamera) {
-    ;		Vector2 scaled = scaling.apply(getWorldWidth(), getWorldHeight(), screenWidth, screenHeight);
-    ;		int viewportWidth = Math.round(scaled.x);
-    ;		int viewportHeight = Math.round(scaled.y);
-    ;
-    ;		// Center.
-    ;		setScreenBounds((screenWidth - viewportWidth) / 2, (screenHeight - viewportHeight) / 2, viewportWidth, viewportHeight);
-    ;
-    ;		apply(centerCamera);
-    ;	}
-
     (.update viewport screen-width screen-height center-camera?))
 
   (unproject [viewport position]
-
-    ; from Viewport
-
-    ;		tmp.set(touchCoords.x, touchCoords.y, 1);
-    ;		camera.unproject(tmp, screenX, screenY, screenWidth, screenHeight);
-    ;		touchCoords.set(tmp.x, tmp.y);
-    ;		return touchCoords;
-
     (-> viewport
         (.unproject (vector2/->java position))
         vector2/->clj)))
