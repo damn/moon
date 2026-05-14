07b0daf4ce129e42d80008229691f265264a044f
Michael Sappler <michaelsappler@gmail.com>
2026-05-14 10:51:26 +0530

    Integrate moon.gdx again (transitive)

--- src/moon/application.clj
+++ src/moon/application.clj
@@ -2,7 +2,15 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.walk :as walk]
-            [moon.gdx :as gdx])
+            [com.badlogic.gdx.graphics.color :as color]
+            [moon.app :as app])
+  (:import (com.badlogic.gdx ApplicationListener
+                             Gdx)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration)
+           (com.badlogic.gdx.graphics Colors
+                                      GL20)
+           (com.badlogic.gdx.scenes.scene2d.ui TooltipManager))
   (:gen-class))
 
 (defn edn-resource [path]
@@ -23,23 +31,80 @@
   (let [{:keys [create-pipeline
                 dispose!
                 render-pipeline
-                resize!] :as config} (edn-resource "game.edn")]
-    (gdx/application!
-     (merge config
-            {:create! (fn [app]
-                        (reset! state
-                                (reduce (fn [ctx [f & params]]
-                                          (apply f ctx params))
-                                        {:ctx/app app}
-                                        create-pipeline)))
-             :dispose! (fn []
-                         (dispose! @state))
-             :render! (fn []
-                        (swap! state
-                               (fn [ctx]
-                                 (reduce (fn [ctx [f & params]]
-                                           (apply f ctx params))
-                                         ctx
-                                         render-pipeline))))
-             :resize! (fn [width height]
-                        (resize! @state width height))}))))
+                resize!
+                title
+                windowed-mode
+                foreground-fps
+                colors
+                ]} (edn-resource "game.edn")]
+    (doseq [[name rgba] colors]
+      (Colors/put name (color/create rgba)))
+    (Lwjgl3ApplicationConfiguration/useGlfwAsync)
+    (Lwjgl3Application. (reify ApplicationListener
+                          (create [_]
+                            (set! (.initialTime (TooltipManager/getInstance)) 0)
+                            (reset! state
+                                    (reduce (fn [ctx [f & params]]
+                                              (apply f ctx params))
+                                            {:ctx/app Gdx/app}
+                                            create-pipeline)))
+
+                          (dispose [_]
+                            (dispose! @state))
+
+                          (render [_]
+                            (swap! state
+                                   (fn [ctx]
+                                     (reduce (fn [ctx [f & params]]
+                                               (apply f ctx params))
+                                             ctx
+                                             render-pipeline))))
+
+                          (resize [_ width height]
+                            (resize! @state width height))
+
+                          (pause [_])
+
+                          (resume [_]))
+                        (doto (Lwjgl3ApplicationConfiguration.)
+                          (.setTitle title)
+                          (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
+                          (.setForegroundFPS foreground-fps)))))
+
+(extend-type Lwjgl3Application
+  app/App
+  (new-sound [app path]
+    (.newSound (.getAudio app)
+               (.internal (.getFiles app) path)))
+
+  (frames-per-second [app]
+    (.getFramesPerSecond (.getGraphics app)))
+
+  (delta-time [app]
+    (.getDeltaTime (.getGraphics app)))
+
+  (new-cursor [app pixmap hotspot-x hotspot-y]
+    (.newCursor (.getGraphics app) pixmap hotspot-x hotspot-y))
+
+  (set-cursor! [app cursor]
+    (.setCursor (.getGraphics app) cursor))
+
+  (clear! [app r g b a]
+    (.glClearColor (.getGL20 (.getGraphics app)) r g b a)
+    (.glClear      (.getGL20 (.getGraphics app)) GL20/GL_COLOR_BUFFER_BIT))
+
+  (set-input-processor! [this input-processor]
+    (.setInputProcessor (.getInput this) input-processor))
+
+  (key-pressed? [this key]
+    (.isKeyPressed (.getInput this) key))
+
+  (key-just-pressed? [this key]
+    (.isKeyJustPressed (.getInput this) key))
+
+  (button-just-pressed? [this button]
+    (.isButtonJustPressed (.getInput this) button))
+
+  (mouse-position [this]
+    [(.getX (.getInput this))
+     (.getY (.getInput this))]))
