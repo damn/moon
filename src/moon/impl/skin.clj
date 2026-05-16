4d9724f03bc7884e3d3f61ed56a29a0eb295d49b
Michael Sappler <michaelsappler@gmail.com>
2026-05-16 17:08:05 +0530

    SKIN

--- src/moon/impl/skin.clj
+++ src/moon/impl/skin.clj
@@ -1,10 +1,15 @@
 (ns moon.impl.skin
   (:require [clojure.gdx.app :as app]
-            [clojure.gdx.files :as files])
-  (:import (com.badlogic.gdx.scenes.scene2d.ui Skin)))
+            [clojure.gdx.files :as files]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font :as font]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.data :as font.data]
+            [com.badlogic.gdx.scenes.scene2d.ui.skin :as skin]))
 
 (defn create
   [{:keys [ctx/app]}]
-  (let [skin (Skin. (files/internal (app/files app) "uiskin.json"))]
-    (set! (.markupEnabled (.getData (.getFont skin "default-font"))) true)
+  (let [skin (skin/create (files/internal (app/files app) "uiskin.json"))]
+    (-> skin
+        (skin/font "default-font")
+        font/data
+        (font.data/set-markup-enabled! true))
     skin))
