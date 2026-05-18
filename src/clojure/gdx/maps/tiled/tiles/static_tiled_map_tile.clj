ff92bfd41c99d896392ee00b29c73e3186d807df
Michael Sappler <michaelsappler@gmail.com>
2026-05-18 15:57:44 +0530

    Remove com.badlogic.gdx.maps.tiled.tiles.static-tiled-map-tile

--- src/clojure/gdx/maps/tiled/tiles/static_tiled_map_tile.clj
+++ src/clojure/gdx/maps/tiled/tiles/static_tiled_map_tile.clj
@@ -1,13 +1,19 @@
 (ns clojure.gdx.maps.tiled.tiles.static-tiled-map-tile
-  (:require [clojure.tiled-map.props :as props]
-            [com.badlogic.gdx.maps.tiled.tiles.static-tiled-map-tile :as tile]))
+  (:require [clojure.tiled-map.tile :as tile]
+            [clojure.tiled-map.props :as props])
+  (:import (com.badlogic.gdx.graphics.g2d TextureRegion)
+           (com.badlogic.gdx.maps.tiled.tiles StaticTiledMapTile)))
 
-(def copy (memoize tile/copy))
+(def copy
+  (memoize
+   (fn [^StaticTiledMapTile tile]
+     (assert tile)
+     (StaticTiledMapTile. tile))))
 
 (defn create
   [texture-region property-name property-value]
   {:pre [texture-region
          (string? property-name)]}
-  (let [tile (tile/create texture-region)]
+  (let [tile (StaticTiledMapTile. ^TextureRegion texture-region)]
     (props/add! (tile/properties tile) {property-name property-value})
     tile))
