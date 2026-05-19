01d800a99d3130a26fe487e267349fb337bf6342
Michael Sappler <michaelsappler@gmail.com>
2026-05-20 00:58:27 +0530

    just create-context

--- src/com/badlogic/gdx/gdx.clj
+++ src/com/badlogic/gdx/gdx.clj
@@ -44,14 +44,10 @@
             [gdl.utils.viewport :as viewport])
   (:import (clojure.lang ILookup)
            (com.badlogic.gdx Application
-                             ApplicationListener
                              Files
                              Gdx
                              Graphics
                              Input)
-           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
-                                             Lwjgl3ApplicationConfiguration)
-           (com.badlogic.gdx.files FileHandle)
            (com.badlogic.gdx.graphics Colors
                                       Pixmap
                                       Pixmap$Format
@@ -110,111 +106,69 @@
   (doto (OrthographicCamera.)
     (.setToOrtho y-down? world-width world-height)))
 
-(def state (atom nil))
-
-(defn start!
-  [{:keys [create
-           dispose
-           render
-           resize
-           title
-           windowed-mode
-           foreground-fps
-           colors]}]
-  (doseq [[name rgba] colors]
-    (Colors/put name (color/create rgba)))
-  (Lwjgl3ApplicationConfiguration/useGlfwAsync)
-  (Lwjgl3Application. (reify ApplicationListener
-                        (create [_]
-                          (set! (.initialTime (TooltipManager/getInstance)) 0)
-                          (reset! state
-                                  (reduce (fn [ctx [f & params]]
-                                            (apply f ctx params))
-                                          (let [batch (SpriteBatch.)
-                                                white-pixel-texture (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
-                                                                                   (.setColor 1 1 1 1)
-                                                                                   (.drawPixel 0 0))
-                                                                          texture (Texture. pixmap)]
-                                                                      (.dispose pixmap)
-                                                                      texture)
-                                                world-unit-scale (float (/ 48))
-                                                ]
-                                            {:ctx/app Gdx/app
-                                             :ctx/audio (into {}
-                                                              (for [sound-name (-> "sounds.edn" io/resource slurp edn/read-string)]
-                                                                [sound-name
-                                                                 (.newSound Gdx/audio
-                                                                            (.internal Gdx/files (format "sounds/%s.wav" sound-name)))]))
-                                             :ctx/batch batch
-                                             :ctx/shape-drawer-texture white-pixel-texture
-                                             :ctx/shape-drawer (ShapeDrawer. batch (texture/region white-pixel-texture 1 0 1 1))
-                                             :ctx/default-font (let [path "exocet/films.EXL_____.ttf"
-                                                                     size 16
-                                                                     quality-scaling 2
-                                                                     generator (FreeTypeFontGenerator. (.internal Gdx/files path))
-                                                                     font (.generateFont generator (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
-                                                                                                     (set! (.size params) (* size quality-scaling))
-                                                                                                     ; Texture$TextureFilter/Linear because scaling to world-units
-                                                                                                     (set! (.minFilter params) Texture$TextureFilter/Linear)
-                                                                                                     (set! (.magFilter params) Texture$TextureFilter/Linear)
-                                                                                                     params))]
-                                                                 (.dispose generator)
-                                                                 (font.data/set-scale! (.getData font) (/ quality-scaling))
-                                                                 (font.data/set-markup-enabled! (.getData font) true)
-                                                                 (.setUseIntegerPositions font false)
-                                                                 font)
-                                             :ctx/world-unit-scale world-unit-scale
-                                             :ctx/world-viewport (let [world-width  (* 1440 world-unit-scale)
-                                                                       world-height (* 900  world-unit-scale)]
-                                                                   (fit-viewport world-width
-                                                                                 world-height
-                                                                                 (orthographic-camera {:y-down? false
-                                                                                                       :world-width world-width
-                                                                                                       :world-height world-height})))
-                                             :ctx/cursors (let [{:keys [data path-format]} (-> "cursors.edn" io/resource slurp edn/read-string)]
-                                                            (update-vals data
-                                                                         (fn [[path [hotspot-x hotspot-y]]]
-                                                                           (let [pixmap (Pixmap. (.internal Gdx/files (format path-format path)))
-                                                                                 cursor (.newCursor Gdx/graphics pixmap hotspot-x hotspot-y)]
-                                                                             (.dispose pixmap)
-                                                                             cursor))))
-                                             :ctx/stage (let [stage (create-stage (fit-viewport 1440 900) batch)]
-                                                          (.setInputProcessor Gdx/input stage)
-                                                          stage)
-                                             :ctx/skin (let [skin (Skin. (.internal Gdx/files "uiskin.json"))]
-                                                         (-> skin
-                                                             (skin/font "default-font")
-                                                             bitmap-font/data
-                                                             (font.data/set-markup-enabled! true))
-                                                         skin)
-                                             :ctx/unit-scale (atom 1)
-                                             :ctx/textures (game.impl.textures/create)
-                                             })
-                                          create)))
-
-                        (dispose [_]
-                          (doseq [f dispose]
-                            (f @state)))
-
-                        (render [_]
-                          (swap! state
-                                 (fn [ctx]
-                                   (reduce (fn [ctx [f & params]]
-                                             (apply f ctx params))
-                                           ctx
-                                           render))))
-
-                        (resize [_ width height]
-                          (doseq [f resize]
-                            (f @state width height)))
-
-                        (pause [_])
-
-                        (resume [_]))
-                      (doto (Lwjgl3ApplicationConfiguration.)
-                        (.setTitle title)
-                        (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
-                        (.setForegroundFPS foreground-fps))))
+(defn create-context
+  [{:keys [colors]}]
+  (let [_ (doseq [[name rgba] colors]
+            (Colors/put name (color/create rgba)))
+        _ (set! (.initialTime (TooltipManager/getInstance)) 0)
+        batch (SpriteBatch.)
+        white-pixel-texture (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
+                                           (.setColor 1 1 1 1)
+                                           (.drawPixel 0 0))
+                                  texture (Texture. pixmap)]
+                              (.dispose pixmap)
+                              texture)
+        world-unit-scale (float (/ 48))]
+    {:ctx/app Gdx/app
+     :ctx/audio (into {}
+                      (for [sound-name (-> "sounds.edn" io/resource slurp edn/read-string)]
+                        [sound-name
+                         (.newSound Gdx/audio
+                                    (.internal Gdx/files (format "sounds/%s.wav" sound-name)))]))
+     :ctx/batch batch
+     :ctx/shape-drawer-texture white-pixel-texture
+     :ctx/shape-drawer (ShapeDrawer. batch (texture/region white-pixel-texture 1 0 1 1))
+     :ctx/default-font (let [path "exocet/films.EXL_____.ttf"
+                             size 16
+                             quality-scaling 2
+                             generator (FreeTypeFontGenerator. (.internal Gdx/files path))
+                             font (.generateFont generator (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
+                                                             (set! (.size params) (* size quality-scaling))
+                                                             ; Texture$TextureFilter/Linear because scaling to world-units
+                                                             (set! (.minFilter params) Texture$TextureFilter/Linear)
+                                                             (set! (.magFilter params) Texture$TextureFilter/Linear)
+                                                             params))]
+                         (.dispose generator)
+                         (font.data/set-scale! (.getData font) (/ quality-scaling))
+                         (font.data/set-markup-enabled! (.getData font) true)
+                         (.setUseIntegerPositions font false)
+                         font)
+     :ctx/world-unit-scale world-unit-scale
+     :ctx/world-viewport (let [world-width  (* 1440 world-unit-scale)
+                               world-height (* 900  world-unit-scale)]
+                           (fit-viewport world-width
+                                         world-height
+                                         (orthographic-camera {:y-down? false
+                                                               :world-width world-width
+                                                               :world-height world-height})))
+     :ctx/cursors (let [{:keys [data path-format]} (-> "cursors.edn" io/resource slurp edn/read-string)]
+                    (update-vals data
+                                 (fn [[path [hotspot-x hotspot-y]]]
+                                   (let [pixmap (Pixmap. (.internal Gdx/files (format path-format path)))
+                                         cursor (.newCursor Gdx/graphics pixmap hotspot-x hotspot-y)]
+                                     (.dispose pixmap)
+                                     cursor))))
+     :ctx/stage (let [stage (create-stage (fit-viewport 1440 900) batch)]
+                  (.setInputProcessor Gdx/input stage)
+                  stage)
+     :ctx/skin (let [skin (Skin. (.internal Gdx/files "uiskin.json"))]
+                 (-> skin
+                     (skin/font "default-font")
+                     bitmap-font/data
+                     (font.data/set-markup-enabled! true))
+                 skin)
+     :ctx/unit-scale (atom 1)
+     :ctx/textures (game.impl.textures/create)}))
 
 (extend-type Application
   app/App
--- src/game/start.clj
+++ src/game/start.clj
@@ -1,7 +1,52 @@
 (ns game.start
   (:require [clojure.config :refer [edn-resource]]
             [com.badlogic.gdx.gdx :as gdx])
+  (:import (com.badlogic.gdx ApplicationListener)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration))
   (:gen-class))
 
+(def state (atom nil))
+
 (defn -main []
-  (gdx/start! (edn-resource "start.edn")))
+  (Lwjgl3ApplicationConfiguration/useGlfwAsync)
+  (let [{:keys [create
+                dispose
+                render
+                resize
+                title
+                windowed-mode
+                foreground-fps
+                colors
+                ]} (edn-resource "start.edn")]
+    (Lwjgl3Application. (reify ApplicationListener
+                          (create [_]
+                            (reset! state
+                                    (reduce (fn [ctx [f & params]]
+                                              (apply f ctx params))
+                                            (gdx/create-context {:colors colors})
+                                            create)))
+
+                          (dispose [_]
+                            (doseq [f dispose]
+                              (f @state)))
+
+                          (render [_]
+                            (swap! state
+                                   (fn [ctx]
+                                     (reduce (fn [ctx [f & params]]
+                                               (apply f ctx params))
+                                             ctx
+                                             render))))
+
+                          (resize [_ width height]
+                            (doseq [f resize]
+                              (f @state width height)))
+
+                          (pause [_])
+
+                          (resume [_]))
+                        (doto (Lwjgl3ApplicationConfiguration.)
+                          (.setTitle title)
+                          (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
+                          (.setForegroundFPS foreground-fps)))))
