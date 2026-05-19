72732a420624bbdbb2d418a0ab0b67ef910ac8d7
Michael Sappler <michaelsappler@gmail.com>
2026-05-19 22:20:55 +0530

    Integrate table

--- src/com/badlogic/gdx/gdx.clj
+++ src/com/badlogic/gdx/gdx.clj
@@ -3,6 +3,8 @@
             [com.badlogic.gdx.math.vector2 :as vector2]
             [com.badlogic.gdx.math.vector3 :as vector3]
             [com.badlogic.gdx.scenes.scene2d.ui.image]
+            [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell]
+            [com.badlogic.gdx.scenes.scene2d.ui.table :as table]
             [com.badlogic.gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
             [gdl.app :as app]
             [gdl.audio :as audio]
@@ -26,6 +28,8 @@
             [gdl.scene2d.ui.label :as label]
             [gdl.scene2d.ui.select-box :as select-box]
             [gdl.scene2d.ui.skin :as skin]
+            [gdl.scene2d.ui.table]
+            [gdl.scene2d.ui.widget-group :as widget-group]
             [gdl.utils.disposable :as disposable]
             [gdl.utils.viewport :as viewport])
   (:import (clojure.lang ILookup)
@@ -493,3 +497,35 @@
   [{:keys [drawable] :as opts}]
   (doto (ImageButton. (texture-region-drawable/create* drawable))
     (actor/set-opts! opts)))
+
+(extend-type com.badlogic.gdx.scenes.scene2d.ui.Table
+  gdl.scene2d.ui.table/Table
+  (add! [table cell-declaration]
+    (-> (table/add! table (:actor cell-declaration))
+        (cell/set-opts! (dissoc cell-declaration :actor))))
+
+  (add-rows! [table rows]
+    (doseq [row rows]
+      (doseq [props-or-actor row]
+
+        (cond
+         (map? props-or-actor)
+         (gdl.scene2d.ui.table/add! table props-or-actor)
+
+         ; TODO Remove else case
+         :else (table/add! table props-or-actor)
+         ))
+      (table/row! table))
+    table)
+
+   (set-opts! [table opts]
+     (when-let [rows (:table/rows opts)]
+       (gdl.scene2d.ui.table/add-rows! table rows)
+       (widget-group/pack! table))
+     (when-let [defaults (:table/cell-defaults opts)]
+       (cell/set-opts! (table/defaults table) defaults))
+     (widget-group/set-opts! table opts)))
+
+(defmethod actor/create :ui/table [opts]
+  (doto (table/create)
+    (gdl.scene2d.ui.table/set-opts! opts)))
