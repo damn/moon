8381262edab133c7e652e9a700cd19d2c8c1ace8
Michael Sappler <michaelsappler@gmail.com>
2026-05-20 17:19:19 +0530

    Split out table

--- src/com/badlogic/gdx.clj
+++ src/com/badlogic/gdx.clj
@@ -21,9 +21,8 @@
             [com.badlogic.gdx.scenes.scene2d.ui.image]
             [com.badlogic.gdx.scenes.scene2d.ui.text-button :as text-button]
             [com.badlogic.gdx.scenes.scene2d.ui.text-field :as text-field]
-            [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell]
             com.badlogic.gdx.scenes.scene2d.ui.skin
-            [com.badlogic.gdx.scenes.scene2d.ui.table :as table]
+            com.badlogic.gdx.scenes.scene2d.ui.table
             [com.badlogic.gdx.scenes.scene2d.ui.tooltip-manager :as tooltip-manager]
             [com.badlogic.gdx.scenes.scene2d.ui.window :as window]
             [com.badlogic.gdx.scenes.scene2d.ui.widget-group]
@@ -38,7 +37,7 @@
             [gdl.scene2d.ui.image :as image]
             [gdl.scene2d.ui.label :as label]
             [gdl.scene2d.ui.select-box :as select-box]
-            [gdl.scene2d.ui.table]
+            [gdl.scene2d.ui.table :as table]
             [gdl.scene2d.ui.text-field]
             [gdl.scene2d.ui.widget-group :as widget-group])
   (:import (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
@@ -124,38 +123,6 @@
   (doto (ImageButton. (texture-region-drawable/create* drawable))
     (actor/set-opts! opts)))
 
-(extend-type com.badlogic.gdx.scenes.scene2d.ui.Table
-  gdl.scene2d.ui.table/Table
-  (add! [table cell-declaration]
-    (-> (table/add! table (:actor cell-declaration))
-        (cell/set-opts! (dissoc cell-declaration :actor))))
-
-  (add-rows! [table rows]
-    (doseq [row rows]
-      (doseq [props-or-actor row]
-
-        (cond
-         (map? props-or-actor)
-         (gdl.scene2d.ui.table/add! table props-or-actor)
-
-         ; TODO Remove else case
-         :else (table/add! table props-or-actor)
-         ))
-      (table/row! table))
-    table)
-
-   (set-opts! [table opts]
-     (when-let [rows (:table/rows opts)]
-       (gdl.scene2d.ui.table/add-rows! table rows)
-       (widget-group/pack! table))
-     (when-let [defaults (:table/cell-defaults opts)]
-       (cell/set-opts! (table/defaults table) defaults))
-     (widget-group/set-opts! table opts)))
-
-(defmethod actor/create :ui/table [opts]
-  (doto (table/create)
-    (gdl.scene2d.ui.table/set-opts! opts)))
-
 (defmethod actor/create :ui/text-button
   [opts]
   (doto (text-button/create opts)
@@ -186,15 +153,15 @@
     (window/set-modal! window true))
 
   (when-let [skin (:window/close-button? opts)]
-    (gdl.scene2d.ui.table/add! (window/title-table window)
-                               {:actor (actor/create
-                                        {:type :ui/text-button
-                                         :text "X"
-                                         :skin skin
-                                         :actor/listeners {:listener/change (fn [_event _actor]
-                                                                              (actor/remove! window))}})}))
+    (table/add! (window/title-table window)
+                {:actor (actor/create
+                         {:type :ui/text-button
+                          :text "X"
+                          :skin skin
+                          :actor/listeners {:listener/change (fn [_event _actor]
+                                                               (actor/remove! window))}})}))
 
-  (gdl.scene2d.ui.table/set-opts! window opts))
+  (table/set-opts! window opts))
 
 (defmethod actor/create :ui/window
   [opts]
--- src/com/badlogic/gdx/scenes/scene2d/ui/table.clj
+++ src/com/badlogic/gdx/scenes/scene2d/ui/table.clj
@@ -1,15 +1,39 @@
 (ns com.badlogic.gdx.scenes.scene2d.ui.table
+  (:require [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell]
+            [gdl.scene2d.actor :as actor]
+            [gdl.scene2d.ui.table :as table]
+            [gdl.scene2d.ui.widget-group :as widget-group])
   (:import (com.badlogic.gdx.scenes.scene2d Actor)
            (com.badlogic.gdx.scenes.scene2d.ui Table)))
 
-(defn create []
-  (Table.))
+(defmethod actor/create :ui/table [opts]
+  (doto (Table.)
+    (table/set-opts! opts)))
 
-(defn add! [^Table table ^Actor actor]
-  (.add table actor))
+(extend-type Table
+  table/Table
+  (add! [table cell-declaration]
+    (-> (.add table ^Actor (:actor cell-declaration))
+        (cell/set-opts! (dissoc cell-declaration :actor))))
 
-(defn row! [^Table table]
-  (.row table))
+  (add-rows! [table rows]
+    (doseq [row rows]
+      (doseq [props-or-actor row]
 
-(defn defaults [^Table table]
-  (.defaults table))
+        (cond
+         (map? props-or-actor)
+         (table/add! table props-or-actor)
+
+         ; TODO Remove else case
+         :else (.add table ^Actor props-or-actor)
+         ))
+      (.row table))
+    table)
+
+  (set-opts! [table opts]
+    (when-let [rows (:table/rows opts)]
+      (table/add-rows! table rows)
+      (widget-group/pack! table))
+    (when-let [defaults (:table/cell-defaults opts)]
+      (cell/set-opts! (.defaults table) defaults))
+    (widget-group/set-opts! table opts)))
