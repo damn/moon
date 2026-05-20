aa748fa323f2530ae10582aab642f46830400b48
Michael Sappler <michaelsappler@gmail.com>
2026-05-20 15:32:03 +0530

    split orthographic-camera

--- src/com/badlogic/gdx.clj
+++ src/com/badlogic/gdx.clj
@@ -1,7 +1,7 @@
 (ns com.badlogic.gdx
   (:require [com.badlogic.gdx.graphics.colors :as colors]
+            [com.badlogic.gdx.graphics.orthographic-camera :as orthographic-camera]
             [com.badlogic.gdx.math.vector2 :as vector2]
-            [com.badlogic.gdx.math.vector3 :as vector3]
             [com.badlogic.gdx.scenes.scene2d.touchable :as touchable]
             [com.badlogic.gdx.scenes.scene2d.ui.image]
             [com.badlogic.gdx.scenes.scene2d.ui.text-button :as text-button]
@@ -32,7 +32,6 @@
             [gdl.graphics.g2d.bitmap-font :as bitmap-font]
             [gdl.graphics.g2d.bitmap-font.data :as font.data]
             [gdl.graphics.g2d.texture-region :as texture-region]
-            [gdl.graphics.orthographic-camera :as camera]
             [gdl.graphics.g2d.freetype.font-generator :as font-generator]
             [gdl.input :as input]
             [gdl.scene2d.actor :as actor]
@@ -64,8 +63,7 @@
            (com.badlogic.gdx.files FileHandle)
            (com.badlogic.gdx.graphics Pixmap
                                       Pixmap$Format
-                                      Texture
-                                      OrthographicCamera)
+                                      Texture)
            (com.badlogic.gdx.graphics.g2d BitmapFont
                                           BitmapFont$BitmapFontData
                                           SpriteBatch
@@ -105,12 +103,7 @@
     (pixmap/dispose! pixmap)
     texture))
 
-(defn orthographic-camera
-  [{:keys [y-down?
-           world-width
-           world-height]}]
-  (doto (OrthographicCamera.)
-    (.setToOrtho y-down? world-width world-height)))
+(def orthographic-camera orthographic-camera/create)
 
 (defn sprite-batch []
   (SpriteBatch.))
@@ -558,61 +551,6 @@
   check-box/CheckBox
   (checked? [this]
     (.isChecked this)))
-
-(extend-type OrthographicCamera
-  camera/OrthographicCamera
-  (combined [camera]
-    (.combined camera))
-
-  (zoom [camera]
-    (.zoom camera))
-
-  (frustum [camera]
-    (let [plane-points (mapv vector3/->clj (.planePoints (.frustum camera)))
-          frustum-points (take 4 plane-points)
-          left-x   (apply min (map first  frustum-points))
-          right-x  (apply max (map first  frustum-points))
-          bottom-y (apply min (map second frustum-points))
-          top-y    (apply max (map second frustum-points))]
-      [left-x right-x bottom-y top-y]))
-
-  (position [camera]
-    (vector3/->clj (.position camera)))
-
-  (set-position! [camera [x y]]
-    (set! (.x (.position camera)) x)
-    (set! (.y (.position camera)) y)
-    (.update camera))
-
-  (set-zoom! [camera amount]
-    (set! (.zoom camera) amount)
-    (.update camera))
-
-  (inc-zoom! [cam by]
-    (camera/set-zoom! cam (max 0.1 (+ (camera/zoom cam) by))))
-
-  (visible-tiles [camera]
-    (let [[left-x right-x bottom-y top-y] (camera/frustum camera)]
-      (for [x (range (int left-x)   (int right-x))
-            y (range (int bottom-y) (+ 2 (int top-y)))]
-        [x y])))
-
-  (calculate-zoom [camera {:keys [left top right bottom]}]
-    (let [viewport-width  (.viewportWidth  camera)
-          viewport-height (.viewportHeight camera)
-          [px py] (camera/position camera)
-          px (float px)
-          py (float py)
-          leftx (float (left 0))
-          rightx (float (right 0))
-          x-diff (max (- px leftx) (- rightx px))
-          topy (float (top 1))
-          bottomy (float (bottom 1))
-          y-diff (max (- topy py) (- py bottomy))
-          vp-ratio-w (/ (* x-diff 2) viewport-width)
-          vp-ratio-h (/ (* y-diff 2) viewport-height)
-          new-zoom (max vp-ratio-w vp-ratio-h)]
-      new-zoom)))
 
 (extend-type SpriteBatch
   batch/Batch
