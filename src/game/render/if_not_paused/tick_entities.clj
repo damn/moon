f06c4ee0fefd6b47317d0fd9704567768054f6d1
Michael Sappler <michaelsappler@gmail.com>
2026-05-22 04:55:52 +0530

    separate game.entity.movement

--- src/game/render/if_not_paused/tick_entities.clj
+++ src/game/render/if_not_paused/tick_entities.clj
@@ -10,7 +10,6 @@
             [moon.grid :as grid]
             [moon.grid2d :as g2d]
             [moon.raycaster :as raycaster]
-            [moon.number :as number]
             [moon.skill :as skill]
             [moon.stats :as stats]
             [moon.timer :as timer]
@@ -18,24 +17,6 @@
             [moon.txs :as txs]))
 
 ;;; PRIVATE START
-
-(defn- move-position [position {:keys [direction speed delta-time]}]
-  (mapv #(+ %1 (* %2 speed delta-time)) position direction))
-
-(defn- move-body [body movement]
-  (update body :body/position move-position movement))
-
-(defn- try-move [grid body entity-id movement]
-  (let [new-body (move-body body movement)]
-    (when (grid/valid-position? grid new-body entity-id)
-      new-body)))
-
-(defn- try-move-solid-body [grid body entity-id {[vx vy] :direction :as movement}]
-  (let [xdir (math/signum (float vx))
-        ydir (math/signum (float vy))]
-    (or (try-move grid body entity-id movement)
-        (try-move grid body entity-id (assoc movement :direction [xdir 0]))
-        (try-move grid body entity-id (assoc movement :direction [0 ydir])))))
 
 (defn- npc-effect-ctx
   [{:keys [ctx/grid
@@ -73,35 +54,6 @@
     (dissoc effect-ctx :effect/target)))
 
 ;;; PRIVATE END
-
-(defmethod entity/tick :entity/movement
-  [[_k
-    {:keys [direction
-            speed
-            rotate-in-movement-direction?]
-     :as movement}]
-   eid
-   {:keys [ctx/delta-time
-           ctx/grid
-           ctx/max-speed]}]
-  (assert (<= 0 speed max-speed)
-          (pr-str speed))
-  (assert (vector? direction))
-  (assert (or (zero? (v/length direction))
-              (number/nearly-equal? 1 (v/length direction)))
-          (str "cannot understand direction: " (pr-str direction)))
-  (when-not (or (zero? (v/length direction))
-                (nil? speed)
-                (zero? speed))
-    (let [movement (assoc movement :delta-time delta-time)
-          body (:entity/body @eid)]
-      (when-let [body (if (:body/collides? body)
-                        (try-move-solid-body grid body (:entity/id @eid) movement)
-                        (move-body body movement))]
-        [[:tx/assoc-in eid [:entity/body :body/position] (:body/position body)]
-         (when rotate-in-movement-direction?
-           [:tx/assoc-in eid [:entity/body :body/rotation-angle] (v/angle-from-vector direction)])
-         [:tx/move-entity eid]]))))
 
 (defmethod entity/tick :npc-sleeping
   [_ eid {:keys [ctx/grid]}]
