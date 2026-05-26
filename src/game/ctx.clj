c5425efdf78c619abde05dfe5016490cb49d1657
Michael Sappler <michaelsappler@gmail.com>
2026-05-26 06:31:36 +0530

    :ctx/cursors into game.ctx

--- src/game/ctx.clj
+++ src/game/ctx.clj
@@ -15,7 +15,8 @@
             [malli.core :as m]
             [malli.utils :as mu]
             [moon.draws :as draws]
-            [moon.raycaster :as raycaster]))
+            [moon.raycaster :as raycaster]
+            [moon.state :as state]))
 
 (def schema
   (m/schema
@@ -97,9 +98,76 @@
     (gl20/clear-color! gl 0 0 0 0)
     (gl20/clear! gl gl20/color-buffer-bit)))
 
+(defmethod state/cursor :player-dead
+  [_ _eid _ctx]
+  :cursors/black-x)
+
+(defmethod state/cursor :active-skill
+  [_ _eid _ctx]
+  :cursors/sandclock)
+
+(defmethod state/cursor :stunned
+  [_ _eid _ctx]
+  :cursors/denied)
+
+(defmethod state/cursor :player-moving
+  [_ _eid _ctx]
+  :cursors/walking)
+
+(defmethod state/cursor :player-idle
+  [_ eid {:keys [ctx/interaction-state]}]
+  (let [[k params] interaction-state]
+    (case k
+      :interaction-state/mouseover-actor
+      (let [[actor-type params] params
+            inventory-cell-with-item? (and (= actor-type :mouseover-actor/inventory-cell)
+                                           (let [inventory-slot params]
+                                             (get-in (:entity/inventory @eid) inventory-slot)))]
+        (cond
+         inventory-cell-with-item?
+         :cursors/hand-before-grab
+
+         (= actor-type :mouseover-actor/window-title-bar)
+         :cursors/move-window
+
+         (= actor-type :mouseover-actor/button)
+         :cursors/over-button
+
+         (= actor-type :mouseover-actor/unspecified)
+         :cursors/default
+
+         :else
+         :cursors/default))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/item (if in-click-range?
+                            :cursors/hand-before-grab
+                            :cursors/hand-before-grab-gray)
+          :clickable/player :cursors/bag))
+
+      :interaction-state.skill/usable
+      :cursors/use-skill
+
+      :interaction-state.skill/not-usable
+      :cursors/skill-not-usable
+
+      :interaction-state/no-skill-selected
+      :cursors/no-skill-selected)))
+
 (defn set-cursor!
-  [{:keys [ctx/app]} cursor]
-  (graphics/set-cursor! (app/graphics app) cursor))
+  [{:keys [ctx/app
+           ctx/cursors
+           ctx/player-eid]
+    :as ctx}]
+  (let [eid player-eid
+        entity @eid
+        state-k (:state (:entity/fsm entity))
+        cursor-key (state/cursor [state-k (state-k entity)] eid ctx)]
+    (assert (contains? cursors cursor-key))
+    (graphics/set-cursor! (app/graphics app) (get cursors cursor-key))))
 
 (defn key-pressed?
   [{:keys [ctx/app]} input-key]
