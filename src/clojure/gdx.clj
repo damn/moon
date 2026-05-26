ce08aaf9cbd97f6418d6a6776b44f7e2e0053ec2
Michael Sappler <michaelsappler@gmail.com>
2026-05-26 07:30:21 +0530

    Integrate backend code

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -1,42 +1,45 @@
 (ns clojure.gdx
   (:require [clojure.edn :as edn]
-            [clojure.gdx.backends.lwjgl3.application :as application]
-            [clojure.gdx.backends.lwjgl3.application-configuration :as config]
             [clojure.java.io :as io])
-  (:import (com.badlogic.gdx ApplicationListener)))
+  (:import (com.badlogic.gdx ApplicationListener)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration)))
 
-(defn create-listener
-  [{:keys [create!
+(defn application!
+  [{:keys [title
+           windowed-mode
+           foreground-fps
+           create!
            dispose!
            render!
            resize!
            pause!
            resume!]}]
-  (reify ApplicationListener
-    (create [_]
-      (create!))
-
-    (dispose [_]
-      (dispose!))
-
-    (render [_]
-      (render!))
-
-    (resize [_ width height]
-      (resize! width height))
-
-    (pause [_]
-      (pause!))
-
-    (resume [_]
-      (resume!))))
-
-(defn application! [config]
   (->> "clojure.gdx.edn"
        io/resource
        slurp
        edn/read-string
        (run! require))
-  (config/use-glfw-async!)
-  (application/create (create-listener config)
-                      (config/create config)))
+  (Lwjgl3ApplicationConfiguration/useGlfwAsync)
+  (Lwjgl3Application. (reify ApplicationListener
+                        (create [_]
+                          (create!))
+
+                        (dispose [_]
+                          (dispose!))
+
+                        (render [_]
+                          (render!))
+
+                        (resize [_ width height]
+                          (resize! width height))
+
+                        (pause [_]
+                          (pause!))
+
+                        (resume [_]
+                          (resume!)))
+                      (doto (Lwjgl3ApplicationConfiguration.)
+                        (.setTitle title)
+                        (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
+                        (.setForegroundFPS foreground-fps))))
--- src/clojure/gdx/backends/lwjgl3/application.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure.gdx.backends.lwjgl3.application
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)))
-
-(defn create
-  [listener config]
-  (Lwjgl3Application. listener config))
