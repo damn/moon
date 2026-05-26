a350bcd6a0d7d8ab59679bf37e4e9100bb5801f0
Michael Sappler <michaelsappler@gmail.com>
2026-05-26 06:55:54 +0530

    impl-txs into game.ctx

--- src/game/ctx.clj
+++ src/game/ctx.clj
@@ -39,9 +39,74 @@
             [moon.draws :as draws]
             [moon.raycaster :as raycaster]
             [moon.state :as state]
+            [moon.txs :as txs]
             [qrecord.core :as q]))
 
+(defn- actions!
+  [txs-fn-map ctx txs]
+  (loop [ctx ctx
+         txs txs
+         handled-txs []]
+    (if (empty? txs)
+      handled-txs
+      (let [[k & params :as tx] (first txs)]
+        (if tx
+          (let [_ (assert (vector? tx))
+                f (get txs-fn-map k)
+                _ (assert f (str "Cannot find function for tx: " k))
+                new-txs (try
+                         (apply f ctx params)
+                         (catch Throwable t
+                           (throw (ex-info "Error handling tx"
+                                           {:tx tx}
+                                           t))))]
+
+            ; TODO VALID RETURNS -> check!
+            ; either nil? or vector of transactions (vectors = vector with keyword & params?)
+            (recur ctx
+                   (concat (or new-txs []) (rest txs))
+                   (conj handled-txs tx)))
+          (recur ctx
+                 (rest txs)
+                 handled-txs))))))
+
+(defn- reduce-actions!
+  [txs-fn-map ctx txs]
+  (loop [ctx ctx
+         txs txs]
+    (if (empty? txs)
+      ctx
+      (let [[k & params :as tx] (first txs)]
+        (if tx
+          (let [_ (assert (vector? tx))
+                f (get txs-fn-map k)
+                new-ctx (try
+                         (if (nil? f)
+                           ctx
+                           (apply f ctx params))
+                         (catch Throwable t
+                           (throw (ex-info "Error handling tx"
+                                           {:tx tx}
+                                           t))))]
+            (recur new-ctx
+                   (rest txs)))
+          (recur ctx
+                 (rest txs)))))))
+
 (q/defrecord Context [])
+
+(defn impl-txs! [ctx {:keys [txs-fn-map reaction-txs-fn-map]}]
+  (extend-type Context
+    txs/Txs
+    (handle! [ctx txs]
+      (let [handled-txs (try (actions! txs-fn-map ctx txs)
+                             (catch Throwable t
+                               (throw (ex-info "Error handling txs"
+                                               {:txs txs} t))))]
+        (reduce-actions! reaction-txs-fn-map
+                         ctx
+                         handled-txs))))
+  ctx)
 
 (defn create-record [ctx]
   (merge (map->Context {}) ctx))
