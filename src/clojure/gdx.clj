c05b2da2abf56ca53efd85195c2c4296e06849e1
Michael Sappler <michaelsappler@gmail.com>
2026-05-26 07:26:41 +0530

    Integrate ApplicationListener

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -1,9 +1,35 @@
 (ns clojure.gdx
   (:require [clojure.edn :as edn]
-            [clojure.gdx.application-listener :as listener]
             [clojure.gdx.backends.lwjgl3.application :as application]
             [clojure.gdx.backends.lwjgl3.application-configuration :as config]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import (com.badlogic.gdx ApplicationListener)))
+
+(defn create-listener
+  [{:keys [create!
+           dispose!
+           render!
+           resize!
+           pause!
+           resume!]}]
+  (reify ApplicationListener
+    (create [_]
+      (create!))
+
+    (dispose [_]
+      (dispose!))
+
+    (render [_]
+      (render!))
+
+    (resize [_ width height]
+      (resize! width height))
+
+    (pause [_]
+      (pause!))
+
+    (resume [_]
+      (resume!))))
 
 (defn application! [config]
   (->> "clojure.gdx.edn"
@@ -12,5 +38,5 @@
        edn/read-string
        (run! require))
   (config/use-glfw-async!)
-  (application/create (listener/create config)
+  (application/create (create-listener config)
                       (config/create config)))
