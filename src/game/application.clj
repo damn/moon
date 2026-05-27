f90bd9fa128aabaedd272b4d4c9c87670293763a
Michael Sappler <michaelsappler@gmail.com>
2026-05-27 12:32:23 +0530

    state enter/exit to moon.state

--- src/game/application.clj
+++ src/game/application.clj
@@ -121,49 +121,6 @@
 
 (q/defrecord Entity [entity/body])
 
-(defmethod state/enter :active-skill
-  [[_k {:keys [skill]}] eid]
-  [[:tx/sound (:skill/start-action-sound skill)]
-   [:tx/set-cooldown eid skill]
-   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
-
-(defmethod state/enter :npc-dead
-  [_ eid]
-  [[:tx/mark-destroyed eid]])
-
-(defmethod state/enter :player-moving
-  [[_k {:keys [movement-vector]}] eid]
-  [[:tx/assoc eid :entity/movement {:direction movement-vector
-                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
-                                               0)}]])
-
-(defmethod state/enter :player-dead
-  [_ _eid]
-  [[:tx/sound "bfxr_playerdeath"]
-   [:tx/show-modal {:title "YOU DIED - again!"
-                    :text "Good luck next time!"
-                    :button-text "OK"
-                    :on-click (fn [])}]])
-
-(defmethod state/enter :npc-moving
-  [[_k {:keys [movement-vector]}] eid]
-  [[:tx/assoc eid :entity/movement {:direction movement-vector
-                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
-                                               0)}]])
-
-(defmethod state/exit :player-moving
-  [_ eid _ctx]
-  [[:tx/dissoc eid :entity/movement]])
-
-(defmethod state/exit :npc-sleeping
-  [_ eid _ctx]
-  [[:tx/spawn-alert (:body/position (:entity/body @eid)) (:entity/faction @eid) 0.2]
-   [:tx/add-text-effect eid "[WHITE]!" 1]])
-
-(defmethod state/exit :npc-moving
-  [_ eid _ctx]
-  [[:tx/dissoc eid :entity/movement]])
-
 (def txs-fn-map
   {
    :tx/state-exit               (fn [ctx eid [state-k state-v]]
--- src/moon/state.clj
+++ src/moon/state.clj
@@ -182,3 +182,46 @@
 (defmethod cursor :player-item-on-cursor
   [_ _eid _ctx]
   :cursors/hand-grab)
+
+(defmethod enter :active-skill
+  [[_k {:keys [skill]}] eid]
+  [[:tx/sound (:skill/start-action-sound skill)]
+   [:tx/set-cooldown eid skill]
+   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
+
+(defmethod enter :npc-dead
+  [_ eid]
+  [[:tx/mark-destroyed eid]])
+
+(defmethod enter :player-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+(defmethod enter :player-dead
+  [_ _eid]
+  [[:tx/sound "bfxr_playerdeath"]
+   [:tx/show-modal {:title "YOU DIED - again!"
+                    :text "Good luck next time!"
+                    :button-text "OK"
+                    :on-click (fn [])}]])
+
+(defmethod enter :npc-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+(defmethod exit :player-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
+
+(defmethod exit :npc-sleeping
+  [_ eid _ctx]
+  [[:tx/spawn-alert (:body/position (:entity/body @eid)) (:entity/faction @eid) 0.2]
+   [:tx/add-text-effect eid "[WHITE]!" 1]])
+
+(defmethod exit :npc-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
