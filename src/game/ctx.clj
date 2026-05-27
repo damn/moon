8a169c3ae6922444025af50fb79977977cf2d185
Michael Sappler <michaelsappler@gmail.com>
2026-05-27 10:34:09 +0530

    integrate draw on world viewport

--- src/game/ctx.clj
+++ src/game/ctx.clj
@@ -10,6 +10,7 @@
             [clojure.gdx :as gdx]
             [clojure.graphics :as graphics]
             [clojure.graphics.batch :as batch]
+            [clojure.graphics.color :as color]
             [clojure.graphics.texture :as texture]
             [clojure.graphics.texture.filter :as texture.filter]
             [clojure.graphics.pixmap :as pixmap]
@@ -58,6 +59,7 @@
             [moon.map :as map]
             [moon.number :as number]
             [moon.raycaster :as raycaster]
+            [moon.throwable :as throwable]
             [moon.state :as state]
             [moon.stats :as stats]
             [moon.skill :as skill]
@@ -1925,9 +1927,109 @@
   (stage/draw! stage)
   (:stage/ctx stage))
 
-(require 'game.render.draw-on-world-viewport.draw-cell-debug)
-(require 'game.render.draw-on-world-viewport.draw-entities)
-(require 'game.render.draw-on-world-viewport.highlight-mouseover-tile)
+(defn draw-tile-grid
+  [ctx]
+  (let [[left-x _right-x bottom-y _top-y] (camera-frustum ctx)]
+    [[:draw/grid
+      (int left-x)
+      (int bottom-y)
+      (inc (int (world-viewport-width ctx)))
+      (+ 2 (int (world-viewport-height ctx)))
+      1
+      1
+      (color/float-bits [1 1 1 0.8])]]))
+
+(def ^:dbg-flag show-potential-field-colors? false) ; :good, :evil
+(def ^:dbg-flag show-cell-entities? false)
+(def ^:dbg-flag show-cell-occupied? false)
+
+(defn draw-cell-debug
+  [{:keys [ctx/colors
+           ctx/grid
+           ctx/factions-iterations]
+    :as ctx}]
+  (apply concat
+         (for [[x y] (visible-tiles ctx)
+               :let [cell (grid [x y])]
+               :when cell
+               :let [cell* @cell]]
+           [(when (and show-cell-entities? (seq (:entities cell*)))
+              [:draw/filled-rectangle x y 1 1 (:colors/debug-cell-entities colors)])
+            (when (and show-cell-occupied? (seq (:occupied cell*)))
+              [:draw/filled-rectangle x y 1 1 (:colors/debug-cell-occupied colors)])
+            (when-let [faction show-potential-field-colors?]
+              (let [{:keys [distance]} (faction cell*)]
+                (when distance
+                  (let [ratio (/ distance (factions-iterations faction))]
+                    [:draw/filled-rectangle x y 1 1 ((:colors/debug-potential-field colors) ratio)]))))])))
+
+(def ^:private render-layers
+  [#{:entity/mouseover?
+     :stunned
+     :player-item-on-cursor}
+   #{:entity/clickable
+     :entity/animation
+     :entity/image
+     :entity/line-render}
+   #{:npc-sleeping
+     :entity/temp-modifier
+     :entity/string-effect}
+   #{:entity/stats
+     :active-skill}])
+
+(def ^:dbg-flag show-body-bounds? false)
+
+(defn- draw-body-rect [{:keys [body/position body/width body/height]} color-float-bits]
+  (let [[x y] [(- (position 0) (/ width  2))
+               (- (position 1) (/ height 2))]]
+    [[:draw/rectangle x y width height color-float-bits]]))
+
+(defn- draw-entity
+  [{:keys [ctx/colors] :as ctx} entity render-layer]
+  (try (do
+        (when show-body-bounds?
+          (draws/handle ctx (draw-body-rect (:entity/body entity)
+                                             (if (:body/collides? (:entity/body entity))
+                                               (:colors/debug-body-outline-collides colors)
+                                               (:colors/debug-body-outline colors)))))
+        (doseq [[k v] entity
+                :when (get render-layer k)]
+          (draws/handle ctx (entity/render [k v] entity ctx))))
+       (catch Throwable t
+         (draws/handle ctx (draw-body-rect (:entity/body entity) (:colors/debug-body-outline-render-error colors)))
+         (throwable/pretty-pst t))))
+
+(defn draw-entities!
+  [{:keys [ctx/active-entities
+           ctx/player-eid
+           ctx/raycaster
+           ctx/render-z-order]
+    :as ctx}]
+  (let [entities (map deref active-entities)
+        player @player-eid
+        should-draw? (fn [entity z-order]
+                       (or (= z-order :z-order/effect)
+                           (raycaster/line-of-sight? raycaster player entity)))]
+    (doseq [[z-order entities] (order/sort-by-order (group-by (comp :body/z-order :entity/body) entities)
+                                                    first
+                                                    render-z-order)
+            render-layer render-layers
+            entity entities
+            :when (should-draw? entity z-order)]
+      (draw-entity ctx entity render-layer))))
+
+(defn highlight-mouseover-tile
+  [{:keys [ctx/colors
+           ctx/grid
+           ctx/world-mouse-position]}]
+  (let [[x y] (mapv int world-mouse-position)
+        cell (grid [x y])]
+    (when (and cell (#{:air :none} (:movement @cell)))
+      [[:draw/rectangle x y 1 1
+        (case (:movement @cell)
+          :air  (:colors/mouseover-tile-air colors)
+          :none (:colors/mouseover-tile-none colors))]])))
+
 (require 'game.render.if-not-paused.update-time)
 (require 'game.render.if-not-paused.update-potential-fields)
 (require 'game.render.if-not-paused.tick-entities)
@@ -1947,11 +2049,11 @@
            [clear-screen!]
            [draw-tiled-map!]
            [draw-on-world-viewport! [
-                                     #_game.render.draw-on-world-viewport.draw-tile-grid/draws
-                                     game.render.draw-on-world-viewport.draw-cell-debug/draws
-                                     game.render.draw-on-world-viewport.draw-entities/do!
+                                     #_draw-tile-grid
+                                     draw-cell-debug
+                                     draw-entities!
                                      #_moon.geom-test
-                                     game.render.draw-on-world-viewport.highlight-mouseover-tile/draws
+                                     highlight-mouseover-tile
                                      ]]
            [assoc-interaction-state]
            [set-cursor!]
