e6e20c784d47349cfd4d1a7bfdac58ca58a311b9
Michael Sappler <michaelsappler@gmail.com>
2026-05-27 09:18:52 +0530

    defmethod entity/create :entity/body to moon.entity

--- src/game/application.clj
+++ src/game/application.clj
@@ -52,75 +52,8 @@
             game.ui.dev-menu
             game.ui.action-bar
             game.ui.info-window
-            [clojure.gdx :as gdx]
-            [qrecord.core :as q])
-  (:import (com.badlogic.gdx.math Rectangle))
+            [clojure.gdx :as gdx])
   (:gen-class))
-
-(q/defrecord Body [body/position
-                   body/width
-                   body/height
-                   body/collides?
-                   body/z-order
-                   body/rotation-angle]
-  body/Body
-  (rectangle
-    [{:keys [body/position
-             body/width
-             body/height]}]
-    (let [[x y] [(- (position 0) (/ width  2))
-                 (- (position 1) (/ height 2))]]
-      (Rectangle. x y width height)))
-
-  (touched-tiles
-    [{:keys [body/position
-             body/width
-             body/height]}]
-    (rectangle/touched-tiles
-     {:x (- (position 0) (/ width  2))
-      :y (- (position 1) (/ height 2))
-      :width  width
-      :height height}))
-
-  (overlaps? [body other-body]
-    (.overlaps ^Rectangle (body/rectangle body)
-               ^Rectangle (body/rectangle other-body)))
-
-  (distance [body other-body]
-    (v/distance (:body/position body)
-                (:body/position other-body)))
-
-  (direction [body other-body]
-    (v/direction (:body/position body)
-                 (:body/position other-body))))
-
-(defmethod entity/create :entity/body
-  [[_k
-    {[x y] :position
-     :keys [position
-            width
-            height
-            collides?
-            z-order
-            rotation-angle]}]
-   {:keys [ctx/minimum-size
-           ctx/z-orders]}]
-  (assert position)
-  (assert width)
-  (assert height)
-  (assert (>= width  (if collides? minimum-size 0)))
-  (assert (>= height (if collides? minimum-size 0)))
-  (assert (or (boolean? collides?) (nil? collides?)))
-  (assert ((set z-orders) z-order))
-  (assert (or (nil? rotation-angle)
-              (<= 0 rotation-angle 360)))
-  (map->Body
-   {:position (mapv float position)
-    :width  (float width)
-    :height (float height)
-    :collides? collides?
-    :z-order z-order
-    :rotation-angle (or rotation-angle 0)}))
 
 (defmethod entity/render :entity/clickable
   [[_k {:keys [text]}]
--- src/moon/entity.clj
+++ src/moon/entity.clj
@@ -1,4 +1,9 @@
-(ns moon.entity)
+(ns moon.entity
+  (:require [clojure.math.rectangle :as rectangle]
+            [clojure.math.vector2 :as v]
+            [moon.body :as body]
+            [qrecord.core :as q])
+  (:import (com.badlogic.gdx.math Rectangle)))
 
 (defmulti create
   (fn [[k _v] _ctx]
@@ -35,3 +40,68 @@
 (defmulti render
   (fn [[k _v] _entity _ctx]
     k))
+
+(q/defrecord Body [body/position
+                   body/width
+                   body/height
+                   body/collides?
+                   body/z-order
+                   body/rotation-angle]
+  body/Body
+  (rectangle
+    [{:keys [body/position
+             body/width
+             body/height]}]
+    (let [[x y] [(- (position 0) (/ width  2))
+                 (- (position 1) (/ height 2))]]
+      (Rectangle. x y width height)))
+
+  (touched-tiles
+    [{:keys [body/position
+             body/width
+             body/height]}]
+    (rectangle/touched-tiles
+     {:x (- (position 0) (/ width  2))
+      :y (- (position 1) (/ height 2))
+      :width  width
+      :height height}))
+
+  (overlaps? [body other-body]
+    (.overlaps ^Rectangle (body/rectangle body)
+               ^Rectangle (body/rectangle other-body)))
+
+  (distance [body other-body]
+    (v/distance (:body/position body)
+                (:body/position other-body)))
+
+  (direction [body other-body]
+    (v/direction (:body/position body)
+                 (:body/position other-body))))
+
+(defmethod create :entity/body
+  [[_k
+    {[x y] :position
+     :keys [position
+            width
+            height
+            collides?
+            z-order
+            rotation-angle]}]
+   {:keys [ctx/minimum-size
+           ctx/z-orders]}]
+  (assert position)
+  (assert width)
+  (assert height)
+  (assert (>= width  (if collides? minimum-size 0)))
+  (assert (>= height (if collides? minimum-size 0)))
+  (assert (or (boolean? collides?) (nil? collides?)))
+  (assert ((set z-orders) z-order))
+  (assert (or (nil? rotation-angle)
+              (<= 0 rotation-angle 360)))
+  (map->Body
+   {:position (mapv float position)
+    :width  (float width)
+    :height (float height)
+    :collides? collides?
+    :z-order z-order
+    :rotation-angle (or rotation-angle 0)}))
