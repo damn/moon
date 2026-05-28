f2c6b416e1dfce3f97c6f383138ee1acd34b43e0
Michael Sappler <michaelsappler@gmail.com>
2026-05-28 18:02:05 +0530

    defmethods together

--- src/game/state.clj
+++ src/game/state.clj
@@ -7,6 +7,13 @@
             [moon.stats :as stats]
             [moon.timer :as timer]))
 
+(def reaction-time-multiplier 0.016)
+
+(defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
+  (/ action-time
+     (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
+         1)))
+
 (defmulti create
   (fn [[k _v] _eid _ctx]
     k))
@@ -14,59 +21,6 @@
 (defmethod create :default
   [[_k v] _eid _ctx]
   v)
-
-(defmulti enter
-  (fn [[k _v] _eid]
-    k))
-
-(defmethod enter :default
-  [[_k _v] _eid]
-  nil)
-
-(defmulti exit
-  (fn [[k _v] _eid _ctx]
-    k))
-
-(defmethod exit :default
-  [[_k _v] _eid _ctx]
-  nil)
-
-(defmulti cursor
-  (fn [[k _v] _eid _ctx]
-    k))
-
-(defmulti pause-game?
-  (fn [k]
-    k))
-
-(defmulti clicked-inventory-cell
-  (fn [[k _v] _eid _cell]
-    k))
-
-(defmethod clicked-inventory-cell :default
-  [_ _eid _cell]
-  nil)
-
-(defmulti draw-ui-view
-  (fn [[k _v] _eid _ctx]
-    k))
-
-(defmethod draw-ui-view :default
-  [_ _eid _ctx]
-  nil)
-
-(defmulti handle-input
-  (fn [[k _v] _eid _ctx]
-    k))
-
-(defmethod handle-input :default
-  [_ _eid _ctx]
-  nil)
-
-(defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
-  (/ action-time
-     (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
-         1)))
 
 (defmethod create :active-skill
   [[_k [skill effect-ctx]] eid {:keys [ctx/elapsed-time]}]
@@ -85,8 +39,6 @@
   [[_k movement-vector] eid _ctx]
   {:movement-vector movement-vector})
 
-(def reaction-time-multiplier 0.016)
-
 (defmethod create :npc-moving
   [[_k movement-vector] eid {:keys [ctx/elapsed-time]}]
   {:movement-vector movement-vector
@@ -98,9 +50,55 @@
   [[_k item] _eid _ctx]
   {:item item})
 
+(defmulti enter
+  (fn [[k _v] _eid]
+    k))
+
+(defmethod enter :default
+  [[_k _v] _eid]
+  nil)
+
 (defmethod enter :player-item-on-cursor
   [[_k {:keys [item]}] eid]
   [[:tx/assoc eid :entity/item-on-cursor item]])
+
+(defmethod enter :active-skill
+  [[_k {:keys [skill]}] eid]
+  [[:tx/sound (:skill/start-action-sound skill)]
+   [:tx/set-cooldown eid skill]
+   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
+
+(defmethod enter :npc-dead
+  [_ eid]
+  [[:tx/mark-destroyed eid]])
+
+(defmethod enter :player-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+(defmethod enter :player-dead
+  [_ _eid]
+  [[:tx/sound "bfxr_playerdeath"]
+   [:tx/show-modal {:title "YOU DIED - again!"
+                    :text "Good luck next time!"
+                    :button-text "OK"
+                    :on-click (fn [])}]])
+
+(defmethod enter :npc-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+(defmulti exit
+  (fn [[k _v] _eid _ctx]
+    k))
+
+(defmethod exit :default
+  [[_k _v] _eid _ctx]
+  nil)
 
 (defmethod exit :player-item-on-cursor
   [_ eid ctx]
@@ -116,19 +114,42 @@
         (ctx/item-place-position ctx entity)
         (:entity/item-on-cursor entity)]])))
 
-(defmethod draw-ui-view :player-item-on-cursor
-  [_ eid {:keys [ctx/stage
-                 ctx/textures
-                 ctx/ui-mouse-position]
-          :as ctx}]
-  ; TODO see player-item-on-cursor at render layers
-  ; always draw it here at right position, then render layers does not need input/stage
-  ; can pass world to graphics, not handle here at application
-  (when (stage/mouseover-actor stage (ctx/mouse-position ctx))
-    [[:draw/texture-region
-      (textures/texture-region textures (:entity/image (:entity/item-on-cursor @eid)))
-      ui-mouse-position
-      {:center? true}]]))
+(defmethod exit :player-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
+
+(defmethod exit :npc-sleeping
+  [_ eid _ctx]
+  [[:tx/spawn-alert (:body/position (:entity/body @eid)) (:entity/faction @eid) 0.2]
+   [:tx/add-text-effect eid "[WHITE]!" 1]])
+
+(defmethod exit :npc-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
+
+(defmulti cursor
+  (fn [[k _v] _eid _ctx]
+    k))
+
+(defmethod cursor :player-item-on-cursor
+  [_ _eid _ctx]
+  :cursors/hand-grab)
+
+(defmulti pause-game?
+  (fn [k]
+    k))
+
+(defmethod pause-game? :player-item-on-cursor
+  [_]
+  true)
+
+(defmulti clicked-inventory-cell
+  (fn [[k _v] _eid _cell]
+    k))
+
+(defmethod clicked-inventory-cell :default
+  [_ _eid _cell]
+  nil)
 
 (defmethod clicked-inventory-cell :player-item-on-cursor
   [_ eid cell]
@@ -165,11 +186,35 @@
       [:tx/event eid :dropped-item]
       [:tx/event eid :pickup-item item-in-cell]])))
 
-(defmethod pause-game? :player-item-on-cursor
-  [_]
-  true)
+(defmulti draw-ui-view
+  (fn [[k _v] _eid _ctx]
+    k))
 
+(defmethod draw-ui-view :default
+  [_ _eid _ctx]
+  nil)
 
+(defmethod draw-ui-view :player-item-on-cursor
+  [_ eid {:keys [ctx/stage
+                 ctx/textures
+                 ctx/ui-mouse-position]
+          :as ctx}]
+  ; TODO see player-item-on-cursor at render layers
+  ; always draw it here at right position, then render layers does not need input/stage
+  ; can pass world to graphics, not handle here at application
+  (when (stage/mouseover-actor stage (ctx/mouse-position ctx))
+    [[:draw/texture-region
+      (textures/texture-region textures (:entity/image (:entity/item-on-cursor @eid)))
+      ui-mouse-position
+      {:center? true}]]))
+
+(defmulti handle-input
+  (fn [[k _v] _eid _ctx]
+    k))
+
+(defmethod handle-input :default
+  [_ _eid _ctx]
+  nil)
 
 (defmethod handle-input :player-item-on-cursor
   [_ eid {:keys [ctx/stage]
@@ -178,50 +223,3 @@
     (when (and (ctx/button-just-pressed? ctx input.buttons/left)
                (not mouseover-actor))
       [[:tx/event eid :drop-item]])))
-
-(defmethod cursor :player-item-on-cursor
-  [_ _eid _ctx]
-  :cursors/hand-grab)
-
-(defmethod enter :active-skill
-  [[_k {:keys [skill]}] eid]
-  [[:tx/sound (:skill/start-action-sound skill)]
-   [:tx/set-cooldown eid skill]
-   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
-
-(defmethod enter :npc-dead
-  [_ eid]
-  [[:tx/mark-destroyed eid]])
-
-(defmethod enter :player-moving
-  [[_k {:keys [movement-vector]}] eid]
-  [[:tx/assoc eid :entity/movement {:direction movement-vector
-                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
-                                               0)}]])
-
-(defmethod enter :player-dead
-  [_ _eid]
-  [[:tx/sound "bfxr_playerdeath"]
-   [:tx/show-modal {:title "YOU DIED - again!"
-                    :text "Good luck next time!"
-                    :button-text "OK"
-                    :on-click (fn [])}]])
-
-(defmethod enter :npc-moving
-  [[_k {:keys [movement-vector]}] eid]
-  [[:tx/assoc eid :entity/movement {:direction movement-vector
-                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
-                                               0)}]])
-
-(defmethod exit :player-moving
-  [_ eid _ctx]
-  [[:tx/dissoc eid :entity/movement]])
-
-(defmethod exit :npc-sleeping
-  [_ eid _ctx]
-  [[:tx/spawn-alert (:body/position (:entity/body @eid)) (:entity/faction @eid) 0.2]
-   [:tx/add-text-effect eid "[WHITE]!" 1]])
-
-(defmethod exit :npc-moving
-  [_ eid _ctx]
-  [[:tx/dissoc eid :entity/movement]])
