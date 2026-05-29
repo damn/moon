2ae0f602059e27d3dde21991e3a162531055c62d
Michael Sappler <michaelsappler@gmail.com>
2026-05-29 15:31:32 +0530

    stage.hp-mana-bar

--- src/create/add_stage_actors.clj
+++ src/create/add_stage_actors.clj
@@ -1,6 +1,5 @@
 (ns create.add-stage-actors
-  (:require [stage.dev-menu :as dev-menu]
-            [game.ctx :as ctx]
+  (:require [game.ctx :as ctx]
             [game.info :as info]
             [game.state :as state]
             [gdx.scenes.scene2d.actor :as actor]
@@ -16,55 +15,11 @@
             [gdx.scenes.scene2d.ui.window :as window]
             [gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
             [moon.inventory :as inventory]
-            [moon.number :as number]
-            [moon.stats :as stats]
             [moon.textures :as textures]
-            [moon.val-max :as val-max]))
+            [stage.dev-menu :as dev-menu]
+            [stage.hp-mana-bar :as hp-mana-bar]))
 
-(defn create-hp-mana-bar
-  [{:keys [ctx/textures
-           ctx/stage]}]
-  (let [{:keys [rahmen-file
-                rahmenw
-                rahmenh
-                hpcontent-file
-                manacontent-file
-                y-mana]} {:rahmen-file "images/rahmen.png"
-                          :rahmenw 150
-                          :rahmenh 26
-                          :hpcontent-file "images/hp.png"
-                          :manacontent-file "images/mana.png"
-                          :y-mana 80}
-        [x y-mana] [(/ (:viewport/world-width (:stage/viewport stage)) 2)
-                    y-mana]
-        rahmen-tex-reg (textures/texture-region textures {:image/file rahmen-file})
-        y-hp (+ y-mana rahmenh)
-        render-hpmana-bar (fn [x y content-file minmaxval name]
-                            [[:draw/texture-region rahmen-tex-reg [x y]]
-                             [:draw/texture-region
-                              (textures/texture-region textures
-                                                       {:image/file content-file
-                                                        :image/bounds [0 0 (* rahmenw (val-max/ratio minmaxval)) rahmenh]})
-                              [x y]]
-                             [:draw/text {:text (str (number/readable (minmaxval 0))
-                                                     "/"
-                                                     (minmaxval 1)
-                                                     " "
-                                                     name)
-                                          :x (+ x 75)
-                                          :y (+ y 2)
-                                          :up? true}]])
-        create-draws (fn [{:keys [ctx/player-eid]}]
-                       (let [stats (:entity/stats @player-eid)
-                             x (- x (/ rahmenw 2))]
-                         (concat
-                          (render-hpmana-bar x y-hp   hpcontent-file   (stats/get-hitpoints stats) "HP")
-                          (render-hpmana-bar x y-mana manacontent-file (stats/get-mana      stats) "MP"))))]
-    (actor/create
-     {:draw! (fn [this _batch _parent-alpha]
-               (when-let [stage (actor/stage this)]
-                 (ctx/draw! (:stage/ctx stage)
-                            (create-draws (:stage/ctx stage)))))})))
+
 
 (defn create-player-state-draw []
   (actor/create
@@ -227,7 +182,7 @@
     :as ctx}]
   (doseq [actor [(dev-menu/create ctx)
                  (action-bar/create)
-                 (create-hp-mana-bar ctx)
+                 (hp-mana-bar/create ctx)
                  (group/create
                   {:group/actors [(create-info-window ctx)
                                   (create-inventory-window ctx)]
