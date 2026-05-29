740b48e7bb5cb65d0d65798c6dc8ea8862fd0cba
Michael Sappler <michaelsappler@gmail.com>
2026-05-29 17:11:51 +0530

    entity.state.load

--- src/create/require_components.clj
+++ src/create/require_components.clj
@@ -2,12 +2,12 @@
   (:require entity.load
             entity.create.animation
             entity.create.body
+            entity.state.load
             handle-input.player-idle
             handle-input.player-moving
             handle-input.player-item-on-cursor
             tx.load
-            editor-widget.load
-            ))
+            editor-widget.load))
 
 (defn step [ctx]
   ctx)
---
+++ src/entity/state/load.clj
@@ -0,0 +1,258 @@
+(ns entity.state.load
+  (:require [game.ctx :as ctx]
+            [game.entity :as entity]
+            [game.state :as state]
+            [gdx.scenes.scene2d.stage :as stage]
+            [moon.inventory :as inventory]
+            [moon.stats :as stats]
+            [moon.textures :as textures]
+            [moon.timer :as timer]))
+
+(def reaction-time-multiplier 0.016)
+
+(defn- apply-action-speed-modifier [{:keys [entity/stats]} skill action-time]
+  (/ action-time
+     (or (stats/get-stat-value stats (:skill/action-time-modifier-key skill))
+         1)))
+
+
+(defmethod state/create :active-skill
+  [[_k [skill effect-ctx]] eid {:keys [ctx/elapsed-time]}]
+  {:skill skill
+   :effect-ctx effect-ctx
+   :counter (->> skill
+                 :skill/action-time
+                 (apply-action-speed-modifier @eid skill)
+                 (timer/create elapsed-time))})
+
+(defmethod state/create :stunned
+  [[_k duration] _eid {:keys [ctx/elapsed-time]}]
+  {:counter (timer/create elapsed-time duration)})
+
+(defmethod state/create :player-moving
+  [[_k movement-vector] eid _ctx]
+  {:movement-vector movement-vector})
+
+(defmethod state/create :npc-moving
+  [[_k movement-vector] eid {:keys [ctx/elapsed-time]}]
+  {:movement-vector movement-vector
+   :timer (timer/create elapsed-time
+                        (* (stats/get-stat-value (:entity/stats @eid) :stats/reaction-time)
+                           reaction-time-multiplier))})
+
+(defmethod state/create :player-item-on-cursor
+  [[_k item] _eid _ctx]
+  {:item item})
+
+
+(defmethod state/enter :player-item-on-cursor
+  [[_k {:keys [item]}] eid]
+  [[:tx/assoc eid :entity/item-on-cursor item]])
+
+(defmethod state/enter :active-skill
+  [[_k {:keys [skill]}] eid]
+  [[:tx/sound (:skill/start-action-sound skill)]
+   [:tx/set-cooldown eid skill]
+   [:tx/update eid :entity/stats stats/pay-mana-cost (:skill/cost skill)]])
+
+(defmethod state/enter :npc-dead
+  [_ eid]
+  [[:tx/mark-destroyed eid]])
+
+(defmethod state/enter :player-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+(defmethod state/enter :player-dead
+  [_ _eid]
+  [[:tx/sound "bfxr_playerdeath"]
+   [:tx/show-modal {:title "YOU DIED - again!"
+                    :text "Good luck next time!"
+                    :button-text "OK"
+                    :on-click (fn [])}]])
+
+(defmethod state/enter :npc-moving
+  [[_k {:keys [movement-vector]}] eid]
+  [[:tx/assoc eid :entity/movement {:direction movement-vector
+                                    :speed (or (stats/get-stat-value (:entity/stats @eid) :stats/movement-speed)
+                                               0)}]])
+
+
+(defmethod state/exit :player-item-on-cursor
+  [_ eid ctx]
+  ; at clicked-cell when we put it into a inventory-cell
+  ; we do not want to drop it on the ground too additonally,
+  ; so we dissoc it there manually. Otherwise it creates another item
+  ; on the ground
+  (let [entity @eid]
+    (when (:entity/item-on-cursor entity)
+      [[:tx/sound "bfxr_itemputground"]
+       [:tx/dissoc eid :entity/item-on-cursor]
+       [:tx/spawn-item
+        (ctx/item-place-position ctx entity)
+        (:entity/item-on-cursor entity)]])))
+
+(defmethod state/exit :player-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
+
+(defmethod state/exit :npc-sleeping
+  [_ eid _ctx]
+  [[:tx/spawn-alert (:body/position (:entity/body @eid)) (:entity/faction @eid) 0.2]
+   [:tx/add-text-effect eid "[WHITE]!" 1]])
+
+(defmethod state/exit :npc-moving
+  [_ eid _ctx]
+  [[:tx/dissoc eid :entity/movement]])
+
+
+(defmethod state/cursor :player-item-on-cursor
+  [_ _eid _ctx]
+  :cursors/hand-grab)
+
+(defmethod state/cursor :player-dead
+  [_ _eid _ctx]
+  :cursors/black-x)
+
+(defmethod state/cursor :active-skill
+  [_ _eid _ctx]
+  :cursors/sandclock)
+
+(defmethod state/cursor :stunned
+  [_ _eid _ctx]
+  :cursors/denied)
+
+(defmethod state/cursor :player-moving
+  [_ _eid _ctx]
+  :cursors/walking)
+
+(defmethod state/cursor :player-idle
+  [_ eid {:keys [ctx/interaction-state]}]
+  (let [[k params] interaction-state]
+    (case k
+      :interaction-state/mouseover-actor
+      (let [[actor-type params] params
+            inventory-cell-with-item? (and (= actor-type :mouseover-actor/inventory-cell)
+                                           (let [inventory-slot params]
+                                             (get-in (:entity/inventory @eid) inventory-slot)))]
+        (cond
+         inventory-cell-with-item?
+         :cursors/hand-before-grab
+
+         (= actor-type :mouseover-actor/window-title-bar)
+         :cursors/move-window
+
+         (= actor-type :mouseover-actor/button)
+         :cursors/over-button
+
+         (= actor-type :mouseover-actor/unspecified)
+         :cursors/default
+
+         :else
+         :cursors/default))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/item (if in-click-range?
+                            :cursors/hand-before-grab
+                            :cursors/hand-before-grab-gray)
+          :clickable/player :cursors/bag))
+
+      :interaction-state.skill/usable
+      :cursors/use-skill
+
+      :interaction-state.skill/not-usable
+      :cursors/skill-not-usable
+
+      :interaction-state/no-skill-selected
+      :cursors/no-skill-selected)))
+
+
+(defmethod state/pause-game? :active-skill
+  [_]
+  false)
+
+(defmethod state/pause-game? :stunned
+  [_]
+  false)
+
+(defmethod state/pause-game? :player-moving
+  [_]
+  false)
+
+(defmethod state/pause-game? :player-idle
+  [_]
+  true)
+
+(defmethod state/pause-game? :player-dead
+  [_]
+  true)
+
+(defmethod state/pause-game? :player-item-on-cursor
+  [_]
+  true)
+
+
+(defmethod state/clicked-inventory-cell :default
+  [_ _eid _cell]
+  nil)
+
+(defmethod state/clicked-inventory-cell :player-idle
+  [_ eid cell]
+  (when-let [item (get-in (:entity/inventory @eid) cell)]
+    [[:tx/sound "bfxr_takeit"]
+     [:tx/event eid :pickup-item item]
+     [:tx/remove-item eid cell]]))
+
+(defmethod state/clicked-inventory-cell :player-item-on-cursor
+  [_ eid cell]
+  (let [entity @eid
+        inventory (:entity/inventory entity)
+        item-in-cell (get-in inventory cell)
+        item-on-cursor (:entity/item-on-cursor entity)]
+    (cond
+     ; PUT ITEM IN EMPTY CELL
+     (and (not item-in-cell)
+          (inventory/valid-slot? cell item-on-cursor))
+     [[:tx/sound "bfxr_itemput"]
+      [:tx/dissoc eid :entity/item-on-cursor]
+      [:tx/set-item eid cell item-on-cursor]
+      [:tx/event eid :dropped-item]]
+
+     ; STACK ITEMS
+     (and item-in-cell
+          (inventory/stackable? item-in-cell item-on-cursor))
+     [[:tx/sound "bfxr_itemput"]
+      [:tx/dissoc eid :entity/item-on-cursor]
+      [:tx/stack-item eid cell item-on-cursor]
+      [:tx/event eid :dropped-item]]
+
+     ; SWAP ITEMS
+     (and item-in-cell
+          (inventory/valid-slot? cell item-on-cursor))
+     [[:tx/sound "bfxr_itemput"]
+      ; need to dissoc and drop otherwise state enter does not trigger picking it up again
+      ; TODO? coud handle pickup-item from item-on-cursor state also
+      [:tx/dissoc eid :entity/item-on-cursor]
+      [:tx/remove-item eid cell]
+      [:tx/set-item eid cell item-on-cursor]
+      [:tx/event eid :dropped-item]
+      [:tx/event eid :pickup-item item-in-cell]])))
+
+(defmethod state/draw-ui-view :player-item-on-cursor
+  [_ eid {:keys [ctx/stage
+                 ctx/textures
+                 ctx/ui-mouse-position]
+          :as ctx}]
+  ; TODO see player-item-on-cursor at render layers
+  ; always draw it here at right position, then render layers does not need input/stage
+  ; can pass world to graphics, not handle here at application
+  (when (stage/mouseover-actor stage (ctx/mouse-position ctx))
+    [[:draw/texture-region
+      (textures/texture-region textures (:entity/image (:entity/item-on-cursor @eid)))
+      ui-mouse-position
+      {:center? true}]]))
