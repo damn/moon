e16f15e7b8b348cf828e033b7cd9cc9de5975d07
Michael Sappler <michaelsappler@gmail.com>
2026-05-30 13:56:36 +0530

    textures use API

--- src/gdx/textures.clj
+++ src/gdx/textures.clj
@@ -1,27 +1,27 @@
 (ns gdx.textures
-  (:require [clojure.string :as str])
-  (:import (com.badlogic.gdx Files)
-           (com.badlogic.gdx.files FileHandle)
-           (com.badlogic.gdx.graphics Texture)))
+  (:require [clojure.string :as str]
+            [com.badlogic.gdx.files :as files]
+            [com.badlogic.gdx.files.file-handle :as file]
+            [com.badlogic.gdx.graphics.texture :as texture]))
 
 (def folder "resources/")
 (def extensions #{"png" "bmp"})
 
 (defn create
-  [^Files files]
+  [files]
   (into {} (for [path (map (fn [path]
                              (str/replace-first path folder ""))
-                           (loop [[^FileHandle file & remaining] (.list (.internal files folder))
+                           (loop [[file & remaining] (file/list (files/internal files folder))
                                   result []]
                              (cond (nil? file)
                                    result
 
-                                   (.isDirectory file)
-                                   (recur (concat remaining (.list file)) result)
+                                   (file/directory? file)
+                                   (recur (concat remaining (file/list file)) result)
 
-                                   (extensions (.extension file))
-                                   (recur remaining (conj result (.path file)))
+                                   (extensions (file/extension file))
+                                   (recur remaining (conj result (file/path file)))
 
                                    :else
                                    (recur remaining result))))]
-             [path (Texture. (.internal files path))])))
+             [path (texture/create (files/internal files path))])))
