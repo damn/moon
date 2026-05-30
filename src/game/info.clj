dc2126179e2df1e499902d76f743cd99a35ea508
Michael Sappler <michaelsappler@gmail.com>
2026-05-30 14:34:53 +0530

    separate info-fns mapping

--- src/game/info.clj
+++ src/game/info.clj
@@ -1,70 +1,13 @@
 (ns game.info
   (:require [clojure.core-ext :refer [sort-by-k-order]]
-            [clojure.math :as math]
             [clojure.string :as str]
-            [moon.ops :as ops]
-            [moon.number :as number]
-            [moon.stats :as stats]
-            [moon.timer :as timer]))
+            [game.info-fns :as info-fns]))
 
 (defmulti text (fn [object ctx]
                  (cond (:item/slot object)
                        :info/item
                        :else
                        :info/entity)))
-
-(def ^:private non-val-max-stat-ks
-  [:stats/movement-speed
-   :stats/aggro-range
-   :stats/reaction-time
-   :stats/strength
-   :stats/cast-speed
-   :stats/attack-speed
-   :stats/armor-save
-   :stats/armor-pierce])
-
-(defn- ops-info [ops modifier-k]
-  (str/join "\n"
-            (keep
-             (fn [[k v]]
-               (when-not (zero? v)
-                 (str (case (math/signum v)
-                        0.0 ""
-                        1.0 "+"
-                        -1.0 "")
-                      (case k
-                        :op/inc  (str v)
-                        :op/mult (str v "%"))
-                      " "
-                      (str/capitalize (name modifier-k)))))
-             (ops/sort ops))))
-
-(comment
- (deftest info-texts
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 24}
-                         "Strength")
-          "-4 Strength\n+24% Strength"))
-
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 0}
-                         "Strength")
-          "-4 Strength"))
-
-   (is (= (ops/info-text {:op/mult 35}
-                         "Hitpoints")
-          "+35% Hitpoints"))
-
-   (is (= (ops/info-text {:op/inc -30
-                          :op/mult 5}
-                         "Hitpoints")
-          "-30 Hitpoints\n+5% Hitpoints")))
- )
-
-(defn stats-modifiers-info [mods]
-  (when (seq mods)
-    (str/join "\n" (keep (fn [[k ops]]
-                           (ops-info ops k)) mods))))
 
 (defn- remove-newlines [s]
   (let [new-s (-> s
@@ -107,100 +50,6 @@
    :maxrange
    :entity-effects])
 
-(def ^:private info-fns
-  {:creature/level (fn [[_ v] _ctx]
-                     (str "Level: " v))
-
-   :entity/stats (fn [[_ stats] _ctx]
-                   (str/join "\n" (concat
-                                   ["*STATS*"
-                                    (str "Mana: " (stats/get-mana stats))
-                                    (str "Hitpoints: " (stats/get-hitpoints stats))]
-                                   (for [stat-k non-val-max-stat-ks]
-                                     (str (str/capitalize (name stat-k)) ": "
-                                          (stats/get-stat-value stats stat-k))))))
-
-   :effects.target/convert (fn [_ _ctx]
-                             "Converts target to your side.")
-
-   :effects.target/damage (fn [[_ {[min max] :damage/min-max}] _ctx]
-                            (str min "-" max " damage")
-                            #_(if source
-                                (let [modified (stats/damage @source damage)]
-                                  (if (= damage modified)
-                                    (damage/info-text damage)
-                                    (str (damage/info-text damage) "\nModified: " (damage/info modified))))
-                                (damage/info-text damage)) )
-
-   :effects.target/kill (fn [_ _ctx]
-                          "Kills target")
-
-   :effects.target/melee-damage (fn [_ _ctx]
-                                  (str "Damage based on entity strength."
-                                       #_(when source
-                                           (str "\n" (damage-info (entity->melee-damage @source))))))
-
-   :effects.target/spiderweb (fn [_ _ctx]
-                               "Spiderweb slows 50% for 5 seconds.")
-
-   :effects.target/stun (fn [[_ duration] _ctx]
-                          (str "Stuns for " (number/readable duration) " seconds"))
-
-   :effects/spawn (fn [[_ {:keys [property/pretty-name]}] _ctx]
-                    (str "Spawns a " pretty-name))
-
-   :effects/target-all (fn [_ _ctx]
-                         "All visible targets")
-
-   :entity/delete-after-duration (fn [[_ counter] {:keys [ctx/elapsed-time]}]
-                                   (str "Remaining: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
-
-   :entity/faction (fn [[_ faction] _ctx]
-                     (str "Faction: " (name faction)))
-
-   :entity/fsm (fn [[_ fsm] _ctx]
-                 (str "State: " (name (:state fsm))))
-
-   :stats/modifiers (fn [[_ mods] _ctx]
-                      (stats-modifiers-info mods))
-
-   :entity/skills (fn [[_ skills] _ctx]
-                    ; => recursive info-text leads to endless text wall
-                    (when (seq skills)
-                      (str "Skills: " (str/join "," (map name (keys skills))))))
-
-   :entity/species (fn [[_ species] _ctx]
-                     (str "Creature - " (str/capitalize (name species))))
-
-   :entity/temp-modifier (fn [[_ {:keys [counter]}] {:keys [ctx/elapsed-time]}]
-                           (str "Spiderweb - remaining: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
-
-   :projectile/piercing? (fn [_ _ctx]
-                           "Piercing")
-
-   :property/pretty-name (fn [[_ v] _ctx]
-                           v)
-
-   :skill/cooling-down? (fn [[_ counter] {:keys [ctx/elapsed-time]}]
-                          (str "Cooldown: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
-
-   :skill/action-time (fn [[_ v] _ctx]
-                        (str "Action-Time: " (number/readable v) " seconds"))
-
-   :skill/action-time-modifier-key (fn [[_ v] _ctx]
-                                     (case v
-                                       :stats/cast-speed "Spell"
-                                       :stats/attack-speed "Attack"))
-
-   :skill/cooldown (fn [[_ v] _ctx]
-                     (str "Cooldown: " (number/readable v) " seconds"))
-
-   :skill/cost (fn [[_ v] _ctx]
-                 (str "Cost: " v " Mana"))
-
-   :maxrange (fn [[_ v] _ctx]
-               (str "Range: " v " Meters."))})
-
 (comment
  (:skills/death-ray (:entity/skills @(:ctx/player-eid @dev.application/state)))
  ; cooling-down? is not set in the action-bar ....
@@ -209,7 +58,7 @@
 
 (defmethod text :info/entity [entity ctx]
   (let [component-info (fn [[k v]]
-                         (let [s (if-let [info-fn (info-fns k)]
+                         (let [s (if-let [info-fn (info-fns/mapping k)]
                                    (do
                                     (str k " - " (info-fn [k v] ctx))))]
                            (if-let [color (k->colors k)]
@@ -246,7 +95,7 @@
                      (str "[LIME]" (str/capitalize (name (:item/slot item))) "[]")
                      ; seq because they can be empty map ?
                      (when (seq (:stats/modifiers item))
-                       (str "[CYAN]" (stats-modifiers-info (:stats/modifiers item)) "[]"))])))
+                       (str "[CYAN]" (info-fns/stats-modifiers-info (:stats/modifiers item)) "[]"))])))
 
 (comment
  (let [item (get (:inventory.slot/shield (:entity/inventory @(:ctx/player-eid @dev.application/state)))
---
+++ src/game/info_fns.clj
@@ -0,0 +1,155 @@
+(ns game.info-fns
+  (:require [clojure.math :as math]
+            [clojure.string :as str]
+            [moon.number :as number]
+            [moon.ops :as ops]
+            [moon.stats :as stats]
+            [moon.timer :as timer]))
+
+(defn- ops-info [ops modifier-k]
+  (str/join "\n"
+            (keep
+             (fn [[k v]]
+               (when-not (zero? v)
+                 (str (case (math/signum v)
+                        0.0 ""
+                        1.0 "+"
+                        -1.0 "")
+                      (case k
+                        :op/inc  (str v)
+                        :op/mult (str v "%"))
+                      " "
+                      (str/capitalize (name modifier-k)))))
+             (ops/sort ops))))
+
+(comment
+ (deftest info-texts
+   (is (= (ops/info-text {:op/inc -4
+                          :op/mult 24}
+                         "Strength")
+          "-4 Strength\n+24% Strength"))
+
+   (is (= (ops/info-text {:op/inc -4
+                          :op/mult 0}
+                         "Strength")
+          "-4 Strength"))
+
+   (is (= (ops/info-text {:op/mult 35}
+                         "Hitpoints")
+          "+35% Hitpoints"))
+
+   (is (= (ops/info-text {:op/inc -30
+                          :op/mult 5}
+                         "Hitpoints")
+          "-30 Hitpoints\n+5% Hitpoints")))
+ )
+
+(defn stats-modifiers-info [mods]
+  (when (seq mods)
+    (str/join "\n" (keep (fn [[k ops]]
+                           (ops-info ops k)) mods))))
+
+
+(def ^:private non-val-max-stat-ks
+  [:stats/movement-speed
+   :stats/aggro-range
+   :stats/reaction-time
+   :stats/strength
+   :stats/cast-speed
+   :stats/attack-speed
+   :stats/armor-save
+   :stats/armor-pierce])
+
+(def mapping
+  {:creature/level (fn [[_ v] _ctx]
+                     (str "Level: " v))
+
+   :entity/stats (fn [[_ stats] _ctx]
+                   (str/join "\n" (concat
+                                   ["*STATS*"
+                                    (str "Mana: " (stats/get-mana stats))
+                                    (str "Hitpoints: " (stats/get-hitpoints stats))]
+                                   (for [stat-k non-val-max-stat-ks]
+                                     (str (str/capitalize (name stat-k)) ": "
+                                          (stats/get-stat-value stats stat-k))))))
+
+   :effects.target/convert (fn [_ _ctx]
+                             "Converts target to your side.")
+
+   :effects.target/damage (fn [[_ {[min max] :damage/min-max}] _ctx]
+                            (str min "-" max " damage")
+                            #_(if source
+                                (let [modified (stats/damage @source damage)]
+                                  (if (= damage modified)
+                                    (damage/info-text damage)
+                                    (str (damage/info-text damage) "\nModified: " (damage/info modified))))
+                                (damage/info-text damage)) )
+
+   :effects.target/kill (fn [_ _ctx]
+                          "Kills target")
+
+   :effects.target/melee-damage (fn [_ _ctx]
+                                  (str "Damage based on entity strength."
+                                       #_(when source
+                                           (str "\n" (damage-info (entity->melee-damage @source))))))
+
+   :effects.target/spiderweb (fn [_ _ctx]
+                               "Spiderweb slows 50% for 5 seconds.")
+
+   :effects.target/stun (fn [[_ duration] _ctx]
+                          (str "Stuns for " (number/readable duration) " seconds"))
+
+   :effects/spawn (fn [[_ {:keys [property/pretty-name]}] _ctx]
+                    (str "Spawns a " pretty-name))
+
+   :effects/target-all (fn [_ _ctx]
+                         "All visible targets")
+
+   :entity/delete-after-duration (fn [[_ counter] {:keys [ctx/elapsed-time]}]
+                                   (str "Remaining: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
+
+   :entity/faction (fn [[_ faction] _ctx]
+                     (str "Faction: " (name faction)))
+
+   :entity/fsm (fn [[_ fsm] _ctx]
+                 (str "State: " (name (:state fsm))))
+
+   :stats/modifiers (fn [[_ mods] _ctx]
+                      (stats-modifiers-info mods))
+
+   :entity/skills (fn [[_ skills] _ctx]
+                    ; => recursive info-text leads to endless text wall
+                    (when (seq skills)
+                      (str "Skills: " (str/join "," (map name (keys skills))))))
+
+   :entity/species (fn [[_ species] _ctx]
+                     (str "Creature - " (str/capitalize (name species))))
+
+   :entity/temp-modifier (fn [[_ {:keys [counter]}] {:keys [ctx/elapsed-time]}]
+                           (str "Spiderweb - remaining: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
+
+   :projectile/piercing? (fn [_ _ctx]
+                           "Piercing")
+
+   :property/pretty-name (fn [[_ v] _ctx]
+                           v)
+
+   :skill/cooling-down? (fn [[_ counter] {:keys [ctx/elapsed-time]}]
+                          (str "Cooldown: " (number/readable (timer/ratio elapsed-time counter)) "/1"))
+
+   :skill/action-time (fn [[_ v] _ctx]
+                        (str "Action-Time: " (number/readable v) " seconds"))
+
+   :skill/action-time-modifier-key (fn [[_ v] _ctx]
+                                     (case v
+                                       :stats/cast-speed "Spell"
+                                       :stats/attack-speed "Attack"))
+
+   :skill/cooldown (fn [[_ v] _ctx]
+                     (str "Cooldown: " (number/readable v) " seconds"))
+
+   :skill/cost (fn [[_ v] _ctx]
+                 (str "Cost: " v " Mana"))
+
+   :maxrange (fn [[_ v] _ctx]
+               (str "Range: " v " Meters."))})
