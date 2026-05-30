144592ecb94c99eab27d5300c5a6fd54fb9beae3
Michael Sappler <michaelsappler@gmail.com>
2026-05-30 17:14:49 +0530

    split map widget table

--- src/editor_widget/map.clj
+++ src/editor_widget/map.clj
@@ -3,182 +3,8 @@
             [clojure.set :as set]
             [game.ctx :as ctx]
             [game.schema :as schema]
-            [gdx.scenes.scene2d.actor :as actor]
-            [com.badlogic.gdx.scenes.scene2d.event :as event]
-            [gdx.scenes.scene2d.group :as group]
-            [gdx.stage :as stage]
-            [gdx.scenes.scene2d.ui.label :as label]
-            [gdx.scenes.scene2d.ui.table :as table]
-            [gdx.scenes.scene2d.ui.text-button :as text-button]
-            [gdx.scenes.scene2d.ui.widget-group :as widget-group]
-            [gdx.scenes.scene2d.ui.window :as window]
             [moon.schemas :as schemas]
-            [moon.ui.error-window]
-            [moon.ui.property-overview-window]))
-
-(defn- map-widget-table-value [table schemas]
-  (into {}
-        (for [widget (filter (comp vector? actor/user-object) (group/children table))
-              :let [[k _] (actor/user-object widget)]]
-          [k (schema/value (get schemas k) widget schemas)])))
-
-(defn- build-value-widget [ctx schema k v]
-  (let [widget (schema/create schema v ctx)] ; - wait its used also somewhere else w/o this schema/create?
-    ; FIXME assert no user object !
-    (actor/set-user-object! widget [k v])
-    widget))
-
-(defn- rebuild!
-  [{:keys [ctx/db
-           ctx/stage]
-    :as ctx}]
-  (let [window (-> stage
-                   (stage/find-actor "moon.ui.editor.window"))
-        map-widget-table (group/find-actor window "moon.db.schema.map.ui.widget")
-        property (map-widget-table-value map-widget-table (:db/schemas db))]
-    (actor/remove! window)
-    (stage/add-actor! stage
-                      (schema/property-editor-window
-                       {:ctx ctx
-                        :property property}))))
-
-(defn- k->label-text [k]
-  (name k) ;(str "[GRAY]:" (namespace k) "[]/" (name k))
-  )
-
-(defn- component-row*
-  [{:keys [skin
-           editor-widget
-           display-remove-component-button?
-           k
-           table
-           label-text]}]
-  [{:actor (table/create
-            {:table/cell-defaults {:pad 2}
-             :table/rows [[{:actor (when display-remove-component-button?
-                                     (text-button/create
-                                      {:text "-"
-                                       :skin skin
-                                       :actor/listeners {:listener/change
-                                                         (fn [event _actor]
-                                                           (actor/remove! (first (filter (fn [actor]
-                                                                                           (and (actor/user-object actor)
-                                                                                                (= k ((actor/user-object actor) 0))))
-                                                                                         (group/children table))))
-                                                           (rebuild! (:stage/ctx (event/stage event))))}}))
-                            :left? true}
-                           {:actor (label/create
-                                    {:text label-text
-                                     :skin skin})}]]})
-    :right? true}
-   {:actor nil #_(com.kotcrab.vis.ui.widget.Separator. "vertical")
-    :pad-top 2
-    :pad-bottom 2
-    :fill-y? true
-    :expand-y? true}
-   {:actor editor-widget
-    :left? true}])
-
-(defn- component-row [skin editor-widget k optional-key? table]
-  (component-row*
-   {:skin skin
-    :editor-widget editor-widget
-    :display-remove-component-button? optional-key?
-    :k k
-    :table table
-    :label-text (k->label-text k)}))
-
-(defn add-component-window
-  [{:keys [schemas schema map-widget-table skin]}]
-  (let [window (window/create
-                {:title "Choose"
-                 :skin skin
-                 :window/close-button? skin
-                 :window/modal? true
-                 :table/cell-defaults {:pad 5}})
-        remaining-ks (sort (remove (set (keys (schema/value schema map-widget-table schemas)))
-                                   (schemas/map-keys schemas schema)))]
-    (table/add-rows!
-     window
-     (for [k remaining-ks]
-       [{:actor (text-button/create
-                 {:skin skin
-                  :text (name k)
-                  :actor/listeners {:listener/change
-                                    (fn [event _actor]
-                                      (actor/remove! window)
-                                      (let [ctx (:stage/ctx (event/stage event))]
-                                        (table/add-rows! map-widget-table [(component-row skin
-                                                                                          (build-value-widget ctx
-                                                                                                              (get schemas k)
-                                                                                                              k
-                                                                                                              (schemas/default-value schemas k))
-                                                                                          k
-                                                                                          (schemas/optional? schemas schema k)
-                                                                                          map-widget-table)])
-                                        (rebuild! ctx)))}})}]))
-    (widget-group/pack! window)
-    window))
-
-(defn- horiz-sep [colspan]
-  (fn []
-    [{:actor nil #_(com.kotcrab.vis.ui.widget.Separator. "default")
-      :pad-top 2
-      :pad-bottom 2
-      :colspan colspan
-      :fill-x? true
-      :expand-x? true}]))
-
-(defn- interpose-f [f coll]
-  (drop 1 (interleave (repeatedly f) coll)))
-
-(defn- create-map-widget-table
-  [{:keys [skin
-           schema
-           k->widget
-           k->optional?
-           ks-sorted
-           opt?]}]
-  (let [table (table/create
-               {:table/cell-defaults {:pad 5}
-                :actor/name "moon.db.schema.map.ui.widget"})
-        colspan 3
-        component-rows (interpose-f (horiz-sep colspan)
-                                    (map (fn [k]
-                                           (component-row skin
-                                                          (k->widget k)
-                                                          k
-                                                          (k->optional? k)
-                                                          table))
-                                         ks-sorted))]
-    (table/add-rows!
-     table
-     (concat [(when opt?
-                [{:actor (text-button/create
-                          {:text "Add component"
-                           :skin skin
-                           :actor/listeners {:listener/change
-                                             (fn [event actor]
-                                               (let [{:keys [ctx/db
-                                                             ctx/stage
-                                                             ctx/skin]} (:stage/ctx (event/stage event))]
-                                                 (stage/add-actor!
-                                                  stage
-                                                  (add-component-window
-                                                   {:skin skin
-                                                    :schemas (:db/schemas db)
-                                                    :schema schema
-                                                    :map-widget-table table}))))}})
-                  :colspan colspan}])]
-             [(when opt?
-                [{:actor  nil #_(com.kotcrab.vis.ui.widget.Separator. "default")
-                  :pad-top 2
-                  :pad-bottom 2
-                  :colspan colspan
-                  :fill-x? true
-                  :expand-x? true}])]
-             component-rows))
-    table))
+            [moon.ui.map-widget-table :as map-widget-table]))
 
 (def ^:private property-k-sort-order
   [:property/id
@@ -205,12 +31,12 @@
            ctx/skin]
     :as ctx}]
   (let [schemas (:db/schemas db)]
-    (create-map-widget-table
+    (map-widget-table/create
      {:skin skin
       :schema schema
       :k->widget (into {}
                        (for [[k v] m]
-                         [k (build-value-widget ctx (get schemas k) k v)]))
+                         [k (map-widget-table/build-value-widget ctx (get schemas k) k v)]))
       :k->optional? #(schemas/optional? schemas schema %)
       :ks-sorted (map first (sort-by-k-order property-k-sort-order m))
       :opt? (seq (set/difference (schemas/optional-keyset schemas schema)
@@ -218,4 +44,4 @@
 
 (defmethod schema/value :s/map
   [_ table schemas]
-  (map-widget-table-value table schemas))
+  (map-widget-table/get-value table schemas))
