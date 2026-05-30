126c42c1628f4866242ac56ed82436bdb4a20414
Michael Sappler <michaelsappler@gmail.com>
2026-05-30 17:08:37 +0530

    split register eid

--- src/tx/load.clj
+++ src/tx/load.clj
@@ -1,5 +1,6 @@
 (ns tx.load
   (:require tx.spawn-projectile
+            tx.register-eid
             [clojure.core-ext :refer [safe-merge]]
             [game.ctx]
             [game.effect :as effect]
@@ -111,26 +112,7 @@
                                                    [:tx/dissoc      eid old-state-k]
                                                    [:tx/state-exit  eid old-state-obj]
                                                    [:tx/state-enter eid new-state-obj]])))))
-            :tx/register-eid             (fn [ctx eid]
-                                           (assert (and (not (contains? @eid :entity/id))))
-                                           (let [id (swap! (:ctx/id-counter ctx) inc)]
-                                             (assert (number? id))
-                                             (swap! eid assoc :entity/id id)
-                                             (swap! (:ctx/entity-ids ctx) assoc id eid))
-
-                                           (assert (:entity/body @eid)) ; -< inside content grid
-                                           (content-grid/add-entity! (:ctx/content-grid ctx) eid)
-
-                                           (assert (:entity/body @eid)) ; <- inside the grid add fn ?
-                                           (when (:body/collides? (:entity/body @eid))
-                                             (assert (grid/valid-position? (:ctx/grid ctx) (:entity/body @eid) (:entity/id @eid))))
-                                           (grid/set-touched-cells! (:ctx/grid ctx) eid)
-                                           (when (:body/collides? (:entity/body @eid)) ; entity/collides? separate fooziboosh, no 'when' just a callback?
-                                             (grid/set-occupied-cells! (:ctx/grid ctx) eid))
-
-                                           nil
-                                           ; TODO what should a tx return? nil? ctx?
-                                           )
+            :tx/register-eid             tx.register-eid/do!
             :tx/unregister-eid           (fn
                                            [{:keys [ctx/content-grid
                                                     ctx/entity-ids
