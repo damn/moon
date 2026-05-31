27a579e15f8354fdb6d62d43c51ee316650a4269
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 20:19:59 +0530

    split state exit player item on cursor

---
+++ src/entity/state/exit/player_item_on_cursor.clj
@@ -0,0 +1,17 @@
+(ns entity.state.exit.player-item-on-cursor
+  (:require [game.ctx :as ctx]
+            [game.state :as state]))
+
+(defmethod state/exit :player-item-on-cursor
+  [_ eid ctx]
+  ; at clicked-cell when we put it into a inventory-cell
+  ; we do not want to drop it on the ground too additonally,
+  ; so we dissoc it there manually. Otherwise it creates another item
+  ; on the ground
+  (let [entity @eid]
+    (when (:entity/item-on-cursor entity)
+      [[:tx/sound "bfxr_itemputground"]
+       [:tx/dissoc eid :entity/item-on-cursor]
+       [:tx/spawn-item
+        (ctx/item-place-position ctx entity)
+        (:entity/item-on-cursor entity)]])))
--- src/entity/state/load.clj
+++ src/entity/state/load.clj
@@ -1,20 +1,5 @@
 (ns entity.state.load
-  (:require [game.ctx :as ctx]
-            [game.state :as state]))
-
-(defmethod state/exit :player-item-on-cursor
-  [_ eid ctx]
-  ; at clicked-cell when we put it into a inventory-cell
-  ; we do not want to drop it on the ground too additonally,
-  ; so we dissoc it there manually. Otherwise it creates another item
-  ; on the ground
-  (let [entity @eid]
-    (when (:entity/item-on-cursor entity)
-      [[:tx/sound "bfxr_itemputground"]
-       [:tx/dissoc eid :entity/item-on-cursor]
-       [:tx/spawn-item
-        (ctx/item-place-position ctx entity)
-        (:entity/item-on-cursor entity)]])))
+  (:require [game.state :as state]))
 
 (defmethod state/exit :player-moving
   [_ eid _ctx]
