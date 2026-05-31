5d3e4e103763ad291eb259363d1675aa9e974355
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 18:40:51 +0530

    split potential fields step

--- src/moon/grid/update_potential_fields.clj
+++ src/moon/grid/update_potential_fields.clj
@@ -1,7 +1,6 @@
 (ns moon.grid.update-potential-fields
   (:require [moon.cell :as cell]
-            [moon.grid :as grid]
-            [moon.position :as position]))
+            [moon.grid.update-potential-fields.step :as step]))
 
 ; Assumption: The map contains no not-allowed diagonal cells, diagonal wall cells where both
 ; adjacent cells are walls and blocked.
@@ -36,34 +35,8 @@
    (def last-marked-cells (generate-potential-field :good tl->es)))
  (println *1)
  (def marked *2)
- (step :good *1)
+ (step/f :good *1)
  )
-
-; TODO performance
-; * cached-adjacent-non-blocked-cells ? -> no need for cell blocked check?
-; * sorted-set-by ?
-; * do not refresh the potential-fields EVERY frame, maybe very 100ms & check for exists? target if they died inbetween.
-; (or teleported?)
-(defn- step [grid faction last-marked-cells]
-  (let [marked-cells (transient [])
-        distance       #(cell/nearest-entity-distance % faction)
-        nearest-entity #(cell/nearest-entity          % faction)
-        marked? faction]
-    ; sorting important because of diagonal-cell values, flow from lower dist first for correct distance
-    (doseq [cell (sort-by #(distance @%) last-marked-cells)
-            adjacent-cell (grid/cached-adjacent-cells grid cell)
-            :let [cell* @cell
-                  adjacent-cell* @adjacent-cell]
-            :when (not (or (cell/pf-blocked? adjacent-cell*)
-                           (marked? adjacent-cell*)))
-            :let [distance-value (+ (float (distance cell*))
-                                    (float (if (position/diagonal? (:position cell*)
-                                                                   (:position adjacent-cell*))
-                                             1.4 ; square root of 2 * 10
-                                             1)))]]
-      (swap! adjacent-cell cell/add-field-data faction distance-value (nearest-entity cell*))
-      (conj! marked-cells adjacent-cell))
-    (persistent! marked-cells)))
 
 (defn- generate-potential-field
   [grid faction tiles->entities max-iterations]
@@ -77,7 +50,7 @@
            iterations 0]
       (if (= iterations max-iterations)
         marked-cells
-        (let [new-marked (step grid faction new-marked-cells)]
+        (let [new-marked (step/f grid faction new-marked-cells)]
           (recur (concat marked-cells new-marked)
                  new-marked
                  (inc iterations)))))))
---
+++ src/moon/grid/update_potential_fields/step.clj
@@ -0,0 +1,30 @@
+(ns moon.grid.update-potential-fields.step
+  (:require [moon.cell :as cell]
+            [moon.grid :as grid]
+            [moon.position :as position]))
+
+; TODO performance
+; * cached-adjacent-non-blocked-cells ? -> no need for cell blocked check?
+; * sorted-set-by ?
+; * do not refresh the potential-fields EVERY frame, maybe very 100ms & check for exists? target if they died inbetween.
+; (or teleported?)
+(defn f [grid faction last-marked-cells]
+  (let [marked-cells (transient [])
+        distance       #(cell/nearest-entity-distance % faction)
+        nearest-entity #(cell/nearest-entity          % faction)
+        marked? faction]
+    ; sorting important because of diagonal-cell values, flow from lower dist first for correct distance
+    (doseq [cell (sort-by #(distance @%) last-marked-cells)
+            adjacent-cell (grid/cached-adjacent-cells grid cell)
+            :let [cell* @cell
+                  adjacent-cell* @adjacent-cell]
+            :when (not (or (cell/pf-blocked? adjacent-cell*)
+                           (marked? adjacent-cell*)))
+            :let [distance-value (+ (float (distance cell*))
+                                    (float (if (position/diagonal? (:position cell*)
+                                                                   (:position adjacent-cell*))
+                                             1.4 ; square root of 2 * 10
+                                             1)))]]
+      (swap! adjacent-cell cell/add-field-data faction distance-value (nearest-entity cell*))
+      (conj! marked-cells adjacent-cell))
+    (persistent! marked-cells)))
