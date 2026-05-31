2c66e8151ec358a82a488a01f001466e82d14539
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 19:29:46 +0530

    split draw texture region

--- src/draw/load.clj
+++ src/draw/load.clj
@@ -2,6 +2,7 @@
   (:require [com.badlogic.gdx.graphics.g2d.batch :as batch]
             [com.badlogic.gdx.graphics.g2d.texture-region :as texture-region]
             [draw.text]
+            [draw.texture-region]
             [game.constants :as constants]
             [game.ctx :as ctx]
             [space.earlygrey.shape-drawer :as shape-drawer]))
@@ -51,32 +52,7 @@
                                      (shape-drawer/set-color! shape-drawer color-float-bits)
                                      (shape-drawer/sector! shape-drawer center-x center-y radius start-radians radians))
             :draw/text             draw.text/f
-            :draw/texture-region   (fn
-                                     [{:keys [ctx/batch
-                                              ctx/unit-scale
-                                              ctx/world-unit-scale]}
-                                      texture-region
-                                      [x y]
-                                      & {:keys [center? rotation]}]
-                                     (let [[w h] (let [dimensions [(texture-region/width  texture-region)
-                                                                   (texture-region/height texture-region)]]
-                                                   (if (= @unit-scale 1)
-                                                     dimensions
-                                                     (mapv (comp float (partial * world-unit-scale))
-                                                           dimensions)))]
-                                       (if center?
-                                         (batch/draw! batch
-                                                      texture-region
-                                                      (- (float x) (/ (float w) 2))
-                                                      (- (float y) (/ (float h) 2))
-                                                      (/ (float w) 2)
-                                                      (/ (float h) 2)
-                                                      w
-                                                      h
-                                                      1
-                                                      1
-                                                      (or rotation 0))
-                                         (batch/draw! batch texture-region x y w h))))
+            :draw/texture-region   draw.texture-region/f!
             :draw/with-line-width  (fn
                                      [{:keys [ctx/shape-drawer]
                                        :as ctx}
---
+++ src/draw/texture_region.clj
@@ -0,0 +1,30 @@
+(ns draw.texture-region
+  (:require [com.badlogic.gdx.graphics.g2d.batch :as batch]
+            [com.badlogic.gdx.graphics.g2d.texture-region :as texture-region]))
+
+(defn f!
+  [{:keys [ctx/batch
+           ctx/unit-scale
+           ctx/world-unit-scale]}
+   texture-region
+   [x y]
+   & {:keys [center? rotation]}]
+  (let [[w h] (let [dimensions [(texture-region/width  texture-region)
+                                (texture-region/height texture-region)]]
+                (if (= @unit-scale 1)
+                  dimensions
+                  (mapv (comp float (partial * world-unit-scale))
+                        dimensions)))]
+    (if center?
+      (batch/draw! batch
+                   texture-region
+                   (- (float x) (/ (float w) 2))
+                   (- (float y) (/ (float h) 2))
+                   (/ (float w) 2)
+                   (/ (float h) 2)
+                   w
+                   h
+                   1
+                   1
+                   (or rotation 0))
+      (batch/draw! batch texture-region x y w h))))
