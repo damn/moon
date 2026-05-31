ec190e2f73179a7490f395e1f986e6df7c558300
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 12:26:43 +0530

    Move cliff/floor to images

--- resources/maps/floor.tsx
+++ resources/maps/floor.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="floor" tilewidth="48" tileheight="48" tilecount="1568" columns="56">
- <image source="floor.png" width="2688" height="1344"/>
+ <image source="../images/floor.png" width="2688" height="1344"/>
  <tile id="0">
   <properties>
    <property name="movement" value="all"/>
