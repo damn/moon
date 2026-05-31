fefafede91203566bab522268193a745e7781bf9
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 19:12:30 +0530

    split inventory window create

--- src/moon/ui/inventory_window/create.clj
+++ src/moon/ui/inventory_window/create.clj
@@ -1,18 +1,10 @@
 (ns moon.ui.inventory-window.create
-  (:require [com.badlogic.gdx.scenes.scene2d.event :as event]
-            [com.badlogic.gdx.scenes.scene2d.ui.widget :as widget]
-            [game.ctx :as ctx]
-            [game.state :as state]
-            [gdx.scenes.scene2d.actor :as actor]
-            [gdx.scenes.scene2d.actor :as actor]
-            [gdx.scenes.scene2d.ui.image :as image]
-            [gdx.scenes.scene2d.ui.image :as image]
-            [gdx.scenes.scene2d.ui.stack :as stack]
-            [gdx.scenes.scene2d.ui.table :as table]
+  (:require [gdx.scenes.scene2d.ui.table :as table]
             [gdx.scenes.scene2d.ui.window :as window]
             [gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
             [gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
-            [moon.inventory :as inventory]))
+            [moon.inventory :as inventory]
+            [moon.ui.inventory-window.create-cell :refer [->cell]]))
 
 (defn f
   [{:keys [item-rect-color
@@ -36,40 +28,7 @@
                                           droppable-item-color
                                           not-allowed-drop-item-color)]
                               [:draw/filled-rectangle (inc x) (inc y) (- cell-size 2) (- cell-size 2) color]))])
-        ->cell (fn [slot & {:keys [position]}]
-                 (let [cell [slot (or position [0 0])]
-                       background-drawable (slot->drawable slot)]
-                   {:actor
-                    (stack/create
-                     {:actor/name "inventory-cell"
-                      :actor/user-object cell
-                      :actor/listeners {:listener/click (fn [event _x _y]
-                                                          (let [{:keys [ctx/player-eid] :as ctx} (:stage/ctx (event/stage event))
-                                                                entity @player-eid
-                                                                state-k (:state (:entity/fsm entity))]
-                                                            (ctx/do! ctx
-                                                                     (state/clicked-inventory-cell [state-k (state-k entity)]
-                                                                                                   player-eid
-                                                                                                   cell))))}
-                      :group/actors [(widget/create
-                                      {:draw! (fn [this _batch _parent-alpha]
-                                                (when-let [stage (actor/stage this)]
-                                                  (let [{:keys [ctx/player-eid
-                                                                ctx/ui-mouse-position]
-                                                         :as ctx} (:stage/ctx stage)]
-                                                    (ctx/draw! ctx
-                                                               (draw-cell-rect @player-eid
-                                                                               (actor/x this)
-                                                                               (actor/y this)
-                                                                               (actor/hit this
-                                                                                          (actor/stage->local-coordinates this ui-mouse-position)
-                                                                                          true)
-                                                                               (actor/user-object (actor/parent this)))))))})
-                                     (image/create
-                                      {:content background-drawable
-                                       :actor/name "image-widget"
-                                       :actor/user-object {:background-drawable background-drawable
-                                                           :cell-size cell-size}})]})}))]
+        ->cell (partial ->cell slot->drawable draw-cell-rect cell-size)]
     (window/create
      {:title "Inventory"
       :skin skin
---
+++ src/moon/ui/inventory_window/create_cell.clj
@@ -0,0 +1,45 @@
+(ns moon.ui.inventory-window.create-cell
+  (:require [com.badlogic.gdx.scenes.scene2d.event :as event]
+            [com.badlogic.gdx.scenes.scene2d.ui.widget :as widget]
+            [game.ctx :as ctx]
+            [game.state :as state]
+            [gdx.scenes.scene2d.actor :as actor]
+            [gdx.scenes.scene2d.actor :as actor]
+            [gdx.scenes.scene2d.ui.image :as image]
+            [gdx.scenes.scene2d.ui.image :as image]
+            [gdx.scenes.scene2d.ui.stack :as stack]))
+
+(defn ->cell [slot->drawable draw-cell-rect cell-size slot & {:keys [position]}]
+  (let [cell [slot (or position [0 0])]
+        background-drawable (slot->drawable slot)]
+    {:actor
+     (stack/create
+      {:actor/name "inventory-cell"
+       :actor/user-object cell
+       :actor/listeners {:listener/click (fn [event _x _y]
+                                           (let [{:keys [ctx/player-eid] :as ctx} (:stage/ctx (event/stage event))
+                                                 entity @player-eid
+                                                 state-k (:state (:entity/fsm entity))]
+                                             (ctx/do! ctx
+                                                      (state/clicked-inventory-cell [state-k (state-k entity)]
+                                                                                    player-eid
+                                                                                    cell))))}
+       :group/actors [(widget/create
+                       {:draw! (fn [this _batch _parent-alpha]
+                                 (when-let [stage (actor/stage this)]
+                                   (let [{:keys [ctx/player-eid
+                                                 ctx/ui-mouse-position]
+                                          :as ctx} (:stage/ctx stage)]
+                                     (ctx/draw! ctx
+                                                (draw-cell-rect @player-eid
+                                                                (actor/x this)
+                                                                (actor/y this)
+                                                                (actor/hit this
+                                                                           (actor/stage->local-coordinates this ui-mouse-position)
+                                                                           true)
+                                                                (actor/user-object (actor/parent this)))))))})
+                      (image/create
+                       {:content background-drawable
+                        :actor/name "image-widget"
+                        :actor/user-object {:background-drawable background-drawable
+                                            :cell-size cell-size}})]})}))
