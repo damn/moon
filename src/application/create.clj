7e79fa70785b2e707f81ed2df765716a4b30f3bd
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 09:47:35 +0530

    create with params

--- src/application/create.clj
+++ src/application/create.clj
@@ -1,7 +1,7 @@
 (ns application.create)
 
 (defn do! [app stepfns]
-  (reduce (fn [ctx f]
-            (f ctx))
+  (reduce (fn [ctx [f & params]]
+            (apply f ctx params))
           {:ctx/app app}
           stepfns))
