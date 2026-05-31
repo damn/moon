d7dfc8fcfe89824d0f56d697f0335091f0b9486f
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 17:08:31 +0530

    separate player item on cursor

--- src/entity/state/load.clj
+++ src/entity/state/load.clj
@@ -1,7 +1,6 @@
 (ns entity.state.load
   (:require [game.ctx :as ctx]
-            [game.state :as state]
-            [moon.inventory :as inventory]))
+            [game.state :as state]))
 
 (defmethod state/exit :player-item-on-cursor
   [_ eid ctx]
@@ -84,38 +83,3 @@
     [[:tx/sound "bfxr_takeit"]
      [:tx/event eid :pickup-item item]
      [:tx/remove-item eid cell]]))
-
-(defmethod state/clicked-inventory-cell :player-item-on-cursor
-  [_ eid cell]
-  (let [entity @eid
-        inventory (:entity/inventory entity)
-        item-in-cell (get-in inventory cell)
-        item-on-cursor (:entity/item-on-cursor entity)]
-    (cond
-     ; PUT ITEM IN EMPTY CELL
-     (and (not item-in-cell)
-          (inventory/valid-slot? cell item-on-cursor))
-     [[:tx/sound "bfxr_itemput"]
-      [:tx/dissoc eid :entity/item-on-cursor]
-      [:tx/set-item eid cell item-on-cursor]
-      [:tx/event eid :dropped-item]]
-
-     ; STACK ITEMS
-     (and item-in-cell
-          (inventory/stackable? item-in-cell item-on-cursor))
-     [[:tx/sound "bfxr_itemput"]
-      [:tx/dissoc eid :entity/item-on-cursor]
-      [:tx/stack-item eid cell item-on-cursor]
-      [:tx/event eid :dropped-item]]
-
-     ; SWAP ITEMS
-     (and item-in-cell
-          (inventory/valid-slot? cell item-on-cursor))
-     [[:tx/sound "bfxr_itemput"]
-      ; need to dissoc and drop otherwise state enter does not trigger picking it up again
-      ; TODO? coud handle pickup-item from item-on-cursor state also
-      [:tx/dissoc eid :entity/item-on-cursor]
-      [:tx/remove-item eid cell]
-      [:tx/set-item eid cell item-on-cursor]
-      [:tx/event eid :dropped-item]
-      [:tx/event eid :pickup-item item-in-cell]])))
