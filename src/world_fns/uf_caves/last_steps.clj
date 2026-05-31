b7922b3726ad8e553ddab29e43bfd8c8f6e85579
Michael Sappler <michaelsappler@gmail.com>
2026-05-31 18:38:23 +0530

    position-tile-fn

--- src/world_fns/uf_caves/last_steps.clj
+++ src/world_fns/uf_caves/last_steps.clj
@@ -1,46 +1,12 @@
 (ns world-fns.uf-caves.last-steps
-  (:require [clojure.rand :as rand]
-            [clojure.grid2d.flood-fill :as flood-fill]
+  (:require [clojure.grid2d.flood-fill :as flood-fill]
+            [world-fns.uf-caves.position-tile-fn :as position-tile-fn]
             [clojure.grid2d.adjacent-wall-positions :as adjacent-wall-positions]
             [gdx.tiled-map.movement-property :as movement-property]
             [moon.grid2d :as g2d]
             [clojure.grid2d.printgrid :as printgrid]
             [gdx.tiled-map.create :as create-tiled-map]
             [gdx.tiled-map.add-creatures-layer :as add-creatures-layer]))
-
-(defn- position->tile-fn [grid]
-  (let [uf-grounds (for [x [1 5]
-                         y (range 5 11)
-                         :when (not= [x y] [5 5])] ; wooden
-                     [x y])
-        uf-walls (for [x [1]
-                       y [13,16,19,22,25,28]]
-                   [x y])
-        transition? (fn [[x y]]
-                      (= :ground (get grid [x (dec y)])))
-        rand-0-3 (fn [] (rand/get-rand-weighted-item {0 60 1 1 2 1 3 1}))
-        rand-0-5 (fn [] (rand/get-rand-weighted-item {0 30 1 1 2 1 3 1 4 1 5 1}))
-        [ground-x ground-y] (rand-nth uf-grounds)
-        {wall-x 0 wall-y 1} (rand-nth uf-walls)
-        [transition-x transition-y] [wall-x (inc wall-y)]
-        wall-tile (fn []
-                    {:sprite-idx [(+ wall-x (rand-0-5)) wall-y]
-                     :movement "none"})
-        transition-tile (fn []
-                          {:sprite-idx [(+ transition-x (rand-0-5))
-                                        transition-y]
-                           :movement "none"})
-        ground-tile (fn []
-                      {:sprite-idx [(+ ground-x (rand-0-3))
-                                    ground-y]
-                       :movement "all"})]
-    (fn [position]
-      (case (get grid position)
-        :wall (wall-tile)
-        :transition (if (transition? position)
-                      (transition-tile)
-                      (wall-tile))
-        :ground (ground-tile)))))
 
 (defn- scale-grid [grid start scale]
   (let [grid (g2d/scalegrid grid scale)]
@@ -73,12 +39,9 @@
     :as lvlctx
     }]
   (assert (= #{:wall :ground} (set (g2d/cells grid))))
-  (let [
-        {:keys [start-position grid]} (scale-grid grid start scaling)
-
+  (let [{:keys [start-position grid]} (scale-grid grid start scaling)
         grid (assoc-transition-cells grid)
-
-        position->tile (position->tile-fn grid)
+        position->tile (position-tile-fn/f grid)
         tiled-map (create-tiled-map/f
                    {:properties {"width"  (g2d/width  grid)
                                  "height" (g2d/height grid)
