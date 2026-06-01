566cd1f5e02b9977b77da0375153082f2d1a6d07
Michael Sappler <michaelsappler@gmail.com>
2026-06-01 20:50:49 +0530

    integrate add-listener

--- src/clojure/gdx/scene2d/actor/add_listener.clj
+++ src/clojure/gdx/scene2d/actor/add_listener.clj
@@ -1,6 +1,32 @@
 (ns clojure.gdx.scene2d.actor.add-listener
-  (:require [clojure.gdx.scene2d.listener :as listener])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor)))
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)
+           (com.badlogic.gdx.scenes.scene2d.ui Skin
+                                               TextTooltip)
+           (com.badlogic.gdx.scenes.scene2d.utils ChangeListener
+                                                  ClickListener)))
 
-(defn add-listener! [actor [listener-k listener-params]]
-  (Actor/.addListener actor (listener/create [listener-k listener-params])))
+(defmulti create
+  (fn [[listener-k listener-params]]
+    listener-k))
+
+(defmethod create
+  :listener/text-tooltip
+  [[_ [tooltip skin]]]
+  (TextTooltip. ^String tooltip ^Skin skin))
+
+(defmethod create
+  :listener/change
+  [[_ f]]
+  (proxy [ChangeListener] []
+    (changed [event actor]
+      (f event actor))))
+
+(defmethod create
+  :listener/click
+  [[_ f]]
+  (proxy [ClickListener] []
+    (clicked [event x y]
+      (f event x y))))
+
+(defn add-listener! [actor listener]
+  (Actor/.addListener actor (create listener)))
