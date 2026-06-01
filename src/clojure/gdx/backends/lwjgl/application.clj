d9b2ee4ff353ad0750e535e850d2a7f119759af0
Michael Sappler <michaelsappler@gmail.com>
2026-06-01 22:14:45 +0530

    superdoper

--- src/clojure/gdx/backends/lwjgl/application.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure.gdx.backends.lwjgl.application
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)))
-
-(defn create [listener config]
-  (Lwjgl3Application. listener config))
--- src/gdx/backends/lwjgl.clj
+++ src/gdx/backends/lwjgl.clj
@@ -1,23 +1,33 @@
 (ns gdx.backends.lwjgl
-  (:require [clojure.gdx :as gdx]
-            [clojure.gdx.application-listener :as application-listener]
-            [clojure.gdx.backends.lwjgl.application :as lwjgl3-application]
-            [clojure.gdx.backends.lwjgl.application-config :as lwjgl3-application-configuration]))
+  (:import (com.badlogic.gdx ApplicationListener
+                             Gdx)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration)))
 
-(def use-glfw-async! lwjgl3-application-configuration/use-glfw-async!)
+(defn use-glfw-async! []
+  (Lwjgl3ApplicationConfiguration/useGlfwAsync))
 
-(defn application! [config]
-  (lwjgl3-application/create (application-listener/create
-                              (let [state @(:state-var config)]
-                                {:create!  (fn []
-                                             (let [app (gdx/app)]
-                                               (reset! state ((:create! config) app (:create-params config)))))
-                                 :dispose! (fn []
-                                             ((:dispose! config) @state))
-                                 :render!  (fn []
-                                             (swap! state (:render! config) (:render-params config)))
-                                 :resize!  (fn [width height]
-                                             ((:resize! config) @state width height))
-                                 :pause!   (fn [])
-                                 :resume!  (fn [])}))
-                             (lwjgl3-application-configuration/create config)))
+(defn application!
+  [{:keys [title windowed-mode foreground-fps]
+    :as config}]
+  (Lwjgl3Application. (let [state @(:state-var config)]
+                        (reify ApplicationListener
+                          (create [_]
+                            (reset! state ((:create! config) Gdx/app (:create-params config))))
+
+                          (dispose [_]
+                            ((:dispose! config) @state))
+
+                          (render [_]
+                            (swap! state (:render! config) (:render-params config)))
+
+                          (resize [_ width height]
+                            ((:resize! config) @state width height))
+
+                          (pause [_])
+
+                          (resume [_])))
+                      (doto (Lwjgl3ApplicationConfiguration.)
+                        (.setTitle title)
+                        (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
+                        (.setForegroundFPS foreground-fps))))
