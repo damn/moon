1efb01a5c32a01838054bcc2c241d6345b11bf62
Michael Sappler <michaelsappler@gmail.com>
2026-06-01 18:56:39 +0530

    Remove type hint

--- src/com/badlogic/gdx/math/vector2.clj
+++ src/com/badlogic/gdx/math/vector2.clj
@@ -1,7 +1,7 @@
 (ns com.badlogic.gdx.math.vector2
   (:import (com.badlogic.gdx.math Vector2)))
 
-(defn ->java ^Vector2 [[x y]]
+(defn create [[x y]]
   (Vector2. x y))
 
 (defn ->clj [^Vector2 v2]
