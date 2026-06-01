5ec6c34d36d63cb65ddabd503f6909f00dcc1073
Michael Sappler <michaelsappler@gmail.com>
2026-06-01 19:07:09 +0530

    clojure.audio.sound

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -1,5 +1,12 @@
 (ns clojure.gdx
-  (:import (com.badlogic.gdx Gdx)))
+  (:require clojure.audio.sound)
+  (:import (com.badlogic.gdx Gdx)
+           (com.badlogic.gdx.audio Sound)))
 
 (defn app []
   Gdx/app)
+
+(extend-type Sound
+  clojure.audio.sound/Sound
+  (play! [this]
+    (.play this)))
