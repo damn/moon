1ff9d4b80fe430d1f64e1db26487384b7e2d3d17
Michael Sappler <michaelsappler@gmail.com>
2026-06-02 22:36:35 +0530

    Remove reaction-tx add skill

--- src/tx/add_skill.clj
+++ src/tx/add_skill.clj
@@ -1,6 +1,8 @@
-(ns tx.add-skill)
+(ns tx.add-skill
+  (:require [reaction-txs.add-skill :as add-skill-reaction]))
 
-(defn do! [_ctx eid {:keys [property/id] :as skill}]
+(defn do! [ctx eid {:keys [property/id] :as skill}]
   {:pre [(not (contains? (:entity/skills @eid) id))]}
   (swap! eid update :entity/skills assoc id skill)
+  (add-skill-reaction/f ctx eid skill)
   nil)
