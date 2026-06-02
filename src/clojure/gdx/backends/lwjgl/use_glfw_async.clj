2b93a04d55cc88128e550c4d8ccb6a8e3fabd2c2
Michael Sappler <michaelsappler@gmail.com>
2026-06-02 17:50:51 +0530

    Write useGlfwAsync in native language

--- src/clojure/gdx/backends/lwjgl/use_glfw_async.clj
+++ src/clojure/gdx/backends/lwjgl/use_glfw_async.clj
@@ -1,5 +1,13 @@
 (ns clojure.gdx.backends.lwjgl.use-glfw-async
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3ApplicationConfiguration)))
+  "Whether to use the \"glfw_async\" library. This method only does something on mac operating system.
+
+  This means you do not have to set the JVM argument \"-XstartOnFirstThread\".
+
+  See: `https://libgdx.com/news/2021/07/devlog-7-lwjgl3#do-i-need-to-do-anything-else`."
+  (:import (com.badlogic.gdx.utils Os
+                                   SharedLibraryLoader)
+           (org.lwjgl.system Configuration)))
 
 (defn f! []
-  (Lwjgl3ApplicationConfiguration/useGlfwAsync))
+  (when (= SharedLibraryLoader/os Os/MacOsX)
+    (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")))
