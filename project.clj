7dbbd1054422199ad9374277db839a66c4c38690
Michael Sappler <michaelsappler@gmail.com>
2026-06-02 20:33:47 +0530

    sort dependencies by name

--- project.clj
+++ project.clj
@@ -1,27 +1,19 @@
 (defproject moon "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
-  :dependencies [
-                 [com.badlogicgames.gdx/gdx "1.14.0"]
-                 [com.badlogicgames.gdx/gdx-platform          "1.14.0" :classifier "natives-desktop"]
+  :dependencies [[com.badlogicgames.gdx/gdx                   "1.14.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3    "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.14.0"]
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.14.0" :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-platform          "1.14.0" :classifier "natives-desktop"]
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
                  [fr.reuz/qrecord "0.1.0"]
                  [metosin/malli "0.13.0"]
-                 [space.earlygrey/shapedrawer "2.6.0"]
+                 [nrepl "0.9.0"]
                  [org.clj-commons/pretty "3.2.0"]
                  [org.clojure/clojure "1.12.0"]
-
-                 ; DEV:
-                 [nrepl "0.9.0"]
                  [org.clojure/tools.namespace "1.3.0"]
-                 ;
-
-                 ; Unused:
-                 [lein-hiera "2.0.0"]
-                 ;
-                 ]
+                 [space.earlygrey/shapedrawer "2.6.0"]
+                 [lein-hiera "2.0.0"]]
   :java-source-paths ["java-src"]
   :source-paths ["src"]
   :resource-paths ["resources/"]
