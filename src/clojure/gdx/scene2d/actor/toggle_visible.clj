bc157578770766eefe8994823c74cba1798e69f6
Michael Sappler <michaelsappler@gmail.com>
2026-06-03 09:39:56 +0530

    Use visible?

--- src/clojure/gdx/scene2d/actor/toggle_visible.clj
+++ src/clojure/gdx/scene2d/actor/toggle_visible.clj
@@ -1,6 +1,6 @@
 (ns clojure.gdx.scene2d.actor.toggle-visible
-  (:require [clojure.gdx.scene2d.actor :refer [set-visible!]])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor)))
+  (:require [clojure.gdx.scene2d.actor :refer [set-visible!
+                                               visible?]]))
 
-(defn toggle-visible! [^Actor actor]
-  (set-visible! actor (not (.isVisible actor))))
+(defn toggle-visible! [actor]
+  (set-visible! actor (not (visible? actor))))
