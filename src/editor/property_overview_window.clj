99fce68d0b71f67412e91812b05b847df28e0abc
Michael Sappler <michaelsappler@gmail.com>
2026-06-03 20:40:52 +0530

    split property-overview-window

--- src/editor/property_overview_window.clj
+++ src/editor/property_overview_window.clj
@@ -1,40 +1,10 @@
 (ns editor.property-overview-window
-  (:require [clojure.gdx.scene2d.actor.set-touchable :refer [set-touchable!]]
-            [clojure.gdx.scene2d.actor.add-listener :refer [add-listener!]]
-            [clojure.gdx.scene2d.group.add-actor :refer [add-actors!]]
-            [clojure.gdx.scene2d.event :as event]
-            [editor.constants :refer [property-type->overview-table-props]]
-            [gdx.scenes.scene2d.ui.label :as label]
-            [clojure.gdx.scene2d.ui.stack :as stack]
-            [gdx.scenes.scene2d.ui.image-button :as image-button]
+  (:require [editor.constants :refer [property-type->overview-table-props]]
+            [editor.property-overview-window.table-rows :refer [overview-table-rows*]]
             [gdx.scenes.scene2d.ui.window :as window]
-            [clojure.gdx.scene2d.ui.text-tooltip :as text-tooltip]
-            [clojure.gdx.scene2d.utils.change-listener :as change-listener]
-            [gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]
-            [clojure.gdx.scene2d.touchable :as touchable]
             [moon.db :as db]
             [moon.property :as property]
             [moon.textures :as textures]))
-
-(defn- overview-table-rows* [skin image-scale rows]
-  (for [row rows]
-    (for [{:keys [texture-region
-                  on-clicked
-                  tooltip
-                  extra-info-text]} row]
-      {:actor (doto (stack/create)
-                (add-actors! [(doto (image-button/create
-                                     {:drawable (texture-region-drawable/create*
-                                                 {:drawable/texture-region texture-region
-                                                  :drawable/scale image-scale})})
-                                (add-listener! (change-listener/create
-                                                (fn [event actor]
-                                                  (on-clicked actor (:stage/ctx (event/stage event))))))
-                                (add-listener! (text-tooltip/create tooltip skin)))
-                              (doto (label/create
-                                     {:text extra-info-text
-                                      :skin skin})
-                                (set-touchable! touchable/disabled))]))})))
 
 (defn- overview-table-rows
   [db
---
+++ src/editor/property_overview_window/table_rows.clj
@@ -0,0 +1,32 @@
+(ns editor.property-overview-window.table-rows
+  (:require [clojure.gdx.scene2d.actor.add-listener :refer [add-listener!]]
+            [clojure.gdx.scene2d.actor.set-touchable :refer [set-touchable!]]
+            [clojure.gdx.scene2d.event :as event]
+            [clojure.gdx.scene2d.group.add-actor :refer [add-actors!]]
+            [clojure.gdx.scene2d.touchable :as touchable]
+            [clojure.gdx.scene2d.ui.stack :as stack]
+            [clojure.gdx.scene2d.ui.text-tooltip :as text-tooltip]
+            [clojure.gdx.scene2d.utils.change-listener :as change-listener]
+            [gdx.scenes.scene2d.ui.image-button :as image-button]
+            [gdx.scenes.scene2d.ui.label :as label]
+            [gdx.scenes.scene2d.utils.texture-region-drawable :as texture-region-drawable]))
+
+(defn overview-table-rows* [skin image-scale rows]
+  (for [row rows]
+    (for [{:keys [texture-region
+                  on-clicked
+                  tooltip
+                  extra-info-text]} row]
+      {:actor (doto (stack/create)
+                (add-actors! [(doto (image-button/create
+                                     {:drawable (texture-region-drawable/create*
+                                                 {:drawable/texture-region texture-region
+                                                  :drawable/scale image-scale})})
+                                (add-listener! (change-listener/create
+                                                (fn [event actor]
+                                                  (on-clicked actor (:stage/ctx (event/stage event))))))
+                                (add-listener! (text-tooltip/create tooltip skin)))
+                              (doto (label/create
+                                     {:text extra-info-text
+                                      :skin skin})
+                                (set-touchable! touchable/disabled))]))})))
