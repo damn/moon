169b7dc6c8505db6c61d9e8d14a5f8f9018b8f9b
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 04:59:45 +0530

    pass draw fns to draw on world viewport

---
+++ src/draw_on_world_viewport/draw_tile_grid.clj
@@ -0,0 +1,15 @@
+(ns draw-on-world-viewport.draw-tile-grid
+  (:require [clojure.gdx.graphics.color.float-bits :refer [float-bits]]
+            [gdx.graphics.orthographic-camera :as camera]))
+
+(defn f
+  [{:keys [ctx/world-viewport]}]
+  (let [[left-x _right-x bottom-y _top-y] (camera/frustum (:viewport/camera world-viewport))]
+    [[:draw/grid
+      (int left-x)
+      (int bottom-y)
+      (inc (int (:viewport/world-width world-viewport)))
+      (+ 2 (int (:viewport/world-height world-viewport)))
+      1
+      1
+      (float-bits [1 1 1 0.8])]]))
--- src/render/draw_on_world_viewport.clj
+++ src/render/draw_on_world_viewport.clj
@@ -1,34 +1,7 @@
 (ns render.draw-on-world-viewport
-  (:require [clojure.gdx.graphics.color.float-bits :refer [float-bits]]
-            [draw-on-world-viewport.draw-entities]
-            [draw-on-world-viewport.draw-cell-debug]
-            [game.ctx.draw :refer [draw!]]
+  (:require [game.ctx.draw :refer [draw!]]
             [gdx.graphics.orthographic-camera :as camera]
             [space.earlygrey.shape-drawer :as shape-drawer]))
-
-(defn draw-tile-grid
-  [{:keys [ctx/world-viewport]}]
-  (let [[left-x _right-x bottom-y _top-y] (camera/frustum (:viewport/camera world-viewport))]
-    [[:draw/grid
-      (int left-x)
-      (int bottom-y)
-      (inc (int (:viewport/world-width world-viewport)))
-      (+ 2 (int (:viewport/world-height world-viewport)))
-      1
-      1
-      (float-bits [1 1 1 0.8])]]))
-
-(defn highlight-mouseover-tile
-  [{:keys [ctx/colors
-           ctx/grid
-           ctx/world-mouse-position]}]
-  (let [[x y] (mapv int world-mouse-position)
-        cell (grid [x y])]
-    (when (and cell (#{:air :none} (:movement @cell)))
-      [[:draw/rectangle x y 1 1
-        (case (:movement @cell)
-          :air  (:colors/mouseover-tile-air colors)
-          :none (:colors/mouseover-tile-none colors))]])))
 
 (defn step
   [{:keys [^com.badlogic.gdx.graphics.g2d.SpriteBatch ctx/batch
@@ -36,7 +9,8 @@
            ctx/unit-scale
            ctx/world-unit-scale
            ctx/world-viewport]
-    :as ctx}]
+    :as ctx}
+   draw-fns]
   ; fix scene2d.ui.tooltip flickering
   ; _everything_ flickers with TextToolTip!
   ; it changes batch color somehow and does not change it back ! FIXME
@@ -47,13 +21,7 @@
   (let [old-line-width (shape-drawer/default-line-width shape-drawer)]
     (shape-drawer/set-default-line-width! shape-drawer (* world-unit-scale old-line-width))
     (reset! unit-scale world-unit-scale)
-    (doseq [f [
-               #_draw-tile-grid
-               draw-on-world-viewport.draw-cell-debug/f
-               draw-on-world-viewport.draw-entities/do!
-               #_moon.geom-test
-               highlight-mouseover-tile
-               ]]
+    (doseq [f draw-fns]
       (draw! ctx (f ctx)))
     (reset! unit-scale 1)
     (shape-drawer/set-default-line-width! shape-drawer old-line-width))
