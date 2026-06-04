fde9b1207d377d506aee32cc1943cad41fe8745a
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 04:22:31 +0530

    Remove gdx.stage

--- src/game/ctx/mouseover_actor.clj
+++ src/game/ctx/mouseover_actor.clj
@@ -1,8 +1,12 @@
 (ns game.ctx.mouseover-actor
-  (:require [game.ctx.mouse-position :refer [mouse-position]]
-            [gdx.stage :as stage]))
+  (:require [clojure.gdx.scene2d.stage.hit :refer [hit]]
+            [clojure.gdx.utils.viewport :as viewport]
+            [game.ctx.mouse-position :refer [mouse-position]]))
 
 (defn mouseover-actor
   [{:keys [ctx/stage]
     :as ctx}]
-  (stage/mouseover-actor stage (mouse-position ctx)))
+  (hit stage
+       (-> stage
+           :stage/viewport
+           (viewport/unproject (mouse-position ctx)))))
--- src/gdx/stage.clj
+++
@@ -1,9 +0,0 @@
-(ns gdx.stage
-  (:require [clojure.gdx.utils.viewport :as viewport]
-            [clojure.gdx.scene2d.stage.hit :refer [hit]]))
-
-(defn mouseover-actor [stage position]
-  (hit stage
-       (-> stage
-           :stage/viewport
-           (viewport/unproject position))))
