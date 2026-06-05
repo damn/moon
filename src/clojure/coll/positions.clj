7e6c3a1a6039394c0489e3516f14c08d34218f47
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 19:12:56 +0530

    clojure.coll.* -> clojure.*

--- src/clojure/coll/positions.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure.coll.positions
-  (:require [clojure.coll.indexed :as indexed]))
-
-(defn f
-  "Returns a lazy sequence containing the positions at which pred
-  is true for items in coll."
-  [pred coll]
-  (for [[idx elt] (indexed/f coll) :when (pred elt)] idx))
--- src/moon/grid/npc_pathing/diagonal_check_indizes.clj
+++ src/moon/grid/npc_pathing/diagonal_check_indizes.clj
@@ -1,11 +1,11 @@
 (ns moon.grid.npc-pathing.diagonal-check-indizes
-  (:require [clojure.coll.positions :as positions]
+  (:require [clojure.positions :refer [positions]]
             [clojure.math.vector2 :as v]
             [clojure.math.position.get-8-neighbours :refer [get-8-neighbours]]))
 
 (let [order (get-8-neighbours [0 0])]
   (def v
     (into {} (for [[x y] (filter v/diagonal-direction? order)]
-               [(first (positions/f #(= % [x y]) order))
-                (vec (positions/f #(some #{%} [[x 0] [0 y]])
-                                  order))]))))
+               [(first (positions #(= % [x y]) order))
+                (vec (positions #(some #{%} [[x 0] [0 y]])
+                                order))]))))
