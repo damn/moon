cb0d3e1920aea165e51b7c82ca1e0b01f56cd354
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 21:57:42 +0530

    add comment

--- src/clojure/maps/tiled/tmx_map_loader.clj
+++ src/clojure/maps/tiled/tmx_map_loader.clj
@@ -1,5 +1,5 @@
 (ns clojure.maps.tiled.tmx-map-loader
   (:import (com.badlogic.gdx.maps.tiled TmxMapLoader)))
 
-(defn load! [tmx-file]
+(defn load! [tmx-file] ; TODO clojure.file.load-tmx-map!
   (.load (TmxMapLoader.) tmx-file))
