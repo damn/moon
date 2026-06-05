c32d8dbaad8d4fe69815e4bb80719fc3316db0a6
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 18:55:00 +0530

    Fix get-region-width

--- src/gdx/scenes/scene2d/utils/texture_region_drawable.clj
+++ src/gdx/scenes/scene2d/utils/texture_region_drawable.clj
@@ -1,6 +1,6 @@
 (ns gdx.scenes.scene2d.utils.texture-region-drawable
   (:require [clojure.gdx.graphics.color :refer [Color]]
-            [clojure.gdx.graphics.g2d.texture-region.get-region-height :refer [get-region-width]]
+            [clojure.gdx.graphics.g2d.texture-region.get-region-height :refer [get-region-height]]
             [clojure.gdx.graphics.g2d.texture-region.get-region-width :refer [get-region-width]]
             [clojure.gdx.scene2d.utils.texture-region-drawable :as drawable]))
 
@@ -18,5 +18,5 @@
   [{:keys [drawable/texture-region
            drawable/scale]}]
   (doto (drawable/create texture-region)
-    (drawable/set-min-size! (* scale (texture-region/width texture-region))
+    (drawable/set-min-size! (* scale (get-region-width texture-region))
                             (* scale (get-region-height texture-region)))))
