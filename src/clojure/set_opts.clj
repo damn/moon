498eebfe1094580946a0ed36fab0d145c431393a
Michael Sappler <michaelsappler@gmail.com>
2026-06-05 19:59:09 +0530

    cell in set-opts

--- src/clojure/set_opts.clj
+++ src/clojure/set_opts.clj
@@ -1,4 +1,26 @@
-(ns clojure.set-opts)
+(ns clojure.set-opts
+  (:import (com.badlogic.gdx.scenes.scene2d.ui Cell)))
 
 (defprotocol SetOpts
   (set-opts! [_ opts]))
+
+(extend-type Cell
+  clojure.set-opts/SetOpts
+  (set-opts! [cell opts]
+    (doseq [[option arg] opts]
+      (case option
+        :fill-x?    (.fillX     cell)
+        :fill-y?    (.fillY     cell)
+        :expand?    (.expand    cell)
+        :expand-x?  (.expandX   cell)
+        :expand-y?  (.expandY   cell)
+        :bottom?    (.bottom    cell)
+        :colspan    (.colspan   cell (int arg))
+        :pad        (.pad       cell (float arg))
+        :pad-top    (.padTop    cell (float arg))
+        :pad-bottom (.padBottom cell (float arg))
+        :width      (.width     cell (float arg))
+        :height     (.height    cell (float arg))
+        :center?    (.center    cell)
+        :right?     (.right     cell)
+        :left?      (.left      cell)))))
