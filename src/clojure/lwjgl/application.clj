25b0b4d7152f91f500a7eae34529bf028c8ecd7f
Michael Sappler <michaelsappler@gmail.com>
2026-06-06 20:42:46 +0530

    clojure.gdx,application-listener

--- src/clojure/lwjgl/application.clj
+++ src/clojure/lwjgl/application.clj
@@ -1,10 +1,39 @@
 (ns clojure.lwjgl.application
-  (:require [clojure.application-listener :as listener]
+  (:require [clojure.gdx :as gdx]
+            [clojure.gdx.application-listener :as application-listener]
             [gdx.backends.lwjgl.application :as application]
             [gdx.backends.lwjgl.application-config :as config]))
 
 (defn start!
-  [config]
+  [{:keys [state-var
+           create-pipeline
+           dispose!
+           render-pipeline
+           resize!]
+    :as config}]
   (config/use-glfw-async!)
-  (application/create (listener/create config)
+  (application/create (application-listener/create
+                       (let [state @state-var]
+                         {:create! (fn []
+                                     (reset! state (reduce (fn [ctx [f & params]]
+                                                             (apply f ctx params))
+                                                           {:ctx/app (gdx/app)}
+                                                           create-pipeline)))
+
+                          :dispose! (fn []
+                                      (dispose! @state))
+
+                          :render! (fn []
+                                     (swap! state (fn [ctx]
+                                                    (reduce (fn [ctx [f & params]]
+                                                              (apply f ctx params))
+                                                            ctx
+                                                            render-pipeline))))
+
+                          :resize! (fn [width height]
+                                     (resize! @state width height))
+
+                          :pause! (fn [])
+
+                          :resume! (fn [])}))
                       (config/create config)))
