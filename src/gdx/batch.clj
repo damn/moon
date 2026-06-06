da5797a5fe044df9156649877ba3c8569fd46df2
Michael Sappler <michaelsappler@gmail.com>
2026-06-06 22:50:59 +0530

    batch/setup-drawing!

--- src/gdx/batch.clj
+++ src/gdx/batch.clj
@@ -17,3 +17,14 @@
           rotation))
   ([^Batch batch ^TextureRegion texture-region x y w h]
    (.draw batch texture-region (float x) (float y) (float w) (float h))))
+
+(defn setup-drawing! [^Batch batch projection-matrix f]
+  ; fix scene2d.ui.tooltip flickering
+  ; _everything_ flickers with TextToolTip!
+  ; it changes batch color somehow and does not change it back ! FIXME
+  (.setColor batch 1 1 1 1)
+  ;
+  (.setProjectionMatrix batch projection-matrix)
+  (.begin batch)
+  (f)
+  (.end batch))
--- src/render/draw_on_world_viewport.clj
+++ src/render/draw_on_world_viewport.clj
@@ -1,29 +1,26 @@
 (ns render.draw-on-world-viewport
   (:require [game.ctx.draw :refer [draw!]]
+            [gdx.batch :as batch]
             [gdx.graphics.orthographic-camera :as camera]
             [gdx.shape-drawer :as shape-drawer]))
 
 (defn step
-  [{:keys [^com.badlogic.gdx.graphics.g2d.SpriteBatch ctx/batch
+  [{:keys [ctx/batch
            ctx/shape-drawer
            ctx/unit-scale
            ctx/world-unit-scale
            ctx/world-viewport]
     :as ctx}
    draw-fns]
-  ; fix scene2d.ui.tooltip flickering
-  ; _everything_ flickers with TextToolTip!
-  ; it changes batch color somehow and does not change it back ! FIXME
-  (.setColor batch 1 1 1 1)
-  ;
-  (.setProjectionMatrix batch (camera/combined (:viewport/camera world-viewport)))
-  (.begin batch)
-  (let [old-line-width (shape-drawer/default-line-width shape-drawer)]
-    (shape-drawer/set-default-line-width! shape-drawer (* world-unit-scale old-line-width))
-    (reset! unit-scale world-unit-scale)
-    (doseq [f draw-fns]
-      (draw! ctx (f ctx)))
-    (reset! unit-scale 1)
-    (shape-drawer/set-default-line-width! shape-drawer old-line-width))
-  (.end batch)
+  (batch/setup-drawing! batch
+                        (camera/combined (:viewport/camera world-viewport))
+                        (fn []
+                          (let [old-line-width (shape-drawer/default-line-width shape-drawer)]
+                            (shape-drawer/set-default-line-width! shape-drawer (* world-unit-scale old-line-width))
+                            (reset! unit-scale world-unit-scale)
+                            (doseq [f draw-fns]
+                              (draw! ctx (f ctx)))
+                            (reset! unit-scale 1)
+                            (shape-drawer/set-default-line-width! shape-drawer old-line-width))                        ))
+
   ctx)
