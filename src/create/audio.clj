ffe5dd5e89968db90ff316a8a94dee8aeadfec10
Michael Sappler <michaelsappler@gmail.com>
2026-06-06 21:08:25 +0530

    clojure.m.assoc-create/f :ctx/audio

--- src/create/audio.clj
+++ src/create/audio.clj
@@ -4,8 +4,8 @@
             [game.ctx.create-sound :refer [create-sound]]))
 
 (defn step [ctx]
-  (assoc ctx :ctx/audio (into {}
-                              (for [sound-name (-> "config/sounds.edn" io/resource slurp edn/read-string)
-                                    :let [path (format "sounds/%s.wav" sound-name)]]
-                                [sound-name
-                                 (create-sound ctx path)]))))
+  (into {}
+        (for [sound-name (-> "config/sounds.edn" io/resource slurp edn/read-string)
+              :let [path (format "sounds/%s.wav" sound-name)]]
+          [sound-name
+           (create-sound ctx path)])))
