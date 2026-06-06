7bc9d92cea4435fcc0b9059393a62d8cbc129909
Michael Sappler <michaelsappler@gmail.com>
2026-06-06 21:12:10 +0530

    clojure.white-pixel-texture

---
+++ src/clojure/white_pixel_texture.clj
@@ -0,0 +1,10 @@
+(ns clojure.white-pixel-texture
+  (:require [clojure.pixmap :as pixmap]))
+
+(defn f []
+  (let [pixmap (doto (pixmap/create 1 1)
+                 (pixmap/set-color! 1 1 1 1)
+                 (pixmap/draw-pixel! 0 0))
+        texture (pixmap/texture pixmap)]
+    (pixmap/dispose! pixmap)
+    texture))
--- src/create/shape_drawer_texture.clj
+++ src/create/shape_drawer_texture.clj
@@ -1,11 +1,5 @@
 (ns create.shape-drawer-texture
-  (:require [clojure.pixmap :as pixmap]))
+  (:require [clojure.white-pixel-texture :as white-pixel-texture]))
 
 (defn step [ctx]
-  (assoc ctx :ctx/shape-drawer-texture
-         (let [pixmap (doto (pixmap/create 1 1)
-                        (pixmap/set-color! 1 1 1 1)
-                        (pixmap/draw-pixel! 0 0))
-               texture (pixmap/texture pixmap)]
-           (pixmap/dispose! pixmap)
-           texture)))
+  (assoc ctx :ctx/shape-drawer-texture (white-pixel-texture/f)))
