33f7ebaae01790edbb38810882de0fd2f8d113a6
Michael Sappler <michaelsappler@gmail.com>
2026-06-07 21:45:45 +0530

    tx/ui-remove-item

--- src/tx/remove_item.clj
+++ src/tx/remove_item.clj
@@ -1,14 +1,13 @@
 (ns tx.remove-item
   (:require [moon.inventory :as inventory]
-            [moon.stats :as stats]
-            [reaction-txs.remove-item :as remove-item-reaction]))
+            [moon.stats :as stats]))
 
 (defn do! [ctx eid cell]
   (let [entity @eid
         item (get-in (:entity/inventory entity) cell)]
     (assert item)
-    (swap! eid assoc-in (cons :entity/inventory cell) nil)
-    (when (inventory/applies-modifiers? cell)
-      (swap! eid update :entity/stats stats/remove-mods (:stats/modifiers item)))
-    (remove-item-reaction/f ctx eid cell)
-    nil))
+    [[:tx/assoc-in eid (cons :entity/inventory cell) nil]
+     (when (inventory/applies-modifiers? cell)
+       [:tx/update eid :entity/stats stats/remove-mods (:stats/modifiers item)])
+     (when (:entity/player? @eid)
+       [:tx/ui-remove-item eid cell])]))
---
+++ src/tx/ui_remove_item.clj
@@ -0,0 +1,11 @@
+(ns tx.ui-remove-item
+  (:require [com.badlogic.gdx.scenes.scene2d.group.find-actor :refer [find-actor]]
+            [moon.ui.inventory-window :as inventory-window]))
+
+(defn f
+  [{:keys [ctx/stage] :as ctx} eid cell]
+  (-> stage
+      :stage/root
+      (find-actor "moon.ui.windows.inventory")
+      (inventory-window/remove-item! cell))
+  nil)
