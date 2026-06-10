df1e7d29887b52377fd3a3cb72bf5685ebc54fa7
Michael Sappler <michaelsappler@gmail.com>
2026-06-10 21:49:39 +0530

    add comment

--- src/game/constants.clj
+++ src/game/constants.clj
@@ -1,5 +1,10 @@
 (ns game.constants)
 
+; if I set as ctx/key
+; then at beginning render stage/get-ctx :stage/ctx
+; sets it to old one
+; even if I use postRunnable
+; so we need a solution
 (def ^:dbg-flag show-potential-field-colors? false) ; :good, :evil
 (def ^:dbg-flag show-cell-entities? false)
 (def ^:dbg-flag show-cell-occupied? false)
