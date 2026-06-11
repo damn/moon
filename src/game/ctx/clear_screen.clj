62fc37f329f40a706a2289dd1966ed6fe7479519
Michael Sappler <michaelsappler@gmail.com>
2026-06-11 21:30:17 +0530

    com.badlogic.gdx.graphics.clear/f!

--- src/game/ctx/clear_screen.clj
+++ src/game/ctx/clear_screen.clj
@@ -1,9 +1,6 @@
 (ns game.ctx.clear-screen
-  (:require [com.badlogic.gdx.graphics.get-gl20 :as get-gl20]
-            [com.badlogic.gdx.graphics.gl20 :as gl20]))
+  (:require [com.badlogic.gdx.graphics.clear :as clear]))
 
 (defn clear-screen!
   [{:keys [ctx/graphics]} r g b a]
-  (let [gl (get-gl20/f graphics)]
-    (gl20/clear-color! gl r g b a)
-    (gl20/clear! gl gl20/color-buffer-bit)))
+  (clear/f! graphics r g b a))
