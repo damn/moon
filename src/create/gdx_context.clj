8ee94a4bb4e5b7cc796a4be28b07db79c89fd497
Michael Sappler <michaelsappler@gmail.com>
2026-06-17 21:34:53 +0530

    Split gdx.listener

---
+++ src/create/gdx_context.clj
@@ -0,0 +1,9 @@
+(ns create.gdx-context
+  (:require [com.badlogic.gdx.gdx :as gdx]))
+
+(defn f [ctx]
+  (assoc ctx
+         :ctx/audio    (gdx/audio)
+         :ctx/files    (gdx/files)
+         :ctx/graphics (gdx/graphics)
+         :ctx/input    (gdx/input)))
--- src/gdx/listener.clj
+++ src/gdx/listener.clj
@@ -1,6 +1,10 @@
 (ns gdx.listener
-  (:require [com.badlogic.gdx.application-listener :refer [application-listener]]
-            [com.badlogic.gdx.gdx :as gdx]))
+  (:require [com.badlogic.gdx.application-listener :refer [application-listener]]))
+
+; TODO
+; 1. move application-listener one up
+; 2. initial context? @ state-var ?
+; 3. dispose/resize pipelined
 
 (defn f
   [{:keys [state-var
@@ -13,12 +17,7 @@
      {:create! (fn []
                  (reset! state (reduce (fn [ctx [f & params]]
                                          (apply f ctx params))
-                                       {
-                                        :ctx/audio    (gdx/audio)
-                                        :ctx/files    (gdx/files)
-                                        :ctx/graphics (gdx/graphics)
-                                        :ctx/input    (gdx/input)
-                                        }
+                                       {}
                                        create-pipeline)))
 
       :dispose! (fn []
