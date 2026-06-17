0017d53b40ccb1db989535466d129b14e7ce00c6
Michael Sappler <michaelsappler@gmail.com>
2026-06-17 21:28:02 +0530

    split listener

--- src/gdx/application.clj
+++ src/gdx/application.clj
@@ -1,43 +1,10 @@
 (ns gdx.application
-  (:require [com.badlogic.gdx.application-listener :refer [application-listener]]
-            [com.badlogic.gdx.backends.lwjgl.application :as application]
-            [com.badlogic.gdx.backends.lwjgl.application-config :as config]
-            [com.badlogic.gdx.gdx :as gdx]))
+  (:require [com.badlogic.gdx.backends.lwjgl.application :as application]
+            [com.badlogic.gdx.backends.lwjgl.application-config :as config]))
 
 (defn f!
-  [{:keys [state-var
-           create-pipeline
-           dispose!
-           render-pipeline
-           resize!]
+  [{:keys [listener]
     :as config}]
-  (application/create (application-listener
-                       (let [state @state-var]
-                         {:create! (fn []
-                                     (reset! state (reduce (fn [ctx [f & params]]
-                                                             (apply f ctx params))
-                                                           {
-                                                            :ctx/audio    (gdx/audio)
-                                                            :ctx/files    (gdx/files)
-                                                            :ctx/graphics (gdx/graphics)
-                                                            :ctx/input    (gdx/input)
-                                                            }
-                                                           create-pipeline)))
-
-                          :dispose! (fn []
-                                      (dispose! @state))
-
-                          :render! (fn []
-                                     (swap! state (fn [ctx]
-                                                    (reduce (fn [ctx [f & params]]
-                                                              (apply f ctx params))
-                                                            ctx
-                                                            render-pipeline))))
-
-                          :resize! (fn [width height]
-                                     (resize! @state width height))
-
-                          :pause! (fn [])
-
-                          :resume! (fn [])}))
+  (application/create (let [[f params] listener]
+                        (f params))
                       (config/create config)))
---
+++ src/gdx/listener.clj
@@ -0,0 +1,39 @@
+(ns gdx.listener
+  (:require [com.badlogic.gdx.application-listener :refer [application-listener]]
+            [com.badlogic.gdx.gdx :as gdx]))
+
+(defn f
+  [{:keys [state-var
+           create-pipeline
+           dispose!
+           render-pipeline
+           resize!]}]
+  (application-listener
+   (let [state @state-var]
+     {:create! (fn []
+                 (reset! state (reduce (fn [ctx [f & params]]
+                                         (apply f ctx params))
+                                       {
+                                        :ctx/audio    (gdx/audio)
+                                        :ctx/files    (gdx/files)
+                                        :ctx/graphics (gdx/graphics)
+                                        :ctx/input    (gdx/input)
+                                        }
+                                       create-pipeline)))
+
+      :dispose! (fn []
+                  (dispose! @state))
+
+      :render! (fn []
+                 (swap! state (fn [ctx]
+                                (reduce (fn [ctx [f & params]]
+                                          (apply f ctx params))
+                                        ctx
+                                        render-pipeline))))
+
+      :resize! (fn [width height]
+                 (resize! @state width height))
+
+      :pause! (fn [])
+
+      :resume! (fn [])})))
