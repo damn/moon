ef649fd3713ce6620d06227775c03d1fcac5f9a4
Michael Sappler <michaelsappler@gmail.com>
2026-06-19 14:36:30 +0530

    Remove game.ctx.create-stage

--- src/create/stage.clj
+++ src/create/stage.clj
@@ -1,6 +1,11 @@
 (ns create.stage
-  (:require [game.ctx.create-stage :refer [create-stage]]))
+  (:require [com.badlogic.gdx.input.set-processor :as set-processor!]
+            [com.badlogic.gdx.utils.fit-viewport :as fit-viewport]
+            [com.badlogic.gdx.scenes.scene2d.stage :as stage]))
 
 (defn step
-  [ctx]
-  (create-stage ctx 1440 900))
+  [{:keys [ctx/input
+           ctx/batch]}]
+  (let [stage (stage/create (fit-viewport/create 1440 900) batch)]
+    (set-processor!/f input stage)
+    stage))
