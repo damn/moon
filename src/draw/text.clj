ed834bf6f1f22d08a60d0a4a7c9fefb8f6d45ecb
Michael Sappler <michaelsappler@gmail.com>
2026-06-21 12:29:48 +0530

    Remove gdx.draw-text

--- src/draw/text.clj
+++ src/draw/text.clj
@@ -1,16 +1,36 @@
 (ns draw.text
-  (:require [gdx.draw-text :refer [draw-text!]]))
+  (:require [clojure.string :as str]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.draw :as draw]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.get-scale-x :as get-scale-x]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.get-line-height :as get-line-height]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.set-scale :as set-scale]
+            [com.badlogic.gdx.utils.align :as align]))
 
 (defn f
   [{:keys [ctx/batch
            ctx/unit-scale
            ctx/default-font]}
    {:keys [font scale x y text up?]}]
-  (draw-text! (or font default-font)
-              batch
-              @unit-scale
-              (or scale 1)
-              text
-              x
-              y
-              up?))
+  (let [font (or font default-font)
+        unit-scale @unit-scale
+        scale (or scale 1)
+        old-scale (get-scale-x/f font)
+        target-width 0
+        wrap? false
+        scale (* (float unit-scale)
+                 (float scale))]
+    (set-scale/f! font (* old-scale scale))
+    (draw/f! font
+             batch
+             text
+             x
+             (+ y (if up?
+                    (-> text
+                        (str/split #"\n")
+                        count
+                        (* (get-line-height/f font)))
+                    0))
+             target-width
+             align/center
+             wrap?)
+    (set-scale/f! font old-scale)))
