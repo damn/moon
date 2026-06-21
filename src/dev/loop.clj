c1aee8e782691baf87f0fd5114c76a759a6a29fb
Michael Sappler <michaelsappler@gmail.com>
2026-06-21 14:48:31 +0530

    Remove unnecessary private at dev.loop

--- src/dev/loop.clj
+++ src/dev/loop.clj
@@ -6,11 +6,11 @@
 
 (clojure.tools.namespace.repl/disable-reload!) ; keep same connection/nrepl-server up throughout refreshs
 
-(def ^:private ^Object obj (Object.))
+(def ^Object obj (Object.))
 
-(def ^:private thrown (atom false))
+(def thrown (atom false))
 
-(defn- handle-throwable! [t]
+(defn handle-throwable! [t]
   (binding [*print-level* 3]
     (clj-commons.pretty.repl/pretty-pst t))
   (reset! thrown t))
@@ -26,11 +26,11 @@
        (.notify obj)))
     (println "\n Application still running! Cannot restart.")))
 
-(declare ^:private refresh-error)
+(declare refresh-error)
 
-(declare ^:no-doc start-app-expression)
+(declare start-app-expression)
 
-(defn ^:no-doc start-dev-loop! []
+(defn start-dev-loop! []
   (try (eval start-app-expression)
        (catch Throwable t
          (handle-throwable! t)))
@@ -46,7 +46,7 @@
     (recur)))
 
 ; ( I dont know why nrepl start-server does not have this included ... )
-(defn- save-port-file!
+(defn save-port-file!
   "Writes a file relative to project classpath with port number so other tools
   can infer the nREPL server port.
   Takes nREPL server map and processed CLI options map.
@@ -58,7 +58,7 @@
     (.deleteOnExit ^java.io.File port-file)
     (spit port-file port)))
 
-(declare ^:private nrepl-server)
+(declare nrepl-server)
 
 (defn -main [start-expression]
   (.bindRoot #'start-app-expression (read-string start-expression))
