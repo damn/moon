ac15de5662db4591648097736641589effda48d3
Michael Sappler <michaelsappler@gmail.com>
2026-06-21 16:19:08 +0530

    forgot file

--- src/file_handle/pixmap.clj
+++ src/file_handle/pixmap.clj
@@ -1,4 +1,5 @@
 (ns file-handle.pixmap
+  "Creates a new Pixmap instance from the given file. The file must be a Png, Jpeg or Bitmap. Paletted formats are not supported."
   (:import (com.badlogic.gdx.files FileHandle)
            (com.badlogic.gdx.graphics Pixmap)))
 
