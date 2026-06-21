841850c12258b0521cf444e39d4031985acee547
Michael Sappler <michaelsappler@gmail.com>
2026-06-21 15:23:48 +0530

    Remove printcell

--- src/clojure/grid2d/printgrid.clj
+++ src/clojure/grid2d/printgrid.clj
@@ -2,23 +2,21 @@
   (:require [clojure.grid2d.height :refer [->height]]
             [clojure.grid2d.width :refer [->width]]))
 
-(defn- print-cell [celltype]
-  (print (if (number? celltype)
-           celltype
-           (case celltype
-             nil               "?"
-             :undefined        " "
-             :ground           "_"
-             :wall             "#"
-             :airwalkable      "."
-             :module-placement "X"
-             :start-module     "@"
-             :transition       "+"))))
-
-(defn f ; print-grid in data.grid2d is y-down
+(defn f
   "Prints with y-up coordinates."
   [grid]
   (doseq [y (range (dec (->height grid)) -1 -1)]
     (doseq [x (range (->width grid))]
-      (print-cell (grid [x y])))
+      (let [celltype (grid [x y])]
+        (print (if (number? celltype)
+                 celltype
+                 (case celltype
+                   nil               "?"
+                   :undefined        " "
+                   :ground           "_"
+                   :wall             "#"
+                   :airwalkable      "."
+                   :module-placement "X"
+                   :start-module     "@"
+                   :transition       "+")))))
     (println)))
--- src/com/badlogic/gdx/scenes/scene2d/ui/image.clj
+++ src/com/badlogic/gdx/scenes/scene2d/ui/image.clj
@@ -6,13 +6,13 @@
 
 (defmulti create class)
 
-(defmethod create Texture [texture]
+(defmethod create Texture [texture] ; new TextureRegionDrawable(new TextureRegion(texture))
   (Image. ^Texture texture))
 
-(defmethod create TextureRegion [texture-region]
+(defmethod create TextureRegion [texture-region] ; new TextureRegionDrawable(region), Scaling.stretch, Align.center
   (Image. ^TextureRegion texture-region))
 
-(defmethod create TextureRegionDrawable [drawable]
+(defmethod create TextureRegionDrawable [drawable] ; this(drawable, Scaling.stretch, Align.center)
   (Image. ^TextureRegionDrawable drawable))
 
 (defn set-drawable! [^Image image drawable]
