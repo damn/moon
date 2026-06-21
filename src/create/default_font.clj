7aa79241b2f2a69d0e101f2d38c2c027c58559ba
Michael Sappler <michaelsappler@gmail.com>
2026-06-21 12:18:48 +0530

    Remove gdx.freetype

--- src/create/default_font.clj
+++ src/create/default_font.clj
@@ -1,10 +1,32 @@
 (ns create.default-font
   (:require [com.badlogic.gdx.files :as files]
-            [gdx.freetype :refer [generate-font]]))
+            [com.badlogic.gdx.graphics.texture.filter :as texture.filter]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.enable-markup :as enable-markup]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.set-scale :as set-scale]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.set-use-integer-positions :as set-use-integer-positions]
+            [com.badlogic.gdx.graphics.g2d.freetype.font-generator :as font-generator]
+            [com.badlogic.gdx.graphics.g2d.freetype.font-generator.generate-font :as generate-font]
+            [com.badlogic.gdx.graphics.g2d.freetype.font-generator.dispose :refer [dispose!]]
+            [com.badlogic.gdx.graphics.g2d.freetype.font-generator.parameter :as parameter]))
 
 (defn step
   [{:keys [ctx/files]}]
-  (generate-font (files/internal files "fonts/films.EXL_____.ttf")
-                 {:size 16
-                  :quality-scaling 2
-                  :use-integer-positions? false}))
+  (let [{:keys [path
+                size
+                quality-scaling
+                use-integer-positions?]} {:path "fonts/films.EXL_____.ttf"
+                                          :size 16
+                                          :quality-scaling 2
+                                          :use-integer-positions? false}
+        generator (font-generator/create (files/internal files path))
+        font (generate-font/f generator
+                              (parameter/create
+                               {:size (* size quality-scaling)
+                                ; texture.filter/linear because scaling to world-units
+                                :min-filter texture.filter/linear
+                                :mag-filter texture.filter/linear}))]
+    (dispose! generator)
+    (set-scale/f! font (/ quality-scaling))
+    (enable-markup/f! font)
+    (set-use-integer-positions/f! font use-integer-positions?)
+    font))
