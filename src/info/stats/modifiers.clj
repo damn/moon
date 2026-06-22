2aa9251cd56a5a5860de1aa629a7ac15f4c2c18d
Michael Sappler <michaelsappler@gmail.com>
2026-06-22 19:06:19 +0530

    Only one def in info.stats.modifiers

--- src/info/stats/modifiers.clj
+++ src/info/stats/modifiers.clj
@@ -1,47 +1,8 @@
 (ns info.stats.modifiers
-  (:require [clojure.math :as math]
-            [clojure.string :as str]
-            [moon.ops :as ops]))
-
-(comment
- (deftest info-texts
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 24}
-                         "Strength")
-          "-4 Strength\n+24% Strength"))
-
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 0}
-                         "Strength")
-          "-4 Strength"))
-
-   (is (= (ops/info-text {:op/mult 35}
-                         "Hitpoints")
-          "+35% Hitpoints"))
-
-   (is (= (ops/info-text {:op/inc -30
-                          :op/mult 5}
-                         "Hitpoints")
-          "-30 Hitpoints\n+5% Hitpoints")))
- )
-
-(defn- ops-info [ops modifier-k]
-  (str/join "\n"
-            (keep
-             (fn [[k v]]
-               (when-not (zero? v)
-                 (str (case (math/signum v)
-                        0.0 ""
-                        1.0 "+"
-                        -1.0 "")
-                      (case k
-                        :op/inc  (str v)
-                        :op/mult (str v "%"))
-                      " "
-                      (str/capitalize (name modifier-k)))))
-             (ops/sort ops))))
+  (:require [clojure.string :as str]
+            [moon.ops.info :as ops-info]))
 
 (defn info [mods _ctx]
   (when (seq mods)
     (str/join "\n" (keep (fn [[k ops]]
-                           (ops-info ops k)) mods))))
+                           (ops-info/f ops k)) mods))))
