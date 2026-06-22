a974a4db1542b64b7200c9b3c7c406eda5ecb1c1
Michael Sappler <michaelsappler@gmail.com>
2026-06-22 19:43:58 +0530

    Split clojure.ui.skin

--- src/clojure/ctx/skin.clj
+++ src/clojure/ctx/skin.clj
@@ -2,10 +2,11 @@
   (:require [clojure.files.internal :as internal]
             [clojure.bitmap-font.get-data :refer [get-data]]
             [clojure.bitmap-font-data.enable-markup :refer [enable-markup!]]
+            [clojure.file.skin :as skin]
             [clojure.ui.skin :as skin]))
 
 (defn step
   [{:keys [ctx/files]}]
-  (let [skin (skin/create (internal/f files "skin/uiskin.json"))]
+  (let [skin (skin/f (internal/f files "skin/uiskin.json"))]
     (enable-markup! (get-data (skin/font skin "default-font")))
     skin))
