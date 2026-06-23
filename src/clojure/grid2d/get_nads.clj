782c263ae6dbfa28eab56df3feb67d9de4ededae
Michael Sappler <michaelsappler@gmail.com>
2026-06-23 11:19:35 +0530

    Remove clojure.math.position.diagonal-steps

--- src/clojure/grid2d/get_nads.clj
+++ src/clojure/grid2d/get_nads.clj
@@ -1,21 +1,21 @@
 (ns clojure.grid2d.get-nads
   (:require [clojure.grid2d.posis :as posis]
-            [clojure.grid2d.is-nad-corner :as nad-corner?]
-            [clojure.math.position.diagonal-steps :refer [diagonal-steps]]))
+            [clojure.grid2d.is-nad-corner :as nad-corner?]))
 
 ; could be made faster because accessing the same posis oftentimes at nad-corner? check
-(defn get-nads [grid]
-  (loop [checkposis (filter (fn [{y 1 :as posi}]
-                              (and (even? y)
-                                   (= :ground (get grid posi))))
-                            (posis/f grid))
-         result []]
-    (if (seq checkposis)
-      (let [position (first checkposis)
-            diagonal-posis (map #(mapv + position %) diagonal-steps)
-            nads (map (fn [nad] [position nad])
-                      (filter #(nad-corner?/f grid position %) diagonal-posis))]
-        (recur
-         (rest checkposis)
-         (doall (concat result nads)))) ; doall else stackoverflow error
-      result)))
+(let [diagonal-steps [[-1 -1] [-1 1] [1 -1] [1 1]]]
+  (defn get-nads [grid]
+    (loop [checkposis (filter (fn [{y 1 :as posi}]
+                                (and (even? y)
+                                     (= :ground (get grid posi))))
+                              (posis/f grid))
+           result []]
+      (if (seq checkposis)
+        (let [position (first checkposis)
+              diagonal-posis (map #(mapv + position %) diagonal-steps)
+              nads (map (fn [nad] [position nad])
+                        (filter #(nad-corner?/f grid position %) diagonal-posis))]
+          (recur
+           (rest checkposis)
+           (doall (concat result nads)))) ; doall else stackoverflow error
+        result))))
--- src/clojure/math/position/diagonal_steps.clj
+++
@@ -1,3 +0,0 @@
-(ns clojure.math.position.diagonal-steps)
-
-(def diagonal-steps [[-1 -1] [-1 1] [1 -1] [1 1]])
