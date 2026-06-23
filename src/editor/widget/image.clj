20480a566674c45b82a75771a3a2594b56906dd7
Michael Sappler <michaelsappler@gmail.com>
2026-06-23 18:31:47 +0530

    editor.widget.image avoid map.texture-region-drawable

--- src/editor/widget/image.clj
+++ src/editor/widget/image.clj
@@ -2,7 +2,9 @@
   (:require [scene2d.ui.scroll-pane :as scroll-pane]
             [scene2d.ui.image-button :as image-button]
             [scene2d.ui.text-button :as text-button]
-            [map.texture-region-drawable :as texture-region-drawable]
+            [scene2d.utils.texture-region-drawable :as drawable]
+            [texture-region.get-region-height :refer [get-region-height]]
+            [texture-region.get-region-width :refer [get-region-width]]
             [moon.textures :as textures]))
 
 ; too many ! too big ! scroll ... only show files first & preview?
@@ -18,8 +20,11 @@
   [schema image {:keys [ctx/skin
                         ctx/textures]}]
   (image-button/create
-   (texture-region-drawable/create* {:drawable/texture-region (textures/texture-region textures image)
-                                     :drawable/scale 2}))
+   (let [texture-region (textures/texture-region textures image)
+         scale 2]
+     (doto (drawable/create texture-region)
+       (drawable/set-min-size! (* scale (get-region-width texture-region))
+                               (* scale (get-region-height texture-region))))))
   #_(ui/image-button image
                      (fn [_actor ctx]
                        (c/add-actor! ctx (scroll-pane/choose-window (texture-rows ctx))))
