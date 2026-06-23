ae6f578d5f9a42acd2e71a555752140e28f8a28e
Michael Sappler <michaelsappler@gmail.com>
2026-06-23 18:33:16 +0530

    action-bar.add-skill use plain texture-region-drawable

--- src/moon/action_bar/add_skill.clj
+++ src/moon/action_bar/add_skill.clj
@@ -5,7 +5,9 @@
             [scene2d.ui.button-group :as button-group]
             [scene2d.ui.image-button :as image-button]
             [scene2d.ui.text-tooltip :as text-tooltip]
-            [map.texture-region-drawable :as texture-region-drawable]
+            [scene2d.utils.texture-region-drawable :as drawable]
+            [texture-region.get-region-height :refer [get-region-height]]
+            [texture-region.get-region-width :refer [get-region-width]]
             [moon.action-bar.get-data :as get-data]))
 
 (defn f
@@ -14,11 +16,12 @@
            texture-region
            tooltip-text]}
    skin]
-  (let [{:keys [horizontal-group button-group]} (get-data/f action-bar)
+  (let [scale 2
+        {:keys [horizontal-group button-group]} (get-data/f action-bar)
         button (doto (image-button/create
-                      (texture-region-drawable/create*
-                       {:drawable/texture-region texture-region
-                        :drawable/scale 2}))
+                      (doto (drawable/create texture-region)
+                        (drawable/set-min-size! (* scale (get-region-width texture-region))
+                                                (* scale (get-region-height texture-region)))))
                  (add-listener! (text-tooltip/create tooltip-text skin))
                  (set-user-object! skill-id))]
     (add-actor! horizontal-group button)
