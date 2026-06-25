fd73f57899b4e60dda31a70c45d93da28511cf17
Michael Sappler <michaelsappler@gmail.com>
2026-06-25 11:30:36 +0530

    moon.item.is-valid

--- src/info/item.clj
+++ src/info/item.clj
@@ -1,21 +1,10 @@
 (ns info.item
   (:require [clojure.string :as str]
+            [moon.item.is-valid :as valid?]
             info.stats.modifiers))
 
-(defn- valid-item? [item]
-  (let [keyset (set (keys item))]
-    (or (= #{:property/id
-             :property/pretty-name
-             :entity/image
-             :item/slot
-             :stats/modifiers} keyset)
-        (= #{:property/id
-             :property/pretty-name
-             :entity/image
-             :item/slot} keyset))))
-
 (defn info-text [item _ctx]
-  (assert (valid-item? item))
+  (assert (valid?/f item))
   (str/join "\n"
             (remove nil?
                     [(str "[PRETTY_NAME]" (:property/pretty-name item) "[]")
