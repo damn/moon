072c2a9d6b87e8390910ca6bd4041912330e8129
Michael Sappler <michaelsappler@gmail.com>
2026-06-25 11:16:40 +0530

    copy-tile in let

--- src/world_fns/modules/grid_to_tiled_map.clj
+++ src/world_fns/modules/grid_to_tiled_map.clj
@@ -12,24 +12,22 @@
             [grid2d.width :refer [->width]]
             [grid2d.height :refer [->height]]))
 
-(def copy-tile
-  (memoize
-   (fn [tile]
-     (assert tile)
-     (static-tiled-map-tile/copy tile))))
-
 (defn grid->tiled-map
   [schema-tiled-map grid]
-  {:properties (merge (clojurize/f (tiled-map-get-properties/f schema-tiled-map))
-                      {"width" (->width grid)
-                       "height" (->height grid)})
-   :layers (for [layer (get-layers schema-tiled-map)]
-             {:name (get-name layer)
-              :visible? (visible? layer)
-              :properties (clojurize/f (get-properties/f layer))
-              :tiles (for [position (posis/f grid)
-                           :let [local-position (get grid position)]
-                           :when local-position]
-                       (when (vector? local-position)
-                         (when-let [cell (get-cell layer local-position)]
-                           [position (copy-tile (get-tile/f cell))])))})})
+  (let [copy-tile (memoize
+                   (fn [tile]
+                     (assert tile)
+                     (static-tiled-map-tile/copy tile)))]
+    {:properties (merge (clojurize/f (tiled-map-get-properties/f schema-tiled-map))
+                        {"width" (->width grid)
+                         "height" (->height grid)})
+     :layers (for [layer (get-layers schema-tiled-map)]
+               {:name (get-name layer)
+                :visible? (visible? layer)
+                :properties (clojurize/f (get-properties/f layer))
+                :tiles (for [position (posis/f grid)
+                             :let [local-position (get grid position)]
+                             :when local-position]
+                         (when (vector? local-position)
+                           (when-let [cell (get-cell layer local-position)]
+                             [position (copy-tile (get-tile/f cell))])))})}))
