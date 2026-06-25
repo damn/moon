322d9667890dcadf559d5f1aebbd3c816e810654
Michael Sappler <michaelsappler@gmail.com>
2026-06-25 10:52:56 +0530

    world-fns.uf-caves.scale-grid

--- src/world_fns/uf_caves/last_steps.clj
+++ src/world_fns/uf_caves/last_steps.clj
@@ -2,22 +2,15 @@
   (:require [grid2d.flood-fill :as flood-fill]
             [world-fns.uf-caves.position-tile-fn :as position-tile-fn]
             [world-fns.uf-caves.assoc-transition-cells :as assoc-transition-cells]
+            [world-fns.uf-caves.scale-grid :as scale-grid]
             [tiled-map.movement-property :as movement-property]
             [grid2d.height :refer [->height]]
             [grid2d.width :refer [->width]]
             [grid2d.cells :refer [->cells]]
             [grid2d.posis :as posis]
             [grid2d.printgrid :as printgrid]
-            [grid2d.scalegrid :as scalegrid]
             [tiled.tiled-map :as tiled-map]
             [tiled-map.add-creatures-layer :as add-creatures-layer]))
-
-(defn- scale-grid [grid start scale]
-  (let [grid (scalegrid/f grid scale)]
-    ;_ (printgrid/f grid)
-    ;_ (println)
-    {:start-position (mapv #(* % scale) start)
-     :grid grid}))
 
 ; TODO don't spawn my faction vampire w. player items ...
 ; FIXME - overlapping with player - don't spawn creatures on start position
@@ -32,7 +25,7 @@
     :as lvlctx
     }]
   (assert (= #{:wall :ground} (set (->cells grid))))
-  (let [{:keys [start-position grid]} (scale-grid grid start scaling)
+  (let [{:keys [start-position grid]} (scale-grid/f grid start scaling)
         grid (assoc-transition-cells/f grid)
         position->tile (position-tile-fn/f grid)
         tiled-map (tiled-map/f
