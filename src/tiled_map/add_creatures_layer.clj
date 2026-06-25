e96f10688311db23a9bca2cc3fa6909f4b99b066
Michael Sappler <michaelsappler@gmail.com>
2026-06-25 11:19:59 +0530

    createure-tile in let

--- src/tiled_map/add_creatures_layer.clj
+++ src/tiled_map/add_creatures_layer.clj
@@ -2,19 +2,15 @@
   (:require [tiled-map.add-layer :as add-layer]
             [tiled.create-static-tiled-map-tile :as create-tile]))
 
-; out of memory error -> each texture region is a new object
-; so either memoize on id or property/image already calculated !? idk
-(def ^:private creature-tile
-  (memoize
-   (fn [{:keys [tile/id
-                tile/texture-region]}]
-     (assert (and id
-                  texture-region))
-     (create-tile/f texture-region "id" id))))
-
 (defn f [tiled-map spawn-positions]
   (add-layer/f tiled-map
-               {:name "creatures"
-                :visible? false
-                :tiles (for [[position creature-property] spawn-positions]
-                         [position (creature-tile creature-property)])}))
+               (let [creature-tile (memoize
+                                    (fn [{:keys [tile/id
+                                                 tile/texture-region]}]
+                                      (assert (and id
+                                                   texture-region))
+                                      (create-tile/f texture-region "id" id)))]
+                 {:name "creatures"
+                  :visible? false
+                  :tiles (for [[position creature-property] spawn-positions]
+                           [position (creature-tile creature-property)])})))
