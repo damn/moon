a53701d7da32de765c12a10f20df392dca1edca4
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 07:35:41 +0530

    grid2d.fix-nads/f

---
+++ src/grid2d/fix_nads.clj
@@ -0,0 +1,13 @@
+(ns grid2d.fix-nads
+  (:require [map.assoc-ks :refer [assoc-ks]]
+            [grid2d.get-nads :refer [get-nads]]
+            [grid2d.cells :refer [->cells]]
+            [grid2d.get-tiles-needing-fix-for-nad :as get-tiles-needing-fix-for-nad]))
+
+(defn f [grid]
+  {:pre [(= #{:wall :ground} (set (->cells grid)))]
+   :post [(= #{:wall :ground} (set (->cells %)))]}
+  (assoc-ks grid
+            (mapcat #(get-tiles-needing-fix-for-nad/f grid %)
+                    (get-nads grid))
+            :ground))
