4dea9891d6e68bf7f126f4393a08eeb4a93f550a
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 08:40:43 +0530

    More Batch

--- src/batch/draw_tile.clj
+++ src/batch/draw_tile.clj
@@ -1,8 +1,8 @@
 (ns batch.draw-tile
   (:require [com.badlogic.gdx.graphics.texture :as texture]
+            [com.badlogic.gdx.graphics.g2d.batch :as batch]
             [com.badlogic.gdx.graphics.g2d.texture-region :as texture-region])
-  (:import (com.badlogic.gdx.graphics.g2d Batch)
-           (com.badlogic.gdx.maps.tiled TiledMapTile
+  (:import (com.badlogic.gdx.maps.tiled TiledMapTile
                                         TiledMapTileLayer$Cell)))
 
 (defn f!
@@ -13,8 +13,8 @@
    unit-scale
    color-setter
    batch-color
-   ^floats verts
-   ^Batch batch
+   verts
+   batch
    num-vertices]
   (let [region (.getTextureRegion tile)
         x1 (+ x (* (.getOffsetX tile) unit-scale))
@@ -29,28 +29,28 @@
         color12 (float (color-setter batch-color x1 y2))
         color22 (float (color-setter batch-color x2 y2))
         color21 (float (color-setter batch-color x2 y1))]
-    (aset-float verts Batch/X1 x1)
-    (aset-float verts Batch/Y1 y1)
-    (aset-float verts Batch/C1 color11)
-    (aset-float verts Batch/U1 u1)
-    (aset-float verts Batch/V1 v1)
-    (aset-float verts Batch/X2 x1)
-    (aset-float verts Batch/Y2 y2)
-    (aset-float verts Batch/C2 color12)
-    (aset-float verts Batch/U2 u1)
-    (aset-float verts Batch/V2 v2)
-    (aset-float verts Batch/X3 x2)
-    (aset-float verts Batch/Y3 y2)
-    (aset-float verts Batch/C3 color22)
-    (aset-float verts Batch/U3 u2)
-    (aset-float verts Batch/V3 v2)
-    (aset-float verts Batch/X4 x2)
-    (aset-float verts Batch/Y4 y1)
-    (aset-float verts Batch/C4 color21)
-    (aset-float verts Batch/U4 u2)
-    (aset-float verts Batch/V4 v1)
-    (.draw batch
-           (texture/type-hint (texture-region/texture region))
-           ^floats verts
-           (int 0)
-           (int num-vertices))))
+    (aset-float verts batch/x1 x1)
+    (aset-float verts batch/y1 y1)
+    (aset-float verts batch/c1 color11)
+    (aset-float verts batch/u1 u1)
+    (aset-float verts batch/v1 v1)
+    (aset-float verts batch/x2 x1)
+    (aset-float verts batch/y2 y2)
+    (aset-float verts batch/c2 color12)
+    (aset-float verts batch/u2 u1)
+    (aset-float verts batch/v2 v2)
+    (aset-float verts batch/x3 x2)
+    (aset-float verts batch/y3 y2)
+    (aset-float verts batch/c3 color22)
+    (aset-float verts batch/u3 u2)
+    (aset-float verts batch/v3 v2)
+    (aset-float verts batch/x4 x2)
+    (aset-float verts batch/y4 y1)
+    (aset-float verts batch/c4 color21)
+    (aset-float verts batch/u4 u2)
+    (aset-float verts batch/v4 v1)
+    (batch/draw! batch
+                 (texture-region/texture region)
+                 verts
+                 0
+                 num-vertices)))
