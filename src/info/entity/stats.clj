247e143c6b8f62fb73bfd37977a7832da2f034a9
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 06:32:58 +0530

    use let @ info.entity.stats

--- src/info/entity/stats.clj
+++ src/info/entity/stats.clj
@@ -4,21 +4,20 @@
             [moon.stats.get-mana :as get-mana]
             [moon.stats.get-hitpoints :as get-hitpoints]))
 
-(def ^:private non-val-max-stat-ks
-  [:stats/movement-speed
-   :stats/aggro-range
-   :stats/reaction-time
-   :stats/strength
-   :stats/cast-speed
-   :stats/attack-speed
-   :stats/armor-save
-   :stats/armor-pierce])
-
-(defn f [stats _ctx]
-  (str/join "\n" (concat
-                  ["*STATS*"
-                   (str "Mana: " (get-mana/f stats))
-                   (str "Hitpoints: " (get-hitpoints/f stats))]
-                  (for [stat-k non-val-max-stat-ks]
-                    (str (str/capitalize (name stat-k)) ": "
-                         (get-stat-value stats stat-k))))))
+(let [non-val-max-stat-ks
+      [:stats/movement-speed
+       :stats/aggro-range
+       :stats/reaction-time
+       :stats/strength
+       :stats/cast-speed
+       :stats/attack-speed
+       :stats/armor-save
+       :stats/armor-pierce]]
+  (defn f [stats _ctx]
+    (str/join "\n" (concat
+                    ["*STATS*"
+                     (str "Mana: " (get-mana/f stats))
+                     (str "Hitpoints: " (get-hitpoints/f stats))]
+                    (for [stat-k non-val-max-stat-ks]
+                      (str (str/capitalize (name stat-k)) ": "
+                           (get-stat-value stats stat-k)))))))
