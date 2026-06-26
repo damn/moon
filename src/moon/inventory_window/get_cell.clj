02e23f61f5640c4bbac0011f09ab9b4d6c55b836
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 07:28:39 +0530

    Remove moon.inventory-window-get-cell/find-inventory-window-cell

--- src/moon/inventory_window/get_cell.clj
+++ src/moon/inventory_window/get_cell.clj
@@ -3,11 +3,9 @@
             [scene2d.group.children :refer [children]]
             [scene2d.group.find-actor :refer [find-actor]]))
 
-(defn- find-inventory-window-cell [group cell]
-  (first (filter #(= (get-user-object %) cell)
-                 (children group))))
-
 (defn f [inventory-window cell]
-  (-> inventory-window
-      (find-actor "inventory-cell-table")
-      (find-inventory-window-cell cell)))
+  (->> "inventory-cell-table"
+       (find-actor inventory-window)
+       children
+       (filter #(= (get-user-object %) cell))
+       first))
