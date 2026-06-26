348a02e39884b987c0de87c0cee2aa38eac285f4
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 06:32:01 +0530

    ops info test to tests

--- src/moon/ops/info.clj
+++ src/moon/ops/info.clj
@@ -18,25 +18,3 @@
                       " "
                       (str/capitalize (name modifier-k)))))
              (ops/sort ops))))
-
-(comment
- (deftest info-texts
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 24}
-                         "Strength")
-          "-4 Strength\n+24% Strength"))
-
-   (is (= (ops/info-text {:op/inc -4
-                          :op/mult 0}
-                         "Strength")
-          "-4 Strength"))
-
-   (is (= (ops/info-text {:op/mult 35}
-                         "Hitpoints")
-          "+35% Hitpoints"))
-
-   (is (= (ops/info-text {:op/inc -30
-                          :op/mult 5}
-                         "Hitpoints")
-          "-30 Hitpoints\n+5% Hitpoints")))
- )
