6dd796e3f47b0979ac8dbae8197e51804c5c0ff3
Michael Sappler <michaelsappler@gmail.com>
2026-06-26 07:17:56 +0530

    Remove comment

--- src/effect/target_all.clj
+++ src/effect/target_all.clj
@@ -1,19 +1,6 @@
 (ns effect.target-all
   (:require [moon.raycaster.line-of-sight :as line-of-sight?]))
 
-(comment
- ; TODO applicable targets? e.g. projectiles/effect s/???item entiteis ??? check
- ; same code as in render entities on world view screens/world
- ; TODO showing one a bit further up
- ; maybe world view port is cut
- ; not quite showing correctly.
- (let [targets (creatures-in-los-of-player)]
-   (count targets)
-   #_(sort-by #(% 1) (map #(vector (:entity.creature/name @%)
-                                   (:body/position (:entity/body @%))) targets)))
-
- )
-
 (defn affected-targets [active-entities raycaster entity]
   (->> active-entities
        (filter #(:entity/species @%))
