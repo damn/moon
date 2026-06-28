2c44d93f49102de25da761f68d9917ee6a530bae
Michael Sappler <michaelsappler@gmail.com>
2026-06-28 18:22:22 +0530

    Remove tiled.static-tiled-map-tile

--- src/tiled/create_static_tiled_map_tile.clj
+++ src/tiled/create_static_tiled_map_tile.clj
@@ -1,12 +1,13 @@
 (ns tiled.create-static-tiled-map-tile
   (:require [map-properties.put :as put!]
-            [tiled-map-tile.get-properties :as get-properties]
-            [tiled.static-tiled-map-tile :as static-tiled-map-tile]))
+            [tiled-map-tile.get-properties :as get-properties])
+  (:import (com.badlogic.gdx.graphics.g2d TextureRegion)
+           (com.badlogic.gdx.maps.tiled.tiles StaticTiledMapTile)))
 
 (defn f
   [texture-region property-name property-value]
   {:pre [texture-region
          (string? property-name)]}
-  (let [tile (static-tiled-map-tile/create texture-region)]
+  (let [tile (StaticTiledMapTile. ^TextureRegion texture-region)]
     (put!/f (get-properties/f tile) property-name property-value)
     tile))
--- src/tiled/static_tiled_map_tile.clj
+++
@@ -1,6 +0,0 @@
-(ns tiled.static-tiled-map-tile
-  (:import (com.badlogic.gdx.graphics.g2d TextureRegion)
-           (com.badlogic.gdx.maps.tiled.tiles StaticTiledMapTile)))
-
-(defn create [^TextureRegion texture-region]
-  (StaticTiledMapTile. texture-region))
