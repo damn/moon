93423346a1c90c6643833d0b1255a840457f6fd3
Michael Sappler <michaelsappler@gmail.com>
2026-06-29 09:43:58 +0530

    Remove scene2d.ui.button-group

--- src/scene2d/ui/button_group.clj
+++
@@ -1,8 +0,0 @@
-(ns scene2d.ui.button-group
-  (:import (com.badlogic.gdx.scenes.scene2d.ui ButtonGroup)))
-
-(defn create [{:keys [max-check-count
-                      min-check-count]}]
-  (doto (ButtonGroup.)
-    (.setMaxCheckCount max-check-count)
-    (.setMinCheckCount min-check-count)))
--- src/stage/action_bar.clj
+++ src/stage/action_bar.clj
@@ -1,9 +1,9 @@
 (ns stage.action-bar
-  (:require [scene2d.ui.button-group :as button-group]
-            [scene2d.ui.horizontal-group :as horizontal-group]
+  (:require [scene2d.ui.horizontal-group :as horizontal-group]
             [scene2d.utils.layout.set-fill-parent :refer [set-fill-parent!]]
             [gdx.scenes.scene2d.ui.table :as table])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor)))
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)
+           (com.badlogic.gdx.scenes.scene2d.ui ButtonGroup)))
 
 (defn create [_ctx]
   (doto (table/create
@@ -12,9 +12,9 @@
                                        {:space 2
                                         :pad 2})
                                   (Actor/.setName "moon.ui.action-bar.horizontal-group")
-                                  (Actor/.setUserObject (button-group/create
-                                                         {:max-check-count 1
-                                                          :min-check-count 0})))
+                                  (Actor/.setUserObject (doto (ButtonGroup.)
+                                                          (.setMaxCheckCount 1)
+                                                          (.setMinCheckCount 0))))
                          :expand? true
                          :bottom? true}]]})
     (set-fill-parent! true)
