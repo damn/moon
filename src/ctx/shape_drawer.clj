d3f09441e9f771edb9d2e92d12648b4c01056e1f
Michael Sappler <michaelsappler@gmail.com>
2026-06-29 17:24:57 +0530

    Fix reflection

--- src/ctx/shape_drawer.clj
+++ src/ctx/shape_drawer.clj
@@ -1,8 +1,9 @@
 (ns ctx.shape-drawer
-  (:import (com.badlogic.gdx.graphics.g2d TextureRegion)
+  (:import (com.badlogic.gdx.graphics Texture)
+           (com.badlogic.gdx.graphics.g2d TextureRegion)
            (space.earlygrey.shapedrawer ShapeDrawer)))
 
 (defn step
   [{:keys [ctx/batch
            ctx/shape-drawer-texture]}]
-  (ShapeDrawer. batch (TextureRegion. shape-drawer-texture 1 0 1 1)))
+  (ShapeDrawer. batch (TextureRegion. ^Texture shape-drawer-texture 1 0 1 1)))
