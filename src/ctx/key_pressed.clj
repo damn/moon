173e292d0d3a832eaf2641a71acfddca559ec557
Michael Sappler <michaelsappler@gmail.com>
2026-06-30 19:50:23 +0530

    Remove ctx.key-pressed & ctx.key-just-pressed

--- src/ctx/key_pressed.clj
+++
@@ -1,5 +0,0 @@
-(ns ctx.key-pressed
-  (:require [input.key-pressed :as key-pressed?]))
-
-(defn key-pressed? [{:keys [ctx/input]} input-key]
-  (key-pressed?/f input input-key))
--- src/render/assoc_paused.clj
+++ src/render/assoc_paused.clj
@@ -1,9 +1,10 @@
 (ns render.assoc-paused
-  (:require [ctx.key-pressed :refer [key-pressed?]]
-            [ctx.key-just-pressed :refer [key-just-pressed?]]))
+  (:require [input.key-pressed :refer [f] :rename {f key-pressed?}]
+            [input.key-just-pressed :refer [f] :rename {f key-just-pressed?}]))
 
 (defn step
-  [{:keys [ctx/pausing?
+  [{:keys [ctx/input
+           ctx/pausing?
            ctx/controls
            ctx/player-eid
            ctx/state->pause-game?]
@@ -12,5 +13,5 @@
          (or #_error
              (and pausing?
                   (state->pause-game? (:state (:entity/fsm @player-eid)))
-                  (not (or (key-just-pressed? ctx (:unpause-once controls))
-                           (key-pressed? ctx (:unpause-continously controls))))))))
+                  (not (or (key-just-pressed? input (:unpause-once controls))
+                           (key-pressed? input (:unpause-continously controls))))))))
