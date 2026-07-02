2b0519cb74dcfe51e7bee762bf099d512cd869c9
Michael Sappler <michaelsappler@gmail.com>
2026-07-02 06:14:47 +0530

    clojure.gdx.lwjgl3-application-configuration.new

--- src/moon/application/start.clj
+++ src/moon/application/start.clj
@@ -1,18 +1,15 @@
 (ns moon.application.start
   (:require [clojure.gdx.lwjgl3-application.new :as lwjgl3-application]
+            [clojure.gdx.lwjgl3-application-configuration.new :as create-config]
             [clojure.gdx.os.mac-os-x :as mac-os-x]
             [clojure.gdx.shared-library-loader.os :as os]
             [clojure.lwjgl.configuration.glfw-library-name :as glfw-library-name]
-            [clojure.lwjgl.configuration.set! :as conf-set!])
-  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3ApplicationConfiguration)))
+            [clojure.lwjgl.configuration.set! :as conf-set!]))
 
 (defn f!
-  [{:keys [listener title windowed-mode foreground-fps]}]
+  [{:keys [listener] :as config}]
   (when (= os/v mac-os-x/v)
     (conf-set!/f glfw-library-name/v "glfw_async"))
   (lwjgl3-application/f (let [[f params] listener]
                           (f params))
-                        (doto (Lwjgl3ApplicationConfiguration.)
-                          (.setTitle title)
-                          (.setWindowedMode (:width windowed-mode) (:height windowed-mode))
-                          (.setForegroundFPS foreground-fps))))
+                        (create-config/f config)))
