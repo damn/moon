36b2a63378fe41e337f7eaaa6b6099b3970cc54f
Michael Sappler <michaelsappler@gmail.com>
2026-07-02 07:35:22 +0530

    change-listener inline

--- src/levelgen_test/start.clj
+++ src/levelgen_test/start.clj
@@ -157,15 +157,16 @@
 (defn edit-window [skin level-fns]
   {:title "Edit"
    :skin skin
-   :table/rows (for [level-fn level-fns
-                     :let [on-clicked (fn [actor ctx]
-                                        (let [stage (Actor/.getStage actor)
-                                              new-ctx (generate-level ctx level-fn)]
-                                          (set! (.ctx stage) new-ctx)))]]
-                 [{:actor (doto (text-button/create {:text (str "Generate " level-fn) :skin skin})
+   :table/rows (for [level-fn level-fns]
+                 [{:actor (doto (text-button/create
+                                 {:text (str "Generate " level-fn)
+                                  :skin skin})
                             (Actor/.addListener (change-listener/create
                                                  (fn [event actor]
-                                                   (on-clicked actor (:stage/ctx (Event/.getStage event)))))))}])})
+                                                   (let [stage (Actor/.getStage actor)
+                                                         ctx (:stage/ctx stage)
+                                                         new-ctx (generate-level ctx level-fn)]
+                                                     (set! (.ctx stage) new-ctx))))))}])})
 
 (defn create!
   [_ctx]
