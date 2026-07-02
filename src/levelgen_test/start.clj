1ff4dc0e2b799362a7922eda714d0b054bb43b24
Michael Sappler <michaelsappler@gmail.com>
2026-07-02 06:54:50 +0530

    Integrate levelgen-test/create

--- src/levelgen_test/start.clj
+++ src/levelgen_test/start.clj
@@ -3,11 +3,18 @@
             [clojure.gdx.lwjgl3-application.new :as lwjgl3-application]
             [clojure.gdx.lwjgl3-application-configuration.new :as create-config]
             [clojure.gdx.use-glfw-async :as use-glfw-async!]
+            [clojure.gdx.color.float-bits :as float-bits]
+            [clojure.gdx.orthographic-camera.new :as new-camera]
+            [clojure.gdx.orthographic-camera.set-to-ortho :as set-to-ortho!]
+            [gdx.scenes.scene2d.ui.window :as window]
+            [scene2d.stage :as stage]
+            [levelgen-test.create.edit-window :refer [edit-window]]
+            [levelgen-test.generate-level :as generate-level]
+            [clojure.gdx.fit-viewport.new :as fit-viewport]
             [moon.application.listener]
             [ctx.gdx-context]
             [map.assoc-create]
             [map.create]
-            [levelgen-test.create]
             [ctx.db]
             [ctx.textures]
             [ctx.batch]
@@ -20,8 +27,41 @@
             [levelgen-test.camera-zoom-controls]
             [levelgen-test.camera-movement-controls]
             [render.update-draw-stage]
-            [levelgen-test.resize]
-            ))
+            [levelgen-test.resize])
+  (:import (com.badlogic.gdx Input)
+           (com.badlogic.gdx.scenes.scene2d Stage)))
+
+(defn create!
+  [{:keys [ctx/files
+           ctx/input
+           ctx/sprite-batch]
+    :as ctx}]
+  (let [initial-level-fn "config/world_fns/uf_caves.edn"
+        level-fns ["config/world_fns/vampire.edn"
+                   "config/world_fns/uf_caves.edn"
+                   "config/world_fns/modules.edn"]
+        ui-viewport (fit-viewport/create 1440 900)
+        stage (stage/create ui-viewport sprite-batch)
+        _  (.setInputProcessor ^Input input stage)
+        tile-size 48
+        world-unit-scale (float (/ tile-size))
+        ctx (assoc ctx :ctx/stage stage)
+        world-viewport (let [world-width  (* 1440 world-unit-scale)
+                             world-height (* 900  world-unit-scale)]
+                         (fit-viewport/create world-width
+                                              world-height
+                                              (doto (new-camera/f)
+                                                (set-to-ortho!/f! false world-width world-height))))
+        ctx (assoc ctx
+                   :ctx/world-viewport world-viewport
+                   :ctx/camera (:viewport/camera world-viewport)
+                   :ctx/color-setter (constantly (float-bits/f [1 1 1 1]))
+                   :ctx/zoom-speed 0.1
+                   :ctx/camera-movement-speed 1
+                   :ctx/world-unit-scale world-unit-scale)
+        ctx (generate-level/f ctx initial-level-fn)]
+    (Stage/.addActor (:ctx/stage ctx) (window/create (edit-window (:ctx/skin ctx) level-fns)))
+    ctx))
 
 (def state (atom nil))
 
@@ -38,7 +78,7 @@
                                                                            :extensions #{"png" "bmp"}}]]
                                              [map.assoc-create/f :ctx/sprite-batch ctx.batch/step]
                                              [map.assoc-create/f :ctx/skin ctx.skin/step]
-                                             [levelgen-test.create/f!]
+                                             [create!]
                                              ]
                            :dispose! levelgen-test.dispose/f!
                            :render-pipeline [[render.get-stage-ctx/step]
