9c340281d5e631858554d9f6270991231aec18b2
Michael Sappler <michaelsappler@gmail.com>
2026-07-04 23:13:40 +0530

    Split editor.create-widget.animation

--- src/editor/create_widget/animation.clj
+++ src/editor/create_widget/animation.clj
@@ -1,22 +1,10 @@
 (ns editor.create-widget.animation
-  (:require [clojure.gdx.image-button.new :as new-image-button]
-            [clojure.gdx.texture-region.get-region-height :as get-region-height]
-            [clojure.gdx.texture-region.get-region-width :as get-region-width]
-            [clojure.gdx.texture-region-drawable.new :as new-texture-region-drawable]
-            [clojure.gdx.texture-region-drawable.set-min-size :as set-min-size]
-            [gdx.scenes.scene2d.ui.table :as table]
-            [moon.textures :as textures]))
-
-(defn- image-button [textures image scale]
-  (let [texture-region (textures/texture-region textures image)]
-    (new-image-button/f
-     (doto (new-texture-region-drawable/f texture-region)
-       (set-min-size/f (* scale (get-region-width/f texture-region))
-                       (* scale (get-region-height/f texture-region)))))))
+  (:require [editor.create-widget.animation.image-button :as image-button]
+            [gdx.scenes.scene2d.ui.table :as table]))
 
 (defn f
   [_ animation {:keys [ctx/textures]}]
   (table/create
    {:table/cell-defaults {:pad 1}
     :table/rows [(for [image (:animation/frames animation)]
-                   {:actor (image-button textures image 2)})]}))
+                   {:actor (image-button/f textures image 2)})]}))
---
+++ src/editor/create_widget/animation_image_button.clj
@@ -0,0 +1,14 @@
+(ns editor.create-widget.animation.image-button
+  (:require [clojure.gdx.image-button.new :as new-image-button]
+            [clojure.gdx.texture-region.get-region-height :as get-region-height]
+            [clojure.gdx.texture-region.get-region-width :as get-region-width]
+            [clojure.gdx.texture-region-drawable.new :as new-texture-region-drawable]
+            [clojure.gdx.texture-region-drawable.set-min-size :as set-min-size]
+            [moon.textures :as textures]))
+
+(defn f [textures image scale]
+  (let [texture-region (textures/texture-region textures image)]
+    (new-image-button/f
+     (doto (new-texture-region-drawable/f texture-region)
+       (set-min-size/f (* scale (get-region-width/f texture-region))
+                       (* scale (get-region-height/f texture-region)))))))
