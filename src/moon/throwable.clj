1e5be3d8c502d7af61de42cdec18563195b6b38d
Michael Sappler <michaelsappler@gmail.com>
2026-07-05 18:36:39 +0530

    Only one form in moon.throwable

--- src/moon/throwable.clj
+++ src/moon/throwable.clj
@@ -1,9 +1,8 @@
 (ns moon.throwable
   (:require [clj-commons.pretty.repl :as pretty-repl]))
 
-(def print-level 3)
-(def print-depth 24)
-
-(defn pretty-pst [throwable]
-  (binding [*print-level* print-level]
-    (pretty-repl/pretty-pst throwable print-depth)))
+(let [print-level 3
+      print-depth 24]
+  (defn pretty-pst [throwable]
+    (binding [*print-level* print-level]
+      (pretty-repl/pretty-pst throwable print-depth))))
