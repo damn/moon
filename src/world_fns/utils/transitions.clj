a983b43ad06a4f8991f78fc5c7e814757d69440c
Michael Sappler <michaelsappler@gmail.com>
2026-07-05 18:38:36 +0530

    Only one form in src/world_fns/utils/transitions.clj

--- src/world_fns/utils/transitions.clj
+++ src/world_fns/utils/transitions.clj
@@ -3,21 +3,22 @@
 
 (let [idxvalues-order [[1 0] [-1 0] [0 1] [0 -1]]]
   (assert (= (get-4-neighbours [0 0])
-             idxvalues-order)))
+             idxvalues-order))
 
-(comment
-  ; Values for every neighbour:
-  {          [0 1] 1
-   [-1 0]  8          [1 0] 2
-             [0 -1] 4 })
+  ; so the idxvalues-order corresponds to the following values for a neighbour tile:
 
-; so the idxvalues-order corresponds to the following values for a neighbour tile:
-(let [idxvalues [2 8 1 4]]
-  (defn idx-value [position position->transition?]
-    (->> position
-         get-4-neighbours
-         (map-indexed (fn [idx position]
-                        (if (position->transition? position)
-                          (idxvalues idx)
-                          0)))
-         (apply +))))
+  (comment
+   ; Values for every neighbour:
+   {          [0 1] 1
+    [-1 0]  8          [1 0] 2
+              [0 -1] 4 })
+
+  (let [idxvalues [2 8 1 4]]
+    (defn idx-value [position position->transition?]
+      (->> position
+           get-4-neighbours
+           (map-indexed (fn [idx position]
+                          (if (position->transition? position)
+                            (idxvalues idx)
+                            0)))
+           (apply +)))))
