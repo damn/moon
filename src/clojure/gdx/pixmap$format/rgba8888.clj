186c56a8622f38ef9e80e181ce37cf831fcee140
Michael Sappler <michaelsappler@gmail.com>
2026-07-06 18:47:18 +0530

    com.badlogic.gdx.graphics.pixmap$format

--- src/clojure/gdx/pixmap$format/rgba8888.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure.gdx.pixmap$format.rgba8888
-  (:import (com.badlogic.gdx.graphics Pixmap$Format)))
-
-(def v Pixmap$Format/RGBA8888)
--- src/ctx/shape_drawer_texture.clj
+++ src/ctx/shape_drawer_texture.clj
@@ -3,11 +3,11 @@
             [clojure.gdx.pixmap.draw-pixel :as draw-pixel]
             [clojure.gdx.pixmap.new :as pixmap]
             [clojure.gdx.pixmap.set-color :as set-color]
-            [clojure.gdx.pixmap$format.rgba8888 :as rgba8888]
+            [com.badlogic.gdx.graphics.pixmap$format :as pixmap-format]
             [clojure.gdx.texture.new :as texture]))
 
 (defn step [_ctx]
-  (let [pixmap (doto (pixmap/f 1 1 rgba8888/v)
+  (let [pixmap (doto (pixmap/f 1 1 pixmap-format/rgba8888)
                  (set-color/f 1 1 1 1)
                  (draw-pixel/f 0 0))
         texture (texture/f pixmap)]
