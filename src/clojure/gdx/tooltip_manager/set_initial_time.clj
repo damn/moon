1c43eb19a60bb8db7bad1197528a7d4ccd5d25e9
Michael Sappler <michaelsappler@gmail.com>
2026-07-06 18:52:59 +0530

    com.badlogic.gdx.scenes.scene2d.ui.tooltip-manager

--- src/clojure/gdx/tooltip_manager/set_initial_time.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure.gdx.tooltip-manager.set-initial-time
-  (:import (com.badlogic.gdx.scenes.scene2d.ui TooltipManager)))
-
-(defn f [^TooltipManager tooltip-manager initial-time]
-  (set! (.initialTime tooltip-manager) initial-time))
--- src/ctx/tooltip_manager_opts.clj
+++ src/ctx/tooltip_manager_opts.clj
@@ -1,6 +1,5 @@
 (ns ctx.tooltip-manager-opts
-  (:require [clojure.gdx.tooltip-manager.get-instance :as get-instance]
-            [clojure.gdx.tooltip-manager.set-initial-time :as set-initial-time]))
+  (:require [com.badlogic.gdx.scenes.scene2d.ui.tooltip-manager :as tooltip-manager]))
 
 (defn step [_ctx]
-  (set-initial-time/f (get-instance/f) 0))
+  (tooltip-manager/set-initial-time! (tooltip-manager/get-instance) 0))
