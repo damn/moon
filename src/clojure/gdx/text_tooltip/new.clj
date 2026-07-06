d36f9db32b54730a23677f9bfba229b46d2f68d2
Michael Sappler <michaelsappler@gmail.com>
2026-07-06 18:49:27 +0530

    com.badlogic.gdx.scenes.scene2d.ui.text-tooltip

--- src/clojure/gdx/text_tooltip/new.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure.gdx.text-tooltip.new
-  (:import (com.badlogic.gdx.scenes.scene2d.ui Skin TextTooltip)))
-
-(defn f [^String tooltip ^Skin skin]
-  (TextTooltip. tooltip skin))
--- src/scene2d/ui/text_tooltip.clj
+++ src/scene2d/ui/text_tooltip.clj
@@ -1,5 +1,5 @@
 (ns scene2d.ui.text-tooltip
-  (:require [clojure.gdx.text-tooltip.new :as new-text-tooltip]))
+  (:require [com.badlogic.gdx.scenes.scene2d.ui.text-tooltip :as text-tooltip]))
 
 (defn create [tooltip skin]
-  (new-text-tooltip/f tooltip skin))
+  (text-tooltip/new tooltip skin))
