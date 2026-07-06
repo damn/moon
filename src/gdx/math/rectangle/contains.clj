62457d6388581a1f95508c45e68fccb16f9609bd
Michael Sappler <michaelsappler@gmail.com>
2026-07-06 15:58:40 +0530

    Remove gdx.math.rectangle.contains

--- src/gdx/math/rectangle/contains.clj
+++
@@ -1,6 +0,0 @@
-(ns gdx.math.rectangle.contains
-  (:refer-clojure :exclude [contains?])
-  (:require [clojure.gdx.rectangle.contains :as contains]))
-
-(defn contains? [rectangle [x y]]
-  (contains/f rectangle x y))
--- src/moon/grid/point_to_entities.clj
+++ src/moon/grid/point_to_entities.clj
@@ -1,8 +1,8 @@
 (ns moon.grid.point-to-entities
-  (:require [gdx.math.rectangle.contains :as rectangle]
+  (:require [clojure.gdx.rectangle.contains :as rectangle-contains?]
             [moon.body.rectangle :refer [->rectangle]]))
 
 (defn point->entities [g2d pos]
   (when-let [cell (g2d (mapv int pos))]
-    (filter #(rectangle/contains? (->rectangle (:entity/body @%)) pos)
+    (filter #(rectangle-contains?/f (->rectangle (:entity/body @%)) pos)
             (:entities @cell))))
