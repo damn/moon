3d5d2bbfdbdbb39edf3b80180f191a4bda5b4a34
Michael Sappler <michaelsappler@gmail.com>
2026-07-07 20:32:34 +0530

    inline entity state draw ui view

--- src/clojure/draw_ui_view_default.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure.draw-ui-view-default)
-
-(defn f
-  [_ _eid _ctx]
-  nil)
--- src/clojure/entity_state_draw_ui_view.clj
+++ src/clojure/entity_state_draw_ui_view.clj
@@ -1,5 +1,24 @@
-(ns clojure.entity-state-draw-ui-view)
+(ns clojure.entity-state-draw-ui-view
+  (:require [clojure.mouseover-actor :refer [mouseover-actor]]
+            [clojure.moon-textures :as textures]))
 
 (defmulti f
   (fn [[k _v] _eid _ctx]
     k))
+
+(defmethod f :default
+  [_ _eid _ctx]
+  nil)
+
+(defmethod f :player-item-on-cursor
+  [_ eid {:keys [ctx/textures
+                 ctx/ui-mouse-position]
+          :as ctx}]
+  ; TODO see player-item-on-cursor at render layers
+  ; always draw it here at right position, then render layers does not need input/stage
+  ; can pass world to graphics, not handle here at application
+  (when (mouseover-actor ctx)
+    [[:draw/texture-region
+      (textures/texture-region textures (:entity/image (:entity/item-on-cursor @eid)))
+      ui-mouse-position
+      {:center? true}]]))
