e2fac9e7511b31bfac4d836214d1caadd28614f1
Michael Sappler <michaelsappler@gmail.com>
2026-07-07 22:56:20 +0530

    Inline editor stuff

--- src/clojure/editor_start.clj
+++ src/clojure/editor_start.clj
@@ -1,8 +1,6 @@
 (ns clojure.editor-start
   (:require [clojure.add-component-window :as add-component-window]
-            [clojure.add-stage-actors :as add-stage-actors]
             [clojure.application :as application]
-            [clojure.assoc-create :as assoc-create]
             [clojure.clear-screen :as clear-screen]
             [clojure.component-row :as component-row]
             [clojure.create-widget]
@@ -12,13 +10,14 @@
             [clojure.ctx-stage :as ctx-stage]
             [clojure.ctx-textures :as ctx-textures]
             [clojure.disposable :as disposable]
-            [clojure.do :as do]
             [clojure.editor-rebuild :as editor-rebuild]
-            [clojure.gdx-context :as gdx-context]
+            [clojure.gdx.audio :as audio]
+            [clojure.gdx.files :as files]
+            [clojure.gdx.graphics :as graphics]
+            [clojure.gdx.input :as input]
             [clojure.listener :as listener]
             [clojure.lwjgl3-application :as lwjgl3-application]
             [clojure.main-window :as main-window]
-            [clojure.map-create :as map-create]
             [clojure.map-widget-table :as map-widget-table]
             [clojure.property-overview-window :as property-overview-window]
             [clojure.set-ctx :as set-ctx]
@@ -26,8 +25,39 @@
             [clojure.viewport :as viewport]
             [clojure.widget-value]))
 
-(defn- create-app! [ctx data]
-  (merge ctx data))
+(defn- create [_ctx]
+  (let [ctx {:ctx/audio (audio/f)
+             :ctx/files (files/f)
+             :ctx/graphics (graphics/f)
+             :ctx/input (input/f)}
+        ctx (assoc ctx :ctx/batch (ctx-batch/step ctx))
+        ctx (assoc ctx :ctx/skin (ctx-skin/step ctx))
+        ctx (assoc ctx :ctx/db (ctx-db/step ctx))
+        ctx (assoc ctx :ctx/stage (ctx-stage/step ctx))
+        _ (stage/add-actor! (:ctx/stage ctx) (main-window/create ctx))
+        ctx (assoc ctx :ctx/textures (ctx-textures/step ctx {:folder "resources/"
+                                                             :extensions #{"png" "bmp"}}))]
+    (merge ctx
+           {:ctx/property-overview-window property-overview-window/create
+            :ctx/add-component-window add-component-window/f
+            :ctx/create-component-row component-row/create
+            :ctx/create-map-widget-table map-widget-table/create
+            :ctx/rebuild-editor-window! editor-rebuild/f!
+            :ctx/property-k-sort-order [:property/id
+                                        :property/pretty-name
+                                        :entity/image
+                                        :entity/animation
+                                        :entity/species
+                                        :creature/level
+                                        :entity/body
+                                        :item/slot
+                                        :projectile/speed
+                                        :projectile/max-range
+                                        :projectile/piercing?
+                                        :skill/action-time-modifier-key
+                                        :skill/action-time
+                                        :skill/start-action-sound
+                                        :skill/cost]})))
 
 (defn- dispose-app! [{:keys [ctx/skin
                              ctx/batch
@@ -56,37 +86,7 @@
     :foreground-fps 60}
    (listener/f
     {:state-var #'application/state
-     :create-pipeline [[gdx-context/f]
-                       [assoc-create/f :ctx/batch ctx-batch/step]
-                       [assoc-create/f :ctx/skin ctx-skin/step]
-                       [assoc-create/f :ctx/db ctx-db/step]
-                       [assoc-create/f :ctx/stage ctx-stage/step]
-                       [do/step
-                        [add-stage-actors/f! [[main-window/create]]]]
-                       [map-create/f :ctx/textures [ctx-textures/step
-                                                    {:folder "resources/"
-                                                     :extensions #{"png" "bmp"}}]]
-                       [create-app!
-                        {:ctx/property-overview-window property-overview-window/create
-                         :ctx/add-component-window add-component-window/f
-                         :ctx/create-component-row component-row/create
-                         :ctx/create-map-widget-table map-widget-table/create
-                         :ctx/rebuild-editor-window! editor-rebuild/f!
-                         :ctx/property-k-sort-order [:property/id
-                                                     :property/pretty-name
-                                                     :entity/image
-                                                     :entity/animation
-                                                     :entity/species
-                                                     :creature/level
-                                                     :entity/body
-                                                     :item/slot
-                                                     :projectile/speed
-                                                     :projectile/max-range
-                                                     :projectile/piercing?
-                                                     :skill/action-time-modifier-key
-                                                     :skill/action-time
-                                                     :skill/start-action-sound
-                                                     :skill/cost]}]]
+     :create-pipeline [[create]]
      :dispose! dispose-app!
      :render-pipeline [[clear-screen/step]
                        [render-app!]]
