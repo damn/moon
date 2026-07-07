42e04d1135a0d6331a77cc9faec8dc8afa0108a9
Michael Sappler <michaelsappler@gmail.com>
2026-07-07 19:17:50 +0530

    clean up action-bar

--- src/clojure/action_bar.clj
+++ src/clojure/action_bar.clj
@@ -1,9 +1,9 @@
 (ns clojure.action-bar
-  (:require
-            [clojure.set-name]
-            [clojure.set-user-object] [clojure.button-group :as button-group]
+  (:require [clojure.button-group :as button-group]
             [clojure.horizontal-group :as horizontal-group]
             [clojure.layout :as layout]
+            [clojure.set-name :as set-name]
+            [clojure.set-user-object :as set-user-object]
             [clojure.ui-table :as table]))
 
 (defn create [_ctx]
@@ -12,11 +12,11 @@
           :table/rows [[{:actor (doto (horizontal-group/new)
                                   (horizontal-group/space! 2)
                                   (horizontal-group/pad! 2)
-                                  (clojure.set-name/f "moon.ui.action-bar.horizontal-group")
-                                  (clojure.set-user-object/f (doto (button-group/new)
+                                  (set-name/f "moon.ui.action-bar.horizontal-group")
+                                  (set-user-object/f (doto (button-group/new)
                                                        (button-group/set-max-check-count! 1)
                                                        (button-group/set-min-check-count! 0))))
                          :expand? true
                          :bottom? true}]]})
     (layout/set-fill-parent! true)
-    (clojure.set-name/f "moon.ui.action-bar")))
+    (set-name/f "moon.ui.action-bar")))
