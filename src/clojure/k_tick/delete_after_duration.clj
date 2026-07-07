e2166137263c4b90a0637988039e0de5ecc7bbf1
Michael Sappler <michaelsappler@gmail.com>
2026-07-08 01:29:36 +0530

    collapse k_tick

--- src/clojure/k_tick/delete_after_duration.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure.k-tick.delete-after-duration
-  (:require [clojure.stopped :refer [stopped?]]))
-
-(defn f
-  [counter eid {:keys [ctx/elapsed-time]}]
-  (when (stopped? elapsed-time counter)
-    [[:tx/mark-destroyed eid]]))
--- src/clojure/k_tick/npc_moving.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure.k-tick.npc-moving
-  (:require [clojure.stopped :refer [stopped?]]))
-
-(defn f
-  [{:keys [timer]} eid {:keys [ctx/elapsed-time]}]
-  (when (stopped? elapsed-time timer)
-    [[:tx/event eid :timer-finished]]))
