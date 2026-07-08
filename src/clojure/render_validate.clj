418a72360a31be1667a4d9a5d1ebbb56d63111a5
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 04:19:06 +0530

    Remove unused file

--- src/clojure/render_validate.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure.render-validate
-  (:require [clojure.moon.schema :refer [schema]]
-            [clojure.validate-humanize :refer [validate-humanize]]))
-
-(defn step [ctx]
-  (validate-humanize schema ctx)
-  ctx)
