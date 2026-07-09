58b8353cf6c9e43663711aa3ceab9645e4ded286
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 21:14:10 +0530

    Fomatting

--- src/com/badlogic/gdx/backends/lwjgl3/lwjgl3_application.clj
+++ src/com/badlogic/gdx/backends/lwjgl3/lwjgl3_application.clj
@@ -1,7 +1,5 @@
 (ns com.badlogic.gdx.backends.lwjgl3.lwjgl3-application
-  (:import
-           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)
-           ))
+  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)))
 
 (defn create [application-listener lwjgl3-application-configuration]
   (Lwjgl3Application. application-listener
