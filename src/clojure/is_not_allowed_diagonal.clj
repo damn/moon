d9fff550624a75eb13374876041acaa5019fb398
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 19:04:06 +0530

    Inline clojure.diagonal-check-indices

--- src/clojure/is_not_allowed_diagonal.clj
+++ src/clojure/is_not_allowed_diagonal.clj
@@ -1,7 +1,16 @@
 (ns clojure.is-not-allowed-diagonal
-  (:require [clojure.diagonal-check-indizes :as diagonal-check-indizes]))
+  (:require [clojure.positions :refer [positions]]
+            [clojure.diagonal-direction :as diagonal-direction?]
+            [clojure.get-8-neighbours :refer [get-8-neighbours]]))
+
+(let [order (get-8-neighbours [0 0])]
+  (def diagonal-check-indizes
+    (into {} (for [[x y] (filter diagonal-direction?/f order)]
+               [(first (positions #(= % [x y]) order))
+                (vec (positions #(some #{%} [[x 0] [0 y]])
+                                order))]))))
 
 (defn f? [at-idx adjacent-cells]
-  (when-let [[a b] (get diagonal-check-indizes/v at-idx)]
+  (when-let [[a b] (get diagonal-check-indizes at-idx)]
     (and (nil? (adjacent-cells a))
          (nil? (adjacent-cells b)))))
