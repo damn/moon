6b5f43fe59ee30a186fbf18d9d687a02367672dc
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 17:43:43 +0530

    Delete clojure.ctx-skin

--- src/clojure/editor/skin.clj
+++ src/clojure/editor/skin.clj
@@ -1,5 +1,13 @@
 (ns clojure.editor.skin
-  (:require [clojure.ctx-skin :as ctx-skin]))
+  (:require [clojure.files :as files]
+            [clojure.gdx.graphics.g2d.bitmap-font :as bitmap-font]
+            [clojure.gdx.graphics.g2d.bitmap-font$bitmap-font-data :as bitmap-font-data]
+            [clojure.skin :as skin]))
 
-(defn f [ctx]
-  (assoc ctx :ctx/skin (ctx-skin/step ctx)))
+(defn f [{:keys [ctx/files] :as ctx}]
+  (let [skin (skin/new (files/internal files "skin/uiskin.json"))]
+    (-> skin
+        (skin/get-font "default-font")
+        bitmap-font/get-data
+        (bitmap-font-data/set-markup-enabled! true))
+    (assoc ctx :ctx/skin skin)))
