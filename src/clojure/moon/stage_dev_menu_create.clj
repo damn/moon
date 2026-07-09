709b9d923d29abefd707c00681d4ee2693965ff5
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 17:34:26 +0530

    Restore select world logic

--- src/clojure/moon/stage_dev_menu_create.clj
+++ src/clojure/moon/stage_dev_menu_create.clj
@@ -13,17 +13,33 @@
            ctx/skin
            ctx/textures]}]
   (dev-menu/create
-   {:menus [ctx-data/item
-            debug-flags/item
-            {:label "Help"
-             :items [{:label controls-info}]}
-            {:label "Select World"
-             :items (for [[label world-fn] [["Vampire" tmx/vampire]
-                                            ["UF Caves" uf-caves/create]
-                                            ["Modules" modules/create]]]
-                      {:label (str "Start " label)
-                       :on-click (fn [ctx]
-                                   ctx)})}]
+   {:menus
+
+    ; This is a thing - 'menus'
+    ; give a folder and define somewhere
+    [ctx-data/item
+     debug-flags/item
+     {:label "Help"
+      :items [{:label controls-info}]}
+     {:label "Select World"
+      :items (for [[label world-fn] [["Vampire" tmx/vampire]
+                                     ["UF Caves" uf-caves/create]
+                                     ["Modules" modules/create]]]
+               {:label (str "Start " label)
+                :on-click (fn [ctx]
+                            #_(let [rebuild-actors! nil
+                                    #_(fn rebuild-actors! [stage ctx]
+                                        (.clear stage)
+                                        ((requiring-resolve 'game.create.add-actors/step) ctx))
+                                    create-world nil
+                                    #_(requiring-resolve 'game.create.world/step)
+                                    ui stage
+                                    stage (get-stage actor)]  ; get before clear, otherwise the actor does not have a stage anymore
+                                (rebuild-actors! ui ctx)
+                                #_(disposable/dispose! (:ctx/tiled-map ctx))
+                                (set! (.ctx ^Stage stage) (create-world ctx world-fn)))
+                            ctx)})}]
+
     :update-labels (for [item update-labels/v]
                      (if (:icon item)
                        (update item :icon #(get textures %))
