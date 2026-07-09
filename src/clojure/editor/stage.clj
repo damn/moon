d5f61544f7d3808c2c7f289897210f8bedf7a4f3
Michael Sappler <michaelsappler@gmail.com>
2026-07-09 17:45:02 +0530

    Delete clojure.ctx-stage

--- src/clojure/editor/stage.clj
+++ src/clojure/editor/stage.clj
@@ -1,9 +1,14 @@
 (ns clojure.editor.stage
-  (:require [clojure.ctx-stage :as ctx-stage]
-            [clojure.stage :as stage]
-            [clojure.editor.main-window :as main-window]))
+  (:require [clojure.editor.main-window :as main-window]
+            [clojure.fit-viewport :as fit-viewport]
+            [clojure.input :as input]
+            [clojure.scene2d-stage :as scene2d-stage]
+            [clojure.stage :as stage]))
 
-(defn f [ctx]
-  (let [ctx (assoc ctx :ctx/stage (ctx-stage/step ctx))]
-    (stage/add-actor! (:ctx/stage ctx) (main-window/f ctx))
-    ctx))
+(defn f [{:keys [ctx/input
+                ctx/batch] :as ctx}]
+  (let [stage* (scene2d-stage/create (fit-viewport/create 1440 900) batch)]
+    (input/set-input-processor! input stage*)
+    (let [ctx (assoc ctx :ctx/stage stage*)]
+      (stage/add-actor! (:ctx/stage ctx) (main-window/f ctx))
+      ctx)))
--- src/clojure/moon/create_stage.clj
+++ src/clojure/moon/create_stage.clj
@@ -1,5 +1,10 @@
 (ns clojure.moon.create-stage
-  (:require [clojure.ctx-stage :as ctx-stage]))
+  (:require [clojure.fit-viewport :as fit-viewport]
+            [clojure.input :as input]
+            [clojure.scene2d-stage :as scene2d-stage]))
 
-(defn f [ctx]
-  (assoc ctx :ctx/stage (ctx-stage/step ctx)))
+(defn f [{:keys [ctx/input
+                ctx/batch] :as ctx}]
+  (let [stage* (scene2d-stage/create (fit-viewport/create 1440 900) batch)]
+    (input/set-input-processor! input stage*)
+    (assoc ctx :ctx/stage stage*)))
