614e2eced3d25023cddfa974f8c7d0327f28953c
Michael Sappler <michaelsappler@gmail.com>
2026-07-10 21:09:40 +0530

    Fix circular dep

--- src/clojure/menus/help.clj
+++ src/clojure/menus/help.clj
@@ -1,5 +1,16 @@
 (ns clojure.menus.help
-  (:require [clojure.moon.controls-info :refer [controls-info]]))
+  (:require [clojure.string :as str]))
+
+(def controls-info
+  (str/join "\n"
+            ["[W][A][S][D] - Move"
+             "[ESCAPE] - Close windows"
+             "[I] - Inventory window"
+             "[E] - Entity Info window"
+             "[-]/[=] - Zoom"
+             "[P]/[SPACE] - Unpause"
+             "rightclick on tile or entity - open debug data window"
+             "Leftmouse click - use skill/drop item on cursor"]))
 
 (def item
   {:label "Help"
