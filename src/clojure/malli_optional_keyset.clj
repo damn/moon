efd9e0641acfa65ae16f2d560acffd97053b37d0
Michael Sappler <michaelsappler@gmail.com>
2026-07-11 23:01:07 +0530

    forgot

--- src/clojure/malli_optional_keyset.clj
+++ src/clojure/malli_optional_keyset.clj
@@ -1,7 +1,7 @@
 (ns clojure.malli-optional-keyset
-  (:require [clojure.is-optional :as optional?]
+  (:require [clojure.map-schema :as map-schema]
             [clojure.malli-map-keys :as map-keys]))
 
 (defn f [map-schema]
-  (set (filter #(optional?/f % map-schema)
+  (set (filter #(map-schema/optional? map-schema %)
                (map-keys/f map-schema))))
