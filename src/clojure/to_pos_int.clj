66aaaf08baea89703e3125a35b6fca7803317037
Michael Sappler <michaelsappler@gmail.com>
2026-07-12 00:59:13 +0530

    moon.val-max/to-pos-int

--- src/clojure/to_pos_int.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure.to-pos-int)
-
-(defn f [val-max]
-  (mapv #(-> % int (max 0)) val-max))
--- src/moon/val_max.clj
+++ src/moon/val_max.clj
@@ -1,11 +1,13 @@
 (ns moon.val-max
   (:require [clojure.schema :as schema]
-            [clojure.to-pos-int :as ->pos-int]
             [moon.malli :as malli-schema]
             [moon.mods :as mods]))
 
 (defn- valid? [val-max]
   (malli-schema/validate schema/v val-max))
+
+(defn to-pos-int [val-max]
+  (mapv #(-> % int (max 0)) val-max))
 
 (defn ratio
   "If mx and v is 0, returns 0, otherwise (/ v mx)"
@@ -18,7 +20,7 @@
 (defn apply-min [val-max modifiers modifier-k]
   (assert (valid? val-max) val-max)
   (let [val-max (update val-max 0 mods/get-value modifiers modifier-k)
-        [v mx] (->pos-int/f val-max)
+        [v mx] (to-pos-int val-max)
         result [v (max v mx)]]
     (assert (valid? result) result)
     result))
@@ -26,7 +28,7 @@
 (defn apply-max [val-max modifiers modifier-k]
   (assert (valid? val-max) val-max)
   (let [val-max (update val-max 1 mods/get-value modifiers modifier-k)
-        [v mx] (->pos-int/f val-max)
+        [v mx] (to-pos-int val-max)
         result [(min v mx) mx]]
     (assert (valid? result) result)
     result))
