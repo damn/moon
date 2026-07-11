cdb5395ae042d9c38dc3eee30a3728fbba09ac4e
Michael Sappler <michaelsappler@gmail.com>
2026-07-11 19:56:29 +0530

    integrate modules more

--- src/clojure/levels/modules.clj
+++ src/clojure/levels/modules.clj
@@ -1,7 +1,5 @@
 (ns clojure.levels.modules
-  (:require [clojure.grid-to-tiled-map :refer [grid->tiled-map]]
-            [clojure.java.util.random :as random]
-            [clojure.levels.modules.place-step :as place-step]
+  (:require [clojure.java.util.random :as random]
             [clojure.load-tmx-map :as load-tmx-map]
             [clojure.movement-property :as movement-property]
             [clojure.property-value :refer [property-value]]
@@ -9,8 +7,14 @@
             [clojure.tiled-tiled-map :as tiled-tiled-map]
             [com.badlogic.gdx.maps.map-layers :as map-layers]
             [com.badlogic.gdx.maps.tiled.tiled-map :as gdx-tiled-map]
+            [com.badlogic.gdx.maps.tiled.tiled-map-tile-layer$cell :as tiled-map-tile-layer-cell]
+            [com.badlogic.gdx.maps.tiled.tiled-map-tile-layer :as tiled-map-tile-layer]
+            [com.badlogic.gdx.maps.tiled.tiled-map-tile :as tiled-map-tile]
+            [com.badlogic.gdx.maps.tiled.tiles.static-tiled-map-tile :as static-tiled-map-tile]
+            [gdl.maps.map-properties :as map-properties]
             [moon.caves :as caves]
-            [moon.g2d :as g2d]))
+            [moon.g2d :as g2d]
+            [moon.position :as position]))
 
 (defn print-grid [{:keys [grid] :as world-fn-ctx}]
   (g2d/print-y-up grid)
@@ -45,18 +49,136 @@
 (defn- load-schema-tiled-map [w]
   (assoc w :schema-tiled-map (load-tmx-map/f "maps/modules.tmx")))
 
+(defn- module-index->tiled-map-positions
+  [[module-x module-y]
+   [modules-width modules-height]
+   module-offset-tiles]
+  (let [start-x (* module-x (+ modules-width module-offset-tiles))
+        start-y (* module-y (+ modules-height module-offset-tiles))]
+    (for [x (range start-x (+ start-x modules-width))
+          y (range start-y (+ start-y modules-height))]
+      [x y])))
+
+(def ^:private transition-idxvalues [2 8 1 4])
+
+(defn- transition-idx-value [position position->transition?]
+  (->> position
+       position/get-4-neighbours
+       (map-indexed (fn [idx position]
+                      (if (position->transition? position)
+                        (transition-idxvalues idx)
+                        0)))
+       (apply +)))
+
+(defn- place-module*
+  [module-offset-tiles
+   modules-scale
+   scaled-grid
+   unscaled-position
+   & {:keys [transition?
+             transition-neighbor?]}]
+  (let [floor-modules-row-width 4
+        floor-modules-row-height 4
+        floor->module-index (fn []
+                              [(rand-int floor-modules-row-width)
+                               (rand-int floor-modules-row-height)])
+        transition-modules-row-width 4
+        transition-modules-row-height 4
+        transition-modules-offset-x 4
+        transition-idxvalue->module-index (fn [idxvalue]
+                                            [(+ (rem idxvalue transition-modules-row-width)
+                                                transition-modules-offset-x)
+                                             (int (/ idxvalue transition-modules-row-height))])
+        [modules-width modules-height] modules-scale
+        floor-idxvalue 0
+        idxvalue (if transition?
+                   (transition-idx-value unscaled-position transition-neighbor?)
+                   floor-idxvalue)
+        tiled-map-positions (module-index->tiled-map-positions
+                             (if transition?
+                               (transition-idxvalue->module-index idxvalue)
+                               (floor->module-index))
+                             modules-scale
+                             module-offset-tiles)
+        offsets (for [x (range modules-width)
+                      y (range modules-height)]
+                  [x y])
+        offset->tiled-map-position (zipmap offsets tiled-map-positions)
+        scaled-position (mapv * unscaled-position modules-scale)]
+    (reduce (fn [grid offset]
+              (assoc grid
+                     (mapv + scaled-position offset)
+                     (offset->tiled-map-position offset)))
+            scaled-grid
+            offsets)))
+
+(defn- place-step
+  [modules-tiled-map
+   modules-scale
+   scaled-grid
+   unscaled-grid
+   unscaled-floor-positions
+   unscaled-transition-positions]
+  (let [module-offset-tiles 1
+        number-modules-x 8
+        number-modules-y 4
+        [modules-width modules-height] modules-scale
+        _ (assert (and (= (map-properties/get (gdx-tiled-map/getProperties modules-tiled-map) "width")
+                          (* number-modules-x (+ modules-width module-offset-tiles)))
+                       (= (map-properties/get (gdx-tiled-map/getProperties modules-tiled-map) "height")
+                          (* number-modules-y (+ modules-height module-offset-tiles)))))
+        scaled-grid (reduce (fn [scaled-grid unscaled-position]
+                              (place-module* module-offset-tiles
+                                             modules-scale
+                                             scaled-grid
+                                             unscaled-position
+                                             :transition? false))
+                            scaled-grid
+                            unscaled-floor-positions)
+        scaled-grid (reduce (fn [scaled-grid unscaled-position]
+                              (place-module* module-offset-tiles
+                                             modules-scale
+                                             scaled-grid
+                                             unscaled-position
+                                             :transition? true
+                                             :transition-neighbor? #(#{:transition :wall}
+                                                                     (get unscaled-grid %))))
+                            scaled-grid
+                            unscaled-transition-positions)]
+    scaled-grid))
+
 (defn- place-modules
   [{:keys [scale
            scaled-grid
            grid
            schema-tiled-map]
     :as w}]
-  (assoc w :scaled-grid (place-step/f schema-tiled-map
-                                      scale
-                                      scaled-grid
-                                      grid
-                                      (filter #(= :ground (get grid %)) (g2d/posis grid))
-                                      (filter #(= :transition (get grid %)) (g2d/posis grid)))))
+  (assoc w :scaled-grid (place-step schema-tiled-map
+                                    scale
+                                    scaled-grid
+                                    grid
+                                    (filter #(= :ground (get grid %)) (g2d/posis grid))
+                                    (filter #(= :transition (get grid %)) (g2d/posis grid)))))
+
+(defn- grid->tiled-map
+  [schema-tiled-map grid]
+  (let [copy-tile (memoize
+                   (fn [tile]
+                     (assert tile)
+                     (static-tiled-map-tile/new tile)))]
+    {:properties (merge (map-properties/clojurize (gdx-tiled-map/getProperties schema-tiled-map))
+                        {"width" (g2d/width grid)
+                         "height" (g2d/height grid)})
+     :layers (for [layer (gdx-tiled-map/getLayers schema-tiled-map)]
+               {:name (tiled-map-tile-layer/getName layer)
+                :visible? (tiled-map-tile-layer/isVisible layer)
+                :properties (map-properties/clojurize (tiled-map-tile-layer/getProperties layer))
+                :tiles (for [position (g2d/posis grid)
+                             :let [local-position (get grid position)]
+                             :when local-position]
+                         (when (vector? local-position)
+                           (when-let [cell (tiled-map-tile-layer/getCell layer (local-position 0) (local-position 1))]
+                             [position (copy-tile (tiled-map-tile-layer-cell/getTile cell))])))})}))
 
 (defn- convert-to-tiled-map
   [{:keys [scaled-grid
