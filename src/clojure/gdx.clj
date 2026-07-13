9db8e443c56b2c27301e252d96e3e1401b2a345b
Michael Sappler <michaelsappler@gmail.com>
2026-07-13 11:06:18 +0530

    add comment

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -3,3 +3,8 @@
 
 (defn app []
   (gdx/app))
+
+; TODO each call that depends on 'Gdx' somewhere in the call chain
+; (viewport update, sprite-batch, font-generator, camera?, texture?, batch?)
+; put in here so dependencies are explicit?
+; but the're allready in the 'folder' right?
