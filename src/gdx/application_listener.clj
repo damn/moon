9ee04c7bfaf00e9554dad024a936f1b2e2c5f61a
Michael Sappler <michaelsappler@gmail.com>
2026-07-13 21:13:49 +0530

    listener

--- src/gdx/application_listener.clj
+++
@@ -1,5 +0,0 @@
-(ns gdx.application-listener
-  (:require [com.badlogic.gdx.application-listener :as application-listener]))
-
-(defn create [listener-spec]
-  (application-listener/new listener-spec))
--- src/gdx/lwjgl3_application.clj
+++ src/gdx/lwjgl3_application.clj
@@ -1,8 +1,7 @@
 (ns gdx.lwjgl3-application
-  (:require [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl3-application]
-            [gdx.application-listener :as application-listener]))
+  (:require [com.badlogic.gdx.application-listener :as application-listener]
+            [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl3-application]))
 
 (defn create [listener-spec configuration]
-  (let [listener (application-listener/create listener-spec)]
-    (lwjgl3-application/new listener
-                            configuration)))
+  (let [listener (application-listener/new listener-spec)]
+    (lwjgl3-application/new listener configuration)))
