bbd4cf98f7c3ac541d7e427aa8af02355f1f1231
Michael Sappler <michaelsappler@gmail.com>
2026-07-13 20:17:44 +0530

    gdx.config

--- src/gdx/application/lwjgl.clj
+++ src/gdx/application/lwjgl.clj
@@ -1,27 +1,12 @@
 (ns gdx.application.lwjgl
-  (:require [gdx.application-listener :as application-listener]
-            [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl3-application]
-            [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application-configuration :as config]))
+  (:require [com.badlogic.gdx.backends.lwjgl3.lwjgl3-application :as lwjgl3-application]
+            [gdx.application-listener :as application-listener]
+            [gdx.config :as app-config]))
 
 (defn use-glfw-async! []
-  (config/useGlfwAsync))
-
-(let [k->opts
-      {
-       :title          config/setTitle
-       :windowed-mode  (fn [config {:keys [width height]}]
-                                    (config/setWindowedMode config width height))
-       :foreground-fps config/setForegroundFPS
-       }]
-  (defn- create-config [config-opts]
-    (let [config (config/new)]
-      (doseq [[k v] config-opts]
-        (let [apply! (k->opts k)]
-          (assert apply! (str "Unknown lwjgl3 config option: " k))
-          (apply! config v)))
-      config)))
+  (app-config/use-glfw-async!))
 
 (defn create
   [{:keys [config listener]}]
   (lwjgl3-application/new (application-listener/create listener)
-                          (create-config config)))
+                          (app-config/create config)))
