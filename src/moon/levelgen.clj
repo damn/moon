0d99f04943d1cd46add3091732db7d78c00d4ac3
Michael Sappler <michaelsappler@gmail.com>
2026-07-14 12:21:50 +0530

    Move config down

--- src/moon/levelgen.clj
+++ src/moon/levelgen.clj
@@ -33,22 +33,6 @@
 (defn- get-stage [ctx]
   (:ctx/stage ctx))
 
-(def ^:private config
-  {:initial-level-fn uf-caves/create
-   :level-fns [["Vampire" tmx/vampire]
-               ["UF Caves" uf-caves/create]
-               ["Modules" modules/create]]
-   :ui-viewport-width 1440
-   :ui-viewport-height 900
-   :world-viewport-width 1440
-   :world-viewport-height 900
-   :tile-size 48
-   :ui-skin-path "skin/uiskin.json"
-   :textures-config {:folder "resources/"
-                     :extensions #{"png" "bmp"}}
-   :zoom-speed 0.1
-   :camera-movement-speed 1})
-
 (defn- generate-level
   [{:keys [ctx/camera
            ctx/db
@@ -177,6 +161,22 @@
         world-viewport (:ctx/world-viewport ctx)]
     (viewport/update! (:stage/viewport stage) width height true)
     (viewport/update! world-viewport width height false)))
+
+(def ^:private config
+  {:initial-level-fn uf-caves/create
+   :level-fns [["Vampire" tmx/vampire]
+               ["UF Caves" uf-caves/create]
+               ["Modules" modules/create]]
+   :ui-viewport-width 1440
+   :ui-viewport-height 900
+   :world-viewport-width 1440
+   :world-viewport-height 900
+   :tile-size 48
+   :ui-skin-path "skin/uiskin.json"
+   :textures-config {:folder "resources/"
+                     :extensions #{"png" "bmp"}}
+   :zoom-speed 0.1
+   :camera-movement-speed 1})
 
 (def state (atom nil))
 
